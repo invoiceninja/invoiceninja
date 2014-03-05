71c9ca9961b09ab8b659783f04bd07befab1c3f6
Razi KAntorp <razikantorp@gmail.com>
2014-03-05 16:05:36 +0100

    Addded text to the About page. Hided the blur section on the landing page that coused a blur line over the second large image

--- app/views/about_us.blade.php
+++ app/views/about_us.blade.php
@@ -48,12 +48,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                        <h2>This is a really nice headline about the app.</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
+                        <h2>Open Source Platform</h2>
+                        <p>Free yourself from online invoicing platforms with high monthly fees and limited functionality.  Being open source allows us fast app development, security audits by the open-course community, and we can keep it <span style="color:#2299c0"><strong>FREE!</strong></span></p>
                 </div>
             </div>
         </div>
@@ -67,18 +63,9 @@
                     </div>
                 </div>
                 <div class="col-md-7">
-                        <h2>This is a really nice headline about the app.</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
+                        <h2>Live PDF Creation</h2>
+                        <p><strong>Look professional from day #1.</strong> Select one of our beautiful invoice templates to suit your company identity, switch between designs in real time to preview invoices & email them to clients with one click. The live preview PDF function was designed for an efficient and hassle-free experience, and it’s awesome!
+</p>
                 </div>
             </div>
         </div>
@@ -87,66 +74,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-7">
-                        <h2>This is a really nice headline about the app.</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                </div>
-                <div class="col-md-5">
-                    <div class="screendump">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="about white-bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="screendump">
-                    </div>
-                </div>
-                <div class="col-md-7">
-                        <h2>This is a really nice headline about the app.</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7">
-                        <h2>This is a really nice headline about the app.</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
-                        <p>Donec id elit non mi porta gravida at eget metus.
-                        Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                        condimentum nibh, ut fermentum massa justo sit amet
-                        risus. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.
-                        Donec sed odio dui.</p>
+                        <h2>Online Payments</h2>
+                        <p><strong>Authorize.net, Beanstream, PayPal?</strong> InvoiceNinja supports the most popular online payment gateways! If you need help integrating a third party gateway we don’t yet support, please contact us!  We’re happy to help!  If you need assistance of want to learn more about online payment solutions, contact us!</p>
                 </div>
                 <div class="col-md-5">
                     <div class="screendump">
@@ -159,7 +88,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                        <h2>This is a really nice headline about the app.</h2>
+                        <h2>Info about the company/story</h2>
                         <p>Donec id elit non mi porta gravida at eget metus.
                         Fusce dapibus, tellus ac cursus commodo, tortor mauris
                         condimentum nibh, ut fermentum massa justo sit amet
