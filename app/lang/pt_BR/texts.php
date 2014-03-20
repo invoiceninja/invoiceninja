03a0a07347952131078998748cf6d44ea198e7fd
Thiago Coutinho <root@thiagoc.net>
2014-03-20 10:45:59 -0300

    Update texts.php

--- app/lang/pt_BR/texts.php
+++ app/lang/pt_BR/texts.php
@@ -1,1 +1,28 @@
-Test
+<?php
+
+return array(
+
+  'organization' => 'Organização',
+  'name' => 'Nome',
+  'website' => 'Website',
+  'work_phone' => 'Telefone',
+  'address' => 'Endereço',
+  'address1' => 'Rua',
+  'address2' => 'Bloco/apto',
+  'city' => 'Cidade',
+  'state' => 'Estado',
+  'postal_code' => 'CEP',
+  'country_id' => 'País',
+  'contacts' => 'Contatos',
+  'first_name' => 'Primeiro Nome',
+  'last_name' => 'Último Nome',
+  'phone' => 'Telefone',
+  'email' => 'Email',
+  'additional_info' => 'Informações Adicionais',
+  'payment_terms' => 'Termos de Pagamento',
+  'currency_id' => 'Moeda',
+  'size_id' => 'Tamanho',
+  'industry_id' => 'Empresa',
+  'private_notes' => 'Notas Privadas',
+
+);
