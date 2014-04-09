bd0e2ffe559ac1bcbefa511ec86e3a65a5b02330
Joel Limberg <me@joellimberg.com>
2014-04-10 00:53:09 +0300

    Fix "DEFAULT_LOCALE" constant spelling
    Was “DEFUALT_LOCALE”

--- app/models/Account.php
+++ app/models/Account.php
@@ -178,7 +178,7 @@ class Account extends Eloquent
 		Session::put(SESSION_DATE_PICKER_FORMAT, $this->date_format ? $this->date_format->picker_format : DEFAULT_DATE_PICKER_FORMAT);
 		Session::put(SESSION_DATETIME_FORMAT, $this->datetime_format ? $this->datetime_format->format : DEFAULT_DATETIME_FORMAT);			
 		Session::put(SESSION_CURRENCY, $this->currency_id ? $this->currency_id : DEFAULT_CURRENCY);		
-		Session::put(SESSION_LOCALE, $this->language_id ? $this->language->locale : DEFUALT_LOCALE);
+		Session::put(SESSION_LOCALE, $this->language_id ? $this->language->locale : DEFAULT_LOCALE);
 	}
 
 	public function getInvoiceLabels()
