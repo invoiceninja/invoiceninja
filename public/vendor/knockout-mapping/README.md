c2998b888c792f0c48a2cbaa5ea11807781acd09
Hillel Coren <hillelcoren@gmail.com>
2014-05-04 23:34:51 +0300

    Removing bower components from repo

--- public/vendor/knockout-mapping/README.md
+++
@@ -1,1 +0,0 @@
-Object mapping plugin for [Knockout](http://knockoutjs.com/) - Find the documentation [here](http://knockoutjs.com/documentation/plugins-mapping.html).
--- public/vendor/knockout-mapping/build/version-header.js
+++
@@ -1,3 +0,0 @@
-/// Knockout Mapping plugin v2.4.1
-/// (c) 2013 Steven Sanderson, Roy Jacobs - http://knockoutjs.com/
-/// License: MIT (http://www.opensource.org/licenses/mit-license.php)
