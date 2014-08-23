698d42c7e89345a70fad84f6194774e54f47f350
Harald <harald@sandbugt.no>
2014-08-23 21:48:34 +0200

    Norwegian language added
    Started translating into Norwegian

---
+++ app/lang/nb_NO/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passord må være minst seks tegn og samsvare med bekreftelsen.",
+
+	"user" => "Vi kan ikke finne en bruker med den e-postadressen.",
+
+	"token" => "Denne tilbakestillingsnøkkelen er ugyldig.",
+
+	"sent" => "Passord påminnelse sendt!",
+
+);
