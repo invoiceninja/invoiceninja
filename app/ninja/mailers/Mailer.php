b0942449bb623a31a01923a2352ddc51f9074f46
Hillel Coren <hcoren@shoretel.com>
2014-09-12 15:11:59 +0300

    Changed from address when sending from yahoo or aol accounts

--- app/ninja/mailers/Mailer.php
+++ app/ninja/mailers/Mailer.php
@@ -1,6 +1,7 @@
 <?php namespace ninja\mailers;
 
 use Mail;
+use Utils;
 
 class Mailer {
 
@@ -12,9 +13,18 @@ class Mailer {
 		];
 		
 		Mail::send($views, $data, function($message) use ($toEmail, $fromEmail, $fromName, $subject)
-		{			
+		{
+			$replyEmail = $fromEmail;
+
+			// We're unable to set the true fromEmail for emails sent from Yahoo or AOL accounts 
+			// http://blog.mandrill.com/yahoos-recent-dmarc-changes-and-how-that-impacts-senders.html
+			if (strpos($fromEmail, '@yahoo.') !== false || strpos($fromEmail, '@aol.') !== FALSE)			
+			{
+				$fromEmail = CONTACT_EMAIL;
+			}
+
 			$message->to($toEmail)->from($fromEmail, $fromName)->sender($fromEmail, $fromName)
-				->replyTo($fromEmail, $fromName)->returnPath($fromEmail)->subject($subject);
+				->replyTo($replyEmail, $fromName)->subject($subject);
 		});		
 	}
 }
