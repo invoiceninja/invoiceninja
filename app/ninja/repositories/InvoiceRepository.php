9ac9ebca5a96195bbec282aa7373315adeb7b614
Hillel Coren <hillelcoren@gmail.com>
2014-10-19 23:22:18 +0300

    Enabled using date variables for non-recurring invoices

--- app/ninja/repositories/InvoiceRepository.php
+++ app/ninja/repositories/InvoiceRepository.php
@@ -336,8 +336,8 @@ class InvoiceRepository
 
       $invoiceItem = InvoiceItem::createNew();
       $invoiceItem->product_id = isset($product) ? $product->id : null;
-      $invoiceItem->product_key = Utils::processVariables(trim($item->product_key));
-      $invoiceItem->notes = Utils::processVariables(trim($item->notes));
+      $invoiceItem->product_key = trim($invoice->is_recurring ? $item->product_key : Utils::processVariables($item->product_key));
+      $invoiceItem->notes = trim($invoice->is_recurring ? $item->notes : Utils::processVariables($item->notes));
       $invoiceItem->cost = Utils::parseFloat($item->cost);
       $invoiceItem->qty = Utils::parseFloat($item->qty);
       $invoiceItem->tax_rate = 0;
