0b79b69d41c6b62d5375f4c29db5c733728ae2bb
Troels Liebe Bentsen <tlb@rapanden.dk>
2014-10-20 19:22:40 +0200

    Fix dependency issue with requires moving to laravel 4.2

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * An helper file for Laravel 4, to provide autocomplete information to your IDE
- * Generated for Laravel 4.1.25 on 2014-10-14.
+ * Generated for Laravel 4.1.25 on 2014-10-20.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
