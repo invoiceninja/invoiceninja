af9e988100c863b43fca50db5502a93c2dd23036
Hillel Coren <hillelcoren@gmail.com>
2014-11-11 19:47:15 +0200

    Revert "dashboard page changes"

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -28,51 +28,7 @@ class DashboardController extends \BaseController {
             ->where('clients.is_deleted', '=', false)
             ->groupBy('accounts.id')
             ->first();
-	
-	// for 0- 30 day Invoice Price 
-	$thirtyDayInvoice = Invoice::scope()
-                ->where('invoice_date', '<', date('Y-m-d'))
-                ->where('balance', '>', 0)
-                ->where('is_recurring', '=', false)
-                ->where('is_quote', '=', false)
-                ->where('is_deleted', '=', false)
-                ->orderBy('invoice_date', 'dsc')->take(30)->get();
-	
-	$totalThirtyDay = Utils::getTotalValue($thirtyDayInvoice);	
-	
-	// for 31- 60 day Invoice Price 
-	$thirtyToSixtyDay = Invoice::scope()
-                ->where('invoice_date', '<', date('Y-m-d'))
-                ->where('balance', '>', 0)
-                ->where('is_recurring', '=', false)
-                ->where('is_quote', '=', false)
-                ->where('is_deleted', '=', false)
-                ->orderBy('invoice_date', 'dsc')->skip(30)->take(30)->get();
-	
-	$totalThirtyToSixtyDay = Utils::getTotalValue($thirtyToSixtyDay);	
-	
-	// for 61- 90 day Invoice Price 
-	$sixtyToNintyDay = Invoice::scope()
-                ->where('invoice_date', '<', date('Y-m-d'))
-                ->where('balance', '>', 0)
-                ->where('is_recurring', '=', false)
-                ->where('is_quote', '=', false)
-                ->where('is_deleted', '=', false)
-                ->orderBy('invoice_date', 'dsc')->skip(60)->take(30)->get();
-	
-	$totalSixtyToNintyDay = Utils::getTotalValue($sixtyToNintyDay);	
-	
-	// for 90- above day Invoice Price 
-	$nintyAndAboveDay = Invoice::scope()
-                ->where('invoice_date', '<', date('Y-m-d'))
-                ->where('balance', '>', 0)
-                ->where('is_recurring', '=', false)
-                ->where('is_quote', '=', false)
-                ->where('is_deleted', '=', false)
-                ->orderBy('invoice_date', 'dsc')->skip(90)->take(100000)->get();
-	
-	$totalNintyAndAboveDay = Utils::getTotalValue($nintyAndAboveDay);	
-				
+
     $activities = Activity::where('activities.account_id', '=', Auth::user()->account_id)
                 ->orderBy('created_at', 'desc')->take(6)->get();
 
@@ -91,14 +47,8 @@ class DashboardController extends \BaseController {
                   ->where('is_quote', '=', false)
                   ->where('is_deleted', '=', false)
                   ->orderBy('due_date', 'asc')->take(6)->get();
-	
-	//To do 			  
-	$monthValue = '12345.67';	
-	$yearValue = '987654.32';		  
-	$weekValue ='57684.73';
 
     $data = [
-      'account' => Account::with('users')->findOrFail(Auth::user()->account_id),
       'totalIncome' => Utils::formatMoney($totalIncome ? $totalIncome->value : 0, Session::get(SESSION_CURRENCY)),
       'billedClients' => $metrics ? $metrics->billed_clients : 0,
       'invoicesSent' => $metrics ? $metrics->invoices_sent : 0,
@@ -106,14 +56,7 @@ class DashboardController extends \BaseController {
       'invoiceAvg' => Utils::formatMoney(($metrics ? $metrics->invoice_avg : 0), Session::get(SESSION_CURRENCY)),
       'activities' => $activities,
       'pastDue' => $pastDue,
-      'upcoming' => $upcoming,
-      'monthValue' => Utils::formatMoney(($monthValue ), Session::get(SESSION_CURRENCY)),
-      'yearValue' => Utils::formatMoney(($yearValue ), Session::get(SESSION_CURRENCY)),
-      'weekValue' => Utils::formatMoney(($weekValue ), Session::get(SESSION_CURRENCY)),
-      'totalThirtyDayInvoice' => Utils::formatMoney(($totalThirtyDay), Session::get(SESSION_CURRENCY)),
-      'totalThirtyToSixtyDay' => Utils::formatMoney(($totalThirtyToSixtyDay), Session::get(SESSION_CURRENCY)),
-      'totalSixtyToNintyDay' => Utils::formatMoney(($totalSixtyToNintyDay), Session::get(SESSION_CURRENCY)),
-      'totalNintyAndAboveDay' =>Utils::formatMoney(($totalNintyAndAboveDay), Session::get(SESSION_CURRENCY))
+      'upcoming' => $upcoming
     ];
 
     return View::make('dashboard', $data);
