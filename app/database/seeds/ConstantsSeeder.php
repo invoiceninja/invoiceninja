29331a135258aa2e7c519c3720fb17a82c440f04
Sang, Le Thanh <me@sanglt.com>
2015-01-15 13:13:45 +0700

    Using VND for symbol

--- app/database/seeds/ConstantsSeeder.php
+++ app/database/seeds/ConstantsSeeder.php
@@ -137,7 +137,7 @@ class ConstantsSeeder extends Seeder
 		Currency::create(array('name' => 'Indian Rupee', 'code' => 'INR', 'symbol' => 'Rs. ', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'));
 		Currency::create(array('name' => 'Australian Dollar', 'code' => 'AUD', 'symbol' => '$', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'));
 		Currency::create(array('name' => 'New Zealand Dollar', 'code' => 'NZD', 'symbol' => '$', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'));
-		Currency::create(array('name' => 'VietNam Dong', 'code' => 'VND', 'symbol' => 'đ', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'));
+		Currency::create(array('name' => 'VietNam Dong', 'code' => 'VND', 'symbol' => 'VND', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'));
 
 		DatetimeFormat::create(array('format' => 'd/M/Y g:i a', 'label' => '10/Mar/2013'));
 		DatetimeFormat::create(array('format' => 'd-M-Yk g:i a', 'label' => '10-Mar-2013'));
