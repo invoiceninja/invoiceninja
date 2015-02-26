fe2155e139b3617ff09ca9494227cc40d46ed0d2
Hillel Coren <hillelcoren@gmail.com>
2015-02-26 20:20:10 +0200

    Added social buttons to login page

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -73,6 +73,7 @@
             <p class="link">
 			{{ link_to('forgot_password', 'Forgot your password?') }}
             </p>
+
 		
 			<!-- if there are login errors, show them here -->
 			@if ( Session::get('error') )
@@ -85,6 +86,27 @@
             </div>
 
 		{{ Former::close() }}
+
+        @if (!Utils::isNinja())
+            <p/>
+            <center>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=635126583203143";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+
+            <div class="fb-follow" data-href="https://www.facebook.com/invoiceninja" data-colorscheme="light" data-layout="button" data-show-faces="false"></div>&nbsp;&nbsp;
+
+            <a href="https://twitter.com/invoiceninja" class="twitter-follow-button" data-show-count="false" data-related="hillelcoren" data-size="medium">Follow @invoiceninja</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+            <iframe src="https://ghbtns.com/github-btn.html?user=hillelcoren&repo=invoice-ninja&type=star&count=false" frameborder="0" scrolling="0" width="50px" height="20px"></iframe>
+            </center>
+        @endif
 
     </div>
 
