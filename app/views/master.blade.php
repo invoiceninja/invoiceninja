206d57ef6b86177256b895a27bc6a34ff31d14ac
Hillel Coren <hillelcoren@gmail.com>
2015-03-17 16:25:53 +0200

    Fixed analytics tag

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -91,7 +91,7 @@
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', '{{ $_ENV['ANALYTICS_KEY'] }}');        
+        ga('create', '{{ $_ENV['ANALYTICS_KEY'] }}', 'auto');        
         ga('send', 'pageview');
 
         function trackUrl(url) {
