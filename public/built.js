b040b5e242273ee4f66808ade9e0569fa1288d65
Hillel Coren <hillelcoren@gmail.com>
2015-03-22 15:13:05 +0200

    Fixed problem with line wrapping

--- public/built.js
+++ public/built.js
@@ -31565,7 +31565,7 @@ function GetPdf(invoice, javascript){
     footerLeft: 420,
     tablePadding: 12,
     tableTop: 250,
-    descriptionLeft: 162,
+    descriptionLeft: 150,
     unitCostRight: 410,
     qtyRight: 480,
     taxRight: 480,
@@ -32752,7 +32752,7 @@ function displayInvoiceItems(doc, invoice, layout) {
     SetPdfColor('Black', doc);
     doc.setFontType('normal');
 
-    var splitDescription = doc.splitTextToSize(notes, 180);
+    var splitDescription = doc.splitTextToSize(notes, 190);
     doc.text(layout.descriptionLeft, y+2, splitDescription);
     doc.text(costX, y+2, cost);
     if (!hideQuantity) {
