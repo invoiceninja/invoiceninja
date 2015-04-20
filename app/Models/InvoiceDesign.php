6d651a250cd08b2f0238d007e59fd8766823af79
Hillel Coren <hillelcoren@gmail.com>
2015-04-20 17:47:14 +0300

    Working on L5

--- app/Models/InvoiceDesign.php
+++ app/Models/InvoiceDesign.php
@@ -1,23 +1,25 @@
-<?php namespace App\Models;
+<?php namespace app\Models;
 
 use Eloquent;
+use Auth;
 
 class InvoiceDesign extends Eloquent
 {
     public $timestamps = false;
-    
-    public function scopeAvailableDesigns($query) {
-      $designs = $query->where('id', '<=', \Auth::user()->maxInvoiceDesignId())->orderBy('id')->get();
-       
-      foreach($designs as $design) {
-        if($design->filename) {
-          $fileName = public_path(strtolower("js/templates/{$design->name}.js"));
-          if(file_exists($fileName)) {
-            $design->javascript = file_get_contents($fileName);
-          }
+
+    public function scopeAvailableDesigns($query)
+    {
+        $designs = $query->where('id', '<=', \Auth::user()->maxInvoiceDesignId())->orderBy('id')->get();
+
+        foreach ($designs as $design) {
+            if ($design->filename) {
+                $fileName = public_path(strtolower("js/templates/{$design->name}.js"));
+                if (Auth::user()->account->utf8_invoices && file_exists($fileName)) {
+                    $design->javascript = file_get_contents($fileName);
+                }
+            }
         }
-      }
-      
-      return $designs;
+
+        return $designs;
     }
 }
