39ba8d1287e54ffcf80bac6e8cdeadf0164ab830
Hillel Coren <hillelcoren@gmail.com>
2015-07-05 11:01:16 +0300

    Added support for creating payments through the API

--- app/Http/Controllers/PaymentApiController.php
+++ app/Http/Controllers/PaymentApiController.php
@@ -1,8 +1,10 @@
 <?php namespace App\Http\Controllers;
 
+use Input;
 use Utils;
 use Response;
 use App\Models\Payment;
+use App\Models\Invoice;
 use App\Ninja\Repositories\PaymentRepository;
 
 class PaymentApiController extends Controller
@@ -16,7 +18,10 @@ class PaymentApiController extends Controller
 
     public function index()
     {
-        $payments = Payment::scope()->with('client', 'contact', 'invitation', 'user', 'invoice')->orderBy('created_at', 'desc')->get();
+        $payments = Payment::scope()
+                        ->with('client', 'contact', 'invitation', 'user', 'invoice')
+                        ->orderBy('created_at', 'desc')
+                        ->get();
         $payments = Utils::remapPublicIds($payments);
         
         $response = json_encode($payments, JSON_PRETTY_PRINT);
@@ -25,15 +30,38 @@ class PaymentApiController extends Controller
         return Response::make($response, 200, $headers);
     }
 
-  /*
-  public function store()
-  {
-    $data = Input::all();
-    $invoice = $this->invoiceRepo->save(false, $data, false);
 
-    $response = json_encode($invoice, JSON_PRETTY_PRINT);
-    $headers = Utils::getApiHeaders();
-    return Response::make($response, 200, $headers);
-  }
-  */
+    public function store()
+    {
+        $data = Input::all();
+        $error = false;
+
+        if (isset($data['invoice_id'])) {
+            $invoice = Invoice::scope($data['invoice_id'])->with('client')->first();
+
+            if ($invoice) {
+                $data['invoice'] = $invoice->public_id;
+                $data['client'] = $invoice->client->public_id;
+            } else {
+                $error = trans('validation.not_in', ['attribute' => 'invoice_id']);
+            }
+        } else {
+            $error = trans('validation.not_in', ['attribute' => 'invoice_id']);
+        }
+
+        if (!isset($data['transaction_reference'])) {
+            $data['transaction_reference'] = '';
+        }
+
+        if (!$error) {
+            $payment = $this->paymentRepo->save(false, $data);
+            $payment = Payment::scope($payment->public_id)->with('client', 'contact', 'user', 'invoice')->first();
+
+            $payment = Utils::remapPublicIds([$payment]);
+        }
+
+        $response = json_encode($error ?: $payment, JSON_PRETTY_PRINT);
+        $headers = Utils::getApiHeaders();
+        return Response::make($response, 200, $headers);
+    }
 }
