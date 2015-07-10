b425ca9ba04daf0ffec497460a5d945100bd996f
trigras <trigras@gmail.com>
2015-07-10 23:06:06 +0300

    datepicker localization

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -53,6 +53,10 @@
                 'sSearch': ''
             }
         } );
+           
+        $.extend( true, $.fn.datepicker.defaults, {
+            language:'{{App::getLocale()}}'
+        });
     </script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
