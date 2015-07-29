7db4fd8cd07a7cf387cf507f9f2071b014b61580
Hillel Coren <hillelcoren@gmail.com>
2015-07-29 10:01:03 +0300

    Fix #379 Error after /setup: business.js missing but required?

--- database/seeds/PaymentLibrariesSeeder.php
+++ database/seeds/PaymentLibrariesSeeder.php
@@ -147,17 +147,21 @@ class PaymentLibrariesSeeder extends Seeder
             'Photo',
         ];
         
-        foreach ($designs as $design) {
+        for ($i=0; $i<count($designs); $i++) {
+            $design = $designs[$i];
             $fileName = storage_path() . '/templates/' . strtolower($design) . '.js';
-            $pdfmake = file_get_contents($fileName);
-            if ($pdfmake) {
-                $record = InvoiceDesign::whereName($design)->first();
-                if (!$record) {
-                    $record = new InvoiceDesign;
-                    $record->name = $design;
+            if (file_exists($fileName)) {
+                $pdfmake = file_get_contents($fileName);
+                if ($pdfmake) {
+                    $record = InvoiceDesign::whereName($design)->first();
+                    if (!$record) {
+                        $record = new InvoiceDesign;
+                        $record->id = $i + 1;
+                        $record->name = $design;
+                    }
+                    $record->pdfmake = $pdfmake;
+                    $record->save();
                 }
-                $record->pdfmake = $pdfmake;
-                $record->save();
             }
         }
     }
