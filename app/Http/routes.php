dc167356c15748d05debdec755c80dbc3820a303
Hillel Coren <hillelcoren@gmail.com>
2015-08-30 15:13:08 +0300

    Updated version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -373,7 +373,7 @@ if (!defined('CONTACT_EMAIL')) {
     define('NINJA_GATEWAY_CONFIG', '');
     define('NINJA_WEB_URL', 'https://www.invoiceninja.com');
     define('NINJA_APP_URL', 'https://app.invoiceninja.com');
-    define('NINJA_VERSION', '2.3.3');
+    define('NINJA_VERSION', '2.3.4');
     define('NINJA_DATE', '2000-01-01');
 
     define('NINJA_FROM_EMAIL', 'maildelivery@invoiceninja.com');
