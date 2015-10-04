invoiceninja/invoiceninja

commit ef06706911883e21638db00174a693386a9d2fd1

config logger via environment variable

config/app.php
@@ -97,7 +97,7 @@ return [
 	|
 	*/
 
-	'log' => 'single',
+	'log' => evn('LOG', 'single'),
 
 	/*
 	|--------------------------------------------------------------------------

.env.example
@@ -21,3 +21,5 @@ MAIL_FROM_NAME
 MAIL_PASSWORD
 
 PHANTOMJS_CLOUD_KEY='a-demo-key-with-low-quota-per-ip-address'
+
+LOG=single

commit c28df844642b12c13ec9642e23cc2eaa4e2ec025

Fixed a typo in function name
This typo led to a fatal error since the function was undefined. This bug was introduced in ef06706911883e21638db00174a693386a9d2fd1

config/app.php
@@ -97,7 +97,7 @@ return [
 	|
 	*/
 
-	'log' => evn('LOG', 'single'),
+	'log' => env('LOG', 'single'),
 
 	/*
 	|--------------------------------------------------------------------------