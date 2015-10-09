63ede3de458d869b49e6d4621e4115ec28547f43
Hillel Coren <hillelcoren@gmail.com>
2015-10-09 07:43:17 +0300

    Bug fixes

--- app/Ninja/Mailers/Mailer.php
+++ app/Ninja/Mailers/Mailer.php
@@ -45,7 +45,7 @@ class Mailer
             return true;
         } catch (Exception $exception) {
             Utils::logError('Email Error: ' . $exception->getMessage());
-            if (false && isset($_ENV['POSTMARK_API_TOKEN'])) {
+            if (isset($_ENV['POSTMARK_API_TOKEN'])) {
                 $response = $exception->getResponse()->getBody()->getContents();
                 $response = json_decode($response);
                 return nl2br($response->Message);
