fa151580b8ac19e9d007496593f78bab5b6a9984
Hillel Coren <hillelcoren@gmail.com>
2015-10-18 20:19:29 +0300

    Reverted combobox update

--- public/js/bootstrap-combobox.js
+++ public/js/bootstrap-combobox.js
@@ -1,5 +1,5 @@
 /* =============================================================
- * bootstrap-combobox.js v1.1.6
+ * bootstrap-combobox.js v1.1.5
  * =============================================================
  * Copyright 2012 Daniel Farrell
  *
@@ -25,7 +25,6 @@
 
   var Combobox = function ( element, options ) {
     this.options = $.extend({}, $.fn.combobox.defaults, options);
-    this.template = this.options.template || this.template
     this.$source = $(element);
     this.$container = this.setup();
     this.$element = this.$container.find('input[type=text]');
@@ -47,25 +46,12 @@
     constructor: Combobox
 
   , setup: function () {
-      var combobox = $(this.template());
+      var combobox = $(this.options.template);
       this.$source.before(combobox);
       this.$source.hide();
       return combobox;
     }
 
-  , disable: function() {
-      this.$element.prop('disabled', true);
-      this.$button.attr('disabled', true);
-      this.disabled = true;
-      this.$container.addClass('combobox-disabled');
-    }
-
-  , enable: function() {
-      this.$element.prop('disabled', false);
-      this.$button.attr('disabled', false);
-      this.disabled = false;
-      this.$container.removeClass('combobox-disabled');
-    }
   , parse: function () {
       var that = this
         , map = {}
@@ -96,26 +82,32 @@
     }
 
   , transferAttributes: function() {
-    this.options.placeholder = this.$source.attr('data-placeholder') || this.options.placeholder
-    this.$element.attr('placeholder', this.options.placeholder)
-    this.$target.prop('name', this.$source.prop('name'))
-    this.$target.val(this.$source.val())
-    this.$source.removeAttr('name')  // Remove from source otherwise form will pass parameter twice.
-    this.$element.attr('required', this.$source.attr('required'))
-    this.$element.attr('rel', this.$source.attr('rel'))
-    this.$element.attr('title', this.$source.attr('title'))
-    this.$element.attr('class', this.$source.attr('class'))
-    this.$element.attr('tabindex', this.$source.attr('tabindex'))
-    this.$source.removeAttr('tabindex')
-    if (this.$source.attr('disabled')!==undefined)
-      this.disable();
+    this.options.placeholder = this.$source.attr('data-placeholder') || this.options.placeholder;
+    this.$element.attr('placeholder', this.options.placeholder);
+    this.$target.prop('name', this.$source.prop('name'));
+    this.$target.val(this.$source.val());
+    this.$source.removeAttr('name');  // Remove from source otherwise form will pass parameter twice.
+    this.$element.attr('required', this.$source.attr('required'));
+    this.$element.attr('rel', this.$source.attr('rel'));
+    this.$element.attr('title', this.$source.attr('title'));
+    this.$element.attr('class', this.$source.attr('class'));
+    this.$element.attr('tabindex', this.$source.attr('tabindex'));
+    this.$source.removeAttr('tabindex');
+    this.$source.removeAttr('required');
+  }
+
+  , setSelected: function() {
+    this.selected = true;
   }
 
   , select: function () {
       var val = this.$menu.find('.active').attr('data-value');
-      this.$element.val(this.updater(val)).trigger('change');
-      this.$target.val(this.map[val]).trigger('change');
-      this.$source.val(this.map[val]).trigger('change');
+      this.$element.val(this.updater(val));
+      this.$target.val(this.map[val]);
+      this.$source.val(this.map[val]);
+      this.$element.trigger('change');
+      this.$target.trigger('change');
+      this.$source.trigger('change');
       this.$container.addClass('combobox-selected');
       this.selected = true;
       return this.hide();
@@ -138,16 +130,12 @@
         })
         .show();
 
-      $('.dropdown-menu').on('mousedown', $.proxy(this.scrollSafety, this));
-
       this.shown = true;
       return this;
     }
 
   , hide: function () {
       this.$menu.hide();
-      $('.dropdown-menu').off('mousedown', $.proxy(this.scrollSafety, this));
-      this.$element.on('blur', $.proxy(this.blur, this));
       this.shown = false;
       return this;
     }
@@ -171,14 +159,6 @@
       }
 
       return this.render(items.slice(0, this.options.items)).show();
-    }
-
-  , template: function() {
-      if (this.options.bsVersion == '2') {
-        return '<div class="combobox-container"><input type="hidden" /> <div class="input-append"> <input type="text" autocomplete="off" /> <span class="add-on dropdown-toggle" data-dropdown="dropdown"> <span class="caret"/> <i class="icon-remove"/> </span> </div> </div>'
-      } else {
-        return '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <span class="input-group-addon dropdown-toggle" data-dropdown="dropdown"> <span class="caret" /> <span class="glyphicon glyphicon-remove" /> </span> </div> </div>'
-      }
     }
 
   , matcher: function (item) {
@@ -244,27 +224,24 @@
     }
 
   , toggle: function () {
-    if (!this.disabled) {
-      if (this.$container.hasClass('combobox-selected')) {
-        this.clearTarget();
-        this.triggerChange();
-        this.clearElement();
+    if (this.$container.hasClass('combobox-selected')) {
+      this.clearTarget();
+      this.triggerChange();
+      this.clearElement();
+    } else {
+      if (this.shown) {
+        this.hide();
       } else {
-        if (this.shown) {
-          this.hide();
-        } else {
-          this.clearElement();
-          this.lookup();
-        }
+        this.clearElement();
+        this.lookup();
       }
     }
+
+    this.$element.trigger('change');
+    this.$target.trigger('change');
+    this.$source.trigger('change');    
   }
 
-  , scrollSafety: function(e) {
-      if (e.target.tagName == 'UL') {
-          this.$element.off('blur');
-      }
-  }
   , clearElement: function () {
     this.$element.val('').focus();
   }
@@ -356,9 +333,12 @@
         case 37: // left arrow
         case 36: // home
         case 35: // end
+        case 33: // page up
+        case 34: // page down
         case 16: // shift
         case 17: // ctrl
         case 18: // alt
+        case 20: // cap lock
           break;
 
         case 9: // tab
@@ -417,6 +397,7 @@
 
   /* COMBOBOX PLUGIN DEFINITION
    * =========================== */
+
   $.fn.combobox = function ( option ) {
     return this.each(function () {
       var $this = $(this)
@@ -428,7 +409,7 @@
   };
 
   $.fn.combobox.defaults = {
-    bsVersion: '3'
+  template: '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <span class="input-group-addon dropdown-toggle" data-dropdown="dropdown"> <span class="caret" /> <i class="fa fa-times"></i> </span> </div> </div> '
   , menu: '<ul class="typeahead typeahead-long dropdown-menu"></ul>'
   , item: '<li><a href="#"></a></li>'
   };
--- public/js/built.js
+++ public/js/built.js
@@ -29457,7 +29457,7 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
  */
 (function(){var a=jQuery,b=function(){function a(){this.fadeDuration=500,this.fitImagesInViewport=!0,this.resizeDuration=700,this.positionFromTop=50,this.showImageNumberLabel=!0,this.alwaysShowNavOnTouchDevices=!1,this.wrapAround=!1}return a.prototype.albumLabel=function(a,b){return"Image "+a+" of "+b},a}(),c=function(){function b(a){this.options=a,this.album=[],this.currentImageIndex=void 0,this.init()}return b.prototype.init=function(){this.enable(),this.build()},b.prototype.enable=function(){var b=this;a("body").on("click","a[rel^=lightbox], area[rel^=lightbox], a[data-lightbox], area[data-lightbox]",function(c){return b.start(a(c.currentTarget)),!1})},b.prototype.build=function(){var b=this;a("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>").appendTo(a("body")),this.$lightbox=a("#lightbox"),this.$overlay=a("#lightboxOverlay"),this.$outerContainer=this.$lightbox.find(".lb-outerContainer"),this.$container=this.$lightbox.find(".lb-container"),this.containerTopPadding=parseInt(this.$container.css("padding-top"),10),this.containerRightPadding=parseInt(this.$container.css("padding-right"),10),this.containerBottomPadding=parseInt(this.$container.css("padding-bottom"),10),this.containerLeftPadding=parseInt(this.$container.css("padding-left"),10),this.$overlay.hide().on("click",function(){return b.end(),!1}),this.$lightbox.hide().on("click",function(c){return"lightbox"===a(c.target).attr("id")&&b.end(),!1}),this.$outerContainer.on("click",function(c){return"lightbox"===a(c.target).attr("id")&&b.end(),!1}),this.$lightbox.find(".lb-prev").on("click",function(){return b.changeImage(0===b.currentImageIndex?b.album.length-1:b.currentImageIndex-1),!1}),this.$lightbox.find(".lb-next").on("click",function(){return b.changeImage(b.currentImageIndex===b.album.length-1?0:b.currentImageIndex+1),!1}),this.$lightbox.find(".lb-loader, .lb-close").on("click",function(){return b.end(),!1})},b.prototype.start=function(b){function c(a){d.album.push({link:a.attr("href"),title:a.attr("data-title")||a.attr("title")})}var d=this,e=a(window);e.on("resize",a.proxy(this.sizeOverlay,this)),a("select, object, embed").css({visibility:"hidden"}),this.sizeOverlay(),this.album=[];var f,g=0,h=b.attr("data-lightbox");if(h){f=a(b.prop("tagName")+'[data-lightbox="'+h+'"]');for(var i=0;i<f.length;i=++i)c(a(f[i])),f[i]===b[0]&&(g=i)}else if("lightbox"===b.attr("rel"))c(b);else{f=a(b.prop("tagName")+'[rel="'+b.attr("rel")+'"]');for(var j=0;j<f.length;j=++j)c(a(f[j])),f[j]===b[0]&&(g=j)}var k=e.scrollTop()+this.options.positionFromTop,l=e.scrollLeft();this.$lightbox.css({top:k+"px",left:l+"px"}).fadeIn(this.options.fadeDuration),this.changeImage(g)},b.prototype.changeImage=function(b){var c=this;this.disableKeyboardNav();var d=this.$lightbox.find(".lb-image");this.$overlay.fadeIn(this.options.fadeDuration),a(".lb-loader").fadeIn("slow"),this.$lightbox.find(".lb-image, .lb-nav, .lb-prev, .lb-next, .lb-dataContainer, .lb-numbers, .lb-caption").hide(),this.$outerContainer.addClass("animating");var e=new Image;e.onload=function(){var f,g,h,i,j,k,l;d.attr("src",c.album[b].link),f=a(e),d.width(e.width),d.height(e.height),c.options.fitImagesInViewport&&(l=a(window).width(),k=a(window).height(),j=l-c.containerLeftPadding-c.containerRightPadding-20,i=k-c.containerTopPadding-c.containerBottomPadding-120,(e.width>j||e.height>i)&&(e.width/j>e.height/i?(h=j,g=parseInt(e.height/(e.width/h),10),d.width(h),d.height(g)):(g=i,h=parseInt(e.width/(e.height/g),10),d.width(h),d.height(g)))),c.sizeContainer(d.width(),d.height())},e.src=this.album[b].link,this.currentImageIndex=b},b.prototype.sizeOverlay=function(){this.$overlay.width(a(window).width()).height(a(document).height())},b.prototype.sizeContainer=function(a,b){function c(){d.$lightbox.find(".lb-dataContainer").width(g),d.$lightbox.find(".lb-prevLink").height(h),d.$lightbox.find(".lb-nextLink").height(h),d.showImage()}var d=this,e=this.$outerContainer.outerWidth(),f=this.$outerContainer.outerHeight(),g=a+this.containerLeftPadding+this.containerRightPadding,h=b+this.containerTopPadding+this.containerBottomPadding;e!==g||f!==h?this.$outerContainer.animate({width:g,height:h},this.options.resizeDuration,"swing",function(){c()}):c()},b.prototype.showImage=function(){this.$lightbox.find(".lb-loader").hide(),this.$lightbox.find(".lb-image").fadeIn("slow"),this.updateNav(),this.updateDetails(),this.preloadNeighboringImages(),this.enableKeyboardNav()},b.prototype.updateNav=function(){var a=!1;try{document.createEvent("TouchEvent"),a=this.options.alwaysShowNavOnTouchDevices?!0:!1}catch(b){}this.$lightbox.find(".lb-nav").show(),this.album.length>1&&(this.options.wrapAround?(a&&this.$lightbox.find(".lb-prev, .lb-next").css("opacity","1"),this.$lightbox.find(".lb-prev, .lb-next").show()):(this.currentImageIndex>0&&(this.$lightbox.find(".lb-prev").show(),a&&this.$lightbox.find(".lb-prev").css("opacity","1")),this.currentImageIndex<this.album.length-1&&(this.$lightbox.find(".lb-next").show(),a&&this.$lightbox.find(".lb-next").css("opacity","1"))))},b.prototype.updateDetails=function(){var b=this;"undefined"!=typeof this.album[this.currentImageIndex].title&&""!==this.album[this.currentImageIndex].title&&this.$lightbox.find(".lb-caption").html(this.album[this.currentImageIndex].title).fadeIn("fast").find("a").on("click",function(){location.href=a(this).attr("href")}),this.album.length>1&&this.options.showImageNumberLabel?this.$lightbox.find(".lb-number").text(this.options.albumLabel(this.currentImageIndex+1,this.album.length)).fadeIn("fast"):this.$lightbox.find(".lb-number").hide(),this.$outerContainer.removeClass("animating"),this.$lightbox.find(".lb-dataContainer").fadeIn(this.options.resizeDuration,function(){return b.sizeOverlay()})},b.prototype.preloadNeighboringImages=function(){if(this.album.length>this.currentImageIndex+1){var a=new Image;a.src=this.album[this.currentImageIndex+1].link}if(this.currentImageIndex>0){var b=new Image;b.src=this.album[this.currentImageIndex-1].link}},b.prototype.enableKeyboardNav=function(){a(document).on("keyup.keyboard",a.proxy(this.keyboardAction,this))},b.prototype.disableKeyboardNav=function(){a(document).off(".keyboard")},b.prototype.keyboardAction=function(a){var b=27,c=37,d=39,e=a.keyCode,f=String.fromCharCode(e).toLowerCase();e===b||f.match(/x|o|c/)?this.end():"p"===f||e===c?0!==this.currentImageIndex?this.changeImage(this.currentImageIndex-1):this.options.wrapAround&&this.album.length>1&&this.changeImage(this.album.length-1):("n"===f||e===d)&&(this.currentImageIndex!==this.album.length-1?this.changeImage(this.currentImageIndex+1):this.options.wrapAround&&this.album.length>1&&this.changeImage(0))},b.prototype.end=function(){this.disableKeyboardNav(),a(window).off("resize",this.sizeOverlay),this.$lightbox.fadeOut(this.options.fadeDuration),this.$overlay.fadeOut(this.options.fadeDuration),a("select, object, embed").css({visibility:"visible"})},b}();a(function(){{var a=new b;new c(a)}})}).call(this);
 /* =============================================================
- * bootstrap-combobox.js v1.1.6
+ * bootstrap-combobox.js v1.1.5
  * =============================================================
  * Copyright 2012 Daniel Farrell
  *
@@ -29483,7 +29483,6 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
 
   var Combobox = function ( element, options ) {
     this.options = $.extend({}, $.fn.combobox.defaults, options);
-    this.template = this.options.template || this.template
     this.$source = $(element);
     this.$container = this.setup();
     this.$element = this.$container.find('input[type=text]');
@@ -29505,25 +29504,12 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
     constructor: Combobox
 
   , setup: function () {
-      var combobox = $(this.template());
+      var combobox = $(this.options.template);
       this.$source.before(combobox);
       this.$source.hide();
       return combobox;
     }
 
-  , disable: function() {
-      this.$element.prop('disabled', true);
-      this.$button.attr('disabled', true);
-      this.disabled = true;
-      this.$container.addClass('combobox-disabled');
-    }
-
-  , enable: function() {
-      this.$element.prop('disabled', false);
-      this.$button.attr('disabled', false);
-      this.disabled = false;
-      this.$container.removeClass('combobox-disabled');
-    }
   , parse: function () {
       var that = this
         , map = {}
@@ -29554,26 +29540,32 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
     }
 
   , transferAttributes: function() {
-    this.options.placeholder = this.$source.attr('data-placeholder') || this.options.placeholder
-    this.$element.attr('placeholder', this.options.placeholder)
-    this.$target.prop('name', this.$source.prop('name'))
-    this.$target.val(this.$source.val())
-    this.$source.removeAttr('name')  // Remove from source otherwise form will pass parameter twice.
-    this.$element.attr('required', this.$source.attr('required'))
-    this.$element.attr('rel', this.$source.attr('rel'))
-    this.$element.attr('title', this.$source.attr('title'))
-    this.$element.attr('class', this.$source.attr('class'))
-    this.$element.attr('tabindex', this.$source.attr('tabindex'))
-    this.$source.removeAttr('tabindex')
-    if (this.$source.attr('disabled')!==undefined)
-      this.disable();
+    this.options.placeholder = this.$source.attr('data-placeholder') || this.options.placeholder;
+    this.$element.attr('placeholder', this.options.placeholder);
+    this.$target.prop('name', this.$source.prop('name'));
+    this.$target.val(this.$source.val());
+    this.$source.removeAttr('name');  // Remove from source otherwise form will pass parameter twice.
+    this.$element.attr('required', this.$source.attr('required'));
+    this.$element.attr('rel', this.$source.attr('rel'));
+    this.$element.attr('title', this.$source.attr('title'));
+    this.$element.attr('class', this.$source.attr('class'));
+    this.$element.attr('tabindex', this.$source.attr('tabindex'));
+    this.$source.removeAttr('tabindex');
+    this.$source.removeAttr('required');
+  }
+
+  , setSelected: function() {
+    this.selected = true;
   }
 
   , select: function () {
       var val = this.$menu.find('.active').attr('data-value');
-      this.$element.val(this.updater(val)).trigger('change');
-      this.$target.val(this.map[val]).trigger('change');
-      this.$source.val(this.map[val]).trigger('change');
+      this.$element.val(this.updater(val));
+      this.$target.val(this.map[val]);
+      this.$source.val(this.map[val]);
+      this.$element.trigger('change');
+      this.$target.trigger('change');
+      this.$source.trigger('change');
       this.$container.addClass('combobox-selected');
       this.selected = true;
       return this.hide();
@@ -29596,16 +29588,12 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
         })
         .show();
 
-      $('.dropdown-menu').on('mousedown', $.proxy(this.scrollSafety, this));
-
       this.shown = true;
       return this;
     }
 
   , hide: function () {
       this.$menu.hide();
-      $('.dropdown-menu').off('mousedown', $.proxy(this.scrollSafety, this));
-      this.$element.on('blur', $.proxy(this.blur, this));
       this.shown = false;
       return this;
     }
@@ -29629,14 +29617,6 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
       }
 
       return this.render(items.slice(0, this.options.items)).show();
-    }
-
-  , template: function() {
-      if (this.options.bsVersion == '2') {
-        return '<div class="combobox-container"><input type="hidden" /> <div class="input-append"> <input type="text" autocomplete="off" /> <span class="add-on dropdown-toggle" data-dropdown="dropdown"> <span class="caret"/> <i class="icon-remove"/> </span> </div> </div>'
-      } else {
-        return '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <span class="input-group-addon dropdown-toggle" data-dropdown="dropdown"> <span class="caret" /> <span class="glyphicon glyphicon-remove" /> </span> </div> </div>'
-      }
     }
 
   , matcher: function (item) {
@@ -29702,27 +29682,24 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
     }
 
   , toggle: function () {
-    if (!this.disabled) {
-      if (this.$container.hasClass('combobox-selected')) {
-        this.clearTarget();
-        this.triggerChange();
-        this.clearElement();
+    if (this.$container.hasClass('combobox-selected')) {
+      this.clearTarget();
+      this.triggerChange();
+      this.clearElement();
+    } else {
+      if (this.shown) {
+        this.hide();
       } else {
-        if (this.shown) {
-          this.hide();
-        } else {
-          this.clearElement();
-          this.lookup();
-        }
+        this.clearElement();
+        this.lookup();
       }
     }
+
+    this.$element.trigger('change');
+    this.$target.trigger('change');
+    this.$source.trigger('change');    
   }
 
-  , scrollSafety: function(e) {
-      if (e.target.tagName == 'UL') {
-          this.$element.off('blur');
-      }
-  }
   , clearElement: function () {
     this.$element.val('').focus();
   }
@@ -29814,9 +29791,12 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
         case 37: // left arrow
         case 36: // home
         case 35: // end
+        case 33: // page up
+        case 34: // page down
         case 16: // shift
         case 17: // ctrl
         case 18: // alt
+        case 20: // cap lock
           break;
 
         case 9: // tab
@@ -29875,6 +29855,7 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
 
   /* COMBOBOX PLUGIN DEFINITION
    * =========================== */
+
   $.fn.combobox = function ( option ) {
     return this.each(function () {
       var $this = $(this)
@@ -29886,7 +29867,7 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
   };
 
   $.fn.combobox.defaults = {
-    bsVersion: '3'
+  template: '<div class="combobox-container"> <input type="hidden" /> <div class="input-group"> <input type="text" autocomplete="off" /> <span class="input-group-addon dropdown-toggle" data-dropdown="dropdown"> <span class="caret" /> <i class="fa fa-times"></i> </span> </div> </div> '
   , menu: '<ul class="typeahead typeahead-long dropdown-menu"></ul>'
   , item: '<li><a href="#"></a></li>'
   };
@@ -29894,6 +29875,7 @@ links:["Africa/Abidjan|Africa/Bamako","Africa/Abidjan|Africa/Banjul","Africa/Abi
   $.fn.combobox.Constructor = Combobox;
 
 }( window.jQuery );
+
 // http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
 var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
