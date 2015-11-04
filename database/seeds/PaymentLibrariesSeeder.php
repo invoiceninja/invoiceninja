5555000f83d7107345ef7e7ffb804b8df9f37b16
Hillel Coren <hillelcoren@gmail.com>
2015-11-04 20:13:15 +0200

    Added additional OmniPay drivers

--- database/seeds/PaymentLibrariesSeeder.php
+++ database/seeds/PaymentLibrariesSeeder.php
@@ -41,6 +41,23 @@ class PaymentLibrariesSeeder extends Seeder
             ['name' => 'BitPay', 'provider' => 'BitPay', 'payment_library_id' => 1],
             ['name' => 'Dwolla', 'provider' => 'Dwolla', 'payment_library_id' => 1],
             ['name' => 'Eway Rapid', 'provider' => 'Eway_RapidShared', 'payment_library_id' => 1],
+            ['name' => 'AGMS', 'provider' => 'Agms', 'payment_library_id' => 1],
+            ['name' => 'Barclays', 'provider' => 'BarclaysEpdq\Essential', 'payment_library_id' => 1],
+            ['name' => 'Cardgate', 'provider' => 'Cardgate', 'payment_library_id' => 1],
+            ['name' => 'Checkout.com', 'provider' => 'CheckoutCom', 'payment_library_id' => 1],
+            ['name' => 'Creditcall', 'provider' => 'Creditcall', 'payment_library_id' => 1],
+            ['name' => 'Cybersource', 'provider' => 'Cybersource', 'payment_library_id' => 1],
+            ['name' => 'ecoPayz', 'provider' => 'Ecopayz', 'payment_library_id' => 1],
+            ['name' => 'Fasapay', 'provider' => 'Fasapay', 'payment_library_id' => 1],
+            ['name' => 'Komoju', 'provider' => 'Komoju', 'payment_library_id' => 1],
+            ['name' => 'Multicards', 'provider' => 'Multicards', 'payment_library_id' => 1],
+            ['name' => 'Pagar.Me', 'provider' => 'Pagarme', 'payment_library_id' => 1],
+            ['name' => 'Paysafecard', 'provider' => 'Paysafecard', 'payment_library_id' => 1],
+            ['name' => 'Paytrace', 'provider' => 'Paytrace_CreditCard', 'payment_library_id' => 1],
+            ['name' => 'Secure Trading', 'provider' => 'SecureTrading', 'payment_library_id' => 1],
+            ['name' => 'SecPay', 'provider' => 'SecPay', 'payment_library_id' => 1],
+            ['name' => 'WeChat Express', 'provider' => 'WeChat_Express', 'payment_library_id' => 1],
+            ['name' => 'WePay', 'provider' => 'WePay', 'payment_library_id' => 1],
         ];
 
         foreach ($gateways as $gateway) {
