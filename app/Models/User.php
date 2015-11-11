0feb5cb082d991162d52e8a71eadb79de53d5b97
Eduardo Cruz <eduardo@eduardocruz.com>
2015-11-11 13:34:18 -0300

    Updating the User Modal fillable attributes.
    Replacing name with first_name and last_name

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
 
     /**
      * The attributes excluded from the model's JSON form.
