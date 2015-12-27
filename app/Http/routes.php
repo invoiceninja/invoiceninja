ddd5915ca1bb683901935ce489e4fdf3f362be2b
Hillel Coren <hillelcoren@gmail.com>
2015-12-27 09:36:22 +0200

    git update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -551,8 +552,8 @@ if (!defined('CONTACT_EMAIL')) {
     }
 }
 
-// Log all SQL queries to laravel.log
 /*
+// Log all SQL queries to laravel.log
 if (Utils::isNinjaDev()) {
     Event::listen('illuminate.query', function($query, $bindings, $time, $name) {
         $data = compact('bindings', 'time', 'name');
