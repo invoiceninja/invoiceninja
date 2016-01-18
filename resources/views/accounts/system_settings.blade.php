4d5aa1a712d9536d67a94de833b7c278e9b2cb8f
Hillel Coren <hillelcoren@gmail.com>
2016-01-18 12:53:26 +0200

    Disabled autocomplete on self host system settings form

--- resources/views/accounts/system_settings.blade.php
+++ resources/views/accounts/system_settings.blade.php
@@ -3,20 +3,21 @@
 @section('content') 
     @parent
 
-    {!! Former::open('/update_setup')->addClass('warn-on-exit') !!}
-
     @include('accounts.nav', ['selected' => ACCOUNT_SYSTEM_SETTINGS])
 
     <div class="row">
 
-        {!! Former::open()->rules([
-            'app[url]' => 'required',
-            //'database[default]' => 'required',
-            'database[type][host]' => 'required',
-            'database[type][database]' => 'required',
-            'database[type][username]' => 'required',
-            'database[type][password]' => 'required',
-          ]) !!}
+        {!! Former::open('/update_setup')
+            ->addClass('warn-on-exit')
+            ->autocomplete('off')
+            ->rules([
+                'app[url]' => 'required',
+                //'database[default]' => 'required',
+                'database[type][host]' => 'required',
+                'database[type][database]' => 'required',
+                'database[type][username]' => 'required',
+                'database[type][password]' => 'required',
+              ]) !!}
 
 
         @include('partials.system_settings')
