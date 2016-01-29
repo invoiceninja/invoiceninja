020e4286772a236511effdee271841a4a9dd39fe
David Bomba <turbo124@gmail.com>
2016-01-29 12:47:35 +1100

    testing email functionality:

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -230,6 +230,7 @@ Route::group(['middleware' => 'api', 'prefix' => 'api/v1'], function()
     Route::resource('tasks', 'TaskApiController');
     Route::post('hooks', 'IntegrationController@subscribe');
     Route::post('email_invoice', 'InvoiceApiController@emailInvoice');
+    Route::post('email_invoicev2', 'InvoiceApiController@emailInvoicev2');
     Route::get('user_accounts','AccountApiController@getUserAccounts');
 
     // Vendor
