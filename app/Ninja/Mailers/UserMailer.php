5f709f76a80abc84464e69294d1329771bf6ba4b
Hillel Coren <hillelcoren@gmail.com>
2016-02-03 22:53:18 +0200

    Bug fix: notification emails have the wrong entity type

--- app/Ninja/Mailers/UserMailer.php
+++ app/Ninja/Mailers/UserMailer.php
@@ -40,8 +40,8 @@ class UserMailer extends Mailer
             return;
         }
         
-        $entityType = $notificationType == 'approved' ? ENTITY_QUOTE : ENTITY_INVOICE;
-        $view = "{$entityType}_{$notificationType}";
+        $entityType = $invoice->getEntityType();
+        $view = ($notificationType == 'approved' ? ENTITY_QUOTE : ENTITY_INVOICE) . "_{$notificationType}";
         $account = $user->account;
         $client = $invoice->client;
 
