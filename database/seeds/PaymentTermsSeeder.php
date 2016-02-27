694929c05e76710e09c1244d5d9043930c7a24ae
Hillel Coren <hillelcoren@gmail.com>
2016-02-27 22:12:38 +0200

    Broke up seeders

---
+++ database/seeds/PaymentTermsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\PaymentTerm;
+
+class PaymentTermsSeeder extends Seeder
+{
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $paymentTerms = [
+            ['num_days' => -1, 'name' => 'Net 0'],
+        ];
+
+        foreach ($paymentTerms as $paymentTerm) {
+            if (!DB::table('payment_terms')->where('name', '=', $paymentTerm['name'])->get()) {
+                PaymentTerm::create($paymentTerm);
+            }
+        }
+    }
+
+}
