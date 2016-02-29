3f11969c7c1201aa46623d06c5c4c1d47a58ad0a
Hillel Coren <hillelcoren@gmail.com>
2016-02-29 20:08:09 +0200

    Fix for tests after product_key change

--- tests/acceptance/PaymentCest.php
+++ tests/acceptance/PaymentCest.php
@@ -40,6 +40,7 @@ class PaymentCest
         $I->amOnPage('/invoices/create');
         $I->selectDropdown($I, $clientEmail, '.client_select .dropdown-toggle');
         $I->fillField('table.invoice-table tbody tr:nth-child(1) #product_key', $productKey);
+        $I->click('table.invoice-table tbody tr:nth-child(1) .tt-selectable');
         $I->click('Save');
         $I->see($clientEmail);
 
