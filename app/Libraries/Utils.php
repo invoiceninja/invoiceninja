b719f243c234ee1cfc03d5a6923616375b9cbbf4
Hillel Coren <hillelcoren@gmail.com>
2016-03-03 22:24:27 +0200

    Fix for #751

--- app/Libraries/Utils.php
+++ app/Libraries/Utils.php
@@ -127,6 +127,13 @@ class Utils
     {
         return App::getLocale() == 'en';
     }
+    
+    public static function getLocaleRegion()
+    {
+        $parts = explode('_', App::getLocale()); 
+        
+        return count($parts) ? $parts[0] : 'en';
+    }
 
     public static function getUserType()
     {
