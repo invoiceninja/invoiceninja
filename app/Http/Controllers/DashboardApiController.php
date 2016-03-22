b8560187f5c62cce5113b0c88b654fa3c3a205e3
David Bomba <turbo124@gmail.com>
2016-03-22 21:04:23 +1100

    Dashboard API

--- app/Http/Controllers/DashboardApiController.php
+++ app/Http/Controllers/DashboardApiController.php
@@ -161,7 +161,9 @@ class DashboardApiController extends BaseAPIController
         $data = [
             'id' => 1,
             'paidToDate' => $paidToDate[0]->value,
+            'paidToDateCurrency' => $paidToDate[0]->currency_id,
             'balances' => $balances[0]->value,
+            'balancesCurrency' => $balances[0]->currency_id,
             'averageInvoice' => $averageInvoice[0]->invoice_avg,
             'invoicesSent' => $metrics ? $metrics->invoices_sent : 0,
             'activeClients' => $metrics ? $metrics->active_clients : 0,
