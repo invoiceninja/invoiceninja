7a929a51bb06f899490ef8c213bb9b02bcf6f68a
Hillel Coren <hillelcoren@gmail.com>
2016-03-27 17:39:26 +0300

    Added back .env variables #787

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -83,20 +83,23 @@ class AppController extends BaseController
             return Redirect::to('/');
         }
 
-        $_ENV['APP_ENV']='production';
-        $_ENV['APP_DEBUG']=$app['debug'];
-        $_ENV['APP_URL']=$app['url'];
-        $_ENV['APP_KEY']=$app['key'];
-        $_ENV['DB_TYPE']=$dbType;
-        $_ENV['DB_HOST']=$database['type']['host'];
-        $_ENV['DB_DATABASE']=$database['type']['database'];
-        $_ENV['DB_USERNAME']=$database['type']['username'];
-        $_ENV['DB_PASSWORD']=$database['type']['password'];
-        $_ENV['MAIL_DRIVER']=$mail['driver'];
-        $_ENV['MAIL_PORT']=$mail['port'];
-        $_ENV['MAIL_ENCRYPTION']=$mail['encryption'];
-        $_ENV['MAIL_HOST']=$mail['host'];
-        $_ENV['MAIL_USERNAME']=$mail['username'];;
+        $_ENV['APP_ENV'] = 'production';
+        $_ENV['APP_DEBUG'] = $app['debug'];
+        $_ENV['APP_URL'] = $app['url'];
+        $_ENV['APP_KEY'] = $app['key'];
+        $_ENV['DB_TYPE'] = $dbType;
+        $_ENV['DB_HOST'] = $database['type']['host'];
+        $_ENV['DB_DATABASE'] = $database['type']['database'];
+        $_ENV['DB_USERNAME'] = $database['type']['username'];
+        $_ENV['DB_PASSWORD'] = $database['type']['password'];
+        $_ENV['MAIL_DRIVER'] = $mail['driver'];
+        $_ENV['MAIL_PORT'] = $mail['port'];
+        $_ENV['MAIL_ENCRYPTION'] = $mail['encryption'];
+        $_ENV['MAIL_HOST'] = $mail['host'];
+        $_ENV['MAIL_USERNAME'] = $mail['username'];
+        $_ENV['MAIL_FROM_NAME'] = $mail['from']['name'];
+        $_ENV['MAIL_PASSWORD'] = $mail['password'];
+        $_ENV['PHANTOMJS_CLOUD_KEY'] = 'a-demo-key-with-low-quota-per-ip-address';
 
         $config = '';
         foreach ($_ENV as $key => $val) {
