fbb0c29ed85f3176e14648ef018bc836733c99f0
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2016-05-05 14:16:56 +0700

    Add Thai translation

---
+++ resources/lang/th/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "รหัสผ่านต้องมีความยาวอย่างน้อย 6 ตัวอักษร และต้องยืนยันให้ตรงกัน",
+    "user" => "ไม่พบผู้ใช้ตาม e-mail ที่ระบุ",
+    "token" => "Token สำหรับการตั้งรหัสผ่านใหม่ ไม่ถูกต้อง",
+    "sent" => "ระบบได้ส่งลิงค์สำหรับกำหนดรหัสผ่านใหม่ให้ท่านแล้ว!",
+    "reset" => "รหัสผ่านของคุณได้ถูกกำหนดใหม่แล้ว!",
+
+];
