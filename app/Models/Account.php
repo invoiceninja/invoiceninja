7296c157307e1a2892c715eb72fa21f417e6696e
Hillel Coren <hillelcoren@gmail.com>
2016-05-06 15:54:58 +0300

    Fix for footer font size in iOS mail

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -1180,7 +1180,7 @@ class Account extends Eloquent
             // Add line breaks if HTML isn't already being used
             return strip_tags($this->email_footer) == $this->email_footer ? nl2br($this->email_footer) : $this->email_footer;
         } else {
-            return "<p>" . trans('texts.email_signature') . "\n<br>\$account</p>";
+            return "<p><div>" . trans('texts.email_signature') . "\n<br>\$account</div></p>";
         }
     }
 
