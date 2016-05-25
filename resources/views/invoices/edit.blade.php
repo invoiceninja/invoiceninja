ba561c7d0d3b3f3672608344d382735d908f611f
Hillel Coren <hillelcoren@gmail.com>
2016-05-25 14:42:20 +0300

    Prevent clicking save while document is still uploading

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -1265,6 +1265,11 @@
 	}
 
     function onFormSubmit(event) {
+        if (window.countUploadingDocuments > 0) {
+            alert("{!! trans('texts.wait_for_upload') !!}");
+            return false;
+        }
+
         if (!isSaveValid()) {
             model.showClientForm();
             return false;
@@ -1435,6 +1440,7 @@
         model.invoice().invoice_number(number);
     }
 
+    window.countUploadingDocuments = 0;
     @if ($account->hasFeature(FEATURE_DOCUMENTS))
     function handleDocumentAdded(file){
         // open document when clicked
@@ -1446,6 +1452,7 @@
         if(file.mock)return;
         file.index = model.invoice().documents().length;
         model.invoice().addDocument({name:file.name, size:file.size, type:file.type});
+        window.countUploadingDocuments++;
     }
 
     function handleDocumentRemoved(file){
@@ -1456,6 +1463,7 @@
     function handleDocumentUploaded(file, response){
         file.public_id = response.document.public_id
         model.invoice().documents()[file.index].update(response.document);
+        window.countUploadingDocuments--;
         refreshPDF(true);
 
         if(response.document.preview_url){
