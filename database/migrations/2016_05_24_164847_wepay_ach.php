5cda4c0dd96beccf2f229656972db5088ae0f513
Hillel Coren <hillelcoren@gmail.com>
2016-06-03 11:29:03 +0300

    Fix capitalization of migration file name

--- database/migrations/2016_05_24_164847_wepay_ach.php
+++ database/migrations/2016_05_24_164847_wepay_ach.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class WePayAch extends Migration
+class WepayAch extends Migration
 {
     /**
      * Run the migrations.
