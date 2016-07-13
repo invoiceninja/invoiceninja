f3a278b9b56790d4ef364b38d819a0723fb00895
Hillel Coren <hillelcoren@gmail.com>
2016-07-13 16:33:49 +0300

    Moved buy now div id

--- resources/views/accounts/client_portal.blade.php
+++ resources/views/accounts/client_portal.blade.php
@@ -79,12 +79,12 @@
             </div>
         </div>
 
-        <div class="panel panel-default">
+        <div class="panel panel-default" id="buyNow">
             <div class="panel-heading">
                 <h3 class="panel-title">{!! trans('texts.buy_now_buttons') !!}</h3>
             </div>
             <div class="panel-body">
-                <div class="col-md-10 col-md-offset-1" id="buyNow">
+                <div class="col-md-10 col-md-offset-1">
 
                     @if (count($gateway_types) && count($products))
 
