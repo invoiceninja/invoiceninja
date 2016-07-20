f5a5457d2b2b6fda7ffa557f668a8419dc3a9003
Holger Lösken <holger.loesken@codedge.de>
2016-07-20 19:38:56 +0200

    JS problems after using elixir - Added missing JS files - Added missing images - Recreated merged files

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,8 @@ var elixir = require('laravel-elixir');
 elixir.config.sourcemaps = true;
 
 /**
- * Directory for JS source files that are handled by Bower.
- * Bower source file directory.
- *
- * @type {string}
- */
-var bowerDirectoy = './resources/assets/bower/';
-
-/**
  * Directory for JS assets that are not handled by Bower
- * 
+ *
  * @type {string}
  */
 var resourcesJsDirectory = './resources/assets/js/';
@@ -28,6 +20,14 @@ var resourcesJsDirectory = './resources/assets/js/';
  * @type {string}
  */
 var resourcesCssDirectory = './resources/assets/css/';
+
+/**
+ * Directory for JS source files that are handled by Bower.
+ * Bower source file directory.
+ *
+ * @type {string}
+ */
+var bowerDirectoy = './resources/assets/bower/';
 
 elixir(function(mix) {
 
@@ -55,8 +55,7 @@ elixir(function(mix) {
         resourcesCssDirectory + 'public.style.css'
     ], 'public/css/built.public.min.css');
 
-    mix.copy(resourcesCssDirectory + 'img', 'public/css/img')
-        .copy(resourcesCssDirectory + 'themes', 'public/css/themes');
+    mix.copy(resourcesCssDirectory + 'themes', 'public/css/themes');
 
     // Copy other CSS files to public
     mix.styles(bowerDirectoy + 'quill/dist/quill.snow.css', 'public/css/quill.snow.min.css')
@@ -97,7 +96,7 @@ elixir(function(mix) {
         bowerDirectoy + 'accounting/accounting.js',
         bowerDirectoy + 'spectrum/spectrum.js',
         bowerDirectoy + 'jspdf/dist/jspdf.min.js',
-        bowerDirectoy + 'jspdf-plugins/plugins/split_text_to_size.js',
+        bowerDirectoy + 'jsPDF-plugins/plugins/split_text_to_size.js',
         bowerDirectoy + 'moment/moment.js',
         bowerDirectoy + 'moment-timezone/builds/moment-timezone-with-data.js',
         bowerDirectoy + 'stacktrace-js/dist/stacktrace-with-promises-and-json-polyfills.min.js',
@@ -124,6 +123,9 @@ elixir(function(mix) {
     // Copy VFS Fonts to public directory
     mix.copy(resourcesJsDirectory + 'vfs_fonts', 'public/js/vfs_fonts');
 
+    // Copy SVG file for Jsoneditor to public directory
+    mix.copy(bowerDirectoy + 'jsoneditor/dist/img', 'public/css/img');
+
     // Copy other JS files to public
     mix.scripts(resourcesJsDirectory + 'Chart.js', 'public/js/Chart.min.js')
         .scripts(bowerDirectoy + 'datetimepicker/build/jquery.datetimepicker.full.js', 'public/js/jquery.datetimepicker.min.js')
@@ -131,6 +133,8 @@ elixir(function(mix) {
         .scripts(bowerDirectoy + 'quill/dist/quill.js', 'public/js/quill.min.js')
         .scripts(bowerDirectoy + 'lightbox2/dist/js/lightbox.js', 'public/js/lightbox.min.js')
         .scripts(bowerDirectoy + 'bootstrap-combobox/js/bootstrap-combobox.js', 'public/js/bootstrap-combobox.min.js')
+        .scripts(bowerDirectoy + 'jsoneditor/dist/jsoneditor.js', 'public/js/jsoneditor.min.js')
         .scripts(resourcesJsDirectory + 'pdf.js', 'public/js/pdf.min.js')
-        .scripts(resourcesJsDirectory + 'pdf_viewer.worker.js', 'public/js/pdf_viewer.worker.js');
+        .scripts(resourcesJsDirectory + 'pdf_viewer.worker.js', 'public/js/pdf_viewer.worker.js')
+        .scripts(resourcesJsDirectory + 'script.js', 'public/js/script.js');
 });
