9c30dae5bd91d231f36b4a769ca2cc10a60ca4b2
Holger Lösken <holger.loesken@codedge.de>
2016-07-21 22:59:33 +0200

    Move bower dir to resources - Exclude new bower dir from Git - Set new bower dir in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
+/**
+ * Directory for Bower source files
+ *
+ * @type {string}
  */
+var bowerDir = 'resources/assets/bower';
 
 elixir(function(mix) {
-    mix.less('app.less');
+    
 });
