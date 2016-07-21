d13e95399e8f113e835e08c7417c0f319acd524c
Hillel Coren <hillelcoren@gmail.com>
2016-07-21 15:30:11 +0300

    Revert "Translate error template"

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 
-    <p>&nbsp;<p>
-    <p>&nbsp;<p>
+<p>&nbsp;<p>
+<p>&nbsp;<p>
 
-    <div class="well">
-        <div class="container" style="min-height:400px">
-            <h3>{{ trans('texts.error_title') }}...</h3>
-            <h4>{{ $error }}</h4>
-            <h4>{{ trans('texts.error_contact_text', ['mailaddress' => env('MAIL_USERNAME')]) }}</h4>
-        </div>
-    </div>
+<div class="well">
+  <div class="container" style="min-height:400px">
+  <h3>Something went wrong...</h3>
+  <h4>{{ $error }}</h4>
+  <h4>If you'd like help please email us at {{ env('MAIL_USERNAME') }}.</h4>
+</div>
+</div>
 
-    <p>&nbsp;<p>
-    <p>&nbsp;<p>
+<p>&nbsp;<p>
+<p>&nbsp;<p>
 
 @stop
