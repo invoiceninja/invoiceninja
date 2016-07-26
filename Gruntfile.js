ecd6422156156b962a968d5ab5863c07d4591d2f
Hillel Coren <hillelcoren@gmail.com>
2016-07-26 23:04:25 +0300

    Fix for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,12 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     dump_dir: (function() {
       var out = {};
-      
+
       grunt.file.expand({ filter: 'isDirectory'}, 'public/fonts/invoice-fonts/*').forEach(function(path) {
         var fontName = /[^/]*$/.exec(path)[0],
             files = {},
             license='';
-        
+
         // Add license text
         grunt.file.expand({ filter: 'isFile'}, path+'/*.txt').forEach(function(path) {
             var licenseText = grunt.file.read(path);
@@ -19,10 +19,10 @@ module.exports = function(grunt) {
 
             license += "/*\n"+licenseText+"\n*/";
         });
-          
+
         // Create files list
         files['public/js/vfs_fonts/'+fontName+'.js'] = [path+'/*.ttf'];
-          
+
         out[fontName] = {
           options: {
             pre: license+'window.ninjaFontVfs=window.ninjaFontVfs||{};window.ninjaFontVfs.'+fontName+'=',
@@ -30,8 +30,8 @@ module.exports = function(grunt) {
           },
           files: files
         };
-      });      
-         
+      });
+
       // Return the computed object
       return out;
     }()),
@@ -162,8 +162,8 @@ module.exports = function(grunt) {
         nonull: true,
         options: {
             process: false
-        }*/
-      },
+        }
+      },*/
       /*js_pdf: {
         src: [
           'public/js/pdf_viewer.js',
