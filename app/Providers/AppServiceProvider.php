1b19d09f79370d4b7ba541e6623335491371b1d4
Hillel Coren <hillelcoren@gmail.com>
2016-08-24 08:49:28 +0300

    Fixed url2 error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,7 +33,8 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Form::macro('nav_link', function($url, $text) {
-            $class = ( Request::is($url) || Request::is($url.'/*') || Request::is($url2.'/*') ) ? ' class="active"' : '';
+            //$class = ( Request::is($url) || Request::is($url.'/*') || Request::is($url2.'/*') ) ? ' class="active"' : '';
+            $class = ( Request::is($url) || Request::is($url.'/*') ) ? ' class="active"' : '';
             $title = trans("texts.$text")  . Utils::getProLabel($text);
 
             return '<li'.$class.'><a href="'.URL::to($url).'">'.$title.'</a></li>';
