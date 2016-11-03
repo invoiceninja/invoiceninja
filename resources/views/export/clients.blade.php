50c63329f2cf95e62883fa2da8cd731c2609e473
Hillel Coren <hillelcoren@gmail.com>
2016-11-03 10:49:17 +0200

    Add additional fields to client CSV export #1136

--- resources/views/export/clients.blade.php
+++ resources/views/export/clients.blade.php
@@ -11,6 +11,10 @@
     <td>{{ trans('texts.state') }}</td>
     <td>{{ trans('texts.postal_code') }}</td>
     <td>{{ trans('texts.country') }}</td>
+    <td>{{ trans('texts.id_number') }}</td>
+    <td>{{ trans('texts.vat_number') }}</td>
+    <td>{{ trans('texts.website') }}</td>
+    <td>{{ trans('texts.phone') }}</td>
     @if ($account->custom_client_label1)
         <td>{{ $account->custom_client_label1 }}</td>
     @endif
@@ -33,6 +37,10 @@
         <td>{{ $client->state }}</td>
         <td>{{ $client->postal_code }}</td>
         <td>{{ $client->present()->country }}</td>
+        <td>{{ $client->id_number }}</td>
+        <td>{{ $client->vat_number }}</td>
+        <td>{{ $client->website }}</td>
+        <td>{{ $client->work_phone }}</td>
         @if ($account->custom_client_label1)
             <td>{{ $client->custom_value1 }}</td>
         @endif
