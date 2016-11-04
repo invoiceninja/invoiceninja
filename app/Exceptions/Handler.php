3858490d64c56f3a021ece315ea9e0178bc3db81
David Bomba <turbo124@gmail.com>
2016-11-04 20:40:11 +1100

    generic catch all HTTP error handler for API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -96,8 +96,8 @@ class Handler extends ExceptionHandler
                         $error = json_encode($error, JSON_PRETTY_PRINT);
                         $headers = Utils::getApiHeaders();
 
-                        //return $response()->make($error, 404, $headers);
-                        return $response()->json(['error']['message'=>'Route does not exist']);
+                        return $response()->make($error, 404, $headers);
+                        //return $response()->json(['error']['message'=>'Route does not exist']);
                     }
                     break;
 
