04bbf1b2ae3a7d6c80e9b4928ad98e91b222f519
Hillel Coren <hillelcoren@gmail.com>
2016-11-16 12:52:39 +0200

    Fix for migration

--- database/migrations/2016_11_03_161149_add_bluevine_fields.php
+++ database/migrations/2016_11_03_161149_add_bluevine_fields.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddBlueVineFields extends Migration {
+class AddBluevineFields extends Migration {
 	/**
 	 * Run the migrations.
 	 *
