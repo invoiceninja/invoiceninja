dff371e7bdae18ffe35bc911d5e4b93d6656d6f3
Hillel Coren <hillelcoren@gmail.com>
2016-11-29 21:06:28 +0200

    Fix for self host database migrations

--- database/migrations/2016_09_05_150625_create_gateway_types.php
+++ database/migrations/2016_09_05_150625_create_gateway_types.php
@@ -34,11 +34,13 @@ class CreateGatewayTypes extends Migration
 
             $table->unsignedInteger('min_limit')->nullable();
             $table->unsignedInteger('max_limit')->nullable();
+        });
 
+        Schema::table('account_gateway_settings', function($table)
+        {
             $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('gateway_type_id')->references('id')->on('gateway_types')->onDelete('cascade');
-
         });
 
         Schema::table('payment_types', function($table)
@@ -54,7 +56,7 @@ class CreateGatewayTypes extends Migration
         });
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
-    
+
     /**
      * Reverse the migrations.
      *
