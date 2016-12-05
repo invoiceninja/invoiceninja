de7664f205662818c79035a9eecdf7e145504ba1
Hillel Coren <hillelcoren@gmail.com>
2016-12-05 12:30:41 +0200

    Fix for tests

--- tests/acceptance/OnlinePaymentCest.php
+++ tests/acceptance/OnlinePaymentCest.php
@@ -53,7 +53,7 @@ class OnlinePaymentCest
         $I->selectDropdown($I, $clientEmail, '.client_select .dropdown-toggle');
         $I->fillField('table.invoice-table tbody tr:nth-child(1) #product_key', $productKey);
         $I->click('table.invoice-table tbody tr:nth-child(1) .tt-selectable');
-        $I->click('Save');
+        $I->click('Mark Sent');
         $I->see($clientEmail);
 
         // enter payment
