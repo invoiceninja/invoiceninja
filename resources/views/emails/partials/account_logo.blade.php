6e104d7ebe3f2a851ae849e21f96d9b5329e52a2
Hillel Coren <hillelcoren@gmail.com>
2017-01-08 13:06:48 +0200

    Add alt tag to HTML email image

--- resources/views/emails/partials/account_logo.blade.php
+++ resources/views/emails/partials/account_logo.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ $account->website }}" style="color: #19BB40; text-decoration: underline;">
     @endif
 
-    <img src="{{ isset($message) ? $message->embed($account->getLogoPath()) : $account->getLogoURL() }}" style="max-height:50px; max-width:140px; margin-left: 33px;" />
+    <img src="{{ isset($message) ? $message->embed($account->getLogoPath()) : $account->getLogoURL() }}" style="max-height:50px; max-width:140px; margin-left: 33px;" alt="{{ trans('texts.logo') }}"/>
 
     @if ($account->website)
         </a>
