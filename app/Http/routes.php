12d1c39d163cacc3d0f1ecc0542d4a901f5edca7
Hillel Coren <hillelcoren@gmail.com>
2017-01-10 13:03:56 +0200

    Working on tests

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -361,13 +361,8 @@ if (Utils::isNinjaDev())
 }
 */
 
-if (!defined('CONTACT_EMAIL'))
+if (!defined('ENV_DEVELOPMENT'))
 {
-    define('APP_NAME', env('APP_NAME', 'Invoice Ninja'));
-    define('CONTACT_EMAIL', env('MAIL_FROM_ADDRESS'));
-    define('CONTACT_NAME', env('MAIL_FROM_NAME'));
-    define('SITE_URL', env('APP_URL'));
-
     define('ENV_DEVELOPMENT', 'local');
     define('ENV_STAGING', 'staging');
 
