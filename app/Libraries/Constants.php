04ed02d0d52e6cf099f58a8f888505f429524cca
Hillel Coren <hillelcoren@gmail.com>
2017-01-10 13:14:36 +0200

    Working on tests

--- app/Libraries/Constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!defined('APP_NAME'))
-{
-    define('APP_NAME', env('APP_NAME', 'Invoice Ninja'));
-    define('CONTACT_EMAIL', env('MAIL_FROM_ADDRESS'));
-    define('CONTACT_NAME', env('MAIL_FROM_NAME'));
-    define('SITE_URL', env('APP_URL'));
-}
