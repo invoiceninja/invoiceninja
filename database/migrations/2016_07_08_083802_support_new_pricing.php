2431a46d354485ee1dd25137a0448efee1a96ed9
Hillel Coren <hillelcoren@gmail.com>
2017-01-11 19:30:16 +0200

    Merge fix for migrations

--- database/migrations/2016_07_08_083802_support_new_pricing.php
+++ database/migrations/2016_07_08_083802_support_new_pricing.php
@@ -30,7 +30,7 @@ class SupportNewPricing extends Migration
         // https://github.com/invoiceninja/invoiceninja/pull/955
         Schema::table('activities', function (Blueprint $table) {
             $table->integer('task_id')->after('invitation_id')->nullable();
-            $table->dropForeign('activities_client_id_foreign');
+            $table->unsignedInteger('client_id')->nullable()->change();
         });
 
         // https://github.com/invoiceninja/invoiceninja/pull/950
