ac705a8739e76a0218458db5b87b3652e62adb0b
Hillel Coren <hillelcoren@gmail.com>
2017-01-30 10:17:52 +0200

    Update version

--- app/Constants.php
+++ app/Constants.php
@@ -292,7 +292,7 @@ if (!defined('APP_NAME'))
     define('NINJA_APP_URL', env('NINJA_APP_URL', 'https://app.invoiceninja.com'));
     define('NINJA_DOCS_URL', env('NINJA_DOCS_URL', 'http://docs.invoiceninja.com/en/latest'));
     define('NINJA_DATE', '2000-01-01');
-    define('NINJA_VERSION', '3.0.2' . env('NINJA_VERSION_SUFFIX'));
+    define('NINJA_VERSION', '3.0.3' . env('NINJA_VERSION_SUFFIX'));
 
     define('SOCIAL_LINK_FACEBOOK', env('SOCIAL_LINK_FACEBOOK', 'https://www.facebook.com/invoiceninja'));
     define('SOCIAL_LINK_TWITTER', env('SOCIAL_LINK_TWITTER', 'https://twitter.com/invoiceninja'));
