c57c0837404ea9fb8532d31a0924515f53fddf71
Hillel Coren <hillelcoren@gmail.com>
2017-01-31 13:53:50 +0200

    Prevent editing reminder text when feature is disabled

--- resources/views/accounts/templates_and_reminders.blade.php
+++ resources/views/accounts/templates_and_reminders.blade.php
@@ -245,6 +245,7 @@
         function enableReminder(id) {
             var checked = $('#enable_reminder' + id).is(':checked');
             $('.enable-reminder' + id).attr('disabled', !checked)
+            editors['reminder' + id].editor.enable(checked);
         }
 
         function setDirectionShown(field) {
