88bed420b182eee0b96478139655672f9634b486
Hillel Coren <hillelcoren@gmail.com>
2017-03-13 19:25:29 +0200

    Show contact email on error screen

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -9,7 +9,7 @@
   <div class="container" style="min-height:400px">
   <h3>{{ trans('texts.error_title') }}...</h3>
   <h4>{{ $error }}</h4>
-  <h4>{{ trans('texts.error_contact_text', ['mailaddress' => env('MAIL_USERNAME')]) }}</h4>
+  <h4>{{ trans('texts.error_contact_text', ['mailaddress' => env('CONTACT_EMAIL', env('MAIL_USERNAME'))]) }}</h4>
 </div>
 </div>
 
