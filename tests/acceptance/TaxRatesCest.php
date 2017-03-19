3ac6c42efb11c8c63adb84bbd5b81e22d0af6627
Hillel Coren <hillelcoren@gmail.com>
2017-03-19 16:02:49 +0200

    Fix for tax tests

--- tests/acceptance/TaxRatesCest.php
+++ tests/acceptance/TaxRatesCest.php
@@ -47,7 +47,7 @@ class TaxRatesCest
         $I->fillField(['name' => 'product_key'], $productKey);
         $I->fillField(['name' => 'notes'], $this->faker->text(80));
         $I->fillField(['name' => 'cost'], $itemCost);
-        $I->selectOption('select[name=default_tax_rate_id]', $itemTaxName . ' ' . $itemTaxRate . '%');
+        $I->selectOption('select[name=default_tax_rate_id]', $itemTaxName . ': ' . $itemTaxRate . '%');
         $I->click('Save');
         $I->wait(1);
         //$I->see($productKey);
@@ -63,7 +63,7 @@ class TaxRatesCest
         $I->selectDropdown($I, $clientEmail, '.client_select .dropdown-toggle');
         $I->fillField('table.invoice-table tbody tr:nth-child(1) #product_key', $productKey);
         $I->click('table.invoice-table tbody tr:nth-child(1) .tt-selectable');
-        $I->selectOption('#taxRateSelect1', $invoiceTaxName . ' ' . floatval($invoiceTaxRate) . '%');
+        $I->selectOption('#taxRateSelect1', $invoiceTaxName . ': ' . floatval($invoiceTaxRate) . '%');
         $I->wait(3);
 
         // check total is right before saving
