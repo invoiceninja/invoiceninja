f1ee28935619b87bb81279863bd1eaf329635115
Hillel Coren <hillelcoren@gmail.com>
2017-03-19 16:01:31 +0200

    Fix for favicon_checker

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,7 +43,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-config" content="none"/>
     <link rel="canonical" href="{{ NINJA_APP_URL }}/{{ Request::path() }}"/>
 
     <script src="{{ asset('built.js') }}?no_cache={{ NINJA_VERSION }}" type="text/javascript"></script>
