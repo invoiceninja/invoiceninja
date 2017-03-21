7159dd13ceb3e939d56565bfd365b0d429dcb981
Hillel Coren <hillelcoren@gmail.com>
2017-03-22 00:40:39 +0200

    Working on tests

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -1429,7 +1429,7 @@
             $('#saveButton, #emailButton, #draftButton').attr('disabled', true);
             // if save fails ensure user can try again
             $.post('{{ url($url) }}', $('.main-form').serialize(), function(data) {
-				if (data && data.startsWith('http')) {
+				if (data && data.startsWith && data.startsWith('http')) {
 					NINJA.formIsChanged = false;
 					location.href = data;
 				} else {
