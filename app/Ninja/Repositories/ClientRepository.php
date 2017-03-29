06acbe5460eb0a74da42d6a1c99f991f6270d7df
Hillel Coren <hillelcoren@gmail.com>
2017-03-29 13:22:09 +0300

    Set client id number when updating existing records

--- app/Ninja/Repositories/ClientRepository.php
+++ app/Ninja/Repositories/ClientRepository.php
@@ -81,11 +81,13 @@ class ClientRepository extends BaseRepository
             // do nothing
         } elseif (! $publicId || $publicId == '-1') {
             $client = Client::createNew();
-            if (Auth::check() && Auth::user()->account->client_number_counter && empty($data['id_number'])) {
-                $data['id_number'] = Auth::user()->account->getNextNumber();
-            }
         } else {
             $client = Client::scope($publicId)->with('contacts')->firstOrFail();
+        }
+
+        // auto-set the client id number
+        if (Auth::check() && Auth::user()->account->client_number_counter && !$client->id_number && empty($data['id_number'])) {
+            $data['id_number'] = Auth::user()->account->getNextNumber();
         }
 
         if ($client->is_deleted) {
