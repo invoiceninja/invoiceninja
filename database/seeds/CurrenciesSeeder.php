0f4ed61e3659da444c516a0aa1f18ab4601b4db1
Hillel Coren <hillelcoren@gmail.com>
2017-04-09 20:19:14 +0300

    Add TWD currency

--- database/seeds/CurrenciesSeeder.php
+++ database/seeds/CurrenciesSeeder.php
@@ -69,6 +69,7 @@ class CurrenciesSeeder extends Seeder
             ['name' => 'Omani Rial', 'code' => 'OMR', 'symbol' => '', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
             ['name' => 'Ukrainian Hryvnia', 'code' => 'UAH', 'symbol' => '', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
             ['name' => 'Macanese Pataca', 'code' => 'MOP', 'symbol' => 'MOP$', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
+            ['name' => 'Taiwan New Dollar', 'code' => 'TWD', 'symbol' => 'NT$', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
         ];
 
         foreach ($currencies as $currency) {
