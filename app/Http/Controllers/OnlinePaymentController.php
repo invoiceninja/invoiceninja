e617e9260332d0f8985b1024bea804eabcccc28c
Hillel Coren <hillelcoren@gmail.com>
2017-04-26 22:07:42 +0300

    Create buy now invoices as draft

--- app/Http/Controllers/OnlinePaymentController.php
+++ app/Http/Controllers/OnlinePaymentController.php
@@ -342,7 +342,6 @@ class OnlinePaymentController extends BaseController
 
         $data = [
             'client_id' => $client->id,
-            'is_public' => true,
             'is_recurring' => filter_var(Input::get('is_recurring'), FILTER_VALIDATE_BOOLEAN),
             'frequency_id' => Input::get('frequency_id'),
             'auto_bill_id' => Input::get('auto_bill_id'),
