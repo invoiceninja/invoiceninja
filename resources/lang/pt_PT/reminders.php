c73d55d74fecbe302dd6d2290ea0b633347d2a1c
António Pinto <apinto@vanguardly.com>
2017-05-02 18:25:56 +0100

    #1467 Add pt_PT translation files

---
+++ resources/lang/pt_PT/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+/*
+|--------------------------------------------------------------------------
+| Password Reminder Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are the default lines which match reasons
+| that are given by the password broker for a password update attempt
+| has failed, such as for an invalid token or invalid new password.
+|
+*/
+
+"password" => "As palavras-passe devem conter no mínimo seis caracteres e devem ser iguais.",
+
+"user" => "Não foi encontrado um utilizador com o endereço de email indicado.",
+
+"token" => "Este token de redefinição de palavra-passe é inválido.",
+
+"sent" => "Lembrete de palavra-passe enviado!",
+
+);
