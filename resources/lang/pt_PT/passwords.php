625ed040d8347176f9772a706adc7ef57018acda
António Pinto <apinto@vanguardly.com>
2017-05-02 17:27:10 +0100

    #1467 Add Portuguese Portugal Translation (pt_PT)

---
+++ resources/lang/pt_PT/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "A palavra-passe deve conter pelo menos seis caracteres e combinar com a confirmação.",
+    "user" => "Utilizador não encontrado.",
+    "token" => "Token inválido.",
+    "sent" => "Link para reposição da palavra-passe enviado por email!",
+    "reset" => "Palavra-passe reposta!",
+
+];
