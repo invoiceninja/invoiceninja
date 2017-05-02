a45e14757f27bcec75a9ab0e48ac91d0a87d06ba
Hillel Coren <hillelcoren@gmail.com>
2017-05-02 12:09:14 +0300

    Multi-db support

---
+++ database/seeds/DbServerSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\DbServer;
+
+class DbServerSeeder extends Seeder
+{
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $servers = [
+            ['name' => 'db-ninja-1'],
+            ['name' => 'db-ninja-2'],
+            ['name' => 'db-ninja-3'],
+        ];
+
+        foreach ($servers as $server) {
+            $record = DbServer::where('name', '=', $server['name'])->first();
+
+            if ($record) {
+                // do nothing
+            } else {
+                DbServer::create($server);
+            }
+        }
+    }
+}
