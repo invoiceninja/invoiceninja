1ce6454ac267719be91f9594c0e84db89586f3d5
Hillel Coren <hillelcoren@gmail.com>
2017-06-16 12:36:51 +0300

    Removed duplicate status from invitation transformer

--- app/Ninja/Transformers/InvitationTransformer.php
+++ app/Ninja/Transformers/InvitationTransformer.php
@@ -13,7 +13,6 @@ class InvitationTransformer extends EntityTransformer
         return [
             'id' => (int) $invitation->public_id,
             'key' => $invitation->getName(),
-            'status' => $invitation->getStatus(),
             'link' => $invitation->getLink(),
             'sent_date' => $invitation->sent_date,
             'viewed_date' => $invitation->sent_date,
