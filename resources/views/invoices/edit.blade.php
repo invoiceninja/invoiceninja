30397bb0faecc38139c97a5c0b5cd6cdfc11c025
Hillel Coren <hillelcoren@gmail.com>
2017-06-20 13:51:47 +0300

    Merge invoice item type fix

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -936,7 +936,7 @@
                     item.notes(task.description);
                     item.qty(task.duration);
                     item.task_public_id(task.publicId);
-                    item.invoice_item_type_id = {{ INVOICE_ITEM_TYPE_TASK }};
+                    item.invoice_item_type_id({{ INVOICE_ITEM_TYPE_TASK }});
                 }
                 model.invoice().invoice_items.push(blank);
                 model.invoice().has_tasks(true);
