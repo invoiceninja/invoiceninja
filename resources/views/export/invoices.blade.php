e899be7c55d7a9e1b8eccd6cc9edba45995f79c9
Hillel Coren <hillelcoren@gmail.com>
2017-08-23 11:26:00 +0300

    Working on export

--- resources/views/export/invoices.blade.php
+++ resources/views/export/invoices.blade.php
@@ -25,37 +25,40 @@
     @if ($account->custom_invoice_text_label2)
         <td>{{ $account->custom_invoice_text_label2 }}</td>
     @endif
+    <td>{{ trans('texts.product') }}</td>
 </tr>
 
 @foreach ($invoices as $invoice)
     @if (!$invoice->client->is_deleted)
-        <tr>
-            <td>{{ $invoice->present()->client }}</td>
-            <td>{{ $invoice->present()->email }}</td>
-            @if ($multiUser)
-                <td>{{ $invoice->present()->user }}</td>
-            @endif
-            <td>{{ $invoice->invoice_number }}</td>
-            <td>{{ $account->formatMoney($invoice->amount, $invoice->client) }}</td>
-            <td>{{ $account->formatMoney($invoice->amount - $invoice->balance, $invoice->client) }}</td>
-            <td>{{ $invoice->po_number }}</td>
-            <td>{{ $invoice->present()->status }}</td>
-            <td>{{ $invoice->present()->invoice_date }}</td>
-            <td>{{ $invoice->present()->due_date }}</td>
-            <td>{{ $invoice->public_notes }}</td>
-            <td>{{ $invoice->private_notes }}</td>            
-            @if ($account->custom_invoice_label1)
-                <td>{{ $invoice->custom_value1 }}</td>
-            @endif
-            @if ($account->custom_invoice_label2)
-                <td>{{ $invoice->custom_value2 }}</td>
-            @endif
-            @if ($account->custom_invoice_label1)
-                <td>{{ $invoice->custom_text_value1 }}</td>
-            @endif
-            @if ($account->custom_invoice_label2)
-                <td>{{ $invoice->custom_text_value2 }}</td>
-            @endif
-        </tr>
+        @foreach ($invoice->invoice_items as $item)
+            <tr>
+                <td>{{ $invoice->present()->client }}</td>
+                <td>{{ $invoice->present()->email }}</td>
+                @if ($multiUser)
+                    <td>{{ $invoice->present()->user }}</td>
+                @endif
+                <td>{{ $invoice->invoice_number }}</td>
+                <td>{{ $account->formatMoney($invoice->amount, $invoice->client) }}</td>
+                <td>{{ $account->formatMoney($invoice->amount - $invoice->balance, $invoice->client) }}</td>
+                <td>{{ $invoice->po_number }}</td>
+                <td>{{ $invoice->present()->status }}</td>
+                <td>{{ $invoice->present()->invoice_date }}</td>
+                <td>{{ $invoice->present()->due_date }}</td>
+                <td>{{ $invoice->public_notes }}</td>
+                <td>{{ $invoice->private_notes }}</td>
+                @if ($account->custom_invoice_label1)
+                    <td>{{ $invoice->custom_value1 }}</td>
+                @endif
+                @if ($account->custom_invoice_label2)
+                    <td>{{ $invoice->custom_value2 }}</td>
+                @endif
+                @if ($account->custom_invoice_label1)
+                    <td>{{ $invoice->custom_text_value1 }}</td>
+                @endif
+                @if ($account->custom_invoice_label2)
+                    <td>{{ $invoice->custom_text_value2 }}</td>
+                @endif
+            </tr>
+        @endforeach
     @endif
 @endforeach
