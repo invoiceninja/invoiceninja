e9064f812d3611781c39f4b62e5b4c1c9f73670b
Hillel Coren <hillelcoren@gmail.com>
2017-09-14 11:41:37 +0300

    Added expire_on_close .env praram

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
 
 	'lifetime' => env('SESSION_LIFETIME', (60 * 8)),
 
-	'expire_on_close' => true,
+	'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', true),
 
 	/*
 	|--------------------------------------------------------------------------
