858d2bd40b6362915d5745910e4892c897420dae
David Bomba <turbo124@gmail.com>
2017-10-25 17:00:08 +1100

    Allow ability for Vendors to be deleted from API

---
+++ app/Http/Requests/DeleteVendorRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+class DeleteVendorRequest extends VendorRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->entity() && $this->user()->can('edit', $this->entity());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
