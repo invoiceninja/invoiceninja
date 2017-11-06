d936ed09a4dac5aeade76b27cdde9bfdaeafbfeb
David Bomba <turbo124@gmail.com>
2017-11-06 20:13:45 +1100

    refactor to include zones

--- app/Ninja/DNS/Cloudflare.php
+++ app/Ninja/DNS/Cloudflare.php
@@ -11,38 +11,41 @@ class Cloudflare
 
     public static function addDNSRecord(Account $account){
 
-        addRecordToCloudflare($account->subdomain);
+        $zones = json_decode(env("CLOUDFLARE_ZONE_IDS"));
+
+        foreach($zones as $zone)
+        {
+
+            $curl = curl_init();
+            $jsonEncodedData = json_encode(['type'=>'A', 'name'=>$account->subdomain, 'content'=>env('CLOUDFLARE_TARGET_IP_ADDRESS','')]);
+
+            $opts = [
+                CURLOPT_URL => URL.'zones/'.$zone.'/dns_records',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => $jsonEncodedData,
+                CURLOPT_HTTPHEADER => [ 'Content-Type: application/json',
+                    'Content-Length: '.strlen($jsonEncodedData),
+                    'X-Auth-Email: '.env('CLOUDFLARE_EMAIL', ''),
+                    'X-Auth-Key: '.env('CLOUDFLARE_API_KEY', '')
+                ],
+            ];
+
+            curl_setopt_array($curl, $opts);
+
+            $result = curl_exec($curl);
+            $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+            curl_close($curl);
+
+            if ($status != 200)
+                Utils::logError('unable to update subdomain ' . $account->subdomain . ' @ Cloudflare - '.$result);
+
+        }
+
 
     }
 
-    private function addRecordToCloudflare($subDomain)
-    {
-        $curl = curl_init();
-        $jsonEncodedData = json_encode(['type'=>'A', 'name'=>$subDomain, 'content'=>env('CLOUDFLARE_TARGET_IP_ADDRESS','')]);
-
-        $opts = [
-            CURLOPT_URL => URL,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => $jsonEncodedData,
-            CURLOPT_HTTPHEADER => [ 'Content-Type: application/json',
-                                    'Content-Length: '.strlen($jsonEncodedData),
-                                    'X-Auth-Email: '.env('CLOUDFLARE_EMAIL', ''),
-                                    'X-Auth-Key: '.env('CLOUDFLARE_API_KEY', '')
-                                    ],
-        ];
-
-        curl_setopt_array($curl, $opts);
-
-        $result = curl_exec($curl);
-        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-        curl_close($curl);
-
-        if ($status != 200)
-            Utils::logError('unable to update subdomain ' . $subDomain . ' @ Cloudflare - '.$result);
-
-    }
 
 }
