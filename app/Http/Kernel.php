7c1c8898a0b9a7a7fc65b1a6f87a57b4bf51fd2e
Hillel Coren <hillelcoren@gmail.com>
2017-11-15 20:05:31 +0200

    Fix tests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,7 +53,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
