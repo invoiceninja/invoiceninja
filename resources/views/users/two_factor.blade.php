8e6cfe706ff074c9354c9d5f2e5e966803c7bfdd
Hillel Coren <hillelcoren@gmail.com>
2017-11-24 11:15:02 +0200

    Merge: Require to enable 2FA

--- resources/views/users/two_factor.blade.php
+++ resources/views/users/two_factor.blade.php
@@ -8,7 +8,7 @@
         @include('accounts.nav', ['selected' => ACCOUNT_USER_DETAILS])
     @endif
 
-    {!! Former::open() !!}
+    {!! Former::open()->rules(['one_time_password' => 'required']) !!}
 
     <div class="row">
         <div class="col-md-12">
@@ -22,8 +22,12 @@
                         <p class="text-muted">{{ $secret }}</p><br/>
                         <p>{!! trans('texts.two_factor_setup_help', ['link' => link_to('https://github.com/antonioribeiro/google2fa#google-authenticator-apps', 'Google Authenticator', ['target' => '_blank'])]) !!}</p>
                     </div>
-                    <p>&nbsp;</p>
                     <center class="buttons">
+                        {!! Former::text('one_time_password')
+                                ->placeholder('one_time_password')
+                                ->style('width:300px;font-size:18px')
+                                ->raw() !!}
+                        <p>&nbsp;</p>
                         {!! Button::normal(trans('texts.cancel'))->large()->asLinkTo(url('settings/user_details'))->appendIcon(Icon::create('remove-circle')) !!}
                         {!! Button::success(trans('texts.enable'))->large()->submit()->appendIcon(Icon::create('lock')) !!}
                     </center>
