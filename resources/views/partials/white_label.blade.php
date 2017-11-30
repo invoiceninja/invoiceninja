b3c6fbe535f9af5ff58aa4044eb9f35b5a4c99fb
Hillel Coren <hillelcoren@gmail.com>
2017-11-30 19:34:01 +0200

    Fix for white label purchase

--- resources/views/partials/white_label.blade.php
+++ resources/views/partials/white_label.blade.php
@@ -6,7 +6,7 @@
 
 @if (Auth::user()->account->hasFeature(FEATURE_WHITE_LABEL))
   {{ trans('texts.white_labeled') }}
-  @if (false && $company->hasActivePlan() && $company->daysUntilPlanExpires() <= 10)
+  @if ($company->hasActivePlan() && $company->daysUntilPlanExpires() <= 10)
     - <b>{!! trans('texts.license_expiring', [
         'count' => $company->daysUntilPlanExpires(),
         'link' => '<a href="#" onclick="buyWhiteLabel()">' . trans('texts.click_here') . '</a>',
