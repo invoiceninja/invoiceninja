476fdff9583e1c269ff3e00cec1965861d297703
Hillel Coren <hillelcoren@gmail.com>
2017-12-07 14:53:32 +0200

    Remove expense section from dashboard if module is disabled

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -83,6 +83,7 @@ class DashboardController extends BaseController
             'tasks' => $tasks,
             'showBlueVinePromo' => $showBlueVinePromo,
             'showWhiteLabelExpired' => $showWhiteLabelExpired,
+            'showExpenses' => count($expenses) && $account->isModuleEnabled(ENTITY_EXPENSE),
             'headerClass' => in_array(\App::getLocale(), ['lt', 'pl', 'cs', 'sl', 'tr_TR']) ? 'in-large' : 'in-thin',
             'footerClass' => in_array(\App::getLocale(), ['lt', 'pl', 'cs', 'sl', 'tr_TR']) ? '' : 'in-thin',
         ];
