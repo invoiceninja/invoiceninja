3a32d109afe8d087a2cf7b38a4d13cce6d40c22a
David Bomba <turbo124@gmail.com>
2018-01-13 18:47:31 +1100

    Create stubs for Payment Term API Controller

---
+++ app/Http/Controllers/PaymentTermApiController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreatePaymentTermAPIRequest;
+use App\Http\Requests\PaymentTermRequest;
+use App\Http\Requests\UpdatePaymentTermRequest;
+use App\Models\PaymentTerm;
+
+class PaymentTermApiController extends BaseAPIController
+{
+    /**
+     * @var PaymentTermService
+     */
+    protected $paymentTermService;
+
+    /**
+     * PaymentTermApiController constructor.
+     *
+     * @param PaymentTermService $paymentTermService
+     */
+    public function __construct(PaymentTermService $paymentTermService)
+    {
+        $this->paymentTermService = $paymentTermService;
+    }
+
+    /**
+     * @SWG\Get(
+     *   path="/paymentTerms",
+     *   summary="List payment terms",
+     *   operationId="listPaymentTerms",
+     *   tags={"payment terms"},
+     *   @SWG\Response(
+     *     response=200,
+     *     description="A list of payment terms",
+     *      @SWG\Schema(type="array", @SWG\Items(ref="#/definitions/PaymentTerms"))
+     *   ),
+     *   @SWG\Response(
+     *     response="default",
+     *     description="an ""unexpected"" error"
+     *   )
+     * )
+     */
+
+    public function index()
+    {
+        $defaultPaymentTerms = PaymentTerm::where('account_id',0)->toArray();
+
+        $paymentTerms = PaymentTerm::scope()
+            ->withTrashed()
+            ->orderBy('num_days', 'asc')
+            ->toArray();
+
+        return $this->listResponse(array_merge($defaultPaymentTerms, $paymentTerms));
+    }
+
+        /**
+         * @SWG\Get(
+         *   path="/paymentTerms/{payment_term_id}",
+         *   summary="Retrieve a payment term",
+         *   operationId="getPaymentTermId",
+         *   tags={"payment term"},
+         *   @SWG\Parameter(
+         *     in="path",
+         *     name="payment_term_id",
+         *     type="integer",
+         *     required=true
+         *   ),
+         *   @SWG\Response(
+         *     response=200,
+         *     description="A single payment term",
+         *      @SWG\Schema(type="object", @SWG\Items(ref="#/definitions/PaymentTerms"))
+         *   ),
+         *   @SWG\Response(
+         *     response="default",
+         *     description="an ""unexpected"" error"
+         *   )
+         * )
+         */
+
+    public function show(PaymentTermRequest $request)
+    {
+        return $this->itemResponse($request->entity());
+    }
+
+
+        /**
+         * @SWG\Post(
+         *   path="/paymentTerms",
+         *   summary="Create a payment Term",
+         *   operationId="createPaymentTerm",
+         *   tags={"payment term"},
+         *   @SWG\Parameter(
+         *     in="body",
+         *     name="payment term",
+         *     @SWG\Schema(ref="#/definitions/PaymentTerm")
+         *   ),
+         *   @SWG\Response(
+         *     response=200,
+         *     description="New payment Term",
+         *      @SWG\Schema(type="object", @SWG\Items(ref="#/definitions/PaymentTerm"))
+         *   ),
+         *   @SWG\Response(
+         *     response="default",
+         *     description="an ""unexpected"" error"
+         *   )
+         * )
+         */
+    public function store(CreatePaymentTermAPIRequestt $request)
+    {
+        //stub
+
+    }
+
+    /**
+     * @SWG\Put(
+     *   path="/paymentTerm/{payment_term_id}",
+     *   summary="Update a payment term",
+     *   operationId="updatePaymentTerm",
+     *   tags={"payment"},
+     *   @SWG\Parameter(
+     *     in="path",
+     *     name="payment_term_id",
+     *     type="integer",
+     *     required=true
+     *   ),
+     *   @SWG\Parameter(
+     *     in="body",
+     *     name="payment term",
+     *     @SWG\Schema(ref="#/definitions/PaymentTerm")
+     *   ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Updated payment term",
+     *      @SWG\Schema(type="object", @SWG\Items(ref="#/definitions/PaymentTerm"))
+     *   ),
+     *   @SWG\Response(
+     *     response="default",
+     *     description="an ""unexpected"" error"
+     *   )
+     * )
+     *
+     * @param mixed $publicId
+     */
+    public function update(UpdatePaymentTermRequest $request, $publicId)
+    {
+        //stub
+    }
+
+    /**
+     * @SWG\Delete(
+     *   path="/paymentTerm/{payment_term_id}",
+     *   summary="Delete a payment term",
+     *   operationId="deletePaymentTerm",
+     *   tags={"payment term"},
+     *   @SWG\Parameter(
+     *     in="path",
+     *     name="payment_term_id",
+     *     type="integer",
+     *     required=true
+     *   ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="Deleted payment Term",
+     *      @SWG\Schema(type="object", @SWG\Items(ref="#/definitions/PaymentTerm"))
+     *   ),
+     *   @SWG\Response(
+     *     response="default",
+     *     description="an ""unexpected"" error"
+     *   )
+     * )
+     */
+    public function destroy(UpdatePaymentRequest $request)
+    {
+        //stub
+    }
+}
