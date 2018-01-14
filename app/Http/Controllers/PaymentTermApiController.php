e10f52ff72ff676280f794c25f2bfdf09a67c462
David Bomba <turbo124@gmail.com>
2018-01-14 15:13:17 +1100

    merge collections

--- app/Http/Controllers/PaymentTermApiController.php
+++ app/Http/Controllers/PaymentTermApiController.php
@@ -53,7 +53,7 @@ class PaymentTermApiController extends BaseAPIController
             ->orderBy('num_days', 'asc')
             ->get();
 
-        return $this->listResponse($defaultPaymentTerms->merge($paymentTerms));
+        return $this->listResponse($defaultPaymentTerms->merge($paymentTerms)->all());
     }
 
         /**
