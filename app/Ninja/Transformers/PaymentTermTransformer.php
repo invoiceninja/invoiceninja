da6957ebaa4fbf512cff819dc897470b73996fec
David Bomba <turbo124@gmail.com>
2018-01-14 14:57:46 +1100

    Payment Term Transformer

---
+++ app/Ninja/Transformers/PaymentTermTransformer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Ninja\Transformers;
+
+use App\Models\PaymentTerm;
+
+/**
+ * @SWG\Definition(definition="PaymentTerm", required={"payment_term_id"}, @SWG\Xml(name="PaymentTerm"))
+ */
+class PaymentTermTransformer extends EntityTransformer
+{
+    /**
+     * @SWG\Property(property="id", type="integer", example=1, readOnly=true)
+     * @SWG\Property(property="num_days", type="number", format="integer", example=10, readOnly=true)
+     * @SWG\Property(property="name", type="string", example="Net 7")
+     */
+    protected $defaultIncludes = [];
+
+    protected $availableIncludes = [];
+
+    public function __construct($account = null, $serializer = null, $paymentTerm = null)
+    {
+        parent::__construct($account, $serializer);
+
+        $this->paymentTerm = $paymentTerm;
+    }
+
+
+    public function transform(PaymentTerm $paymentTerm)
+    {
+        return array_merge($this->getDefaults($paymentTerm), [
+            'id' => (int) $paymentTerm->public_id,
+            'num_days' => (int) $paymentTerm->amount,
+            'name' => $paymentTerm->transaction_reference,
+            'updated_at' => $this->getTimestamp($paymentTerm->updated_at),
+            'archived_at' => $this->getTimestamp($paymentTerm->deleted_at),
+
+        ]);
+    }
+}
