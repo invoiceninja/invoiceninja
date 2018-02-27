f1c350419d3e6630813c43a055f4baf0ff81b5d6
Hillel Coren <hillelcoren@gmail.com>
2018-02-27 10:41:07 +0200

    PHP 7.2 fixes

--- tests/acceptance/APICest.php
+++ tests/acceptance/APICest.php
@@ -23,7 +23,7 @@ class APICest
         $response = $this->sendRequest('login', $data);
         $userAccounts = $response->data;
 
-        PHPUnit_Framework_Assert::assertGreaterThan(0, count($userAccounts));
+        PHPUnit_Framework_Assert::assertGreaterThan(0, count((array) $userAccounts));
 
         $userAccount = $userAccounts[0];
         $this->token = $userAccount->token;
