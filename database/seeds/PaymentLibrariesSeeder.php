f131b2720a0943502603bce120fa8d837bfbb044
Hillel Coren <hillelcoren@gmail.com>
2018-03-04 20:54:40 +0200

    Set SagePay Server to offsite

--- database/seeds/PaymentLibrariesSeeder.php
+++ database/seeds/PaymentLibrariesSeeder.php
@@ -29,7 +29,7 @@ class PaymentLibrariesSeeder extends Seeder
             ['name' => 'PayPal Pro', 'provider' => 'PayPal_Pro'],
             ['name' => 'Pin', 'provider' => 'Pin'],
             ['name' => 'SagePay Direct', 'provider' => 'SagePay_Direct'],
-            ['name' => 'SagePay Server', 'provider' => 'SagePay_Server'],
+            ['name' => 'SagePay Server', 'provider' => 'SagePay_Server', 'is_offsite' => true],
             ['name' => 'SecurePay DirectPost', 'provider' => 'SecurePay_DirectPost'],
             ['name' => 'Stripe', 'provider' => 'Stripe', 'sort_order' => 1],
             ['name' => 'TargetPay Direct eBanking', 'provider' => 'TargetPay_Directebanking'],
