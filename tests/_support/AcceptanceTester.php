44800d985e1c468ce6aca06791ecf9ec9adc9de8
Hillel Coren <hillelcoren@gmail.com>
2018-03-08 08:31:49 +0200

    Working on tests

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -26,18 +26,15 @@ class AcceptanceTester extends \Codeception\Actor
     */
     function checkIfLogin(\AcceptanceTester $I)
     {
-        //if ($I->loadSessionSnapshot('login')) return;
-
-        $I->amOnPage('/login?lang=en');
-
-        if ($I->seePageHasElement('Login')) {
+        try {
+            $I->amOnPage('/login?lang=en');
             $I->see('Login');
             $I->fillField(['name' => 'email'], Fixtures::get('username'));
             $I->fillField(['name' => 'password'], Fixtures::get('password'));
             $I->click('Login');
+        } catch (Exception $e) {
+            // do nothing
         }
-
-        //$I->saveSessionSnapshot('login');
     }
 
     function selectDataPicker(\AcceptanceTester $I, $element, $date = 'now')
