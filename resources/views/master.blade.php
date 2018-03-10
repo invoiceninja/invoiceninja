9d894daea96979e254fbbc4dfd5a4f6e77f0443b
Hillel Coren <hillelcoren@gmail.com>
2018-03-10 22:07:47 +0200

    Fix white labeled page title

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,7 @@
     @endif
     <meta charset="utf-8">
 
-    @if (Utils::isWhiteLabel() && ! isset($title))
+    @if (Utils::isWhiteLabel() && ! auth()->check())
         <title>{{ trans('texts.client_portal') }}</title>
         <link href="{{ asset('ic_cloud_circle.png') }}" rel="shortcut icon" type="image/png">
     @else
