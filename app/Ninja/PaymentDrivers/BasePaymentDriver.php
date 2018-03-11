fcbab2fd0239e7939992946a56fe971a56172831
Hillel Coren <hillelcoren@gmail.com>
2018-03-11 12:36:24 +0200

    Working on tests

--- app/Ninja/PaymentDrivers/BasePaymentDriver.php
+++ app/Ninja/PaymentDrivers/BasePaymentDriver.php
@@ -266,7 +266,7 @@ class BasePaymentDriver
 
     public function completeOnsitePurchase($input = false, $paymentMethod = false)
     {
-        $this->input = count($input) ? $input : false;
+        $this->input = $input && $input->count() ? $input : false;
         $gateway = $this->gateway();
 
         if ($input) {
