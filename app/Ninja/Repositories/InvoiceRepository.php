6ac1982aef969db24c2b916332b9b1a7b9b36e36
Hillel Coren <hillelcoren@gmail.com>
2018-03-19 11:12:37 +0200

    Merge line item discount rounding fix

--- app/Ninja/Repositories/InvoiceRepository.php
+++ app/Ninja/Repositories/InvoiceRepository.php
@@ -549,7 +549,7 @@ class InvoiceRepository extends BaseRepository
                 if ($invoice->is_amount_discount) {
                     $lineTotal -= $discount;
                 } else {
-                    $lineTotal -= $lineTotal * $discount / 100;
+                    $lineTotal -= round($lineTotal * $discount / 100, 2);
                 }
             }
 
