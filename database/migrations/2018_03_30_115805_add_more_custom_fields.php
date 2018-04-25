7d44f3d1a3533b0f6094741e077cc02d1ad2d7aa
Hillel Coren <hillelcoren@gmail.com>
2018-04-25 09:39:18 +0300

    Fix for migration

--- database/migrations/2018_03_30_115805_add_more_custom_fields.php
+++ database/migrations/2018_03_30_115805_add_more_custom_fields.php
@@ -137,7 +137,7 @@ class AddMoreCustomFields extends Migration
         });
 
         DB::statement('UPDATE gateways SET provider = "Custom1" WHERE id = 62');
-        DB::statement('UPDATE gateway_types SET alias = "custom1", name = "Custom 1" WHERE id = 6');
+        DB::statement('UPDATE gateway_types SET alias = "custom1" WHERE id = 6');
         DB::statement('ALTER TABLE recurring_expenses MODIFY COLUMN last_sent_date DATE');
     }
 
