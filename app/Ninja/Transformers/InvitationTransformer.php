cecd2ad3893d018cade4130e0b997da05da15de3
Anmol Gupta <guptaanmol184@gmail.com>
2018-06-18 17:03:08 +0530

    Add swagger docs for AccountTokenTransformer, InvitationTransformer, InvoiceItemTransformer, PaymentTermTransformer, UserAccountTransformer, VendorContactTransformer

--- app/Ninja/Transformers/InvitationTransformer.php
+++ app/Ninja/Transformers/InvitationTransformer.php
@@ -6,6 +6,13 @@ use App\Models\Invitation;
 
 class InvitationTransformer extends EntityTransformer
 {
+	  /**
+    * @SWG\Property(property="id", type="integer", example=1, readOnly=true)
+    * @SWG\Property(property="key", type="string", example="Key")
+    * @SWG\Property(property="link", type="string", example="Link")
+    * @SWG\Property(property="sent_date", type="string", format="date", example="2018-01-01")
+    * @SWG\Property(property="viewed_date", type="string", format="date", example="2018-01-01")
+    */
     public function transform(Invitation $invitation)
     {
         $invitation->setRelation('account', $this->account);
