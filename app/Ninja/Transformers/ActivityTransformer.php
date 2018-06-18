b0780704b299f95eda038ddbe30ab84b1cad1c7f
Anmol Gupta <guptaanmol184@gmail.com>
2018-06-18 17:27:13 +0530

    Add int typecast in ActivityTransformer

--- app/Ninja/Transformers/ActivityTransformer.php
+++ app/Ninja/Transformers/ActivityTransformer.php
@@ -25,17 +25,17 @@ class ActivityTransformer extends EntityTransformer
     {
         return [
             'id' => $activity->key(),
-            'activity_type_id' => $activity->activity_type_id,
-            'client_id' => $activity->client ? $activity->client->public_id : null,
-            'user_id' => $activity->user->public_id + 1,
-            'invoice_id' => $activity->invoice ? $activity->invoice->public_id : null,
-            'payment_id' => $activity->payment ? $activity->payment->public_id : null,
-            'credit_id' => $activity->credit ? $activity->credit->public_id : null,
+            'activity_type_id' => (int) $activity->activity_type_id,
+            'client_id' => $activity->client ? (int) $activity->client->public_id : null,
+            'user_id' => (int) $activity->user->public_id + 1,
+            'invoice_id' => $activity->invoice ? (int) $activity->invoice->public_id : null,
+            'payment_id' => $activity->payment ? (int) $activity->payment->public_id : null,
+            'credit_id' => $activity->credit ? (int) $activity->credit->public_id : null,
             'updated_at' => $this->getTimestamp($activity->updated_at),
-            'expense_id' => $activity->expense_id ? $activity->expense->public_id : null,
+            'expense_id' => $activity->expense_id ? (int) $activity->expense->public_id : null,
             'is_system' => $activity->is_system ? (bool) $activity->is_system : null,
-            'contact_id' => $activity->contact_id ? $activity->contact->public_id : null,
-            'task_id' => $activity->task_id ? $activity->task->public_id : null,
+            'contact_id' => $activity->contact_id ? (int) $activity->contact->public_id : null,
+            'task_id' => $activity->task_id ? (int) $activity->task->public_id : null,
         ];
     }
 }
