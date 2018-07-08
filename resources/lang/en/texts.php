239f492311ef907bec6959c1f48848fcc4e5c035
Hillel Coren <hillelcoren@gmail.com>
2018-07-08 14:01:01 +0300

    Bug fixes

--- resources/lang/en/texts.php
+++ resources/lang/en/texts.php
@@ -2849,6 +2849,7 @@ $LANG = array(
     'all_invoices' => 'All Invoices',
     'my_invoices' => 'My Invoices',
     'mobile_refresh_warning' => 'If you\'re using the mobile app you may need to do a full refresh.',
+    'enable_proposals_for_background' => 'To upload a background image :link to enable the proposals module.',
 
 );
 
--- resources/views/accounts/invoice_design.blade.php
+++ resources/views/accounts/invoice_design.blade.php
@@ -298,7 +298,11 @@
 									->label('background_image')
 									->addOption('', '')
 									->fromQuery(\App\Models\Document::scope()->proposalImages()->get(), function($model) { return $model->name . ' - ' . Utils::formatNumber($model->size / 1000, null, 1) . ' KB'; }, 'public_id')
-									->help(trans('texts.background_image_help', ['link' => link_to('/proposals/create?show_assets=true', trans('texts.proposal_editor'), ['target' => '_blank'])])) !!}
+									->help($account->isModuleEnabled(ENTITY_PROPOSAL)
+											? trans('texts.background_image_help', ['link' => link_to('/proposals/create?show_assets=true', trans('texts.proposal_editor'), ['target' => '_blank'])])
+											//: trans('texts.enable_proposals_for_background', ['link' => link_to('/settings/account_management', trans('texts.click_here'), ['target' => '_blank'])])
+											: 'To upload a background image <a href="http://www.ninja.test/settings/account_management" target="_blank">click here</a> to enable the proposals module.' 
+										) !!}
 						@endif
 
 						{!! Former::checkbox('hide_paid_to_date')->text(trans('texts.hide_paid_to_date_help'))->value(1) !!}
