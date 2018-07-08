ea2d58e30f46f39c63f97b84b8891866beb862e2
Hillel Coren <hillelcoren@gmail.com>
2018-07-08 14:33:11 +0300

    Merge bug fixes

--- resources/assets/js/pdf.pdfmake.js
+++ resources/assets/js/pdf.pdfmake.js
@@ -203,6 +203,9 @@ function GetPdfMake(invoice, javascript, callback) {
 
     if (window.accountBackground) {
         var origBackground = dd.background;
+        if (! origBackground) {
+            origBackground = [{"image": window.accountBackground, "alignment": "center"}];
+        }
         dd.background = function(currentPage) {
             var allPages = origBackground.length && origBackground[0].pages == 'all';
             return currentPage == 1 || allPages ? origBackground : false;
@@ -406,7 +409,9 @@ NINJA.decodeJavascript = function(invoice, javascript)
                 value = formatMoneyInvoice(value, invoice);
             }
 
-            javascript = javascript.replace(match, value);
+            if (['$pageNumber', '$pageCount'].indexOf(match) == -1) {
+                javascript = javascript.replace(match, value);
+            }
         }
     }
 
