0ee6ac1d9a0269130ff9433b2dbe95b76772c19f
David Bomba <turbo124@gmail.com>
2018-07-24 22:33:52 +1000

    Create QuoteRequest.php

---
+++ app/Http/Requests/QuoteRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Invoice;
+
+class QuoteRequest extends EntityRequest
+{
+    protected $entityType = ENTITY_QUOTE;
+
+    public function entity()
+    {
+        $invoice = parent::entity();
+
+        // support loading an invoice by its invoice number
+        if ($this->invoice_number && ! $invoice) {
+            $invoice = Invoice::scope()
+                        ->whereInvoiceNumber($this->invoice_number)
+                        ->withTrashed()
+                        ->first();
+
+            if (! $invoice) {
+                abort(404);
+            }
+        }
+
+        // eager load the invoice items
+        if ($invoice && ! $invoice->relationLoaded('invoice_items')) {
+            $invoice->load('invoice_items');
+        }
+
+        return $invoice;
+    }
+}
