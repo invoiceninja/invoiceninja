64be30d75302d930c6912baf7eb8d046f8f28d09
Hillel Coren <hillelcoren@gmail.com>
2018-07-26 20:15:53 +0300

    Make license fields nullable

--- database/migrations/2018_07_26_170243_fix_payment_contact_foreign_key.php
+++ database/migrations/2018_07_26_170243_fix_payment_contact_foreign_key.php
@@ -21,6 +21,16 @@ class FixPaymentContactForeignKey extends Migration
             Schema::table('payments', function ($table) {
                 $table->foreign('contact_id')->references('id')->on('contacts')->onDelete('cascade');
             });
+
+            Schema::table('licenses', function ($table) {
+                $table->unsignedInteger('affiliate_id')->nullable()->change();
+                $table->string('first_name')->nullable()->change();
+                $table->string('last_name')->nullable()->change();
+                $table->string('email')->nullable()->change();
+                $table->string('license_key')->unique()->nullable()->change();
+                $table->boolean('is_claimed')->nullable()->change();
+                $table->string('transaction_reference')->nullable()->change();
+            });
         } catch (Exception $exception) {
             // do nothing, change only needed for invoiceninja servers
         }
