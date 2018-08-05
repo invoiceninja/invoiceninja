5adae58c2ba06fc7e21bf69cde3769cb63c362c9
Yannik <InfinityWebMe@users.noreply.github.com>
2018-08-05 13:59:04 +0200

    FIX - link canonical href
    Hello,
    
    little fix that replaces "app.invoiceninja.com" by the real url of the hosting server.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -50,7 +50,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="canonical" href="{{ NINJA_APP_URL }}/{{ Request::path() }}"/>
+    <link rel="canonical" href="{{ SITE_URL }}{{ Request::path() }}"/>
 
     @yield('head_css')
 
