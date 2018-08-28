e3eb2a2eb1b4d7765cebe8622bfd31ce1e1df202
Christopher Di Carlo <chris@dicarlosystems.ca>
2018-08-28 07:38:10 -0400

    Added simple select component

---
+++ resources/views/components/simple_select.blade.php
@@ -0,0 +1,61 @@
+{!! Former::select($selectId)
+    ->addOption('', '')
+    ->label($fieldLabel) !!}
+
+@push('component_scripts')
+    <script type="text/javascript">
+
+        $(function() {
+            var entityType = '{!! $entityType !!}';
+            var items = {!! $items !!};
+            var secondaryItemLabel = '{!! $secondaryItemLabel !!}';
+
+            var itemMap = {};
+            var $itemSelect = $('select#{!! $selectId !!}');
+            for (var i=0; i<items.length; i++) {
+                var item = items[i];
+                var itemName = '';
+
+                itemMap[item.public_id] = item;
+
+                switch(entityType) {
+                    case '{!! ENTITY_CLIENT !!}':
+                        itemName = getClientDisplayName(item);
+                        break;
+                    case '{!! ENTITY_CONTACT !!}':
+                        itemName = getContactDisplayName(item);
+                        break;
+                    default:
+                        itemName = item.{!! $itemLabel !!};
+
+                }
+
+                if (!itemName) {
+                    continue;
+                }
+
+                var itemNameLabel = '';
+
+                if(secondaryItemLabel != '') {
+                    switch(entityType) {
+                        case '{!! ENTITY_CLIENT !!}':
+                            itemNameLabel = getContactDisplayName(item.{!! $secondaryItemLabel !!});
+                            break;
+                        default:
+                            itemNameLabel = item.{!! $secondaryItemLabel !!};
+                    }
+                }
+
+                $itemSelect.append(new Option(itemName + ((itemNameLabel) ? ' - ' + itemNameLabel : ''), item.public_id));
+            }
+            @if (! empty($itemPublicId))
+                $itemSelect.val({{ $itemPublicId }});
+            @endif
+
+            $itemSelect.combobox({highlighter: comboboxHighlighter}).change(function() {
+                var item = itemMap[$('#{!! $selectId !!}').val()];
+            });
+        });
+    </script>
+@endpush
+
