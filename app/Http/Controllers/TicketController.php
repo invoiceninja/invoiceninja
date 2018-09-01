526d96c0659d4e1e82ed66b6ac1beefb65cc3c70
David Bomba <turbo124@gmail.com>
2018-09-01 12:03:49 +1000

    debug

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -223,7 +223,7 @@ class TicketController extends BaseController
 
     public function store(CreateTicketRequest $request)
     {
-        $input = $request->entity();
+        $input = $request->input();
         $input['action'] = TICKET_AGENT_NEW;
 
         $ticket = $this->ticketService->save($input, $request->input());
