33438f2c3fbb74150721bf6e6f51e9cfb32b4d35
David Bomba <turbo124@gmail.com>
2018-09-01 22:00:07 +1000

    inbound ticket testing

--- app/Http/Requests/TicketInboundRequest.php
+++ app/Http/Requests/TicketInboundRequest.php
@@ -5,11 +5,12 @@ namespace App\Http\Requests;
 use App\Ninja\Repositories\TicketRepository;
 use App\Ninja\Tickets\Inbound\InboundTicketFactory;
 use App\Ninja\Tickets\Inbound\InboundTicketService;
+use Illuminate\Support\Facades\Log;
 
 class TicketInboundRequest extends Request
 {
     public function entity()
-    {
+    {Log::error('inbound service hit!');
         $inboundTicketService = new InboundTicketService(new InboundTicketFactory(request()->getContent()), new TicketRepository());
         return $inboundTicketService->process();
 
