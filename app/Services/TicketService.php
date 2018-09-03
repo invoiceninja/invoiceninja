98ef0c8e5b0ebf802d6a4782b436c48758551e06
David Bomba <turbo124@gmail.com>
2018-09-03 13:14:33 +1000

    create ticket_save_only action which bypasses ticket actions

--- app/Services/TicketService.php
+++ app/Services/TicketService.php
@@ -135,32 +135,25 @@ class TicketService extends BaseService
 
         //Close ticket
         $data['merged_parent_ticket_id'] = Ticket::getPrivateId($data['updated_ticket_id']);
-
         $data['closed'] = \Carbon::now();
-
         $data['status_id'] = TICKET_STATUS_MERGED;
 
         $ticketComment = TicketComment::createNew($ticket);
-
         $ticketComment->description = $data['old_ticket_comment'];
-        
         $ticket->comments()->save($ticketComment);
 
         $data['action'] = TICKET_MERGE;
         $this->ticketRepo->save($data, $ticket);
 
         //Update parent ticket
-        $data = [];
-        $data['action'] = TICKET_SAVE_ONLY;
 
         $updatedTicket = Ticket::scope($data['updated_ticket_id'])->first();
-
         $updatedTicketComment = TicketComment::createNew($updatedTicket);
-
         $updatedTicketComment->description = $data['updated_ticket_comment'];
-
         $updatedTicket->comments()->save($updatedTicketComment);
 
+        $data = [];
+        $data['action'] = TICKET_SAVE_ONLY;
         $this->ticketRepo->save($data, $updatedTicket);
 
     }
