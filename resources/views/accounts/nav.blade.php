invoiceninja/invoiceninja

commit d340939077271aa9485d44f374b71c3702f57076

Display Module settings panel only if an enabled module has settings

resources/views/accounts/nav.blade.php
@@ -23,7 +23,6 @@
 </script>
 
 <div class="row">
-
     <div class="col-md-3">
         @foreach([
             BASIC_SETTINGS => \App\Models\Account::$basicSettings,
@@ -50,8 +49,8 @@
                 </div>
             </div>
         @endforeach
-        
-        @if (Utils::isSelfHost())
+
+        @if (Utils::isSelfHost() && $showModuleSettings)
             <div class="panel panel-default">
                 <div class="panel-heading" style="color:white">
                     {{ trans('texts.module_settings') }}
@@ -69,5 +68,4 @@
             </div>
         @endif
     </div>
-
-    <div class="col-md-9">
+</div>

app/Http/Controllers/AccountController.php
@@ -289,7 +289,10 @@ class AccountController extends BaseController
         } elseif ($section == ACCOUNT_INVOICE_SETTINGS) {
             return self::showInvoiceSettings();
         } elseif ($section == ACCOUNT_IMPORT_EXPORT) {
-            return View::make('accounts.import_export', ['title' => trans('texts.import_export')]);
+            return View::make('accounts.import_export', [
+                'title' => trans('texts.import_export'),
+                'showModuleSettings' => Utils::hasModuleSettings()
+            ]);
         } elseif ($section == ACCOUNT_MANAGEMENT) {
             return self::showAccountManagement();
         } elseif ($section == ACCOUNT_INVOICE_DESIGN || $section == ACCOUNT_CUSTOMIZE_DESIGN) {
@@ -316,6 +319,7 @@ class AccountController extends BaseController
                 'account' => Account::with('users')->findOrFail(Auth::user()->account_id),
                 'title' => trans("texts.{$section}"),
                 'section' => $section,
+                'showModuleSettings' => Utils::hasModuleSettings(),
             ];
 
             return View::make($view, $data);
@@ -335,6 +339,7 @@ class AccountController extends BaseController
             'account' => Account::with('users')->findOrFail(Auth::user()->account_id),
             'title' => trans('texts.system_settings'),
             'section' => ACCOUNT_SYSTEM_SETTINGS,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.system_settings', $data);
@@ -362,6 +367,7 @@ class AccountController extends BaseController
             'title' => trans('texts.invoice_settings'),
             'section' => ACCOUNT_INVOICE_SETTINGS,
             'recurringHours' => $recurringHours,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.invoice_settings', $data);
@@ -382,6 +388,7 @@ class AccountController extends BaseController
             'account' => Account::with('users')->findOrFail(Auth::user()->account_id),
             'sizes' => Cache::get('sizes'),
             'title' => trans('texts.company_details'),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.details', $data);
@@ -408,6 +415,7 @@ class AccountController extends BaseController
             'portalLink' => $portalLink,
             'planDetails' => $planDetails,
             'title' => trans('texts.account_management'),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.management', $data);
@@ -434,6 +442,7 @@ class AccountController extends BaseController
             'oauthProviderName' => AuthService::getProviderName(Auth::user()->oauth_provider_id),
             'oauthLoginUrls' => $oauthLoginUrls,
             'referralCounts' => $this->referralRepository->getCounts(Auth::user()->referral_code),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.user_details', $data);
@@ -452,6 +461,7 @@ class AccountController extends BaseController
             'title' => trans('texts.localization'),
             'weekdays' => Utils::getTranslatedWeekdayNames(),
             'months' => Utils::getMonthOptions(),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.localization', $data);
@@ -468,6 +478,7 @@ class AccountController extends BaseController
             'title' => trans('texts.bank_accounts'),
             'advanced' => ! Auth::user()->hasFeature(FEATURE_EXPENSES),
             'warnPaymentGateway' => ! $account->account_gateways->count(),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ]);
     }
 
@@ -499,6 +510,7 @@ class AccountController extends BaseController
             'currency' => Utils::getFromCache(Session::get(SESSION_CURRENCY, DEFAULT_CURRENCY), 'currencies'),
             'taxRates' => TaxRate::scope()->whereIsInclusive(false)->orderBy('rate')->get(['public_id', 'name', 'rate']),
             'account' => $account,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ]);
     }
 
@@ -510,6 +522,7 @@ class AccountController extends BaseController
         $data = [
             'account' => Auth::user()->account,
             'title' => trans('texts.product_library'),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.products', $data);
@@ -526,6 +539,7 @@ class AccountController extends BaseController
             'taxRates' => TaxRate::scope()->whereIsInclusive(false)->get(),
             'countInvoices' => Invoice::scope()->withTrashed()->count(),
             'hasInclusiveTaxRates' => TaxRate::scope()->whereIsInclusive(true)->count() ? true : false,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.tax_rates', $data);
@@ -539,6 +553,7 @@ class AccountController extends BaseController
         $data = [
             'account' => Auth::user()->account,
             'title' => trans('texts.payment_terms'),
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.payment_terms', $data);
@@ -633,6 +648,7 @@ class AccountController extends BaseController
         $data['invoiceFonts'] = Cache::get('fonts');
         $data['section'] = $section;
         $data['pageSizes'] = array_combine(InvoiceDesign::$pageSizes, InvoiceDesign::$pageSizes);
+        $data['showModuleSettings'] = Utils::hasModuleSettings();
 
         $design = false;
         foreach ($data['invoiceDesigns'] as $item) {
@@ -694,6 +710,7 @@ class AccountController extends BaseController
             'account' => $account,
             'products' => Product::scope()->orderBy('product_key')->get(),
             'gateway_types' => $options,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('accounts.client_portal', $data);
@@ -719,6 +736,7 @@ class AccountController extends BaseController
             ];
         }
         $data['title'] = trans('texts.email_templates');
+        $data['showModuleSettings'] = Utils::hasModuleSettings();
 
         return View::make('accounts.templates_and_reminders', $data);
     }

app/Http/Controllers/ReportController.php
@@ -44,6 +44,7 @@ class ReportController extends BaseController
         $data = [
             'clients' => $clients,
             'message' => $message,
+            'showModuleSettings' => Utils::hasModuleSettings(),
         ];
 
         return View::make('reports.d3', $data);

app/Libraries/Utils.php
@@ -18,6 +18,7 @@ use Session;
 use stdClass;
 use View;
 use WePay;
+use Nwidart\Modules\Facades\Module;
 
 class Utils
 {
@@ -1480,5 +1481,13 @@ class Utils
             'Ы'=>'y', 'ž'=>'z', 'З'=>'z', 'з'=>'z', 'ź'=>'z', 'ז'=>'z', 'ż'=>'z', 'ſ'=>'z', 'Ж'=>'zh', 'ж'=>'zh'
         );
         return strtr($s, $replace);
+    }
+
+    public static function hasModuleSettings() {
+        $module = Module::collections()->first(function($value, $key) {
+            return $value->get('has_settings') == 1;
+        });
+
+        return $module ? true : false;
     }
 }

commit 7ec25f9d2800009b0a379b921e8150a520838f96

Fix regression
Introduced in d340939077271aa9485d44f374b71c3702f57076

resources/views/accounts/nav.blade.php
@@ -68,4 +68,5 @@
             </div>
         @endif
     </div>
-</div>
+
+    <div class="col-md-9">