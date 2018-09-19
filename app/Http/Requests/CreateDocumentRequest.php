824b416c97e1aae7c18a832469c19791ef957903
David Bomba <turbo124@gmail.com>
2018-09-19 14:55:26 +1000

    fix for doc uploads

--- app/Http/Requests/CreateDocumentRequest.php
+++ app/Http/Requests/CreateDocumentRequest.php
@@ -19,19 +19,8 @@ class CreateDocumentRequest extends DocumentRequest
      */
     public function authorize()
     {
-        if (! $this->user()->hasFeature(FEATURE_DOCUMENTS)) {
-            return false;
-        }
-        
-        if ($this->invoice && $this->user()->cannot('edit', $this->invoice)) {
-            return false;
-        }
-
-        if ($this->expense && $this->user()->cannot('edit', $this->expense)) {
-            return false;
-        }
-
-        return $this->user()->can('create', ENTITY_DOCUMENT);
+        if($this->user()->hasFeature(FEATURE_DOCUMENTS))
+            return true;
     }
 
     /**
