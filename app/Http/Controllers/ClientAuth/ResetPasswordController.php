1bb374f120d6c28bed32608e5a7470217e9c514d
Jean-Charles Le Goff <jeancharleslegoff@gmail.com>
2018-10-03 22:29:24 +0200

    Fix redirection after client password reset

--- app/Http/Controllers/ClientAuth/ResetPasswordController.php
+++ app/Http/Controllers/ClientAuth/ResetPasswordController.php
@@ -28,7 +28,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/client/dashboard';
+    protected $redirectTo = '/client/login';
 
     /**
      * Create a new controller instance.
