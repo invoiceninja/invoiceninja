b58d410c3cf0dd19593e0b69297dc041827dfb72
David Bomba <turbo124@gmail.com>
2018-10-05 00:44:07 +1000

    fix for dashboard links for quotes / invoices

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -817,7 +817,11 @@ class Invoice extends EntityModel implements BalanceAffecting
      */
     public static function calcLink($invoice)
     {
-        return link_to('invoices/' . $invoice->public_id, $invoice->invoice_number);
+        if(isset($invoice->invoice_type_id))
+            $linkPrefix = ($invoice->invoice_type_id == INVOICE_TYPE_QUOTE) ? 'quotes/' : 'invoices/';
+        else
+            $linkPrefix = 'invoices/';
+        return link_to($linkPrefix . $invoice->public_id, $invoice->invoice_number);
     }
 
     /**
