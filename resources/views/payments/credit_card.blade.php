fffff252af9eb19c2b1f1f6943eb6de7dbcbeb9b
David Bomba <turbo124@gmail.com>
2018-10-10 22:58:34 +1100

    formatting stripe v3

--- resources/views/payments/credit_card.blade.php
+++ resources/views/payments/credit_card.blade.php
@@ -324,21 +324,241 @@
     @if ($accountGateway->isGateway(GATEWAY_WEPAY) && $account->token_billing_type_id == TOKEN_BILLING_DISABLED)
         {{--- do nothing --}}
     @elseif($accountGateway->isGateway(GATEWAY_STRIPE))
-        <div class="row">
-            <div class="col-md-12">     
-                <label for="card-element">
-                    Credit or debit card
-                </label>
-                <div id="card-element">
-                <!-- A Stripe Element will be inserted here. -->
-                </div>
 
-                <!-- Used to display form errors. -->
-                <div id="card-errors" role="alert"></div>
-              </div>
+    <div class="ninja stripe">
+        <div class="row">
+            <div class="field">
+              <div id="card-number" class="input empty"></div>
+              <label for="card-number" data-tid="card_number_label">Card number</label>
+              <div class="baseline"></div>
             </div>
-        </div>
-                <script type="text/javascript">
+          </div>
+          <div class="row">
+            <div class="field half-width">
+              <div id="card-expiry" class="input empty"></div>
+              <label for="card-expiry" data-tid="card_expiry_label">Expiration</label>
+              <div class="baseline"></div>
+            </div>
+            <div class="field half-width">
+              <div id="card-cvc" class="input empty"></div>
+              <label for="card-cvc" data-tid="card_cvc_label">CVC</label>
+              <div class="baseline"></div>
+            </div>
+          </div>
+
+          <div id="card-errors" role="alert"></div>
+    </div>
+        <style>
+        .ninja.stripe {
+  background-color: #fff;
+}
+
+.ninja.stripe * {
+  font-family: Source Code Pro, Consolas, Menlo, monospace;
+  font-size: 16px;
+  font-weight: 500;
+}
+
+.ninja.stripe .row {
+  display: -ms-flexbox;
+  display: flex;
+  margin: 0 5px 10px;
+}
+
+.ninja.stripe .field {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  margin: 0 10px;
+}
+
+.ninja.stripe .field.half-width {
+  width: 50%;
+}
+
+.ninja.stripe .field.quarter-width {
+  width: calc(25% - 10px);
+}
+
+.ninja.stripe .baseline {
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  left: 0;
+  bottom: 0;
+  background-color: #cfd7df;
+  transition: background-color 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.ninja.stripe label {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  bottom: 8px;
+  color: #cfd7df;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  transform-origin: 0 50%;
+  cursor: text;
+  transition-property: color, transform;
+  transition-duration: 0.3s;
+  transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.ninja.stripe .input {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  padding-bottom: 7px;
+  color: #32325d;
+  background-color: transparent;
+}
+
+.ninja.stripe .input::-webkit-input-placeholder {
+  color: transparent;
+  transition: color 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.ninja.stripe .input::-moz-placeholder {
+  color: transparent;
+  transition: color 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.ninja.stripe .input:-ms-input-placeholder {
+  color: transparent;
+  transition: color 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.ninja.stripe .input.StripeElement {
+  opacity: 0;
+  transition: opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+  will-change: opacity;
+}
+
+.ninja.stripe .input.focused,
+.ninja.stripe .input:not(.empty) {
+  opacity: 1;
+}
+
+.ninja.stripe .input.focused::-webkit-input-placeholder,
+.ninja.stripe .input:not(.empty)::-webkit-input-placeholder {
+  color: #cfd7df;
+}
+
+.ninja.stripe .input.focused::-moz-placeholder,
+.ninja.stripe .input:not(.empty)::-moz-placeholder {
+  color: #cfd7df;
+}
+
+.ninja.stripe .input.focused:-ms-input-placeholder,
+.ninja.stripe .input:not(.empty):-ms-input-placeholder {
+  color: #cfd7df;
+}
+
+.ninja.stripe .input.focused + label,
+.ninja.stripe .input:not(.empty) + label {
+  color: #aab7c4;
+  transform: scale(0.85) translateY(-25px);
+  cursor: default;
+}
+
+.ninja.stripe .input.focused + label {
+  color: #24b47e;
+}
+
+.ninja.stripe .input.invalid + label {
+  color: #ffa27b;
+}
+
+.ninja.stripe .input.focused + label + .baseline {
+  background-color: #24b47e;
+}
+
+.ninja.stripe .input.focused.invalid + label + .baseline {
+  background-color: #e25950;
+}
+
+.ninja.stripe input, .ninja.stripe button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: none;
+  border-style: none;
+}
+
+.ninja.stripe input:-webkit-autofill {
+  -webkit-text-fill-color: #e39f48;
+  transition: background-color 100000000s;
+  -webkit-animation: 1ms void-animation-out;
+}
+
+.ninja.stripe .StripeElement--webkit-autofill {
+  background: transparent !important;
+}
+
+.ninja.stripe input, .ninja.stripe button {
+  -webkit-animation: 1ms void-animation-out;
+}
+
+.ninja.stripe button {
+  display: block;
+  width: calc(100% - 30px);
+  height: 40px;
+  margin: 40px 15px 0;
+  background-color: #24b47e;
+  border-radius: 4px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.ninja.stripe input:active {
+  background-color: #159570;
+}
+
+.ninja.stripe .error svg {
+  margin-top: 0 !important;
+}
+
+.ninja.stripe .error svg .base {
+  fill: #e25950;
+}
+
+.ninja.stripe .error svg .glyph {
+  fill: #fff;
+}
+
+.ninja.stripe .error .message {
+  color: #e25950;
+}
+
+.ninja.stripe .success .icon .border {
+  stroke: #abe9d2;
+}
+
+.ninja.stripe .success .icon .checkmark {
+  stroke: #24b47e;
+}
+
+.ninja.stripe .success .title {
+  color: #32325d;
+  font-size: 16px !important;
+}
+
+.ninja.stripe .success .message {
+  color: #8898aa;
+  font-size: 13px !important;
+}
+
+.ninja.stripe .success .reset path {
+  fill: #24b47e;
+}
+        </style>
+        
+        <script type="text/javascript">
             
             // Create a Stripe client.
             var stripe = Stripe('{{ $accountGateway->getPublishableKey() }}');
@@ -348,45 +568,107 @@
 
             // Custom styling can be passed to options when creating an Element.
             // (Note that this demo uses a wider set of styles than the guide below.)
-            var style = {
-              base: {
-                color: '#32325d',
-                lineHeight: '18px',
-                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSmoothing: 'antialiased',
-                fontSize: '16px',
-                '::placeholder': {
-                  color: '#aab7c4'
-                }
-              },
-              invalid: {
-                color: '#fa755a',
-                iconColor: '#fa755a'
-              }
-            };
+            var elementStyles = {
+                base: {
+                  color: '#32325D',
+                  fontWeight: 500,
+                  fontFamily: 'Source Code Pro, Consolas, Menlo, monospace',
+                  fontSize: '16px',
+                  fontSmoothing: 'antialiased',
 
-            // Create an instance of the card Element.
-            var card = elements.create('card', {style: style});
+                  '::placeholder': {
+                    color: '#CFD7DF',
+                  },
+                  ':-webkit-autofill': {
+                    color: '#e39f48',
+                  },
+                },
+                invalid: {
+                  color: '#E25950',
 
-            // Add an instance of the card Element into the `card-element` <div>.
-            card.mount('#card-element');
+                  '::placeholder': {
+                    color: '#FFCCA5',
+                  },
+                },
+              };
 
-            // Handle real-time validation errors from the card Element.
-            card.addEventListener('change', function(event) {
-              var displayError = document.getElementById('card-errors');
-              if (event.error) {
-                displayError.textContent = event.error.message;
-              } else {
-                displayError.textContent = '';
-              }
-            });
+              var elementClasses = {
+                focus: 'focused',
+                empty: 'empty',
+                invalid: 'invalid',
+              };
+
+              var cardNumber = elements.create('cardNumber', {
+                style: elementStyles,
+                classes: elementClasses,
+              });
+              cardNumber.mount('#card-number');
+
+              var cardExpiry = elements.create('cardExpiry', {
+                style: elementStyles,
+                classes: elementClasses,
+              });
+              cardExpiry.mount('#card-expiry');
+
+              var cardCvc = elements.create('cardCvc', {
+                style: elementStyles,
+                classes: elementClasses,
+              });
+              cardCvc.mount('#card-cvc');
+
+
+cardNumber.addEventListener('change', function(event){
+    var displayError = document.getElementById('card-errors');
+        if (event.error) {
+        displayError.textContent = event.error.message;
+        } else {
+        displayError.textContent = '';
+        }
+
+               releaseSubmitButton();
+
+});
+
+cardExpiry.addEventListener('change', function(event){
+    var displayError = document.getElementById('card-errors');
+        if (event.error) {
+        displayError.textContent = event.error.message;
+        } else {
+        displayError.textContent = '';
+        }
+                releaseSubmitButton();
+
+});
+
+cardCvc.addEventListener('change', function(event){
+    var displayError = document.getElementById('card-errors');
+        if (event.error) {
+        displayError.textContent = event.error.message;
+        } else {
+        displayError.textContent = '';
+        }
+
+        releaseSubmitButton();
+});
+
+function releaseSubmitButton(){
+console.log('releasing');
+         var $form = $(this);
+        $form.find('button').prop('disabled', false);
+        return true;
+}
+
 
             // Handle form submission.
             var form = document.getElementById('payment-form');
             form.addEventListener('submit', function(event) {
               event.preventDefault();
 
-              stripe.createToken(card).then(function(result) {
+            var options = {
+                address_zip: document.getElementById('postal_code').value,
+            };
+
+            stripe.createToken(cardNumber, options).then(function(result) {
                 if (result.error) {
                   // Inform the user if there was an error.
                   var errorElement = document.getElementById('card-errors');
