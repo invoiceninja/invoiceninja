23174d2d64d065b356584214fe2e37bf98729e2a
David Bomba <turbo124@gmail.com>
2018-10-16 20:33:02 +1100

    Create layout using CoreUI admin template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('dashboard.index');
     }
 
     public function user()
--- config/ninja.php
+++ config/ninja.php
@@ -4,5 +4,6 @@ return [
 
     'web_url' => 'https://www.invoiceninja.com',
     'app_url' => 'https://app-v5.invoiceninja.com',
+    'site_url' => '',
 
 ];
