dac6792e4c57db2f6273bd6539160f4530b3d946
David Bomba <turbo124@gmail.com>
2018-10-19 08:04:17 +1100

    Privacy Policy & TOS

--- config/ninja.php
+++ config/ninja.php
@@ -5,5 +5,17 @@ return [
     'web_url' => 'https://www.invoiceninja.com',
     'app_url' => 'https://app-v5.invoiceninja.com',
     'site_url' => '',
+    'environment' => env('NINJA_ENVIRONMENT', 'selfhost'), // 'hosted', 'development', 'selfhost', 'reseller'
 
+    // Settings used by invoiceninja.com
+
+    'terms_of_service_url' => [
+        'hosted' => env('TERMS_OF_SERVICE_URL', 'https://www.invoiceninja.com/terms/'),
+        'selfhost' => env('TERMS_OF_SERVICE_URL', 'https://www.invoiceninja.com/self-hosting-terms-service/'),
+    ],
+
+    'privacy_policy_url' => [
+        'hosted' => env('PRIVACY_POLICY_URL', 'https://www.invoiceninja.com/privacy-policy/'),
+        'selfhost' => env('PRIVACY_POLICY_URL', 'https://www.invoiceninja.com/self-hosting-privacy-data-control/'),
+    ],
 ];
