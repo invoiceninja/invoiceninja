cebdf59eeb8581799d69e907830c978c9b2942b6
Hillel Coren <hillelcoren@gmail.com>
2018-10-23 09:08:16 +0300

    Remove alt tag from email templates

--- resources/views/emails/partials/account_logo.blade.php
+++ resources/views/emails/partials/account_logo.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ $account->website }}" style="color: #19BB40; text-decoration: underline;">
     @endif
 
-    <img src="{{ isset($message) ? $message->embed($account->getLogoPath()) : 'cid:' . $account->getLogoName() }}" height="50" style="height:50px; max-width:140px; margin-left: 33px; padding-top: 2px" alt="{{ trans('texts.logo') }}"/>
+    <img src="{{ isset($message) ? $message->embed($account->getLogoPath()) : 'cid:' . $account->getLogoName() }}" height="50" style="height:50px; max-width:140px; margin-left: 33px; padding-top: 2px" alt=""/>
 
     @if ($account->website)
         </a>
