dffafc20af7dba8ec41894d27739f84721945b4c
David Bomba <turbo124@gmail.com>
2018-10-25 21:14:09 +1100

    Fixes for travis (#2467)
    * fixes for travis

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'redis'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
