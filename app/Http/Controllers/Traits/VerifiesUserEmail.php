6634e4229e9527a98db06ce2a2773d00e5fefe44
David Bomba <turbo124@gmail.com>
2018-11-03 10:07:19 +1100

    Ensure default company ID is registered in session variables

--- app/Http/Controllers/Traits/VerifiesUserEmail.php
+++ app/Http/Controllers/Traits/VerifiesUserEmail.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Traits;
 
 use App\Models\User;
+use App\Utils\Traits\UserSessionAttributes;
 use Illuminate\Support\Facades\Auth;
 
 trait VerifiesUserEmail
 {
+    use UserSessionAttributes;
 
     public function confirm($code)
     {
@@ -17,6 +19,8 @@ trait VerifiesUserEmail
             $user->email_verified_at = now();
             $user->confirmation_code = null;
             $user->save();
+
+            $this->setCurrentCompanyId($user->companies()->first()->account->default_company_id);
 
             Auth::loginUsingId($user->id, true);
 
--- app/Jobs/Account/CreateAccount.php
+++ app/Jobs/Account/CreateAccount.php
@@ -5,6 +5,7 @@ namespace App\Jobs\Account;
 use App\Events\Account\AccountCreated;
 use App\Jobs\User\CreateUser;
 use App\Jobs\Company\CreateCompany;
+use App\Utils\Traits\UserSessionAttributes;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Http\Request;
 use App\Models\Account;
@@ -14,6 +15,7 @@ class CreateAccount
 {
 
     use Dispatchable;
+    use UserSessionAttributes;
 
     protected $request;
 
@@ -35,18 +37,40 @@ class CreateAccount
      */
     public function handle()
     {
-
+        /*
+         * Create account
+         */
         $account = Account::create($this->request->toArray());
 
+        /*
+         * Create company
+         */
         $company = CreateCompany::dispatchNow($this->request, $account);
 
+        /*
+         * Set default company
+         */
         $account->default_company_id = $company->id;
         $account->save();
 
+        /*
+         * Create user
+         */
         $user = CreateUser::dispatchNow($this->request, $account, $company);
 
+        /*
+         * Set current company
+         */
+        $this->setCurrentCompanyId($user->companies()->first()->account->default_company_id);
+
+        /*
+         * Login user
+         */
         Auth::loginUsingId($user->id, true);
 
+        /*
+         * Fire related events
+         */
         event(new AccountCreated($user));
 
         return $user;
