ad54b89ba029ef8e08030916bef03fe6a791cbc9
David Bomba <turbo124@gmail.com>
2018-11-03 12:00:35 +1100

    Laravel Telescope

---
+++ config/datatables-fractal.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+     * Request key name to parse includes on fractal.
+     */
+    'includes'   => 'include',
+
+    /*
+     * Default fractal serializer.
+     */
+    'serializer' => League\Fractal\Serializer\DataArraySerializer::class,
+];
