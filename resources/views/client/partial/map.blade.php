0f3b396a399a28f6c3b7a78b595fc97c87725b01
David Bomba <turbo124@gmail.com>
2018-11-05 05:54:13 +1100

    show maps

--- resources/views/client/partial/map.blade.php
+++ resources/views/client/partial/map.blade.php
@@ -1,45 +1,16 @@
-<div id="map"></div>
+<div id="map" style="height:400px; width:100%;"></div>
 
 <script type="text/javascript">
-    function initialize() {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            zoom: 1,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoomControl: true,
-        };
 
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-        var address = {!! json_encode(e("{$location->address1} {$location->address2} {$location->city} {$location->state} {$location->postal_code} " . ($location->country ? $location->country->getName() : ''))) !!};
 
-        geocoder = new google.maps.Geocoder();
-        geocoder.geocode( { 'address': address}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-                    var result = results[0];
-                    map.setCenter(result.geometry.location);
 
-                    var infowindow = new google.maps.InfoWindow(
-                            { content: '<b>'+result.formatted_address+'</b>',
-                                size: new google.maps.Size(150, 50)
-                            });
-
-                    var marker = new google.maps.Marker({
-                        position: result.geometry.location,
-                        map: map,
-                        title:address,
-                    });
-                    google.maps.event.addListener(marker, 'click', function() {
-                        infowindow.open(map, marker);
-                    });
-                } else {
-                    $('#map').hide();
-                }
-            } else {
-                $('#map').hide();
-            }
-        });
+    function initMap() {
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
     }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
 </script>
