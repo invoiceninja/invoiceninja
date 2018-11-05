fcc3ee70820541d341d1de71a7f960f0b2aa04d4
David Bomba <turbo124@gmail.com>
2018-11-05 14:57:36 +1100

    fixes for migrations

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email', 255)->index();
+            $table->string('email', 128)->index();
             $table->string('token', 255);
             $table->timestamp('created_at')->nullable();
         });
