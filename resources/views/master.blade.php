9b8247b6594cb611b4d67d816810431668ae75b1
Hillel Coren <hillelcoren@gmail.com>
2018-11-05 13:45:14 +0200

    Don’t show cookie warning with borderless

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -222,31 +222,33 @@
 
     </script>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/cookieconsent.min.css') }}"/>
-    <script src="{{ asset('js/cookieconsent.min.js') }}"></script>
-    <script>
-    window.addEventListener("load", function(){
-        if (! window.cookieconsent) {
-            return;
-        }
-        window.cookieconsent.initialise({
-            "palette": {
-                "popup": {
-                    "background": "#000"
-                },
-                "button": {
-                    "background": "#f1d600"
-                },
-            },
-            "content": {
-                "href": "{{ Utils::isNinja() ? config('ninja.privacy_policy_url.hosted') : 'https://cookiesandyou.com/' }}",
-                "message": {!! json_encode(trans('texts.cookie_message')) !!},
-                "dismiss": {!! json_encode(trans('texts.got_it')) !!},
-                "link": {!! json_encode(trans('texts.learn_more')) !!},
+    @if (! request()->borderless)
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/cookieconsent.min.css') }}"/>
+        <script src="{{ asset('js/cookieconsent.min.js') }}"></script>
+        <script>
+        window.addEventListener("load", function(){
+            if (! window.cookieconsent) {
+                return;
             }
-        })}
-    );
-    </script>
+            window.cookieconsent.initialise({
+                "palette": {
+                    "popup": {
+                        "background": "#000"
+                    },
+                    "button": {
+                        "background": "#f1d600"
+                    },
+                },
+                "content": {
+                    "href": "{{ Utils::isNinja() ? config('ninja.privacy_policy_url.hosted') : 'https://cookiesandyou.com/' }}",
+                    "message": {!! json_encode(trans('texts.cookie_message')) !!},
+                    "dismiss": {!! json_encode(trans('texts.got_it')) !!},
+                    "link": {!! json_encode(trans('texts.learn_more')) !!},
+                }
+            })}
+        );
+        </script>
+    @endif
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
