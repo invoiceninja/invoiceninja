716aa414d63ab8e97b8c710a2d2235272c915355
David Bomba <turbo124@gmail.com>
2018-11-06 16:21:32 +1100

    New Client UI

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -27,141 +27,18 @@
     <div class="container-fluid">
 
         <div class="row">
-        <div class="col-lg-12">
-        {{ html()->form('POST', route('signup.submit'))->open() }}
+            <div class="col-lg-12">
 
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link rounded-0 active" data-toggle="tab" href="#tab1" role="tab">
-                    <i class="icon-calculator"></i> @lang('texts.details')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab5" role="tab">
-                    <i class="icon-microchip"></i> Details 2
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab2" role="tab">
-                   <i class="icon-user"></i> @lang('texts.contacts')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab3" role="tab">
-                    <i class="icon-map"></i> @lang('texts.locations')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab4" role="tab">
-                    <i class="fa fa-heart-o"></i> @lang('texts.invoices')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab6" role="tab">
-                    <i class="fa fa-coffee"></i> @lang('texts.quotes')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab7" role="tab">
-                    @lang('texts.payments')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab8" role="tab">
-                    @lang('texts.invoices')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab9" role="tab">
-                    @lang('texts.credits')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab10" role="tab">
-                    @lang('texts.tasks')
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link rounded-0" data-toggle="tab" href="#tab11" role="tab">
-                    @lang('texts.tickets')
-                </a>
-            </li>
+            {{ html()->form('PUT', route('signup.submit'))->open() }}
 
-        </ul>
-
-        <!-- Tab panes-->
-        <div class="tab-content">
-
-            <!-- Client Details-->
-            <div class="tab-pane p-3 active" id="tab1" role="tabpanel">
-
-                <div class="row">
-
-                    @include('client.partial.client_details', $client)
-
-                    <div class="col-lg-6">
-
-                        @include('client.partial.client_location', ['location' => $client->primary_billing_location->first(), 'address' => 'Billing'])
-
-                    </div>
-
-                </div>
-
-                <div class="row">
-                    @include('client.partial.map', ['location' => $client->primary_billing_location->first()])
-                </div>
-            </div>
-            <!-- Client Details-->
-
-            <!-- Contact Details-->
-            <div class="tab-pane p-3" id="tab2" role="tabpanel">
-                @foreach($client->contacts as $contact)
-                    @include('client.partial.contact_details', ['contact' => $contact])
-                @endforeach
-            </div>
-            <!-- Contact Details-->
-
-            <!-- Client Locations -->
-            <div class="tab-pane p-3" id="tab3" role="tabpanel">
-
-                @foreach($client->locations as $location)
-                    <div class="row">
-                        <div class="col-lg-6">
-                            @include('client.partial.client_location',['location' => $location, 'address' => 'Shipping'])
-                        </div>
-                        <div class="col-lg-6">
-                            @include('client.partial.map', ['location' => $location, 'address' => 'Shipping'])
-                        </div>
-                    </div>
-                @endforeach
-                @include('client.partial.map_js', ['locations' => $client->locations, 'address' => 'Shipping'])
-
-            </div>
-            <!-- Client Locations -->
-
-
-            <div class="tab-pane p-3" id="tab4" role="tabpanel">
-            </div>
-
-            <div class="tab-pane p-3" id="tab5" role="tabpanel">
-                <div class="row">
-
-                    @include('client.partial.client_details', $client)
-
-                    @include('client.partial.client_stats')
-
-                    @include('client.partial.client_meta', $client)
-
-                </div>
-
+            
             </div>
 
         </div>
-        </div>
+
+            {{ html()->form()->close() }}
     </div>
-</div>
-
-    {{ html()->form()->close() }}
+    
 </main>
 
 
