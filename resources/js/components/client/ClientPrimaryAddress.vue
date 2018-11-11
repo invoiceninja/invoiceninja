51b4e70dd7968aa3b67042468325b87ce6ef6231
David Bomba <turbo124@gmail.com>
2018-11-11 13:52:46 +1100

    Vue components for address

---
+++ resources/js/components/client/ClientPrimaryAddress.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<ul class="nav nav-tabs" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link active" data-toggle="tab" href="#billing" role="tab" aria-controls="billing">{{ trans('texts.billing_address') }}</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#shipping" role="tab" aria-controls="shipping">{{ trans('texts.shipping_address') }}</a>
+			</li>
+		</ul>
+		<div class="tab-content">
+			<div class="tab-pane active" id="billing" role="tabpanel">
+				<generic-address :data="client.primary_billing_location"></generic-address>
+			</div>
+			<div class="tab-pane" id="shipping" role="tabpanel">
+				<generic-address :data="client.primary_shipping_location"></generic-address>
+			</div>
+		</div>
+	</div>	
+</template>
+
+<script>
+export default {
+        props: ['client']
+    }
+</script>
