22805f4a9103dbb5f6870419f6658d1240666f2a
David Bomba <turbo124@gmail.com>
2018-11-19 17:21:01 +1100

    Deploy hashes across models

--- app/Models/ClientLocation.php
+++ app/Models/ClientLocation.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class ClientLocation extends BaseModel
 {
     public $timestamps = false;
+
+    protected $appends = ['client_location_id'];
+
+    public function getRouteKeyName()
+    {
+        return 'client_location_id';
+    }
+
+    public function getClientLocationIdAttribute()
+    {
+        return $this->encodePrimaryKey($this->id);
+    }
+
+    public function client()
+    {
+    	return $this->belongsTo(Client::class);
+    }
 }
