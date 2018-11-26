7b138ea7962794a062637f09af1c6085b86fbdb3
David Bomba <turbo124@gmail.com>
2018-11-26 15:14:08 +1100

    Composers

---
+++ app/Http/ViewComposers/HeaderComposer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use App\Utils\Traits\UserSessionAttributes;
+use Illuminate\View\View;
+
+class HeaderComposer
+{
+    use UserSessionAttributes;
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('header', $this->headerData());
+    }
+
+    private function headerData()
+    {
+        //companies
+        $companies = auth()->user()->companies;
+
+        $data['current_company'] = $companies->first(function ($company){
+            return $company->id == $this->getCurrentCompanyId();
+        });
+
+        $data['companies'] = $companies->reject(function ($company){
+            return $company->id == $this->getCurrentCompanyId();
+        });
+
+        return $data;
+    }
+
+}
---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer(
+            [
+                'client.edit', 
+                'client.list', 
+                'dashboard.index',
+            ],
+            'App\Http\ViewComposers\HeaderComposer'
+        );
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
