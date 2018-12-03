329db2a83c5cab7efb8bad0b91e86f07013df2d4
David Bomba <turbo124@gmail.com>
2018-12-03 14:30:26 +1100

    OO JS forms

---
+++ resources/js/src/utils/form-errors.ts
@@ -0,0 +1,74 @@
+class FormErrors {
+
+	errors:any;
+    /**
+     * Create a new Errors instance.
+     */
+    constructor() {
+        this.errors = {};
+    }
+
+
+    /**
+     * Determine if an errors exists for the given field.
+     *
+     * @param {string} field
+     */
+    has(field:string) {
+        return this.errors.hasOwnProperty(field);
+    }
+
+
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
+
+
+    /**
+     * Retrieve the error message for a field.
+     *
+     * @param {string} field
+     */
+    get(field:string) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+
+    /**
+     * Record the new errors.
+     *
+     * @param {object} errors
+     */
+    record(errors:any) {
+        this.errors = errors;
+    }
+
+
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
+    clear(field:string) {
+        if (field) {
+            delete this.errors[field];
+
+            return;
+        }
+
+        this.errors = {};
+    }
+}
+
+
+
+
+//@keydown="errors.clear($event.target.name)"
+//
+//
+//
