c9b21b9216b656b29b9e6f8fb99f9b5592a49779
David Bomba <turbo124@gmail.com>
2018-12-09 21:27:43 +1100

    Working on localization

--- public/js/vendor/app.js
+++ public/js/vendor/app.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -13204,7 +13204,7 @@ if (token) {
 
 /***/ }),
 
-/***/ 2:
+/***/ 3:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__("./resources/js/app.js");
