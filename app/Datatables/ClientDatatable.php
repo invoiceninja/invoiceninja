73cca389016cd285fcd116b473710b9450e1b907
David Bomba <turbo124@gmail.com>
2018-12-21 20:30:59 +1100

    Apply filter to vue table

--- app/Datatables/ClientDatatable.php
+++ app/Datatables/ClientDatatable.php
@@ -5,6 +5,7 @@ namespace App\Datatables;
 use App\Models\Client;
 use App\Utils\Traits\UserSessionAttributes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ClientDatatable
@@ -18,21 +19,16 @@ class ClientDatatable
 		/**
 		*
 		* $sort_col is returned col|asc
-		* so needs to be exploded
+		* needs to be exploded
 		*
 		*/
 		$sort_col = explode("|", $request->input('sort'));
 
-		return response()->json(self::buildColumns($company_id)->orderBy($sort_col[0], $sort_col[1])->paginate($request->input('per_page')), 200);
-	}
-
-	private static function buildColumns($company_id)
-	{
-
+		return response()->json(self::find($company_id, $request->input('filter'))->orderBy($sort_col[0], $sort_col[1])->paginate($request->input('per_page')), 200);
 	}
 
 
-	public function find($filter = null, $userId = false)
+	private static function find(int $company_id, $filter, $userId = false)
 	    {
 	        $query = DB::table('clients')
 	                    ->join('companies', 'companies.id', '=', 'clients.company_id')
@@ -42,26 +38,26 @@ class ClientDatatable
 	                    ->where('client_contacts.deleted_at', '=', null)
 	                    //->whereRaw('(clients.name != "" or contacts.first_name != "" or contacts.last_name != "" or contacts.email != "")') // filter out buy now invoices
 	                    ->select(
-	                        DB::raw('COALESCE(clients.currency_id, accounts.currency_id) currency_id'),
-	                        DB::raw('COALESCE(clients.country_id, accounts.country_id) country_id'),
-	                        DB::raw("CONCAT(COALESCE(contacts.first_name, ''), ' ', COALESCE(contacts.last_name, '')) contact"),
-	                        'clients.public_id',
+	                        DB::raw('COALESCE(clients.currency_id, companies.currency_id) currency_id'),
+	                        DB::raw('COALESCE(clients.country_id, companies.country_id) country_id'),
+	                        DB::raw("CONCAT(COALESCE(client_contacts.first_name, ''), ' ', COALESCE(client_contacts.last_name, '')) contact"),
+	                        'clients.id',
 	                        'clients.name',
 	                        'clients.private_notes',
-	                        'contacts.first_name',
-	                        'contacts.last_name',
+	                        'client_contacts.first_name',
+	                        'client_contacts.last_name',
 	                        'clients.balance',
 	                        'clients.last_login',
 	                        'clients.created_at',
 	                        'clients.created_at as client_created_at',
-	                        'clients.work_phone',
-	                        'contacts.email',
+	                        'client_contacts.phone',
+	                        'client_contacts.email',
 	                        'clients.deleted_at',
 	                        'clients.is_deleted',
 	                        'clients.user_id',
 	                        'clients.id_number'
 	                    );
-
+/*
 	         if(Auth::user()->account->customFieldsOption('client1_filter')) {
 	            $query->addSelect('clients.custom_value1');
 	        }
@@ -71,16 +67,16 @@ class ClientDatatable
 	        }
 
 	        $this->applyFilters($query, ENTITY_CLIENT);
-
+*/
 	        if ($filter) {
 	            $query->where(function ($query) use ($filter) {
 	                $query->where('clients.name', 'like', '%'.$filter.'%')
 	                      ->orWhere('clients.id_number', 'like', '%'.$filter.'%')
-	                      ->orWhere('contacts.first_name', 'like', '%'.$filter.'%')
-	                      ->orWhere('contacts.last_name', 'like', '%'.$filter.'%')
-	                      ->orWhere('contacts.email', 'like', '%'.$filter.'%');
+	                      ->orWhere('client_contacts.first_name', 'like', '%'.$filter.'%')
+	                      ->orWhere('client_contacts.last_name', 'like', '%'.$filter.'%')
+	                      ->orWhere('client_contacts.email', 'like', '%'.$filter.'%');
 	            });
-
+/*
 	            if(Auth::user()->account->customFieldsOption('client1_filter')) {
 	                $query->orWhere('clients.custom_value1', 'like' , '%'.$filter.'%');
 	            }
@@ -88,6 +84,7 @@ class ClientDatatable
 	            if(Auth::user()->account->customFieldsOption('client2_filter')) {
 	                $query->orWhere('clients.custom_value2', 'like' , '%'.$filter.'%');
 	            }
+*/
 	        }
 
 	        if ($userId) {
