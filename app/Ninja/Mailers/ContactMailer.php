b585c3800a9da9abeb55a800c20cdd5fd4e6723c
Hillel Coren <hillelcoren@gmail.com>
2019-01-23 17:25:19 +0200

    Improve throttle email

--- app/Ninja/Mailers/ContactMailer.php
+++ app/Ninja/Mailers/ContactMailer.php
@@ -408,7 +408,7 @@ class ContactMailer extends Mailer
         if ($new_day_throttle > $day) {
             $errorEmail = env('ERROR_EMAIL');
             if ($errorEmail && ! Cache::get("throttle_notified:{$key}")) {
-                Mail::raw('Account Throttle', function ($message) use ($errorEmail, $account) {
+                Mail::raw('Account Throttle: ' . $account->account_key, function ($message) use ($errorEmail, $account) {
                     $message->to($errorEmail)
                             ->from(CONTACT_EMAIL)
                             ->subject("Email throttle triggered for account " . $account->id);
