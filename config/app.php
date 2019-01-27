068ff4107d1b416832c22789656683db9a326a12
David Bomba <turbo124@gmail.com>
2019-01-28 09:54:25 +1100

    Remove telescope

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
 
