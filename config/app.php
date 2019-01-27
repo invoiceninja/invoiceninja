b5687c91af1d2ad2e8bf89520d55b7133aa33ab2
David Bomba <turbo124@gmail.com>
2019-01-28 08:52:19 +1100

    Add Telescope

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
 
