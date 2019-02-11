48c3b5fcffdd7fce58e827399e5afe55706e0f48
Wim de Ruijter <wim.deruijter@gmail.com>
2019-02-11 21:12:42 +0100

    Made clear what "account with fees" means (#2664)
    Should have done this in the previous PR: https://github.com/invoiceninja/invoiceninja/pull/2663

--- app/Console/Commands/SendReminders.php
+++ app/Console/Commands/SendReminders.php
@@ -125,7 +125,7 @@ class SendReminders extends Command
     private function chargeLateFees()
     {
         $accounts = $this->accountRepo->findWithFees();
-        $this->info(date('r ') . $accounts->count() . ' accounts found with fees');
+        $this->info(date('r ') . $accounts->count() . ' accounts found with fees enabled');
 
         foreach ($accounts as $account) {
             if (! $account->hasFeature(FEATURE_EMAIL_TEMPLATES_REMINDERS)) {
