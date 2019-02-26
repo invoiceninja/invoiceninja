4226c87a5ff668b6a5d4e7d1fa67a0ba1db1ead3
David Bomba <turbo124@gmail.com>
2019-02-26 20:23:57 +1100

    Move translation helper into its own class

--- app/Utils/TranslationHelper.php
+++ app/Utils/TranslationHelper.php
@@ -2,17 +2,63 @@
 
 namespace App\Utils;
 
+use App\Models\PaymentTerm;
 use Cache;
+use Illuminate\Support\Str;
 
-public class TranslationHelper
+class TranslationHelper
 {
 	
 	public static function getIndustries()
 	{
 		return Cache::get('industries')->each(function ($industry) {
-            $industry->name = trans('texts.industry_'.$industry->name);
+            $industry->name = ctrans('texts.industry_'.$industry->name);
         })->sortBy(function ($industry) {
             return $industry->name;
-        })
+        });
+	}
+
+	public static function getCountries()
+	{
+		return Cache::get('countries')->each(function ($country) {
+            $country->name = ctrans('texts.country_'.$country->name);
+        })->sortBy(function ($country) {
+            return $country->name;
+        });
+	}
+
+	public static function getPaymentTypes()
+	{
+		return Cache::get('paymentTypes')->each(function ($pType) {
+            $pType->name = ctrans('texts.payment_type_'.$pType->name);
+        })->sortBy(function ($pType) {
+            return $pType->name;
+        });
+	}
+
+	public static function getLanguages()
+	{
+		return Cache::get('languages')->each(function ($lang) {
+            $lang->name = ctrans('texts.lang_'.$lang->name);
+        })->sortBy(function ($lang) {
+            return $lang->name;
+        });
+	}
+
+	public static function getCurrencies()
+	{
+		return Cache::get('currencies')->each(function ($currency) {
+            $currency->name = ctrans('texts.currency_' . Str::slug($currency->name, '_'));
+        })->sortBy(function ($currency) {
+            return $currency->name;
+        });
+	}
+
+	public static function getPaymentTerms()
+	{
+		return PaymentTerm::getCompanyTerms()->map(function ($term){
+            $term['name'] = ctrans('texts.payment_terms_net') . ' ' . $term['num_days'];
+            return $term;
+        });
 	}
 }
