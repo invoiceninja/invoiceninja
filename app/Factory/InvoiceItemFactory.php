d28fe81c51572227c91254394b65e4b3458512d1
David Bomba <davidbomba@Davids-iMac.local>
2019-04-04 12:38:39 +1100

    Invoice Item Factory

---
+++ app/Factory/InvoiceItemFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Factory;
+
+class InvoiceItemFactory
+{
+	public static function create() :\stdClass
+	{
+		$item = new \stdClass;
+		$item->qty = 0;
+		$item->cost = 0;
+		$item->product_key = '';
+		$item->notes = '';
+		$item->discount = 0;
+		$item->is_amount_discount = false;
+		$item->tax_name1 = '';
+		$item->tax_rate1 = 0;
+		$item->tax_name2 = '';
+		$item->tax_rate2 = 0;
+		$item->sort_id = 0;
+		$item->line_total = 0;
+		$item->invoice_item_type_id = 0;
+		
+		return $item;
+	}
+}
+
