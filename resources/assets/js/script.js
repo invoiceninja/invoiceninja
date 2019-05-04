9df361a378993aca8bc81a8944d15de8d79a3cdb
Kristián Feldsam <feldsam@gmail.com>
2019-05-04 13:07:49 +0200

    invoice items - processVariables - fix year increments in JS
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -71,7 +71,7 @@ function processVariables(str) {
             var match = matches[j];
             var offset = 0;
             if (match.split('+').length > 1) {
-                offset = match.split('+')[1];
+                offset = parseInt(match.split('+')[1]);
             } else if (match.split('-').length > 1) {
                 offset = parseInt(match.split('-')[1]) * -1;
             }
