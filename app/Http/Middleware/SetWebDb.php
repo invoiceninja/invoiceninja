e33273e36068b01c792d0157fdd9b01eb390908b
David Bomba <turbo124@gmail.com>
2019-05-22 13:46:42 +1000

    Add new db discovery middleware

---
+++ app/Http/Middleware/SetWebDb.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Libraries\MultiDB;
+use Closure;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class SetWebDb
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (config('ninja.db.multi_db_enabled'))
+        {
+
+            MultiDB::setDB(Cookie::get('db'));
+        
+        }
+
+
+        return $next($request);
+    }
+}
