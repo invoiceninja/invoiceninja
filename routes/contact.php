8df163c86a566b36267a6203cbe239e2047915e9
David Bomba <davidbomba@Davids-iMac.local>
2019-07-08 15:17:33 +1000

    add route file

---
+++ routes/contact.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['middleware' => ['api_secret_check']], function () {
+
+  //Route::post('api/v1/signup', 'AccountController@store')->name('signup.submit');
+  Route::post('api/v1/contact/login', 'Contact\LoginController@apiLogin');
+  //Route::post('api/v1/oauth_login', 'Auth\LoginController@oauthApiLogin');
+  
+});
+
+Route::group(['middleware' => ['contact_db','api_secret_check','contact_token_auth'], 'prefix' =>'api/v1/contact', 'as' => 'api.contact.'], function () {
+
+
+});
