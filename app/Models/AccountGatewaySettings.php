1f8c4b922e1cb631ea4786793326197e99b988e3
Hillel Coren <hillelcoren@gmail.com>
2019-07-21 09:28:24 +0300

    Minor fixes

--- app/Models/AccountGatewaySettings.php
+++ app/Models/AccountGatewaySettings.php
@@ -53,7 +53,7 @@ class AccountGatewaySettings extends EntityModel
 
     public function hasTaxes()
     {
-        return floatval($this->fee_tax_rate1) || floatval($this->fee_tax_rate1);
+        return floatval($this->fee_tax_rate1) || floatval($this->fee_tax_rate2);
     }
 
     public function feesToString()
