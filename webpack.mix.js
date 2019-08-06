40d2ba7be8d9606ac016e1d2f49052dc8c9c8a0c
David Bomba <davidbomba@Davids-iMac.local>
2019-08-06 11:16:06 +1000

    Update webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,6 +25,10 @@ mix.copyDirectory('node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js'
 mix.copyDirectory('node_modules/select2/dist/css/select2.min.css', 'public/vendors/css/select2.min.css');
 mix.copyDirectory('node_modules/select2/dist/js/select2.full.min.js', 'public/vendors/js/select2.min.js');
 
+mix.copyDirectory('node_modules/dropzone/dist/min/dropzone.min.css', 'public/vendors/css/dropzone.min.css');
+mix.copyDirectory('node_modules/dropzone/dist/min/dropzone.min.js', 'public/vendors/js/dropzone.min.js');
+
+
 mix.copyDirectory('node_modules/font-awesome/fonts', 'public/vendors/fonts');
 
 mix.version();
