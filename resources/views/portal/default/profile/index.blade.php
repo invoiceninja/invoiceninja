5f37553f8771cf1cb2c89ede8c1eee33130fa480
David Bomba <turbo124@gmail.com>
2019-08-14 08:41:19 +1000

    Working on client profile

--- resources/views/portal/default/profile/index.blade.php
+++ resources/views/portal/default/profile/index.blade.php
@@ -13,98 +13,76 @@
 @endpush
 @section('body')
   <main class="main">
-
       <div class="container-fluid">
-
-		    <div class="row" style="padding-top: 30px;">
-		
-          <div class="col-sm-3" style="padding-bottom: 10px;">
-
-              {!! Former::framework('TwitterBootstrap4'); !!}
-
-              {!! Former::vertical_open_for_files()
-                    ->id('update_contact')
-                    ->route('client.profile.update', auth()->user()->hashed_id)
-                    ->method('PUT');	!!}
-              
-              @csrf
-
-              <div class="card">
-
-                  <div class="card-header">
-                  
-                      <strong>{{ ctrans('texts.avatar') }}</strong>
-
-                  </div>
-
-                  <div class="card-body align-items-center">
-
-                      @if(auth()->user()->avatar)
-                      <img src="{{ auth()->user()->avatar }}" class="img-fluid">
-                      @else
-                      <i class="fa fa-user fa-5x"></i>
-                      @endif
-
-                      {!! Former::file('avatar')
-                          ->max(2, 'MB')
-                          ->accept('image')
-                          ->label('')
-                          ->inlineHelp(trans('texts.logo_help')) !!}
-
-                  </div>
-
-                  <div class="card-footer">
-                  </div>
-
+        <div class="row" style="padding-top: 30px;">
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-header">
+                  <strong> {{ ctrans('texts.user_details') }}</strong>
               </div>
 
-          </div>
+                    {!! Former::framework('TwitterBootstrap4'); !!}
 
-          <div class="col-sm-6" style="padding-bottom: 10px;">
-              
-              <div class="card">
+                    {!! Former::vertical_open_for_files()
+                          ->id('update_contact')
+                          ->route('client.profile.update', auth()->user()->hashed_id)
+                          ->method('PUT');  !!}
+                    
+                    @csrf
 
-                  <div class="card-header">
-                  
-                      <strong> {{ ctrans('texts.user_details') }}</strong>
+              <div class="card-body">
 
+                <div class="row">
+                  <div class="col-sm-3">
+                    <div class="card">
+                      <div class="card-body align-items-center">
+                        @if(auth()->user()->avatar)
+                        <img src="{{ auth()->user()->avatar }}" class="img-fluid">
+                        @else
+                        <i class="fa fa-user fa-5x"></i>
+                        @endif
+
+                        {!! Former::file('avatar')
+                            ->max(2, 'MB')
+                            ->accept('image')
+                            ->label('')
+                            ->inlineHelp(trans('texts.logo_help')) !!}
+                      </div>
+                    </div>
                   </div>
+                  <div class="col-sm-6">
+                    <div class="card">
+                      <div class="card-body">
+                        {!! Former::text('first_name')->placeholder( ctrans('texts.first_name'))->label('')->value(auth()->user()->first_name)!!}
 
-                  <div class="card-body">
-                  
-                  {!! Former::text('first_name')->placeholder( ctrans('texts.first_name'))->label('')->value(auth()->user()->first_name)!!}
+                        {!! Former::text('last_name')->placeholder( ctrans('texts.last_name'))->label('')->value(auth()->user()->last_name) !!}
 
-                  {!! Former::text('last_name')->placeholder( ctrans('texts.last_name'))->label('')->value(auth()->user()->last_name) !!}
+                        {!! Former::text('email')->placeholder( ctrans('texts.email'))->label('')->value(auth()->user()->email) !!}
 
-                  {!! Former::text('email')->placeholder( ctrans('texts.email'))->label('')->value(auth()->user()->email) !!}
+                        {!! Former::text('phone')->placeholder( ctrans('texts.phone'))->label('')->value(auth()->user()->phone) !!}
 
-                  {!! Former::text('phone')->placeholder( ctrans('texts.phone'))->label('')->value(auth()->user()->phone) !!}
+                        {!! Former::password('password')->placeholder( ctrans('texts.password'))->label('') !!}
 
-                  {!! Former::password('password')->placeholder( ctrans('texts.password'))->label('') !!}
+                        {!! Former::password('password_confirmation')->placeholder( ctrans('texts.confirm_password'))->label('') !!}
 
-                  {!! Former::password('password_confirmation')->placeholder( ctrans('texts.confirm_password'))->label('') !!}
 
+                        <div>
+                          <button class="btn btn-primary pull-right">{{ ctrans('texts.save') }}</button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-
-                  <div class="card-footer">
-
-                      <button class="btn btn-primary pull-right">{{ ctrans('texts.save') }}</button>
-
-                  </div>
-
+                </div>
               </div>
-
-              {!! Former::close() !!}
-
+            </div>
           </div>
+        </div>
 
-		    </div>
+                        {!! Former::close() !!}
 
-      @include('portal.default.profile.client_information')
-      
-    </div>
+        @include('portal.default.profile.client_information')
 
+      </div>
   </main>
-
 </body>
 @endsection
