daa402c7789d78c945ec91daf6f787ad98a609b0
David Bomba <turbo124@gmail.com>
2019-10-06 22:27:56 +1100

    OpenAPI Docs

---
+++ app/Http/Controllers/OpenAPI/AccountSchema.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @OA\Schema(
+ *   schema="Account",
+ *   type="object",
+ *       @OA\Property(property="id", type="string", example="AS3df3A", description="The account hashed id"),
+ * )
+ */
---
+++ app/Http/Controllers/OpenAPI/CompanyTokenSchema.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @OA\Schema(
+ *   schema="CompanyToken",
+ *   type="object",
+ *       @OA\Property(property="name", type="string", example="Token Name", description="The token name"),
+ *       @OA\Property(property="token", type="string", example="AS3df3jUUH765fhfd9KJuidj3JShjA", description="The token value"),
+ * )
+ */
