27fca823ae6f1c90b1e3dbb3fe8c075d8168e127
David Bomba <turbo124@gmail.com>
2019-10-30 18:47:51 +1100

    Add users relation to company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -100,7 +100,9 @@ class Company extends BaseModel
 
     public function users()
     {
-        return $this->hasMany(CompanyUser::class);
+
+        return $this->hasManyThrough(User::class, CompanyUser::class, 'company_id', 'id', 'id', 'user_id');
+
     }
 
     /**
