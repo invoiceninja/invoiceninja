0415c20f3842a637a77171230db2d22862ac0913
David Bomba <turbo124@gmail.com>
2019-10-31 14:38:51 +1100

    Client Portal middleware

---
+++ app/Http/Middleware/ClientPortalEnabled.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2019. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+
+class ClientPortalEnabled
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if(auth()->user()->client->getSetting('enable_client_portal') == 'false')
+            return redirect()->to('client/dashboard');
+
+    }
+}
