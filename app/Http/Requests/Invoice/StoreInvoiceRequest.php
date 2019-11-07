d3bbc4bc6ad2734db2334915e68bf41eadceaabd
David Bomba <turbo124@gmail.com>
2019-11-08 08:30:37 +1100

    clean line items at the request layer

--- app/Http/Requests/Invoice/StoreInvoiceRequest.php
+++ app/Http/Requests/Invoice/StoreInvoiceRequest.php
@@ -14,11 +14,13 @@ namespace App\Http\Requests\Invoice;
 use App\Http\Requests\Request;
 use App\Models\ClientContact;
 use App\Models\Invoice;
+use App\Utils\Traits\CleanLineItems;
 use App\Utils\Traits\MakesHash;
 
 class StoreInvoiceRequest extends Request
 {
     use MakesHash;
+    use CleanLineItems;
 
     /**
      * Determine if the user is authorized to make this request.
@@ -34,6 +36,7 @@ class StoreInvoiceRequest extends Request
     public function rules()
     {
         $this->sanitize();
+
         return [
             'client_id' => 'required',
            // 'invoice_type_id' => 'integer',
@@ -46,11 +49,12 @@ class StoreInvoiceRequest extends Request
         $input = $this->all();
 
         $input['client_id'] = $this->decodePrimaryKey($input['client_id']);
-        $input['line_items'] = isset($input['line_items']) ? $input['line_items'] : [];
+        $input['line_items'] = isset($input['line_items']) ? $this->cleanItems($input['line_items']) : [];
         //$input['line_items'] = json_encode($input['line_items']);
         $this->replace($input);
 
         return $this->all();
     }
+
 }
 
---
+++ app/Utils/Traits/CleanLineItems.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2019. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Utils\Traits;
+
+use App\DataMapper\BaseSettings;
+use App\DataMapper\InvoiceItem;
+
+
+/**
+ * Class CleanLineItems.
+ */
+trait CleanLineItems
+{
+
+    public function cleanItems($items) :array
+    {
+
+        if(!isset($items))
+            return [];
+
+        $cleaned_items = [];
+
+        foreach($items as $item)
+            $cleaned_items[] = $this->cleanLineItem($item);
+
+        return $cleaned_items;
+
+    }
+
+    /**
+     * Sets default values for the line_items
+     * @return $this 
+     */
+    private function cleanLineItem($item)
+    {
+        $invoice_item = (object)get_class_vars(InvoiceItem::class);
+        unset($invoice_item->casts);
+
+        foreach($invoice_item as $key => $value)
+        {
+
+            if(!array_key_exists($key, $item) || !isset($item[$key])){
+                $item[$key] = $value;
+                $item[$key] = BaseSettings::castAttribute(InvoiceItem::$casts[$key], $value);
+            }         
+
+        }
+
+        if(array_key_exists("id", $item))
+        {
+            $item['id'] = $item['id']*-1;
+        }  
+
+        return $item;
+    }
+
+}
+
