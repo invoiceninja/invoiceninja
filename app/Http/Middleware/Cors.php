1ce62e93788bd82099ee5364957c640879f8ee35
David Bomba <turbo124@gmail.com>
2019-11-13 08:49:35 +1100

    Add additional headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -6,14 +6,14 @@ use Closure;
 
 class Cors
 {
-    
+
   public function handle($request, Closure $next)
   {
 
     return $next($request)
       ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, X-API-TOKEN, X-API-SECRET');
 
   }
 
