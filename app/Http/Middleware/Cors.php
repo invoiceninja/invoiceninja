6c089f994b3f29a401c07db4c5de815a88e12ad6
David Bomba <turbo124@gmail.com>
2019-11-13 08:32:28 +1100

    Add Cors functionality and Company Settings Saver (#3060)
    * Cleanup for settings remove from entities
    
    * Fixes for saving and transforming company settings
    
    * CompanySettingsSaver
    
    * Add Cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    
+  public function handle($request, Closure $next)
+  {
+
+    return $next($request)
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+
+  }
+
+}
