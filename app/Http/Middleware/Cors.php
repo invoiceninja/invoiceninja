12dc613b9bba582462451d663c562bb5be9b87ab
David Bomba <turbo124@gmail.com>
2019-11-13 08:54:06 +1100

    Remove headers (#3063)

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -13,7 +13,7 @@ class Cors
     return $next($request)
       ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, X-API-TOKEN, X-API-SECRET');
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, X-API-TOKEN, X-API-SECRET');
 
   }
 
