2d2d19f30b546a0d5408a1895afbd4760dfa5038
David Bomba <turbo124@gmail.com>
2019-11-14 11:54:56 +1100

    Middleware for setting db by email address

---
+++ app/Http/Middleware/SetEmailDb.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2019. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Http\Middleware;
+
+use App\Libraries\MultiDB;
+use App\Models\CompanyToken;
+use Closure;
+
+class SetEmailDb
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function handle($request, Closure $next)
+    {
+
+        $error = [
+            'message' => 'Email not set or not found',
+            'errors' => []
+        ];
+
+        if( $request->input('email') && config('ninja.db.multi_db_enabled')) 
+        {
+
+            if(! MultiDB::userFindAndSetDb($request->input('email')))
+            {
+
+                return response()->json($error, 403);
+
+            }
+        
+        }
+        else {
+
+
+                return response()->json($error, 403);
+            
+        }
+
+        return $next($request);
+    }
+
+
+}
