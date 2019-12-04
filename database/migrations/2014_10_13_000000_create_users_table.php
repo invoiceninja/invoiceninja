5ef1bf4282d74dc04461a95d12c966460e94290b
David Bomba <turbo124@gmail.com>
2019-12-04 15:37:42 +1100

    Add total_taxes to quotes and recurring_*

--- database/migrations/2014_10_13_000000_create_users_table.php
+++ database/migrations/2014_10_13_000000_create_users_table.php
@@ -511,6 +511,9 @@ class CreateUsersTable extends Migration
             $t->string('tax_name3')->nullable();
             $t->decimal('tax_rate3', 13, 3)->default(0);
 
+            $t->decimal('total_taxes', 13, 3)->default(0);
+
+
             $t->string('custom_value1')->nullable();
             $t->string('custom_value2')->nullable();
             $t->string('custom_value3')->nullable();
@@ -572,6 +575,9 @@ class CreateUsersTable extends Migration
 
             $t->string('tax_name3')->nullable();
             $t->decimal('tax_rate3', 13, 3)->default(0);
+
+            $t->decimal('total_taxes', 13, 3)->default(0);
+
 
             $t->string('custom_value1')->nullable();
             $t->string('custom_value2')->nullable();
@@ -635,6 +641,8 @@ class CreateUsersTable extends Migration
 
             $t->string('tax_name3')->nullable();
             $t->decimal('tax_rate3', 13, 3)->default(0);
+
+            $t->decimal('total_taxes', 13, 3)->default(0);
 
             $t->boolean('uses_inclusive_taxes')->default(0);
             
