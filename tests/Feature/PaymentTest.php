06d0e9738c33122b81d1a2c24378d96c1674610d
David Bomba <turbo124@gmail.com>
2019-12-05 14:01:55 +1100

    payment tests

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -331,10 +331,123 @@ class PaymentTest extends TestCase
     public function testPaymentGreaterThanPartial()
     {
 
+
+        $this->invoice = null;
+
+        $client = ClientFactory::create($this->company->id, $this->user->id);
+        $client->save();
+
+        $this->invoice = InvoiceFactory::create($this->company->id,$this->user->id);//stub the company and user_id
+        $this->invoice->client_id = $client->id;
+
+        $this->invoice->partial = 5.0;
+        $this->invoice->line_items = $this->buildLineItems();
+        $this->invoice->uses_inclusive_taxes = false;
+
+        $this->invoice->save();
+
+        $this->invoice_calc = new InvoiceSum($this->invoice);
+        $this->invoice_calc->build();
+
+        $this->invoice = $this->invoice_calc->getInvoice();
+        $this->invoice->save();
+        $this->invoice->markSent();
+        $this->invoice->save();
+
+
+        $data = [
+            'amount' => 6.0,
+            'client_id' => $client->hashed_id,
+            'invoices' => [
+                [
+                'id' => $this->invoice->hashed_id,
+                'amount' => 6.0
+                ],
+            ],
+            'payment_date' => '2019/12/12',
+        ];
+
+            $response = $this->withHeaders([
+                'X-API-SECRET' => config('ninja.api_secret'),
+                'X-API-TOKEN' => $this->token,
+            ])->post('/api/v1/payments?include=invoices', $data);
+
+            $arr = $response->json();
+            $response->assertStatus(200);
+
+            $payment_id = $arr['data']['id'];
+
+            $payment = Payment::find($this->decodePrimaryKey($payment_id))->first();
+
+            $this->assertNotNull($payment);
+            $this->assertNotNull($payment->invoices());
+            $this->assertEquals(1, $payment->invoices()->count());
+
+            $invoice = $payment->invoices()->first();
+
+            $this->assertEquals($invoice->partial, 0);
+            $this->assertEquals($invoice->balance, 4);
+
     }
 
     public function testPaymentLessThanPartialAmount()
     {
+
+        $this->invoice = null;
+
+        $client = ClientFactory::create($this->company->id, $this->user->id);
+        $client->save();
+
+        $this->invoice = InvoiceFactory::create($this->company->id,$this->user->id);//stub the company and user_id
+        $this->invoice->client_id = $client->id;
+
+        $this->invoice->partial = 5.0;
+        $this->invoice->line_items = $this->buildLineItems();
+        $this->invoice->uses_inclusive_taxes = false;
+
+        $this->invoice->save();
+
+        $this->invoice_calc = new InvoiceSum($this->invoice);
+        $this->invoice_calc->build();
+
+        $this->invoice = $this->invoice_calc->getInvoice();
+        $this->invoice->save();
+        $this->invoice->markSent();
+        $this->invoice->save();
+
+
+        $data = [
+            'amount' => 2.0,
+            'client_id' => $client->hashed_id,
+            'invoices' => [
+                [
+                'id' => $this->invoice->hashed_id,
+                'amount' => 2.0
+                ],
+            ],
+            'payment_date' => '2019/12/12',
+        ];
+
+            $response = $this->withHeaders([
+                'X-API-SECRET' => config('ninja.api_secret'),
+                'X-API-TOKEN' => $this->token,
+            ])->post('/api/v1/payments?include=invoices', $data);
+
+            $arr = $response->json();
+            $response->assertStatus(200);
+
+            $payment_id = $arr['data']['id'];
+
+            $payment = Payment::find($this->decodePrimaryKey($payment_id))->first();
+
+            $this->assertNotNull($payment);
+            $this->assertNotNull($payment->invoices());
+            $this->assertEquals(1, $payment->invoices()->count());
+
+            $invoice = $payment->invoices()->first();
+
+            $this->assertEquals($invoice->partial, 3);
+            $this->assertEquals($invoice->balance, 8);
 
     }
 }
