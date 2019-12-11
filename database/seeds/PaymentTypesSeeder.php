5bfe3a774c3d293513f9ce99c4ed32cd6d9d264f
David Bomba <turbo124@gmail.com>
2019-12-11 17:40:24 +1100

    Fixes for removing the global constants file

--- database/seeds/PaymentTypesSeeder.php
+++ database/seeds/PaymentTypesSeeder.php
@@ -5,43 +5,59 @@ use Illuminate\Database\Seeder;
 
 class PaymentTypesSeeder extends Seeder
 {
+
+    const BANK_LIBRARY_OFX = 1;
+    const GATEWAY_TYPE_CREDIT_CARD = 1;
+    const GATEWAY_TYPE_BANK_TRANSFER = 2;
+    const GATEWAY_TYPE_PAYPAL = 3;
+    const GATEWAY_TYPE_CRYPTO = 4;
+    const GATEWAY_TYPE_DWOLLA = 5;
+    const GATEWAY_TYPE_CUSTOM1 = 6;
+    const GATEWAY_TYPE_ALIPAY = 7;
+    const GATEWAY_TYPE_SOFORT = 8;
+    const GATEWAY_TYPE_SEPA = 9;
+    const GATEWAY_TYPE_GOCARDLESS = 10;
+    const GATEWAY_TYPE_APPLE_PAY = 11;
+    const GATEWAY_TYPE_CUSTOM2 = 12;
+    const GATEWAY_TYPE_CUSTOM3 = 13;
+
     public function run()
     {
         Eloquent::unguard();
 
         $paymentTypes = [
             ['name' => 'Apply Credit'],
-            ['name' => 'Bank Transfer', 'gateway_type_id' => GATEWAY_TYPE_BANK_TRANSFER],
+            ['name' => 'Bank Transfer', 'gateway_type_id' => self::GATEWAY_TYPE_BANK_TRANSFER],
             ['name' => 'Cash'],
-            ['name' => 'Debit', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'ACH', 'gateway_type_id' => GATEWAY_TYPE_BANK_TRANSFER],
-            ['name' => 'Visa Card', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'MasterCard', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'American Express', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Discover Card', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Diners Card', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'EuroCard', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Nova', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Credit Card Other', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'PayPal', 'gateway_type_id' => GATEWAY_TYPE_PAYPAL],
+            ['name' => 'Debit', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'ACH', 'gateway_type_id' => self::GATEWAY_TYPE_BANK_TRANSFER],
+            ['name' => 'Visa Card', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'MasterCard', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'American Express', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Discover Card', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Diners Card', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'EuroCard', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Nova', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Credit Card Other', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'PayPal', 'gateway_type_id' => self::GATEWAY_TYPE_PAYPAL],
             ['name' => 'Google Wallet'],
             ['name' => 'Check'],
-            ['name' => 'Carte Blanche', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'UnionPay', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'JCB', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Laser', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Maestro', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Solo', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Switch', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'iZettle', 'gateway_type_id' => GATEWAY_TYPE_CREDIT_CARD],
-            ['name' => 'Swish', 'gateway_type_id' => GATEWAY_TYPE_BANK_TRANSFER],
+            ['name' => 'Carte Blanche', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'UnionPay', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'JCB', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Laser', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Maestro', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Solo', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Switch', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'iZettle', 'gateway_type_id' => self::GATEWAY_TYPE_CREDIT_CARD],
+            ['name' => 'Swish', 'gateway_type_id' => self::GATEWAY_TYPE_BANK_TRANSFER],
             ['name' => 'Venmo'],
             ['name' => 'Money Order'],
-            ['name' => 'Alipay', 'gateway_type_id' => GATEWAY_TYPE_ALIPAY],
-            ['name' => 'Sofort', 'gateway_type_id' => GATEWAY_TYPE_SOFORT],
-            ['name' => 'SEPA', 'gateway_type_id' => GATEWAY_TYPE_SEPA],
-            ['name' => 'GoCardless', 'gateway_type_id' => GATEWAY_TYPE_GOCARDLESS],
-            ['name' => 'Crypto', 'gateway_type_id' => GATEWAY_TYPE_CRYPTO],
+            ['name' => 'Alipay', 'gateway_type_id' => self::GATEWAY_TYPE_ALIPAY],
+            ['name' => 'Sofort', 'gateway_type_id' => self::GATEWAY_TYPE_SOFORT],
+            ['name' => 'SEPA', 'gateway_type_id' => self::GATEWAY_TYPE_SEPA],
+            ['name' => 'GoCardless', 'gateway_type_id' => self::GATEWAY_TYPE_GOCARDLESS],
+            ['name' => 'Crypto', 'gateway_type_id' => self::GATEWAY_TYPE_CRYPTO],
         ];
 
         foreach ($paymentTypes as $paymentType) {
