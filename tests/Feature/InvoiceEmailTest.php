fd82b1a9aa9c2a8aaf82f231873c5c2168d87e66
David Bomba <turbo124@gmail.com>
2019-12-13 14:22:11 +1100

    TDD for emails

--- tests/Feature/InvoiceEmailTest.php
+++ tests/Feature/InvoiceEmailTest.php
@@ -73,10 +73,28 @@ class InvoiceEmailTest extends TestCase
             $reminder_template = $this->calculateTemplate($invoice);
 
         //Need to determine which email template we are producing
-        $message = $this->generateTemplateData($invoice)
+        $email_data = $this->generateTemplateData($invoice, $reminder_template)
 
-        // $subject = 
-        // $body =
+    }
+
+    private function generateTemplateData(Invoice $invoice, string $reminder_template) :array
+    {
+        $data = [];
+
+        $client = $invoice->client;
+
+        $body_template = $client->getSetting('email_template_'.$reminder_template);
+        $subject_template = $client->getSetting('email_subject_'.$reminder_template);
+
+        $data['message'] = $this->parseTemplate($invoice, $body_template);
+        $data['subject'] = $this->parseTemplate($invoice, $subject_template);
+
+        return $data;
+    }
+
+    private function parseTemplate($invoice, $template_data) :string
+    {
+
     }
 
     private function calculateTemplate(Invoice $invoice) :string
@@ -89,10 +107,38 @@ class InvoiceEmailTest extends TestCase
         {
             return 'invoice';
         }
-        else if($client->getSetting('enable_reminder1') !== false)
+        else if($client->getSetting('enable_reminder1') !== false && $this->inReminderWindow($invoice, $client->getSetting('schedule_reminder1'), $client->getSetting('num_days_reminder1')))
         {
-
+            return 'template1';
         }
-        
+        else if($client->getSetting('enable_reminder2') !== false && $this->inReminderWindow($invoice, $client->getSetting('schedule_reminder2'), $client->getSetting('num_days_reminder2')))
+        {
+            return 'template2';
+        }
+        else if($client->getSetting('enable_reminder3') !== false && $this->inReminderWindow($invoice, $client->getSetting('schedule_reminder3'), $client->getSetting('num_days_reminder3')))
+        {
+            return 'template3';
+        }
+        //also implement endless reminders here
+        //
+           
+    }
+
+    private function inReminderWindow($invoice, $schedule_reminder, $num_days_reminder)
+    {
+        switch ($schedule_reminder) {
+            case 'after_invoice_date':
+                return Carbon::parse($invoice->date)->addDays($num_days_reminder)->startOfDay()->eq(Carbon::now()->startOfDay());
+                break;
+            case 'before_due_date':
+                return Carbon::parse($invoice->due_date)->subDays($num_days_reminder)->startOfDay()->eq(Carbon::now()->startOfDay());
+                break;
+            case 'after_due_date':
+                return Carbon::parse($invoice->due_date)->addDays($num_days_reminder)->startOfDay()->eq(Carbon::now()->startOfDay());
+                break;
+            default:
+                # code...
+                break;
+        }
     }
 }
