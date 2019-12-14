112115d72d40fe4969b5ba6852731f5f03ad0540
David Bomba <turbo124@gmail.com>
2019-12-14 23:34:13 +1100

    Refactor company properties to be presented from settings object instead of company properties

--- app/Models/Presenters/CompanyPresenter.php
+++ app/Models/Presenters/CompanyPresenter.php
@@ -11,6 +11,8 @@
 
 namespace App\Models\Presenters;
 
+use App\Models\Country;
+
 /**
  * Class CompanyPresenter
  * @package App\Models\Presenters
@@ -26,47 +28,47 @@ class CompanyPresenter extends EntityPresenter
         return $this->entity->name ?: ctrans('texts.untitled_account');
     }
 
-    public function logo()
+    public function logo($settings)
     {
-        return strlen($this->entity->getLogo() > 0) ? $this->entity->getLogo() : 'https://www.invoiceninja.com/wp-content/uploads/2019/01/InvoiceNinja-Logo-Round-300x300.png';
+        return strlen($settings->company_logo > 0) ? $settings->company_logo : 'https://www.invoiceninja.com/wp-content/uploads/2019/01/InvoiceNinja-Logo-Round-300x300.png';
     }
 
-    public function address()
+    public function address($settings)
     {
         $str = '';
         $company = $this->entity;
 
-        if ($address1 = $company->settings->address1) {
+        if ($address1 = $settings->address1) {
             $str .= e($address1) . '<br/>';
         }
-        if ($address2 = $company->settings->address2) {
+        if ($address2 = $settings->address2) {
             $str .= e($address2) . '<br/>';
         }
-        if ($cityState = $this->getCompanyCityState()) {
+        if ($cityState = $this->getCompanyCityState($settings)) {
             $str .= e($cityState) . '<br/>';
         }
-        if ($country = $company->country()) {
+        if ($country = Country::find($settings->country_id)->first()) {
             $str .= e($country->name) . '<br/>';
         }
-        if ($company->settings->phone) {
-            $str .= ctrans('texts.work_phone') . ": ". e($company->settings->phone) .'<br/>';
+        if ($settings->phone) {
+            $str .= ctrans('texts.work_phone') . ": ". e($settings->phone) .'<br/>';
         }
-        if ($company->settings->email) {
-            $str .= ctrans('texts.work_email') . ": ". e($company->settings->email) .'<br/>';
+        if ($settings->email) {
+            $str .= ctrans('texts.work_email') . ": ". e($settings->email) .'<br/>';
         }
 
         return $str;
     }
 
-    public function getCompanyCityState()
+    public function getCompanyCityState($settings)
     {
-        $company = $this->entity;
+        $country = Country::find($settings->country_id)->first();
 
-        $swap = $company->country() && $company->country()->swap_postal_code;
+        $swap = $country && $country->swap_postal_code;
 
-        $city = e($company->settings->city);
-        $state = e($company->settings->state);
-        $postalCode = e($company->settings->postal_code);
+        $city = e($settings->city);
+        $state = e($settings->state);
+        $postalCode = e($settings->postal_code);
 
         if ($city || $state || $postalCode) {
             return $this->cityStateZip($city, $state, $postalCode, $swap);
