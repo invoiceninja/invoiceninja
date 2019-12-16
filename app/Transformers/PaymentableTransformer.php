7e54450b10635cb3f4b5576f13a3156132874b80
David Bomba <turbo124@gmail.com>
2019-12-16 21:42:22 +1100

    expose paymentables to API

---
+++ app/Transformers/PaymentableTransformer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2019. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Transformers;
+
+use App\Models\Payment;
+use App\Models\Paymentable;
+use App\Utils\Traits\MakesHash;
+
+class PaymentableTransformer extends EntityTransformer
+{
+    use MakesHash;
+
+    protected $serializer;
+
+    protected $defaultIncludes = [];
+
+    protected $availableIncludes = [];
+
+    public function __construct($serializer = null)
+    {
+
+        $this->serializer = $serializer;
+
+    }
+
+    public function transform(Paymentable $paymentable)
+    {
+        return  [
+            'id' => $this->encodePrimaryKey($paymentable->id),
+            'invoice_id' => $this->encodePrimaryKey($paymentable->paymentable_id),
+            'amount' => $paymentable->amount,
+        ];
+    }
+}
