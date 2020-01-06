1bb956fe43e1617b8bcb5bae351b87c74be9ddda
Benjamin Beganović <k1pstabug@gmail.com>
2020-01-06 10:11:18 +0100

    - Basic controller scaffold - Intro step - Downloading data

--- app/Http/Controllers/Migration/StepsController.php
+++ app/Http/Controllers/Migration/StepsController.php
@@ -2,14 +2,69 @@
 
 namespace App\Http\Controllers\Migration;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\BaseController;
+use App\Ninja\Serializers\ArraySerializer;
+use App\Ninja\Transformers\AccountTransformer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Item;
 
 class StepsController extends BaseController
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function start()
     {
-        return view('migration.start'); 
+        return view('migration.start');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function download()
+    {
+        return view('migration.download'); 
+    }
+
+    /**
+     * Handle data downloading for the migration.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleDownload(Request $request)
+    {
+        $date = date('Y-m-d');
+
+        $output = fopen('php://output', 'w') or Utils::fatalError();
+
+        $fileName = "{$date}-invoiceninja";
+
+        header('Content-Type:application/json');
+        header("Content-Disposition:attachment;filename={$fileName}.json");
+
+        $manager = new Manager();
+        $manager->setSerializer(new ArraySerializer());
+
+        // eager load data, include archived but exclude deleted
+        $account = Auth::user()->account;
+        $account->load(['clients' => function ($query) {
+            $query->withArchived()
+                ->with(['contacts', 'invoices' => function ($query) {
+                    $query->withArchived()
+                        ->with(['invoice_items', 'payments' => function ($query) {
+                            $query->withArchived();
+                        }]);
+                }]);
+        }]);
+
+        $resource = new Item($account, new AccountTransformer());
+        $data = $manager->parseIncludes('clients.invoices.payments')
+            ->createData($resource)
+            ->toArray();
+
+        return response()->json($data);
     }
 }
---
+++ resources/views/migration/download.blade.php
@@ -0,0 +1,8 @@
+<h1>Download your data</h1>
+
+<form action="/migration/download" method="post">
+    {!! csrf_field() !!}
+    <button>Download</button>
+</form>
+
+<a href="/migration/import">Next step</a>
