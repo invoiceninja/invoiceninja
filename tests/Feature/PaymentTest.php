5f99d963905be93248ffd27015db6defc86a98ec
David Bomba <turbo124@gmail.com>
2020-01-19 12:59:43 +1100

    Tests for payments

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -1004,7 +1004,7 @@ class PaymentTest extends TestCase
             $payment_id = $arr['data']['id'];
 
             $this->assertEquals($this->invoice->amount, $arr['data']['amount']);
-            
+
             $payment = Payment::find($this->decodePrimaryKey($payment_id))->first();
 
             $this->assertNotNull($payment);
@@ -1015,6 +1015,55 @@ class PaymentTest extends TestCase
     }
 
 
+    public function testStorePaymentWithZeroAmountField()
+    {
+        $client = ClientFactory::create($this->company->id, $this->user->id);
+        $client->save();
 
+        $this->invoice = InvoiceFactory::create($this->company->id,$this->user->id);//stub the company and user_id
+        $this->invoice->client_id = $client->id;
+        $this->invoice->status_id = Invoice::STATUS_SENT;
+
+        $this->invoice->line_items = $this->buildLineItems();
+        $this->invoice->uses_inclusive_Taxes = false;
+
+        $this->invoice->save();
+
+        $this->invoice_calc = new InvoiceSum($this->invoice);
+        $this->invoice_calc->build();
+
+        $this->invoice = $this->invoice_calc->getInvoice();
+        $this->invoice->save();
+
+        $data = [
+            'amount' => 0,
+            'client_id' => $client->hashed_id,
+            'invoices' => [
+                [
+                'invoice_id' => $this->invoice->hashed_id,
+                'amount' => $this->invoice->amount
+                ],
+            ],
+            'date' => '2020/12/12',
+
+        ];
+
+        try {
+            $response = $this->withHeaders([
+                'X-API-SECRET' => config('ninja.api_secret'),
+                'X-API-TOKEN' => $this->token,
+            ])->post('/api/v1/payments?include=invoices', $data);
+
+        }
+        catch(ValidationException $e) {
+           // \Log::error('in the validator');
+            $message = json_decode($e->validator->getMessageBag(),1);
+           // \Log::error($message);
+            $this->assertNotNull($message);
+
+        }
+        
+
+    }
 
 }
