4b3894e355b943d4ea3185ad018e62820ae01275
Benjamin Beganovic <k1pstabug@gmail.com>
2020-01-20 18:39:56 +0100

    Import.php tests: - Basic test scaffold - Test if exception is thrown when unknown resource - Company update test

--- tests/Unit/Migration/ImportTest.php
+++ tests/Unit/Migration/ImportTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Migration;
 
+use App\Exceptions\ResourceNotAvailableForMigration;
+use App\Jobs\Util\Import;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\MockAccountData;
 use Tests\TestCase;
@@ -14,12 +16,42 @@ class ImportTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->makeTestData();
     }
 
-    public function testImportClassExists(): void
+    public function testImportClassExists()
     {
         $status = class_exists('App\Jobs\Util\Import');
 
         $this->assertTrue($status);
+    }
+
+    /**
+     * Ensure exception is thrown when resource
+     * is not available for the migration.
+     */
+    public function testExceptionOnUnavailableResource()
+    {
+        $data['panda_bears'] = [
+            'name' => 'Awesome Panda Bear',
+        ];
+
+        Import::dispatchNow($data, $this->company, $this->user);
+
+        $this->expectException(ResourceNotAvailableForMigration::class);
+    }
+
+    public function testCompanyUpdating()
+    {
+        $original_key = $this->company->company_key;
+
+        $data['company'] = [
+            'company_key' => 'my_awesome_secret_key',
+        ];
+
+        Import::dispatchNow($data, $this->company, $this->user);
+
+        $this->assertNotEquals($this->company->company_key, $original_key);
     }
 }
