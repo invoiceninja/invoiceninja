69fa60b91c82dff92f12c820afea6254409997aa
Benjamin Beganović <k1pstabug@gmail.com>
2020-01-29 03:31:34 +0100

    Added generateUrl method for getting assets (#3258)
    * Added generateUrl method for getting assets
    
    * Fix return types

---
+++ app/Helpers/GenerateUrl.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Document;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2020. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+ /**
+  * Generate url for the asset.
+  *
+  * @param Document $document
+  * @param boolean $absolute
+  * @return string|null
+  */
+function generateUrl(Document $document, $absolute = false)
+{
+    $url = Storage::disk($document->disk)->url($document->path);
+
+    if($url && $absolute) return url($url);
+
+    if ($url) return $url;
+
+    return null;
+}
