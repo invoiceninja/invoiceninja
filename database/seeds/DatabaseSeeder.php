d72f38c4205da8852b042f8052691cfc36ece13f
David Bomba <turbo124@gmail.com>
2020-02-11 21:24:04 +1100

    Add Design Model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
         $this->call('PaymentTypesSeeder');
         $this->call('GatewayTypesSeeder');
         $this->call('DateFormatsSeeder');
+        $this->call('DesignSeeder');
 
     }
 }
