741a1b33337a33ea962a81dca75fdee6caf7927e
David Bomba <turbo124@gmail.com>
2020-02-13 19:25:35 +1100

    Restart queue after self-update

--- app/Console/Commands/ArtisanUpgrade.php
+++ app/Console/Commands/ArtisanUpgrade.php
@@ -59,5 +59,13 @@ class ArtisanUpgrade extends Command
             \Log::error("I wasn't able to optimize.");
         }
 
+        try {
+        
+            Artisan::call('queue:restart');
+        
+        }catch(Exception $e) {
+
+            \Log::error("I wasn't able to restart the queue");
+        }
     }
 }
