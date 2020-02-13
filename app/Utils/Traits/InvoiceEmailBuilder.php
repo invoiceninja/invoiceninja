3635548e91a5ce91c785610c81e64db39de8d013
David Bomba <turbo124@gmail.com>
2020-02-13 15:27:02 +1100

    Email invoices

--- app/Utils/Traits/InvoiceEmailBuilder.php
+++ app/Utils/Traits/InvoiceEmailBuilder.php
@@ -66,9 +66,9 @@ trait InvoiceEmailBuilder
             }
         }
 
-        $data['body'] = $this->parseTemplate($body_template, false, $contact);
-        
-        $data['subject'] = $this->parseTemplate($subject_template, true, $contact);
+        $data['body'] = $this->parseTemplate($body_template, true, $contact);
+
+        $data['subject'] = $this->parseTemplate($subject_template, false, $contact);
 
         if ($client->getSetting('pdf_email_attachment') !== false) {
             $data['files'][] = $this->pdf_file_path();
@@ -86,11 +86,10 @@ trait InvoiceEmailBuilder
 
         //process markdown
         if ($is_markdown) {
-            //$data = Parsedown::instance()->line($data);
 
             $converter = new CommonMarkConverter([
-                'html_input' => 'strip',
-                'allow_unsafe_links' => false,
+                'html_input' => 'allow',
+                'allow_unsafe_links' => true,
             ]);
 
             $data = $converter->convertToHtml($data);
--- resources/views/email/template/plain.blade.php
+++ resources/views/email/template/plain.blade.php
@@ -1,4 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ App::getLocale() }}">
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+
 {{ $body }}
 
 
 {{ $footer }}
+</body>
+</html>
