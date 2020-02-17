ce2e074c46829b73c597461209aaf6076f9a4f73
Benjamin Beganovic <k1pstabug@gmail.com>
2020-02-17 14:15:26 +0100

    E-mail template

---
+++ resources/views/email/example.blade.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <style>
+        /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */
+        html {
+            line-height: 1.15;
+            -webkit-text-size-adjust: 100%
+        }
+
+        body {
+            margin: 0
+        }
+
+        a {
+            background-color: transparent
+        }
+
+        button {
+            font-size: 100%;
+            line-height: 1.15;
+            margin: 0;
+            overflow: visible;
+            text-transform: none
+        }
+
+        [type=button],
+        [type=reset],
+        [type=submit],
+        button {
+            -webkit-appearance: button
+        }
+
+        [type=button]::-moz-focus-inner,
+        [type=reset]::-moz-focus-inner,
+        [type=submit]::-moz-focus-inner,
+        button::-moz-focus-inner {
+            border-style: none;
+            padding: 0
+        }
+
+        [type=button]:-moz-focusring,
+        [type=reset]:-moz-focusring,
+        [type=submit]:-moz-focusring,
+        button:-moz-focusring {
+            outline: 1px dotted ButtonText
+        }
+
+        [type=checkbox],
+        [type=radio] {
+            box-sizing: border-box;
+            padding: 0
+        }
+
+        [type=number]::-webkit-inner-spin-button,
+        [type=number]::-webkit-outer-spin-button {
+            height: auto
+        }
+
+        [type=search] {
+            -webkit-appearance: textfield;
+            outline-offset: -2px
+        }
+
+        [type=search]::-webkit-search-decoration {
+            -webkit-appearance: none
+        }
+
+        ::-webkit-file-upload-button {
+            -webkit-appearance: button;
+            font: inherit
+        }
+
+        [hidden] {
+            display: none
+        }
+
+        html {
+            box-sizing: border-box;
+            font-family: sans-serif
+        }
+
+        *,
+        :after,
+        :before {
+            box-sizing: inherit
+        }
+
+        h3,
+        p {
+            margin: 0
+        }
+
+        button {
+            background: transparent
+        }
+
+        button:focus {
+            outline: 1px dotted;
+            outline: 5px auto -webkit-focus-ring-color
+        }
+
+        html {
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+            line-height: 1.5
+        }
+
+        *,
+        :after,
+        :before {
+            border: 0 solid #e2e8f0
+        }
+
+        [role=button],
+        button {
+            cursor: pointer
+        }
+
+        h3 {
+            font-size: inherit;
+            font-weight: inherit
+        }
+
+        a {
+            text-decoration: inherit
+        }
+
+        a,
+        button {
+            color: inherit
+        }
+
+        button {
+            padding: 0;
+            line-height: inherit
+        }
+
+        .bg-white {
+            background-color: #fff
+        }
+
+        .bg-gray-200 {
+            background-color: #edf2f7
+        }
+
+        .bg-gray-900 {
+            background-color: #1a202c
+        }
+
+        .bg-blue-600 {
+            background-color: #3182ce
+        }
+
+        .hover\:bg-blue-700:hover {
+            background-color: #2b6cb0
+        }
+
+        .border-b {
+            border-bottom-width: 1px
+        }
+
+        .flex {
+            display: -webkit-box;
+            display: flex
+        }
+
+        .flex-col {
+            -webkit-box-orient: vertical;
+            -webkit-box-direction: normal;
+            flex-direction: column
+        }
+
+        .items-center {
+            -webkit-box-align: center;
+            align-items: center
+        }
+
+        .justify-center {
+            -webkit-box-pack: center;
+            justify-content: center
+        }
+
+        .font-sans {
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji
+        }
+
+        .font-semibold {
+            font-weight: 600
+        }
+
+        .h-64 {
+            height: 16rem
+        }
+
+        .leading-normal {
+            line-height: 1.5
+        }
+
+        .leading-loose {
+            line-height: 2
+        }
+
+        .my-4 {
+            margin-top: 1rem;
+            margin-bottom: 1rem
+        }
+
+        .mx-auto {
+            margin-left: auto;
+            margin-right: auto
+        }
+
+        .-mx-8 {
+            margin-left: -2rem;
+            margin-right: -2rem
+        }
+
+        .mt-4 {
+            margin-top: 1rem
+        }
+
+        .mt-8 {
+            margin-top: 2rem
+        }
+
+        .-mt-8 {
+            margin-top: -2rem
+        }
+
+        .max-w-md {
+            max-width: 28rem
+        }
+
+        .min-h-screen {
+            min-height: 100vh
+        }
+
+        .p-8 {
+            padding: 2rem
+        }
+
+        .p-px {
+            padding: 1px
+        }
+
+        .py-2 {
+            padding-top: .5rem;
+            padding-bottom: .5rem
+        }
+
+        .px-3 {
+            padding-left: .75rem;
+            padding-right: .75rem
+        }
+
+        .px-4 {
+            padding-left: 1rem;
+            padding-right: 1rem
+        }
+
+        .py-10 {
+            padding-top: 2.5rem;
+            padding-bottom: 2.5rem
+        }
+
+        .pb-4 {
+            padding-bottom: 1rem
+        }
+
+        .pt-8 {
+            padding-top: 2rem
+        }
+
+        .shadow {
+            box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)
+        }
+
+        .text-center {
+            text-align: center
+        }
+
+        .text-white {
+            color: #fff
+        }
+
+        .text-gray-600 {
+            color: #718096
+        }
+
+        .text-blue-400 {
+            color: #63b3ed
+        }
+
+        .text-blue-600 {
+            color: #3182ce
+        }
+
+        .hover\:text-gray-700:hover {
+            color: #4a5568
+        }
+
+        .text-sm {
+            font-size: .875rem
+        }
+
+        .text-base {
+            font-size: 1rem
+        }
+
+        .text-2xl {
+            font-size: 1.5rem
+        }
+
+        .uppercase {
+            text-transform: uppercase
+        }
+
+        .tracking-wide {
+            letter-spacing: .025em
+        }
+
+        @media (min-width:640px) {
+            .sm\:text-lg {
+                font-size: 1.125rem
+            }
+        }
+    </style>
+</head>
+
+<body class="bg-gray-200">
+    <div class="bg-blue-600 p-px"></div>
+
+    <div class="font-sans min-w-screen min-h-screen bg-grey-100 py-10 px-4">
+
+        <div class="max-w-md mx-auto">
+
+            <div class="bg-white p-8 shadow">
+
+                <div class="h-64 flex flex-col items-center justify-center text-center tracking-wide leading-normal bg-gray-900 -mx-8 -mt-8">
+                    <p class="text-white text-2xl">Migration completed!</p>
+                    <p class="text-white font-semibold text-sm uppercase text-blue-400">Invoice Ninja / Company name</p>
+                </div>
+
+                <div class="pt-8 pb-4 border-b">
+                    <p id="start-greeting">Hello David,</p>
+                    <p class="my-4">We want to inform you that process for the migration on our new platform has been completed.</p>
+                    <div id="centered" class="flex flex-col items-center">
+                        <button class="bg-blue-600 hover:bg-blue-700 py-2 px-3 text-white my-4 font-semibold">Visit the portal</button>
+                    </div>
+                    <p id="end-greeting" class="mt-4">
+                        Best of luck!
+                    </p>
+                </div>
+
+                <div class="mt-8 text-center">
+                    <h3 class="text-base sm:text-lg">&copy; Invoice Ninja, 2020.</h3>
+                    <a href="#" class="text-blue-600 font-semibold">https://invoiceninja.com</a>
+                </div>
+
+            </div>
+
+            <div class="text-center text-sm mt-8">
+                <div class="text-gray-600 hover:text-gray-700">
+                    <p class="leading-loose">
+                        Hate these e-mails? <a href="#">Unsubscribe.</a>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
