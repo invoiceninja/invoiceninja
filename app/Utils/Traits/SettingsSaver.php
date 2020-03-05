1481ae670848ffdc624776c43214db4cec0cfa6e
David Bomba <turbo124@gmail.com>
2020-03-05 19:05:12 +1100

    Fix for pdf_variables

--- app/Utils/Traits/SettingsSaver.php
+++ app/Utils/Traits/SettingsSaver.php
@@ -66,6 +66,9 @@ trait SettingsSaver
         $settings = (object)$settings;
         $casts = CompanySettings::$casts;
 
+        if(property_exists($settings, 'pdf_variables'))
+            unset($settings->pdf_variables);
+        
         ksort($casts);
 
         foreach ($casts as $key => $value) {
