c9f8cdf58b26000cef14e9f0fc0a3ac13aa4819b
David Bomba <turbo124@gmail.com>
2020-03-08 09:14:46 +1100

    Remove password protection from check version route

--- routes/api.php
+++ routes/api.php
@@ -121,7 +121,7 @@ Route::group(['middleware' => ['api_db', 'token_auth', 'locale'], 'prefix' => 'a
 
 		Route::post('self-update', 'SelfUpdateController@update')->middleware('password_protected');
 
-		Route::post('self-update/check_version', 'SelfUpdateController@checkVersion')->middleware('password_protected');
+		Route::post('self-update/check_version', 'SelfUpdateController@checkVersion');
 
 		/*
 		Route::resource('tasks', 'TaskController'); // name = (tasks. index / create / show / update / destroy / edit
