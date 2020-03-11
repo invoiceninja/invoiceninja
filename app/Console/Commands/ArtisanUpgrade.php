a8454d46ccc1c33e318f6d1066fc8b0e41402a37
David Bomba <turbo124@gmail.com>
2020-03-11 11:32:13 +1100

    Make composer a dependency so we can support self updating!

--- app/Console/Commands/ArtisanUpgrade.php
+++ app/Console/Commands/ArtisanUpgrade.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Composer\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class ArtisanUpgrade extends Command
 {
@@ -39,7 +41,16 @@ class ArtisanUpgrade extends Command
     public function handle()
     {
 
-        exec("composer install");
+        // Composer\Factory::getHomeDir() method 
+        // needs COMPOSER_HOME environment variable set
+        putenv('COMPOSER_HOME=' . __DIR__ . '/vendor/bin/composer');
+
+        // call `composer install` command programmatically
+        $input = new ArrayInput(array('command' => 'install'));
+        $application = new Application();
+        $application->setAutoExit(false); // prevent `$application->run` method from exitting the script
+        $application->run($input);
+
 
         try {
         
