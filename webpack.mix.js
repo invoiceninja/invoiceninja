2f85e5a6add11eede0c3211677109ebd9e39a0c7
Benjamin Beganović <k1pstabug@gmail.com>
2020-03-13 20:12:42 +0100

    Fix Codacy

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
 
 /*
  |--------------------------------------------------------------------------
@@ -12,11 +12,11 @@ const tailwindcss = require('tailwindcss');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+        postCss: [tailwindcss("./tailwind.config.js")]
     });
 
 mix.version();
