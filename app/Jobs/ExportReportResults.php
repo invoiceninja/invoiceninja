b94016264537725c3fbb1f032b1bc607ae46d718
Kristián Feldsam <feldsam@gmail.com>
2020-03-17 11:44:57 +0100

    Fix ExportReportResults
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- app/Jobs/ExportReportResults.php
+++ app/Jobs/ExportReportResults.php
@@ -65,7 +65,17 @@ class ExportReportResults extends Job
         foreach ($totals as $currencyId => $each) {
             foreach ($each as $dimension => $val) {
                 $tmp   = [];
-                $tmp[] = Utils::getFromCache($currencyId, 'currencies')->name . (($dimension) ? ' - ' . $dimension : '');
+
+                $currency = Utils::getFromCache($currencyId, 'currencies');
+                if (!$currency) {
+                    $name = $currencyId;
+                    $account = $this->user->account->first();
+                    $currencyId = $account->currency_id;
+                } else {
+                    $name = $currency->name;
+                }
+
+                $tmp[] = $dimension ? $name . ' - ' . $dimension : $name;
                 foreach ($val as $field => $value) {
                     if ($field == 'duration') {
                         $tmp[] = Utils::formatTime($value);
