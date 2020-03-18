115281be870437ea0d1a53c70cbad2f332352fa0
hillelcoren <hillelcoren@gmail.com>
2020-03-18 09:08:45 +0000

    Update Flutter client

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -1,13 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/main.dart.js": "3899a26d61149f9c4598a8d5a64e3763",
+  "/main.dart.js": "a3d3d232f0696b298aeeccb20721258e",
 "/favicon.ico": "51636d3a390451561744c42188ccd628",
 "/index.html": "02f52d8ed33971b0d7a938e631e89401",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
 "/assets/FontManifest.json": "280b2f61f6810d59bd1bcd4cf01d3bf4",
-"/assets/LICENSE": "20b0da0c096c08cc891c6c4c524cb06c",
+"/assets/LICENSE": "5a747e26fd0a64f7e4364c3d92f0813e",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "0ea892e09437fcaa050b2b15c53173b7",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "51d23d1c30deda6f34673e0d5600fd38",
 "/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "d51b09f7b8345b41dd3b2201f653c62b",
@@ -35,9 +35,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
