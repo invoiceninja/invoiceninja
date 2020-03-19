1f1863b61688cc4622d725e4637df186167c5ac3
David Bomba <turbo124@gmail.com>
2020-03-19 11:56:23 +1100

    Limit Self updating to self hosters only :

--- app/Http/Controllers/SelfUpdateController.php
+++ app/Http/Controllers/SelfUpdateController.php
@@ -11,6 +11,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Utils\Ninja;
 use Codedge\Updater\UpdaterManager;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Support\Facades\Storage;
@@ -57,12 +58,13 @@ class SelfUpdateController extends BaseController
      */
     public function update(UpdaterManager $updater)
     {
+        if(Ninja::isNinja())
+            return response()->json(['message' => 'Self update not available on this system.'], 403);
+
         $res = $updater->update();
 
         return response()->json(['message'=>$res], 200);
     }
 
-    public function checkVersion(UpdaterManager $updater)
-    {
-    }
+ 
 }
