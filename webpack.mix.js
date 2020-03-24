be64d98ebb924c4e895351bf0140054a23f5626f
Benjamin Beganović <k1pstabug@gmail.com>
2020-03-24 17:33:27 +0100

    Fixes for Codacy - Added new codacy.yml file - Applied formatting to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,29 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
 
-require('laravel-mix-purgecss');
+require("laravel-mix-purgecss");
 
 mix.js("resources/js/app.js", "public/js")
-    .js("resources/js/clients/payment_methods/authorize-stripe-card.js", "public/js/clients/payment_methods/authorize-stripe-card.js")
-    .js("resources/js/clients/invoices/action-selectors.js", "public/js/clients/invoices/action-selectors.js")
-    .js("resources/js/clients/invoices/payment.js", "public/js/clients/invoices/payment.js")
-    .js("resources/js/clients/quotes/action-selectors.js", "public/js/clients/quotes/action-selectors.js")
-    .js("resources/js/clients/quotes/approve.js", "public/js/clients/quotes/approve.js");
+    .js(
+        "resources/js/clients/payment_methods/authorize-stripe-card.js",
+        "public/js/clients/payment_methods/authorize-stripe-card.js"
+    )
+    .js(
+        "resources/js/clients/invoices/action-selectors.js",
+        "public/js/clients/invoices/action-selectors.js"
+    )
+    .js(
+        "resources/js/clients/invoices/payment.js",
+        "public/js/clients/invoices/payment.js"
+    )
+    .js(
+        "resources/js/clients/quotes/action-selectors.js",
+        "public/js/clients/quotes/action-selectors.js"
+    )
+    .js(
+        "resources/js/clients/quotes/approve.js",
+        "public/js/clients/quotes/approve.js"
+    );
 
 mix.sass("resources/sass/app.scss", "public/css")
     .options({
@@ -17,10 +32,9 @@ mix.sass("resources/sass/app.scss", "public/css")
     })
     .purgeCss({
         enabled: mix.inProduction(),
-        folders: ['resources', 'views', 'portal', 'ninja2020'],
-        extensions: ['html', 'php']
+        folders: ["resources", "views", "portal", "ninja2020"],
+        extensions: ["html", "php"]
     });
-
 
 mix.version();
 mix.disableSuccessNotifications();
