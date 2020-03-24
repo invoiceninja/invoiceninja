3bdcbfc0012023efaabd38aece7d5cfaa613be3f
Benjamin Beganović <k1pstabug@gmail.com>
2020-03-24 17:01:59 +0100

    PurgeCSS setup with Webpack - Production builds for assets - Added PurgeCSS to 'ninja2020' theme

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require("laravel-mix");
 const tailwindcss = require("tailwindcss");
 
+require('laravel-mix-purgecss');
+
 mix.js("resources/js/app.js", "public/js")
     .js("resources/js/clients/payment_methods/authorize-stripe-card.js", "public/js/clients/payment_methods/authorize-stripe-card.js")
     .js("resources/js/clients/invoices/action-selectors.js", "public/js/clients/invoices/action-selectors.js")
@@ -12,7 +14,13 @@ mix.sass("resources/sass/app.scss", "public/css")
     .options({
         processCssUrls: false,
         postCss: [tailwindcss("./tailwind.config.js")]
+    })
+    .purgeCss({
+        enabled: mix.inProduction(),
+        folders: ['resources', 'views', 'portal', 'ninja2020'],
+        extensions: ['html', 'php']
     });
+
 
 mix.version();
 mix.disableSuccessNotifications();
