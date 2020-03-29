cdd562d3a581fc56df2d1576902d2566bce492e1
hillelcoren <hillelcoren@gmail.com>
2020-03-29 18:57:32 +0000

    Update Flutter client

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/main.dart.js": "cd44c3865c424881f81cb9de22ae885a",
+  "/main.dart.js": "9b6b592f8afd6a1e5ec1564b22ce99b1",
 "/favicon.ico": "51636d3a390451561744c42188ccd628",
 "/index.html": "02f52d8ed33971b0d7a938e631e89401",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
