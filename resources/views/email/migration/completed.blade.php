a76e188a6c2dd0cb692e43c7684f36517d0548a0
Benjamin Beganović <k1pstabug@gmail.com>
2020-04-01 11:37:29 +0200

    Send e-mail when migration has completed
    - New completed.blade.php
    - Apply php-cs-fixer on Import.php

---
+++ resources/views/email/migration/completed.blade.php
@@ -0,0 +1,31 @@
+@component('email.template.master', ['design' => 'light'])
+
+@slot('header')
+    @component('email.components.header')
+        Migration completed
+    @endcomponent
+@endslot
+
+@slot('greeting')
+	Hello,
+@endslot
+
+We're happy to inform you that migration has been completed successfully. It is ready for you to review it.
+
+@component('email.components.button', ['url' => url('/')])
+    Visit portal
+@endcomponent
+
+
+@slot('signature')
+Thank you, <br>
+Invoice Ninja    
+@endslot
+
+@slot('footer')
+    @component('email.components.footer', ['url' => 'https://invoiceninja.com', 'url_text' => '&copy; InvoiceNinja'])
+        For any info, please visit InvoiceNinja.
+    @endcomponent
+@endslot
+
+@endcomponent
