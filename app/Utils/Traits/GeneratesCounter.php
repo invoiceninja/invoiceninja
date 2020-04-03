d815cfa217fef98e1cb461f14c5475599f631666
David Bomba <turbo124@gmail.com>
2020-04-04 09:03:06 +1100

    Fixes when implementing

--- app/Utils/Traits/GeneratesCounter.php
+++ app/Utils/Traits/GeneratesCounter.php
@@ -45,8 +45,14 @@ trait GeneratesCounter
         $pattern = $client->getSetting('invoice_number_pattern');
         //Determine if we are using client_counters
         if (strpos($pattern, 'clientCounter')) {
-            $counter = $client->settings->invoice_number_counter;
+
+            if(property_exists($client->settings, 'invoice_number_counter'))
+                $counter = $client->settings->invoice_number_counter;
+            else
+                $counter = 1;
+
             $counter_entity = $client;
+
         } elseif (strpos($pattern, 'groupCounter')) {
             $counter = $client->group_settings->invoice_number_counter;
             $counter_entity = $client->group_settings;
@@ -297,6 +303,9 @@ trait GeneratesCounter
     {
         $settings = $entity->settings;
         
+        if($counter_name == 'invoice_number_counter' && !property_exists($entity->settings, 'invoice_number_counter'))
+            $settings->invoice_number_counter = 0;
+
         $settings->{$counter_name} = $settings->{$counter_name} + 1;
         
         $entity->settings = $settings;
