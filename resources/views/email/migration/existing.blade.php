10c671bd2376c45731a24adf64c786b9f3545455
Benjamin Beganovic <k1pstabug@gmail.com>
2020-04-13 17:57:05 +0200

    Added new options for migrations
    - New 'existing.blade' e-mail
    - Added .php_cs cache in .gitignore
    - Updated MigrationController with new options

---
+++ resources/views/email/migration/existing.blade.php
@@ -0,0 +1,31 @@
+@component('email.template.master', ['design' => 'light'])
+
+@slot('header')
+    @component('email.components.header')
+        Migration already completed
+    @endcomponent
+@endslot
+
+@slot('greeting')
+	Hello,
+@endslot
+
+Looks like you already migrated your data to V2 version of the Invoice Ninja. In case you want to start over, you can 'force' migrate to wipe existing data.
+
+@component('email.components.button', ['url' => url('/')])
+    Visit portal
+@endcomponent
+
+
+@slot('signature')
+Thank you, <br>
+Invoice Ninja    
+@endslot
+
+@slot('footer')
+    @component('email.components.footer', ['url' => 'https://invoiceninja.com', 'url_text' => '&copy; InvoiceNinja'])
+        For any info, please visit InvoiceNinja.
+    @endcomponent
+@endslot
+
+@endcomponent
