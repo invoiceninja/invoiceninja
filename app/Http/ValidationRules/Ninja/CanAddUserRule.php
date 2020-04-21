787d1c24b3eed3d7896d93510efc151fdc3070c9
David Bomba <turbo124@gmail.com>
2020-04-21 15:21:36 +1000

    Add users in hosted platform

--- app/Http/ValidationRules/Ninja/CanAddUserRule.php
+++ app/Http/ValidationRules/Ninja/CanAddUserRule.php
@@ -35,7 +35,7 @@ class CanAddUserRule implements Rule
      */
     public function passes($attribute, $value)
     {
-
+        return $this->account->num_users < $this->account->pending_num_users;
     }
 
     /**
@@ -43,7 +43,7 @@ class CanAddUserRule implements Rule
      */
     public function message()
     {
-        return ctrans('texts.limit_users', ['limit' => 1]);
+        return ctrans('texts.limit_users', ['limit' => $this->account->pending_num_users]);
     }
 
 }
