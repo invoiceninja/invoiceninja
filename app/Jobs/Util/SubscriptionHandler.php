e76bb6c77c19d6138301550c8a2ccf3af0a1a3a6
David Bomba <turbo124@gmail.com>
2020-05-05 08:11:38 +1000

    Working on subscriptions

--- app/Jobs/Util/SubscriptionHandler.php
+++ app/Jobs/Util/SubscriptionHandler.php
@@ -7,19 +7,26 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\Subscription;
 
 class SubscriptionHandler implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $entity;
+
+    private $event_id;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    
+    public function __construct($event_id, $entity)
     {
-        //
+        $this->event_id = $event_id;
+        $this->entity = $entity;
     }
 
     /**
@@ -29,6 +36,20 @@ class SubscriptionHandler implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $subscriptions = Subscription::where('company_id', $this->entity->company_id)
+                                    ->where('event_id', $this->event_id)
+                                    ->get();
+
+        if(!$subscriptions)
+            return;
+
+        $subscriptions->each(function($subscription) {
+            $this->process($subscription);
+        });
+    }
+
+    private function process($subscription)
+    {
+
     }
 }
