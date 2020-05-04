4a81b5efd396c2b5760966d902ac5a042001566c
David Bomba <turbo124@gmail.com>
2020-05-04 18:47:00 +1000

    Fixes for gateway filtering

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -16,7 +16,6 @@ use App\DataMapper\CompanySettings;
 use App\Factory\CreditFactory;
 use App\Factory\InvoiceFactory;
 use App\Factory\QuoteFactory;
-use App\Models\Client;
 use App\Models\Company;
 use App\Models\CompanyGateway;
 use App\Models\Country;
@@ -346,7 +345,7 @@ class Client extends BaseModel implements HasLocalePreference
         $company_gateways = $this->getSetting('company_gateway_ids');
 
         if (strlen($company_gateways)>=1) {
-            $gateways = $this->company->company_gateways->whereIn('id', $payment_gateways);
+            $gateways = $this->company->company_gateways->whereIn('id', $company_gateways);
         } else {
             $gateways = $this->company->company_gateways;
         }
