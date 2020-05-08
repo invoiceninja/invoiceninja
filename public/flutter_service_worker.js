6fc1d0f6074ee776d41ad110ee030b6965767b5b
hillelcoren <hillelcoren@gmail.com>
2020-05-08 11:54:09 +0000

    Update Flutter client

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -1,20 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "02f52d8ed33971b0d7a938e631e89401",
-"/": "02f52d8ed33971b0d7a938e631e89401",
-"favicon.ico": "51636d3a390451561744c42188ccd628",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
-"assets/FontManifest.json": "280b2f61f6810d59bd1bcd4cf01d3bf4",
+  "favicon.ico": "51636d3a390451561744c42188ccd628",
+"main.dart.js": "ae4b6ec0d44e5ffa24155b6cf0cc6c40",
+"assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"assets/FontManifest.json": "280b2f61f6810d59bd1bcd4cf01d3bf4",
 "assets/LICENSE": "98845e6a0ac2838c7313ef03b0e016fb",
-"main.dart.js": "4315f08dac8d80462a253045e680c266"
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
+"assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"index.html": "02f52d8ed33971b0d7a938e631e89401",
+"/": "02f52d8ed33971b0d7a938e631e89401"
 };
 
 self.addEventListener('activate', function (event) {
