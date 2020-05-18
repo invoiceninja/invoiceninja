72a172c776f526161a1aff0be078a6c83ce1c99b
David Bomba <turbo124@gmail.com>
2020-05-18 13:38:34 +1000

    Entity Notification refactor

---
+++ app/Mail/Admin/EntityViewedObject.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2020. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Mail\Admin;
+
+use App\Models\User;
+use App\Utils\Number;
+
+class EntityViewedObject 
+{
+	public $invitation;
+
+	public $entity_type;
+
+	public $entity;
+
+	public $contact;
+
+	public $company;
+
+	public $settings;
+
+    public function __construct($invitation, $entity_type)
+    {
+        $this->invitation = $invitation;
+        $this->entity_type = $entity_type;
+        $this->entity = $invitation->{$entity_type};
+        $this->contact = $invitation->contact;
+        $this->company = $invitation->company;
+    }
+
+    public function build()
+    {
+    	$mail_obj = new \stdClass;
+    	$mail_obj->amount = $this->getAmount();
+    	$mail_obj->subject = $this->getSubject();
+    	$mail_obj->data = $this->getData();
+    	$mail_obj->markdown = 'email.admin.generic';
+    	$mail_obj->tag = $this->company->company_key;
+    	
+    	return $mail_obj;
+    }
+
+    private function getAmount()
+    {
+    	return Number::formatMoney($this->entity->amount, $this->entity->client);
+    }
+
+    private function getSubject()
+    {
+        return
+            ctrans(
+                "texts.notification_{$this->entity_type}_viewed_subject",
+                [
+                        'client' => $this->contact->present()->name(),
+                        'invoice' => $this->entity->number,
+                    ]
+            );
+    }
+
+    private function getData()
+    {
+
+      $settings = $this->entity->client->getMergedSettings();
+
+    	$data = [
+            'title' => $this->getSubject(),
+            'message' => ctrans(
+                "texts.notification_{$this->entity_type}_viewed",
+                [
+                    'amount' => $this->getAmount(),
+                    'client' => $this->contact->present()->name(),
+                    'invoice' => $this->entity->number,
+                ]
+            ),
+            'url' => $this->invitation->getAdminLink(),
+            'button' => ctrans("texts.view_{$this->entity_type}"),
+            'signature' => $settings->email_signature,
+            'logo' => $this->company->present()->logo(),
+        ];
+
+        return $data;
+    }
+} 
