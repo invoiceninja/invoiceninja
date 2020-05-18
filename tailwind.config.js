07d90417f57d4388bf2519081931dc358a03eeb0
Benjamin Beganović <k1pstabug@gmail.com>
2020-05-19 00:22:29 +0200

    Purge CSS from e-mails  (#3712)
    * Add e-mail support for purging
    
    * update css link

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
     purge: [
         './resources/views/portal/ninja2020/**/*.blade.php',
+        './resources/views/email/**/*.blade.php',
     ],
     theme: {
         extend: {
