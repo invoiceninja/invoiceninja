06f05c6fca27908bbf9031849907b4f3fee37a98
hillelcoren <hillelcoren@gmail.com>
2020-05-21 05:59:31 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,20 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "ab85058c478599bd2ecb1e177864ebfd",
-"favicon.ico": "51636d3a390451561744c42188ccd628",
-"index.html": "02f52d8ed33971b0d7a938e631e89401",
+  "index.html": "02f52d8ed33971b0d7a938e631e89401",
 "/": "02f52d8ed33971b0d7a938e631e89401",
-"assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/LICENSE": "237de29e67e2fe9f4c494aea264dea79",
+"assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
 "assets/FontManifest.json": "280b2f61f6810d59bd1bcd4cf01d3bf4",
-"assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541"
+"assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"assets/LICENSE": "1b3f2ee40163ab6e14be3b52d6888b82",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"favicon.ico": "51636d3a390451561744c42188ccd628",
+"main.dart.js": "b66d85c734d1b9360e1f54f890af4d47"
 };
 
 // The application shell files that are downloaded before a service worker can
