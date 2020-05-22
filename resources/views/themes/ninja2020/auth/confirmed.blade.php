3ef329708ee9138f34b23f25b84da89af0b2d299
Benjamin Beganović <k1pstabug@gmail.com>
2020-05-22 16:03:34 +0200

    Add logo to confirmed page

--- resources/views/themes/ninja2020/auth/confirmed.blade.php
+++ resources/views/themes/ninja2020/auth/confirmed.blade.php
@@ -5,7 +5,8 @@
     <div class="flex h-screen">
         <div class="m-auto md:w-1/3 lg:w-1/5">
             <div class="flex flex-col items-center">
-                <h1 class="text-center text-3xl">{{ ctrans('texts.confirmation') }}</h1>
+                <img src="{{ asset('images/invoiceninja-black-logo.png') }}" class="border-b border-gray-100 h-14 pb-4" alt="Invoice Ninja logo">
+                <h1 class="text-center text-3xl mt-10">{{ ctrans('texts.confirmation') }}</h1>
                 <p class="text-center opacity-75">{{ $message }}</p>
                 <a class="button button-primary text-center mt-8" href="{{ url('/') }}">{{ ctrans('texts.return_to_login') }}</a>
             </div>
