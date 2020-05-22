0e1425c0fd66d844377e3cf78cb60dd8c7c95cbf
Benjamin Beganović <k1pstabug@gmail.com>
2020-05-22 14:37:01 +0200

    implement checkout

--- app/PaymentDrivers/CheckoutPaymentDriver.php
+++ app/PaymentDrivers/CheckoutPaymentDriver.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Invoice Ninja (https://invoiceninja.com)
  *
@@ -11,13 +12,16 @@
 
 namespace App\PaymentDrivers;
 
+use App\Events\Payment\PaymentWasCreated;
 use App\Factory\PaymentFactory;
+use App\Jobs\Util\SystemLogger;
 use App\Models\Client;
 use App\Models\ClientContact;
 use App\Models\CompanyGateway;
 use App\Models\GatewayType;
 use App\Models\Invoice;
 use App\Models\Payment;
+use App\Models\SystemLog;
 use App\Utils\Traits\SystemLogTrait;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -63,7 +67,7 @@ class CheckoutPaymentDriver extends BasePaymentDriver
     /* Authorise payment methods */
     protected $can_authorise_credit_card = true;
 
-    public function getPublishableKey() :?string
+    public function getPublishableKey(): ?string
     {
         // @todo: Doesn't return right property key.
         // return $this->company_gateway->getPublishableKey();
@@ -114,8 +118,63 @@ class CheckoutPaymentDriver extends BasePaymentDriver
         return render($this->viewForType($data['payment_method_id']), $data);
     }
 
-    public function processPaymentResponse($request) 
+    public function processPaymentResponse($request)
     {
-        // ..
-	}
+        $card_token = $request->input('cko-card-token');
+
+        $response = $this->gateway()->purchase([
+            'source' => [
+                'type' => 'token',
+                'token' => $card_token,
+            ],
+            'amount' => 1000,
+            'currency' => $this->getContact()->client->getCurrencyCode(),
+            'reference' => rand(0, 1000),
+            'meta_data' => [],
+        ], []);
+
+        $transaction_reference = $response->getTransactionReference();
+
+        if ($response->isCancelled()) {
+            return redirect()->route('client.invoices.index')->with('warning', ctrans('texts.status_voided'));
+        }
+
+        if ($response->isSuccessful()) {
+            SystemLogger::dispatch(
+                [
+                    'server_response' => $response->getData(),
+                    'data' => $request->all()
+                ],
+                SystemLog::CATEGORY_GATEWAY_RESPONSE,
+                SystemLog::EVENT_GATEWAY_SUCCESS,
+                SystemLog::TYPE_CHECKOUT,
+                $this->client
+            );
+        }
+
+        if (!$response->isSuccessful()) {
+            SystemLogger::dispatch(
+                [
+                    'data' => $request->all(),
+                    'server_response' => $response->getData()
+                ],
+                SystemLog::CATEGORY_GATEWAY_RESPONSE,
+                SystemLog::EVENT_GATEWAY_FAILURE,
+                SystemLog::TYPE_CHECKOUT,
+                $this->client
+            );
+
+            throw new \Exception($response->getMessage());
+        }
+
+        $payment = $this->createPayment($response->getData());
+
+        $this->attachInvoices($payment, $request->input('hashed_ids'));
+
+        $payment->service()->UpdateInvoicePayment();
+
+        event(new PaymentWasCreated($payment, $payment->company));
+
+        return redirect()->route('client.payments.show', ['payment' => $this->encodePrimaryKey($payment->id)]);
+    }
 }
