825f2564a1ab9287e7fbb3b842d4e73bf308004b
Benjamin Beganović <k1pstabug@gmail.com>
2020-05-26 12:35:43 +0200

    ignore local tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  config.ignoreTestFiles = "**/examples/*.spec.js";
+  return config;
 }
