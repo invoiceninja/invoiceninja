be15a9f7e8c5b0c334181f19fba40ee850d1fcab
Benjamin Beganović <k1pstabug@gmail.com>
2020-05-26 13:26:45 +0200

    login page test

---
+++ cypress/integration/client_portal/login.spec.js
@@ -0,0 +1,48 @@
+context('Login', () => {
+    beforeEach(() => {
+        cy.visit('/client/login');
+    });
+
+    it('should type into login form elements', () => {
+        cy.get('#test_email')
+            .invoke('val')
+            .then(emailValue => {
+                cy.get('#email')
+                    .type(emailValue)
+                    .should('have.value', emailValue);
+            });
+
+        cy.get('#test_password')
+            .invoke('val')
+            .then(passwordValue => {
+                cy.get('#password')
+                    .type(passwordValue)
+                    .should('have.value', passwordValue);
+            });
+    });
+
+    it('should login into client portal', () => {
+        cy.get('#test_email')
+            .invoke('val')
+            .then(emailValue => {
+                cy.get('#test_password')
+                    .invoke('val')
+                    .then(passwordValue => {
+                        cy.get('#email')
+                            .type(emailValue)
+                            .should('have.value', emailValue);
+                        cy.get('#password')
+                            .type(passwordValue)
+                            .should('have.value', passwordValue);
+                        cy.get('#loginBtn')
+                            .contains('Login')
+                            .click();
+                        cy.location().should(location => {
+                            expect(location.pathname).to.eq(
+                                '/client/dashboard'
+                            );
+                        });
+                    });
+            });
+    });
+});
