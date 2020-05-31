4e84d83db1bf5f445702ff26885fe9110d0392a9
David Bomba <turbo124@gmail.com>
2020-06-01 09:59:26 +1000

    Api routes upate

--- routes/api.php
+++ routes/api.php
@@ -112,7 +112,7 @@ Route::group(['middleware' => ['api_db', 'token_auth', 'locale'], 'prefix' => 'a
 
     Route::post('templates', 'TemplateController@show')->name('templates.show');
     
-    Route::post('preview', 'PreviewController@show');
+    Route::post('preview', 'PreviewController@show')->name('preview.show');
 
     Route::post('self-update', 'SelfUpdateController@update')->middleware('password_protected');
 
