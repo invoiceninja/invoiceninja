fc0d0591de2baadc15dd91d8248d084672690c65
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-04 16:23:18 +0200

    Storing ACH bank account

--- app/PaymentDrivers/Stripe/ACH.php
+++ app/PaymentDrivers/Stripe/ACH.php
@@ -12,7 +12,10 @@
 
 namespace App\PaymentDrivers\Stripe;
 
+use App\Models\ClientGatewayToken;
+use App\Models\GatewayType;
 use App\PaymentDrivers\StripePaymentDriver;
+use Stripe\Exception\InvalidRequestException;
 
 class ACH
 {
@@ -26,5 +29,75 @@ class ACH
     public function authorizeView(array $data)
     {
         return render('gateways.stripe.ach.authorize', array_merge($data));
+    }
+
+    public function authorizeResponse($request)
+    {
+        $state = [
+            'server_response' => json_decode($request->gateway_response),
+            'gateway_id' => $request->gateway_id,
+            'gateway_type_id' => $request->gateway_type_id,
+            'is_default' => $request->is_default,
+        ];
+
+        $customer = $this->stripe->findOrCreateCustomer();
+
+        $this->stripe->init();
+        
+        $local_stripe = new \Stripe\StripeClient(
+            $this->stripe->company_gateway->getConfigField('apiKey')
+        );
+
+        try {
+            $local_stripe->customers->createSource(
+                $customer->id,
+                ['source' => $state['server_response']->token->id]
+            );
+        } catch(InvalidRequestException $e) {
+            return back()->with('ach_error', $e->getMessage());
+        }
+
+        $payment_meta = $state['server_response']->token->bank_account;
+        $payment_meta->brand = ctrans('texts.ach');
+        $payment_meta->type = ctrans('texts.bank_transfer');
+        $payment_meta->verified_at = null;
+
+        $client_gateway_token = new ClientGatewayToken();
+        $client_gateway_token->company_id = $this->stripe->client->company->id;
+        $client_gateway_token->client_id = $this->stripe->client->id;
+        $client_gateway_token->token = $state['server_response']->token->bank_account->id;
+        $client_gateway_token->company_gateway_id = $this->stripe->company_gateway->id;
+        $client_gateway_token->gateway_type_id = $state['gateway_type_id'];
+        $client_gateway_token->gateway_customer_reference = $customer->id;
+        $client_gateway_token->meta = $payment_meta;
+        $client_gateway_token->save();
+
+        if ($state['is_default'] == 'true' || $this->stripe->client->gateway_tokens->count() == 1) {
+            $this->stripe->client->gateway_tokens()->update(['is_default' => 0]);
+
+            $client_gateway_token->is_default = 1;
+            $client_gateway_token->save();
+        }
+
+        return redirect()->route('client.payment_methods.index');
+    }
+
+    public function paymentResponse($request)
+    {
+        $server_response = json_decode($request->input('gateway_response'));
+
+        $state = [
+            'payment_method' => $server_response->payment_method,
+            'payment_status' => $server_response->status,
+            'save_card' => $request->store_card,
+            'gateway_type_id' => $request->payment_method_id,
+            'hashed_ids' => $request->hashed_ids,
+            'server_response' => $server_response,
+        ];
+    }
+
+    public function acceptWebhook()
+    {
+        // ..
     }
 }
--- resources/views/portal/ninja2020/gateways/stripe/ach/authorize.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/ach/authorize.blade.php
@@ -8,8 +8,8 @@
 @section('body')
 <form action="{{ route('client.payment_methods.store') }}" method="post" id="server_response">
     @csrf
-    <input type="hidden" name="company_gateway_id" value="{{ $gateway->gateway_id }}">
-    <input type="hidden" name="gateway_type_id" value="1">
+    <input type="hidden" name="company_gateway_id" value="{{ $gateway->id }}">
+    <input type="hidden" name="gateway_type_id" value="2">
     <input type="hidden" name="gateway_response" id="gateway_response">
     <input type="hidden" name="is_default" id="is_default">
 </form>
@@ -17,6 +17,11 @@
     <div class="grid grid-cols-6 gap-4">
         <div class="col-span-6 md:col-start-2 md:col-span-4">
             <div class="alert alert-failure mb-4" hidden id="errors"></div>
+            @if(session()->has('ach_error'))
+                <div class="alert alert-failure mb-4">
+                    <p>{{ session('ach_error') }}</p>
+                </div>
+            @endif
             <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                 <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -59,7 +64,7 @@
                                 <dd class="text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                                     <select name="countries" id="country" class="form-select input w-full" required>
                                         @foreach($countries as $country)
-                                            <option value="{{ $country->iso_3166_2 }}">{{ $country->iso_3166_2 }} ({{ $country->name }})</option>
+                                        <option value="{{ $country->iso_3166_2 }}">{{ $country->iso_3166_2 }} ({{ $country->name }})</option>
                                         @endforeach
                                     </select>
                                 </dd>
@@ -71,7 +76,7 @@
                                 <dd class="text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                                     <select name="currencies" id="currency" class="form-select input w-full">
                                         @foreach($currencies as $currency)
-                                            <option value="{{ $currency->code }}">{{ $currency->code }} ({{ $currency->name }})</option>
+                                        <option value="{{ $currency->code }}">{{ $currency->code }} ({{ $currency->name }})</option>
                                         @endforeach
                                     </select>
                                 </dd>
@@ -92,21 +97,13 @@
                                     <input class="input w-full" id="account-number" type="text" required>
                                 </dd>
                             </div>
-                            <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex-col md:flex-row items-center">
-                                <dt class="text-sm leading-5 font-medium text-gray-500">
-                                    {{ ctrans('texts.confirm_account_number') }}
-                                </dt>
-                                <dd class="text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                                    <input class="input w-full" id="account-number-confirmation" type="text" required>
-                                </dd>
-                            </div>
-                            <div class="bg-white px-4 py-5 sm:grid  sm:px-6 flex-col md:flex-row items-center">
+                            <div class="bg-gray-50 px-4 py-5 sm:grid  sm:px-6 flex-col md:flex-row items-center">
                                 <span class="text-sm leading-5 font-medium text-gray-500">
                                     <input type="checkbox" class="form-checkbox mr-1" id="accept-terms" required>
                                     <label for="accept-terms" class="cursor-pointer">{{ ctrans('texts.ach_authorization', ['company' => auth()->user()->company->present()->name, 'email' => auth()->user()->company->email]) }}</label>
                                 </span>
                             </div>
-                            <div class="bg-gray-50 px-4 py-5 flex justify-end">
+                            <div class="bg-white px-4 py-5 flex justify-end">
                                 <button type="submit" class="button button-primary">
                                     {{ ctrans('texts.save') }}
                                 </button>
