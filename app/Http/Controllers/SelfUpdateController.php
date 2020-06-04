a0c5ded5e1053d46f956e2a45c1cdf4699370e0a
David Bomba <turbo124@gmail.com>
2020-06-04 21:41:03 +1000

    Remove unused dependency

--- app/Http/Controllers/SelfUpdateController.php
+++ app/Http/Controllers/SelfUpdateController.php
@@ -60,7 +60,7 @@ class SelfUpdateController extends BaseController
      *     )
      *
      */
-    public function update(UpdaterManager $updater)
+    public function update()
     {
         if (Ninja::isNinja()) {
             return response()->json(['message' => 'Self update not available on this system.'], 403);
