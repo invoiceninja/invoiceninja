8d5a843b36a65e33e257665123ca14a070c04ce4
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-15 12:59:02 +0200

    Fix public api key logic

--- app/PaymentDrivers/CheckoutCom/Utilities.php
+++ app/PaymentDrivers/CheckoutCom/Utilities.php
@@ -16,12 +16,7 @@ trait Utilities
 {
     public function getPublishableKey()
     {
-        // This doesn't work since $gateway->getPublishableKey is looking for 'publishableKey' 
-        // but we use 'publicApiKey' for Checkout.com in .env file.
-
-        // This is dummy implementation and it needs to be fixed.
-
-        return $this->company_gateway->getConfig()->publicApiKey;
+        return $this->company_gateway->getConfigField('publicApiKey');
     }
 
     public function convertToCheckoutAmount($amount, $currency)
