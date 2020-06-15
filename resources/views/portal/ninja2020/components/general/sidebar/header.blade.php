701b9531bff9ac0a4df41fda9461d6f2c6c4de05
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-15 15:53:52 +0200

    Remove search bar from header

--- resources/views/portal/ninja2020/components/general/sidebar/header.blade.php
+++ resources/views/portal/ninja2020/components/general/sidebar/header.blade.php
@@ -4,21 +4,8 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
         </svg>
     </button>
-    <div class="flex-1 px-4 flex justify-between">
-        <div class="flex-1 flex">
-            <div class="w-full flex md:ml-0">
-                <label for="search_field" class="sr-only">{{ ctrans('texts.search') }}</label>
-                <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-                        </svg>
-                    </div>
-                    <input id="search_field" class="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm" placeholder="Search" />
-                </div>
-            </div>
-        </div>
-        <div class="ml-4 flex items-center md:ml-6">
+    <div class="flex-1 px-4 flex justify-end">
+        <div class="flex items-center md:ml-6 md:mr-2">
             @if($multiple_contacts->count() > 1)
             <div class="relative inline-block text-left" x-data="{ open: false }">
                 <div>
