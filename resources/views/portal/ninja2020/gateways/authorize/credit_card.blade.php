64b7884a832feaf6f25051b1ccf0215e67643832
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-17 15:27:11 +0200

    optimize card-js form

--- resources/views/portal/ninja2020/gateways/authorize/credit_card.blade.php
+++ resources/views/portal/ninja2020/gateways/authorize/credit_card.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 flex items-center flex items-center">
+<div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" style="display: flex!important; justify-content: center!important;">
     <div class="card-js" id="my-card" data-capture-name="true">
       <input class="name" id="cardholder_name" name="card-holders-name" placeholder="{{ ctrans('texts.name')}}">
       <input class="card-number my-custom-class" id="card_number" name="card-number">
