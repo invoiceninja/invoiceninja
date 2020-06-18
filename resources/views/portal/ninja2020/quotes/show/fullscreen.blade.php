8efc453c4516b4bcf1d0d6057206ae96c8a4aac6
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-18 12:51:47 +0200

    Support for "Approve" button with fullscreen quotes

--- resources/views/portal/ninja2020/quotes/show/fullscreen.blade.php
+++ resources/views/portal/ninja2020/quotes/show/fullscreen.blade.php
@@ -1,29 +1,29 @@
 @extends('portal.ninja2020.layout.clean')
-@section('meta_title', ctrans('texts.view_invoice'))
+@section('meta_title', ctrans('texts.view_quote'))
 
 @section('body')
-    @if($invoice->isPayable())
-        <form action="{{ route('client.invoices.bulk') }}" method="post">
+    @if(!$quote->isApproved())
+        <form action="{{ route('client.quotes.bulk') }}" method="post">
             @csrf
-            <div class="bg-white">
+            <input type="hidden" name="action" value="approve">
+            <input type="hidden" name="quotes[]" value="{{ $quote->hashed_id }}">
+            <div class="bg-white" translate>
                 <div class="px-4 py-5 sm:p-6">
                     <div class="sm:flex sm:items-start sm:justify-between">
                         <div>
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                {{ ctrans('texts.unpaid') }}
+                            <h3 class="text-lg leading-6 font-medium text-gray-900" translate>
+                                {{ ctrans('texts.waiting_for_approval') }}
                             </h3>
                             <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
                                 <p translate>
-                                    {{ ctrans('texts.invoice_still_unpaid') }}
-                                    <!-- This invoice is still not paid. Click the button to complete the payment. -->
+                                    {{ ctrans('texts.quote_still_not_approved') }}
                                 </p>
                             </div>
                         </div>
                         <div class="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
                             <div class="inline-flex rounded-md shadow-sm">
-                                <input type="hidden" name="invoices[]" value="{{ $invoice->hashed_id }}">
                                 <input type="hidden" name="action" value="payment">
-                                <button class="button button-primary">@lang('texts.pay_now')</button>
+                                <button class="button button-primary">@lang('texts.approve')</button>
                             </div>
                         </div>
                     </div>
@@ -32,5 +32,5 @@
         </form>
     @endif
 
-    <iframe src="{{ $invoice->pdf_file_path() }}" class="h-screen w-full border-0"></iframe>
+    <iframe src="{{ $quote->pdf_file_path() }}" class="h-screen w-full border-0"></iframe>
 @endsection
