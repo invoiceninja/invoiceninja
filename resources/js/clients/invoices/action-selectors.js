829a7a2c1093e59fd8d6692c6539a5d3041630c2
Benjamin Beganović <k1pstabug@gmail.com>
2020-06-26 15:20:15 +0200

    Fix checkboxes for action selectors on invoices & quotes

--- resources/js/clients/invoices/action-selectors.js
+++ resources/js/clients/invoices/action-selectors.js
@@ -10,48 +10,65 @@
 
 class ActionSelectors {
     constructor() {
-        this.parentElement = document.querySelector(".form-check-parent");
-        this.parentForm = document.getElementById("bulkActions");
+        this.parentElement = document.querySelector('.form-check-parent');
+        this.parentForm = document.getElementById('bulkActions');
     }
 
     watchCheckboxes(parentElement) {
-        document.querySelectorAll(".form-check-child").forEach(child => {
+        document
+            .querySelectorAll('.child-hidden-input')
+            .forEach((element) => element.remove());
+
+        document.querySelectorAll('.form-check-child').forEach((child) => {
             if (parentElement.checked) {
                 child.checked = parentElement.checked;
-                this.processChildItem(child, document.getElementById("bulkActions"));
+                this.processChildItem(
+                    child,
+                    document.getElementById('bulkActions')
+                );
             } else {
                 child.checked = false;
                 document
-                    .querySelectorAll(".child-hidden-input")
-                    .forEach(element => element.remove());
+                    .querySelectorAll('.child-hidden-input')
+                    .forEach((element) => element.remove());
             }
         });
     }
 
     processChildItem(element, parent, options = {}) {
-        if (options.hasOwnProperty("single")) {
+        if (options.hasOwnProperty('single')) {
             document
-                .querySelectorAll(".child-hidden-input")
-                .forEach(element => element.remove());
+                .querySelectorAll('.child-hidden-input')
+                .forEach((element) => element.remove());
         }
 
-        let _temp = document.createElement("INPUT");
+        if (element.checked === false) {
+            let inputs = document.querySelectorAll('input.child-hidden-input');
 
-        _temp.setAttribute("name", "invoices[]");
-        _temp.setAttribute("value", element.dataset.value);
-        _temp.setAttribute("class", "child-hidden-input");
+            for (let i of inputs) {
+                if (i.value == element.dataset.value) i.remove();
+            }
+
+            return;
+        }
+
+        let _temp = document.createElement('INPUT');
+
+        _temp.setAttribute('name', 'invoices[]');
+        _temp.setAttribute('value', element.dataset.value);
+        _temp.setAttribute('class', 'child-hidden-input');
         _temp.hidden = true;
 
         parent.append(_temp);
     }
 
     handle() {
-        this.parentElement.addEventListener("click", () => {
+        this.parentElement.addEventListener('click', () => {
             this.watchCheckboxes(this.parentElement);
         });
 
-        for (let child of document.querySelectorAll(".form-check-child")) {
-            child.addEventListener("click", () => {
+        for (let child of document.querySelectorAll('.form-check-child')) {
+            child.addEventListener('click', () => {
                 this.processChildItem(child, this.parentForm);
             });
         }
