58532326b078f310ad06973eb0564267c7867717
hillelcoren <hillelcoren@gmail.com>
2020-06-29 12:46:09 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "4269e927093feb0237b9dc6a4d6bc00a",
+  "main.dart.js": "ffcda7f4b1265934a29dd34ca2cf0ebe",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
 "manifest.json": "77215c1737c7639764e64a192be2f7b8",
 "assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
