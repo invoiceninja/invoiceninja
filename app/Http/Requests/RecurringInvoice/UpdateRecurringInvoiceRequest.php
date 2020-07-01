3179676b1d9b37440d5684dac146f1d460503e40
= <turbo124@gmail.com>
2020-07-01 10:12:53 +1000

    working on recurring invoices - form requests

--- app/Http/Requests/RecurringInvoice/UpdateRecurringInvoiceRequest.php
+++ app/Http/Requests/RecurringInvoice/UpdateRecurringInvoiceRequest.php
@@ -16,11 +16,13 @@ use App\Utils\Traits\ChecksEntityStatus;
 use App\Utils\Traits\CleanLineItems;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
+use App\Utils\Traits\MakesHash;
 
 class UpdateRecurringInvoiceRequest extends Request
 {
     use ChecksEntityStatus;
     use CleanLineItems;
+    use MakesHash;
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -35,22 +37,56 @@ class UpdateRecurringInvoiceRequest extends Request
 
     public function rules()
     {
-        return [
-            'documents' => 'mimes:png,ai,svg,jpeg,tiff,pdf,gif,psd,txt,doc,xls,ppt,xlsx,docx,pptx',
+        $rules = [];
 
-        ];
+        if ($this->input('documents') && is_array($this->input('documents'))) {
+            $documents = count($this->input('documents'));
+
+            foreach (range(0, $documents) as $index) {
+                $rules['documents.' . $index] = 'file|mimes:png,ai,svg,jpeg,tiff,pdf,gif,psd,txt,doc,xls,ppt,xlsx,docx,pptx|max:20000';
+            }
+        } elseif ($this->input('documents')) {
+            $rules['documents'] = 'file|mimes:png,ai,svg,jpeg,tiff,pdf,gif,psd,txt,doc,xls,ppt,xlsx,docx,pptx|max:20000';
+        }
+
+        return $rules;
     }
 
     protected function prepareForValidation()
     {
         $input = $this->all();
 
+        if (array_key_exists('design_id', $input) && is_string($input['design_id'])) {
+            $input['design_id'] = $this->decodePrimaryKey($input['design_id']);
+        }
+        
+        if (isset($input['client_id'])) {
+            $input['client_id'] = $this->decodePrimaryKey($input['client_id']);
+        }
+
         if (array_key_exists('assigned_user_id', $input) && is_string($input['assigned_user_id'])) {
             $input['assigned_user_id'] = $this->decodePrimaryKey($input['assigned_user_id']);
         }
         
+        if (isset($input['invitations'])) {
+            foreach ($input['invitations'] as $key => $value) {
+                if (is_numeric($input['invitations'][$key]['id'])) {
+                    unset($input['invitations'][$key]['id']);
+
+                }
+
+                if (array_key_exists('id', $input['invitations'][$key]) && is_string($input['invitations'][$key]['id'])) {
+                    $input['invitations'][$key]['id'] = $this->decodePrimaryKey($input['invitations'][$key]['id']);
+                }
+
+                if (is_string($input['invitations'][$key]['client_contact_id'])) {
+                    $input['invitations'][$key]['client_contact_id'] = $this->decodePrimaryKey($input['invitations'][$key]['client_contact_id']);
+                }
+            }
+        }
+        
         $input['line_items'] = isset($input['line_items']) ? $this->cleanItems($input['line_items']) : [];
-        //$input['line_items'] = json_encode($input['line_items']);
+
         $this->replace($input);
     }
 }
