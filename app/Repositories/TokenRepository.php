2281b8490ef30e3c485f5fbdf1d9b636edf2e35e
David Bomba <turbo124@gmail.com>
2020-07-06 21:44:12 +1000

    minor fix for tokens

--- app/Repositories/TokenRepository.php
+++ app/Repositories/TokenRepository.php
@@ -37,7 +37,8 @@ class TokenRepository extends BaseRepository
     public function save(array $data, CompanyToken $company_token)
     {
         $company_token->fill($data);
-
+        $company_token->is_system = true;
+        
         $company_token->save();
 
         return $company_token;
