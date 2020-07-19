adb705d7f018a4181c6f243e76fd4878461a4e54
David Bomba <turbo124@gmail.com>
2020-07-20 09:02:31 +1000

    Adding Events

---
+++ app/Events/Credit/CreditWasDeleted.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2020. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+
+namespace App\Events\Credit;
+
+use App\Models\Company;
+use App\Models\Credit;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CreditWasDeleted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $credit;
+
+    public $company;
+    
+    public $event_vars;
+    /**
+     * Create a new event instance.
+     *
+     * @param Credit $credit
+     */
+    public function __construct(Credit $credit, Company $company, array $event_vars)
+    {
+        $this->credit = $credit;
+        $this->company = $company;
+        $this->event_vars = $event_vars;
+    }
+}
