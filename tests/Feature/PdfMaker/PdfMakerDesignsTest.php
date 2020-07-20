1f8ddc0642f5ba8cf0b152022751e370d7bd39d2
Benjamin Beganović <k1pstabug@gmail.com>
2020-07-20 14:10:33 +0200

    Clean design & filters

--- tests/Feature/PdfMaker/PdfMakerDesignsTest.php
+++ tests/Feature/PdfMaker/PdfMakerDesignsTest.php
@@ -33,6 +33,8 @@ class PdfMakerDesignsTest extends TestCase
                 '$terms' => 'Trend and SEO report has been sent via email. This is really long text just to test the width of the elements.',
 
                 '$invoice-issued-to' => 'Invoice issued to:',
+
+                '$entity' => 'Invoice',
             ],
         ];
     }
@@ -135,16 +137,110 @@ class PdfMakerDesignsTest extends TestCase
 
     public function testClean()
     {
-        $state = [];
+        $state = [
+            'template' => [
+                'company-details' => [
+                    'id' => 'company-details',
+                    'elements' => [
+                        ['element' => 'p', 'content' => 'Ninja Sample', 'properties' => ['class' => 'text-blue-500']],
+                        ['element' => 'p', 'content' => 'contact@invoiceninja.com'],
+                        ['element' => 'p', 'content' => '1-800-555-Ninja'],
+                    ],
+                ],
+                'company-address' => [
+                    'id' => 'company-address',
+                    'elements' => [
+                        ['element' => 'p', 'content' => '123 Ninja Blvd.'],
+                        ['element' => 'p', 'content' => 'NinjaLand, 97315'],
+                        ['element' => 'p', 'content' => 'United States'],
+                    ],
+                ],
+                'entity-details' => [
+                    'id' => 'entity-details',
+                    'elements' => [
+                        ['element' => 'tr', 'content' => '', 'elements' => [
+                            ['element' => 'th', 'content' => '$entity-number-label', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                            ['element' => 'th', 'content' => '$entity-number', 'properties' => ['class' => 'text-left pr-4 font-medium']],
+                        ]],
+                        ['element' => 'tr', 'content' => '', 'elements' => [
+                            ['element' => 'th', 'content' => '$entity-date-label', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                            ['element' => 'th', 'content' => '$entity-date', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                        ]],
+                        ['element' => 'tr', 'content' => '', 'elements' => [
+                            ['element' => 'th', 'content' => '$due-date-label', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                            ['element' => 'th', 'content' => '$due-date', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                        ]],
+                        ['element' => 'tr', 'content' => '', 'elements' => [
+                            ['element' => 'th', 'content' => '$balance-due-label', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                            ['element' => 'th', 'content' => '$balance-due', 'properties' => ['class' => 'text-left pr-4 font-normal']],
+                        ]],
+                    ],
+                ],
+                'client-details' => [
+                    'id' => 'client-details',
+                    'elements' => [
+                        ['element' => 'p', 'content' => 'Winterfield Medical Supply', 'properties' => ['class' => 'font-medium']],
+                        ['element' => 'p', 'content' => '65 Medical Complex Rd., D98'],
+                        ['element' => 'p', 'content' => 'Atlanta, GA 22546'],
+                        ['element' => 'p', 'content' => 'United States'],
+                        ['element' => 'p', 'content' => 'demo@invoiceninja.com'],
+                    ],
+                ],
+                'product-table' => [
+                    'id' => 'product-table',
+                    'elements' => [
+                        ['element' => 'thead', 'content' => '', 'properties' => ['class' => 'text-left rounded-t-lg'], 'elements' => [
+                            ['element' => 'th', 'content' => 'Item', 'properties' => ['class' => 'font-semibold px-4 py-5']],
+                            ['element' => 'th', 'content' => 'Description', 'properties' => ['class' => 'font-semibold px-4 py-5']],
+                            ['element' => 'th', 'content' => 'Unit cost', 'properties' => ['class' => 'font-semibold px-4 py-5']],
+                            ['element' => 'th', 'content' => 'Quantity', 'properties' => ['class' => 'font-semibold px-4 py-5']],
+                            ['element' => 'th', 'content' => 'Line total', 'properties' => ['class' => 'font-semibold px-4 py-5']],
+                        ]],
+                        ['element' => 'tbody', 'content' => '', 'elements' => [
+                            ['element' => 'tr', 'content' => '', 'elements' => [
+                                ['element' => 'td', 'content' => 'Painting service', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '25 hours of painting', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '885.00', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '1', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '$885.00', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                            ]],
+                            ['element' => 'tr', 'content' => '', 'elements' => [
+                                ['element' => 'td', 'content' => 'Painting service', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '25 hours of painting', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '885.00', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '1', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                                ['element' => 'td', 'content' => '$885.00', 'properties' => ['class' => 'border-t border-b px-4 py-4']],
+                            ]],
+                        ]],
+                        ['element' => 'tfoot', 'content' => '', 'elements' => [
+                            ['element' => 'tr', 'content' => '', 'elements' => [
+                                ['element' => 'td', 'content' => 'Trend and SEO report has been sent via email. This is really long text just to test the width of the elements.', 'properties' => ['class' => 'border-l-4 border-white px-4 py-4', 'colspan' => '2']],
+                                ['element' => 'td', 'content' => 'Subtotal', 'properties' => ['class' => 'px-4 py-4 text-right', 'colspan' => '2']],
+                                ['element' => 'td', 'content' => '$0', 'properties' => ['class' => 'px-4 py-4 text-right']],
+                            ]],
+                            ['element' => 'tr', 'content' => '', 'elements' => [
+                                ['element' => 'td', 'content' => 'Paid to date', 'properties' => ['class' => 'border-l-4 border-white px-4 text-right', 'colspan' => '4']],
+                                ['element' => 'td', 'content' => '$0.00', 'properties' => ['class' => 'px-4 py-4 text-right']],
+                            ]],
+                            ['element' => 'tr', 'content' => '', 'elements' => [
+                                ['element' => 'td', 'content' => 'Discount %20', 'properties' => ['class' => 'border-l-4 border-white px-4 text-right', 'colspan' => '4']],
+                                ['element' => 'td', 'content' => '$2.00', 'properties' => ['class' => 'px-4 py-4 text-right']],
+                            ]],
+                        ]],
+                    ],
+                ],
+            ],
+            'variables' => array_merge([], $this->state['variables']),
+        ];
 
         $maker = new PdfMaker($state);
 
         $maker
-            ->design(Business::class)
+            ->design(Clean::class)
             ->build();
 
         exec('echo "" > storage/logs/laravel.log');
 
-        info($maker->getCompiledHTML());
+        info($maker->getCompiledHTML(true));
     }
 }
