b42a3ad8e21908e3b19f2e11b6f3be78d160230c
hillelcoren <hillelcoren@gmail.com>
2020-07-27 08:27:39 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
 "assets/FontManifest.json": "280b2f61f6810d59bd1bcd4cf01d3bf4",
 "/": "e65799be52f7bbcaf39d78046726b95a",
-"main.dart.js": "2eb3acaa24924c696c9f527ae7cdb6c2",
+"main.dart.js": "933920a9ca8a68eab12d8634c06ee605",
 "favicon.ico": "51636d3a390451561744c42188ccd628"
 };
 
