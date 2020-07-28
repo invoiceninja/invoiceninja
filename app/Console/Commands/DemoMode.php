44894edb17297e0f3b6fe8ccdc91ee18dcb5783d
David Bomba <turbo124@gmail.com>
2020-07-28 22:47:44 +1000

    Enable shop api on demo account

--- app/Console/Commands/DemoMode.php
+++ app/Console/Commands/DemoMode.php
@@ -102,6 +102,7 @@ class DemoMode extends Command
             'slack_webhook_url' => config('ninja.notification.slack'),
             'enabled_modules' => 32767,
             'company_key' => 'demo',
+            'enable_shop_api' => true
         ]);
 
          $settings = $company->settings;
