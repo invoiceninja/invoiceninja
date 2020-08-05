2333a78775f217a326ee7e46ca7a90d81163e4e1
Benjamin Beganović <k1pstabug@gmail.com>
2020-08-05 12:35:38 +0200

    Business design

--- tests/Feature/PdfMaker/ExampleIntegrationTest.php
+++ tests/Feature/PdfMaker/ExampleIntegrationTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature\PdfMaker;
 
 use App\Models\Design;
 use App\Models\Invoice;
-use App\Services\PdfMaker\Designs\Bold;
+use App\Services\PdfMaker\Designs\Business;
 use App\Services\PdfMaker\PdfMaker;
 use App\Utils\HtmlEngine;
 use App\Utils\Traits\MakesInvoiceValues;
@@ -20,7 +20,7 @@ class ExampleIntegrationTest extends TestCase
         $invitation = $invoice->invitations()->first();
 
         $engine = new HtmlEngine($invitation, 'invoice');
-        $design = new Bold();
+        $design = new Business();
 
         $product_table_columns = json_decode(
             json_encode($invoice->company->settings->pdf_variables),
@@ -39,7 +39,7 @@ class ExampleIntegrationTest extends TestCase
         $maker = new PdfMaker($state, 'invoice');
 
         $maker
-            ->design(Bold::class)
+            ->design(Business::class)
             ->build();
 
         exec('echo "" > storage/logs/laravel.log');
