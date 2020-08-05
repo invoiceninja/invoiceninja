e183238b8e2f3e2bae74a44be1664e60189f146d
David Bomba <turbo124@gmail.com>
2020-08-05 12:12:38 +1000

    Add Shop Profile Route

---
+++ app/Http/Controllers/Shop/ProfileController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2020. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Http\Controllers\Shop;
+
+use App\Http\Controllers\BaseController;
+use App\Models\Company;
+use App\Models\CompanyToken;
+use App\Models\Product;
+use App\Transformers\ProductTransformer;
+use App\Transformers\Shop\CompanyShopProfileTransformer;
+use App\Utils\Traits\MakesHash;
+use Illuminate\Http\Request;
+
+class ProfileController extends BaseController
+{
+    use MakesHash;
+    
+    protected $entity_type = Company::class;
+
+    protected $entity_transformer = CompanyShopProfileTransformer::class;
+
+
+    public function show(Request $request, string $product_key)
+    {
+        $company = Company::where('company_key', $request->header('X-API-COMPANY-KEY'))->first();
+
+        if(!$company->enable_shop_api)
+            return response()->json(['message' => 'Shop is disabled', 'errors' => []],403);
+
+        return $this->itemResponse($company);
+    }
+}
