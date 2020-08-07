da74ecba6e4b5ad49297d92ee8da759b88e379ce
Benjamin Beganović <k1pstabug@gmail.com>
2020-08-07 14:00:23 +0200

    Bold: - Extract table footer into tableFooter() - Add all footer elements

--- tests/Feature/PdfMaker/ExampleIntegrationTest.php
+++ tests/Feature/PdfMaker/ExampleIntegrationTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature\PdfMaker;
 
 use App\Models\Invoice;
-use App\Services\PdfMaker\Designs\Playful;
+use App\Services\PdfMaker\Designs\Bold;
 use App\Services\PdfMaker\PdfMaker;
 use App\Utils\HtmlEngine;
 use App\Utils\Traits\MakesInvoiceValues;
@@ -19,7 +19,7 @@ class ExampleIntegrationTest extends TestCase
         $invitation = $invoice->invitations()->first();
 
         $engine = new HtmlEngine(null, $invitation, 'invoice');
-        $design = new Playful();
+        $design = new Bold();
 
         $product_table_columns = json_decode(
             json_encode($invoice->company->settings->pdf_variables),
@@ -38,7 +38,7 @@ class ExampleIntegrationTest extends TestCase
         $maker = new PdfMaker($state, 'invoice');
 
         $maker
-            ->design(Playful::class)
+            ->design(Bold::class)
             ->build();
 
         exec('echo "" > storage/logs/laravel.log');
