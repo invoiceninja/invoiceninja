d00bbc7a37057906c33da2f2c1a1d477a08e000a
Benjamin Beganović <k1pstabug@gmail.com>
2020-08-10 17:04:42 +0200

    Alias $outstanding as $balance_due

--- app/Utils/HtmlEngine.php
+++ app/Utils/HtmlEngine.php
@@ -151,6 +151,7 @@ class HtmlEngine
         $data['$invoice.balance_due']    = ['value' => Number::formatMoney($this->entity->balance, $this->client) ?: '&nbsp;', 'label' => ctrans('texts.balance_due')];
         $data['$quote.balance_due']      = &$data['$invoice.balance_due'];
         $data['$balance_due']            = &$data['$invoice.balance_due'];
+        $data['$outstanding']            = &$data['$balance_due'];
         $data['$invoice.partial_due']    = ['value' => Number::formatMoney($this->entity->partial, $this->client) ?: '&nbsp;', 'label' => ctrans('texts.partial_due')];
         $data['$total']                  = ['value' => Number::formatMoney($this->entity_calc->getTotal(), $this->client) ?: '&nbsp;', 'label' => ctrans('texts.total')];
         $data['$amount']                 = &$data['$total'];
