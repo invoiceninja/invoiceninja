de24d00315bbeda1b91e0e46341cef9e621178ab
David Bomba <turbo124@gmail.com>
2020-08-12 12:16:44 +1000

    Error handling for emails

--- app/Listeners/Invoice/InvoiceEmailFailedActivity.php
+++ app/Listeners/Invoice/InvoiceEmailFailedActivity.php
@@ -47,10 +47,11 @@ class InvoiceEmailFailedActivity implements ShouldQueue
         $fields = new \stdClass;
 
         $fields->invoice_id = $event->invoice->id;
-             $fields->client_id = $event->invoice->client_id;
-       $fields->user_id = $event->invoice->user_id;
+        $fields->client_id = $event->invoice->client_id;
+        $fields->user_id = $event->invoice->user_id;
         $fields->company_id = $event->invoice->company_id;
         $fields->activity_type_id = Activity::EMAIL_INVOICE_FAILED;
+        $fields->notes = $event->errors;
 
         $this->activity_repo->save($fields, $event->invoice, $event->event_vars);
     }
