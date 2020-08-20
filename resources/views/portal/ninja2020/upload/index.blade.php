b77866c65b481f04dc1ce48bd970711cb12f9854
Benjamin Beganović <k1pstabug@gmail.com>
2020-08-20 03:14:24 +0200

    Setup break words

--- resources/views/portal/ninja2020/upload/index.blade.php
+++ resources/views/portal/ninja2020/upload/index.blade.php
@@ -2,7 +2,7 @@
 <script src="{{ asset('vendor/dropzone-5.7.0/dist/min/dropzone.min.js') }}"></script>
 
 <div class="bg-white rounded shadow p-4 mb-10">
-    <span class="text-sm mb-4 block text-gray-500">{{ ctrans('texts.allowed_file_types' )}} png,ai,svg,jpeg,tiff,pdf,gif,psd,txt,doc,xls,ppt,xlsx,docx,pptx</span>
+    <span class="text-sm mb-4 block text-gray-500 break-words">{{ ctrans('texts.allowed_file_types' )}} png, ai, svg, jpeg, tiff, pdf, gif, psd, txt, doc, xls, ppt, xlsx, docx, pptx</span>
     <form action="{{ route('client.upload.store') }}" class="dropzone" method="post" enctype="multipart/form-data">
         @csrf
         <div class="fallback">
