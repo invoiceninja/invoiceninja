7f825f9ed09f2225b5ada828fe4854cb76775a66
David Bomba <turbo124@gmail.com>
2020-09-17 08:24:56 +1000

    Fixes for paid_to_date formatting

--- app/Utils/Traits/MakesInvoiceValues.php
+++ app/Utils/Traits/MakesInvoiceValues.php
@@ -316,7 +316,7 @@ trait MakesInvoiceValues
         $data['$client.balance'] = ['value' => Number::formatMoney($this->client->balance, $this->client), 'label' => ctrans('texts.account_balance')];
         $data['$client_balance'] = ['value' => Number::formatMoney($this->client->balance, $this->client), 'label' => ctrans('texts.account_balance')];
 
-        $data['$paid_to_date'] = ['value' => $this->client->paid_to_date, 'label' => ctrans('texts.paid_to_date')];
+        $data['$paid_to_date'] = ['value' => Number::formatMoney($this->client->paid_to_date, $this->client), 'label' => ctrans('texts.paid_to_date')];
 
         $data['$client.address1'] = &$data['$address1'];
         $data['$client.address2'] = &$data['$address2'];
