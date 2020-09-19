20e4c4b1ad5110be648f23000f0f39a152b48d9a
= <turbo124@gmail.com>
2020-09-19 12:05:54 +1000

    Improve eager loading

--- app/Mail/Admin/EntityPaidObject.php
+++ app/Mail/Admin/EntityPaidObject.php
@@ -89,7 +89,7 @@ class EntityPaidObject
             'button' => ctrans('texts.view_payment'),
             'signature' => $settings->email_signature,
             'logo' => $this->company->present()->logo(),
-            'setting' => $settings,
+            'settings' => $settings,
         ];
 
         return $data;
