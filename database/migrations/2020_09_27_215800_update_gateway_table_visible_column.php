604827f5511038b7fc51628dc12b0edff256e933
David Bomba <turbo124@gmail.com>
2020-09-28 08:00:14 +1000

    Only show implemented gateways

---
+++ database/migrations/2020_09_27_215800_update_gateway_table_visible_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Gateway;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateGatewayTableVisibleColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Gateway::query()->update(['visible' => 0]);
+
+        Gateway::whereIn('id', [1,15,20,39])->update(['visible' => 1]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
