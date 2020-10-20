2b839f2ce101639a0c4fa3e116c13a1d962949bd
David Bomba <turbo124@gmail.com>
2020-10-21 08:04:19 +1100

    Fixes for password reset route

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,9 @@ Route::group(['middleware' => ['api_secret_check']], function () {
 
 Route::group(['api_secret_check', 'email_db'], function () {
     Route::post('api/v1/login', 'Auth\LoginController@apiLogin')->name('login.submit');
-    Route::post('api/v1/reset_password', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.reset');
+    // Route::post('api/v1/reset_password', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.reset');
+    Route::post('api/v1/reset_password', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+
 });
 
 Route::group(['middleware' => ['api_db', 'token_auth', 'locale'], 'prefix' => 'api/v1', 'as' => 'api.'], function () {
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::post('setup/check_pdf', 'SetupController@checkPdf')->middleware('guest');
 
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-//Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 /*
