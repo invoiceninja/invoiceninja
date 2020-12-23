37b4eb3b51cf7096391324bb6000cc30f2c6ad1f
= <turbo124@gmail.com>
2020-12-24 09:13:19 +1100

    Fix for null values in country table

---
+++ database/migrations/2020_12_23_220648_remove_null_values_in_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Country;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveNullValuesInCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $countries = Country::whereNull('thousand_separator')->update(['thousand_separator' => '']);
+        $countries = Country::whereNull('decimal_separator')->update(['decimal_separator' => '']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('countries', function (Blueprint $table) {
+            //
+        });
+    }
+}
