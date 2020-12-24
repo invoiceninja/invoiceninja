01ad794bcea45bdeb59ba53b548492472b177738
Benjamin Beganović <k1pstabug@gmail.com>
2020-12-24 15:40:20 +0100

    generate setup key command

---
+++ app/Console/Commands/GenerateSetupKey.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateSetupKey extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ninja:generate-setup-key';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate random APP_KEY value';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $randomString = base64_encode(\Illuminate\Support\Str::random(32));
+
+        $this->info('Success! Copy the following content into your .env or docker-compose.yml:');
+        $this->warn('base64:' . $randomString);
+    }
+}
