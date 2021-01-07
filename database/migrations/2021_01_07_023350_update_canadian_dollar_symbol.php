233d4257a9035f35aa0763cc641a04498c313cac
Ween Jiann <lwj5@hotmail.com>
2021-01-07 10:33:56 +0800

    Update Singapore Dollar Symbol

---
+++ database/migrations/2021_01_07_023350_update_canadian_dollar_symbol.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Currency;
+use App\Utils\Traits\AppSetup;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSingaporeDollarSymbol extends Migration
+{
+    use AppSetup;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $currency = Currency::find(13);
+
+        if ($currency) {
+            $currency->update(['symbol' => '$']);
+        }
+
+        $this->buildCache(true);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
