0d0e3510dbde7e76e0ecb66ef0bf3b13404e9b3b
Benjamin Beganović <k1pstabug@gmail.com>
2021-01-13 14:31:28 +0100

    fix issue with button

--- resources/views/portal/ninja2020/invoices/show/fullscreen.blade.php
+++ resources/views/portal/ninja2020/invoices/show/fullscreen.blade.php
@@ -15,7 +15,6 @@
                             <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
                                 <p translate>
                                     {{ ctrans('texts.invoice_still_unpaid') }}
-                                    <!-- This invoice is still not paid. Click the button to complete the payment. -->
                                 </p>
                             </div>
                         </div>
