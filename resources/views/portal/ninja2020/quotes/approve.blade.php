c4defd55d8eae6ef3984ad2cc48e95f70e35fc86
Benjamin Beganović <k1pstabug@gmail.com>
2021-01-20 13:32:49 +0100

    Change quotes terminology: - Changed "balance" to "amount" in approve page & quotes-table

--- resources/views/portal/ninja2020/quotes/approve.blade.php
+++ resources/views/portal/ninja2020/quotes/approve.blade.php
@@ -67,7 +67,7 @@
                                 </div>
                                 <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm leading-5 font-medium text-gray-500">
-                                        {{ ctrans('texts.balance') }}
+                                        {{ ctrans('texts.amount') }}
                                     </dt>
                                     <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                                         {{ App\Utils\Number::formatMoney($quote->balance, $quote->client) }}
