fa5281f5e5113b4b56a229151cfb9c7a4171324f
David Bomba <turbo124@gmail.com>
2021-01-22 21:18:05 +1100

    Security fixes + remove Cybersource gateway

---
+++ database/migrations/2021_01_22_101548_remove_cyber_source_gateway.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Gateway;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveCyberSourceGateway extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        /* No longer supported for V2 Omnipay */
+        $cyber = Gateway::where('provider', 'Cybersource')->first();
+        $cyber->payment_library_id = 2;
+        $cyber->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
