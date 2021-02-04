5baef3f69a1aa5d603fe73b63228f27bfb901c46
David Bomba <turbo124@gmail.com>
2021-02-04 11:08:56 +1100

    add start migration texts

--- resources/lang/en/texts.php
+++ resources/lang/en/texts.php
@@ -4136,6 +4136,8 @@ $LANG = array(
      'online_payments_minimum_note' => 'Note: Online payments are supported only if amount is bigger than $1 or currency equivalent.',
      'payment_token_not_found' => 'Payment token not found, please try again. If an issue still persist, try with another payment method',
 
+     //////////////////////////////////////
+     'start_migration' => 'Start Migration',
 );
 
 return $LANG;
