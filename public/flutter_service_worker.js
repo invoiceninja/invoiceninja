49687002249fe022fa5059977b69dd951a7dc229
hillelcoren <hillelcoren@gmail.com>
2021-02-22 19:49:19 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "9236c28434229999a2cd5a87f4c7141b",
+  "main.dart.js": "a05fae9620c7df06321479dbb57bd8b7",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
-"assets/NOTICES": "1c2603c5ad0cd648934fe488850b4dea",
+"assets/NOTICES": "53c432ede3c60b138e7bcbdf1e19f1ad",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/FontManifest.json": "cf3c681641169319e61b61bd0277378f",
 "assets/AssetManifest.json": "659dcf9d1baf3aed3ab1b9c42112bf8f",
