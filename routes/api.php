d1044a861ebb498d346b7ed4f7a4c656025e6c2d
David Bomba <turbo124@gmail.com>
2021-02-24 10:00:51 +1100

    Bug fix for TwoFactorController

--- routes/api.php
+++ routes/api.php
@@ -177,6 +177,6 @@ Route::match(['get', 'post'], 'payment_webhook/{company_key}/{company_gateway_id
     ->middleware(['guest', 'api_db'])
     ->name('payment_webhook');
 
-Route::post('postmark_webhook', 'PostMarkController@webhook');
+Route::post('api/v1/postmark_webhook', 'PostMarkController@webhook');
 
 Route::fallback('BaseController@notFound');
