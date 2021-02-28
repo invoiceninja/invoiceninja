23de4d6f64ff19bd11fd1fd3b7ee44b69793adde
hillelcoren <hillelcoren@gmail.com>
2021-02-28 20:31:20 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "f14d6aab305418b2115ea36d815e60e8",
+  "main.dart.js": "dbb73a940afa9a6ed17bcfce453637a5",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
@@ -29,7 +29,7 @@ const RESOURCES = {
 "assets/assets/images/google-icon.png": "0f118259ce403274f407f5e982e681c3",
 "assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "3e722fd57a6db80ee119f0e2c230ccff",
-"version.json": "c71c432fdc63e809b2f63fcc64edd8cd",
+"version.json": "b7c8971e1ab5b627fd2a4317c52b843e",
 "manifest.json": "77215c1737c7639764e64a192be2f7b8",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b",
 "/": "23224b5e03519aaa87594403d54412cf"
