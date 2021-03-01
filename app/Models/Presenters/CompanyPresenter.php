2ab76e909886a95ffacd18ca17feccc2b82dd029
David Bomba <turbo124@gmail.com>
2021-03-01 22:00:07 +1100

    v5.1.12

--- app/Models/Presenters/CompanyPresenter.php
+++ app/Models/Presenters/CompanyPresenter.php
@@ -36,7 +36,11 @@ class CompanyPresenter extends EntityPresenter
             $settings = $this->entity->settings;
         }
 
-        return (strlen($settings->company_logo) > 0) ? url('') . $settings->company_logo : 'https://www.invoiceninja.com/wp-content/uploads/2019/01/InvoiceNinja-Logo-Round-300x300.png';
+        if(strlen($settings->company_logo))
+            return asset($settings->company_logo);
+        else
+            return 'https://www.invoiceninja.com/wp-content/uploads/2019/01/InvoiceNinja-Logo-Round-300x300.png';
+
     }
 
     public function address($settings = null)
