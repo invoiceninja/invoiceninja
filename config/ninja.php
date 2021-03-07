6360f62ef373b3b78bb1fe3c52e59df3465ee0f9
= <turbo124@gmail.com>
2021-03-07 17:27:44 +1100

    Integrate phantom js secret default value into config

--- config/ninja.php
+++ config/ninja.php
@@ -29,7 +29,7 @@ return [
     'hash_salt' => env('HASH_SALT', ''),
     'currency_converter_api_key' => env('OPENEXCHANGE_APP_ID', ''),
     'enabled_modules' => 32767,
-    'phantomjs_key' => env('PHANTOMJS_KEY', false),
+    'phantomjs_key' => env('PHANTOMJS_KEY', 'a-demo-key-with-low-quota-per-ip-address'),
     'phantomjs_secret' => env('PHANTOMJS_SECRET', false),
     'phantomjs_pdf_generation' => env('PHANTOMJS_PDF_GENERATION', true),
     'trusted_proxies' => env('TRUSTED_PROXIES', false),
