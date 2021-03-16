9a4b7aeed7ced56557835c339945e7df2d8cf30f
hillelcoren <hillelcoren@gmail.com>
2021-03-16 06:33:36 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "3e722fd57a6db80ee119f0e2c230ccff",
 "assets/FontManifest.json": "cf3c681641169319e61b61bd0277378f",
 "/": "23224b5e03519aaa87594403d54412cf",
-"main.dart.js": "01a845261ee2c1b6937fd8a4af97ce3a",
+"main.dart.js": "2e2a5b63163eee6583babcae5028ca67",
 "version.json": "b7c8971e1ab5b627fd2a4317c52b843e",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b"
 };
