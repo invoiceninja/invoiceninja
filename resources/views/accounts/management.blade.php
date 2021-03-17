63eeb28bc4dbdcf7d3d80edcb913a40661c29473
Hillel Coren <hillelcoren@gmail.com>
2021-03-17 08:39:34 +0200

    Update languages

--- resources/views/accounts/management.blade.php
+++ resources/views/accounts/management.blade.php
@@ -238,23 +238,27 @@
 			</div>
 		</div>
 
-		@if(Auth::user()->eligibleForMigration())
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<h3 class="panel-title">{!! trans('texts.migrate_to_next_version') !!}</h3>
-				</div>
-				<div class="panel-body">
-					<div class="form-group">
-						<label for="modules" class="control-label col-lg-4 col-sm-4"></label>
-						<div class="col-lg-8 col-sm-8">
-							<div class="help-block">{{ trans('texts.migrate_intro_text')}}</div><br/>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">{!! trans('texts.migrate_to_next_version') !!}</h3>
+			</div>
+			<div class="panel-body">
+				<div class="form-group">
+					<label for="modules" class="control-label col-lg-4 col-sm-4"></label>
+					<div class="col-lg-8 col-sm-8">
+						<div class="help-block">{{ trans('texts.migrate_intro_text')}}</div><br/>
+						@if(Auth::user()->eligibleForMigration())
 							<a class="btn btn-primary btn-lg"
-						   		href="{{ url('/migration/start') }}">{!! trans('texts.start_migration') !!}</a>
-						</div>
+								href="{{ url('/migration/start') }}">{!! trans('texts.start_migration') !!}</a>
+						@else
+							{{ trans('texts.not_allowed') }}
+						@endif
+						<br/>
+						<a href="https://invoiceninja.github.io/docs/migration/" target="_blank">{{ trans('texts.learn_more') }}</a>
 					</div>
 				</div>
 			</div>
-		@endif
+		</div>
 
 		{!! Former::close() !!}
 
