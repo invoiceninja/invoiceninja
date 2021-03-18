ee15ea9434185931962e8b807622421c080bf29b
David Bomba <turbo124@gmail.com>
2021-03-18 11:58:10 +1100

    v5.1.27

---
+++ resources/views/billing-portal/purchase.blade.php
@@ -0,0 +1,17 @@
+@extends('portal.ninja2020.layout.clean')
+@section('meta_title', $billing_subscription->product->product_key)
+
+@section('body')
+    @livewire('billing-portal-purchase', ['billing_subscription' => $billing_subscription, 'contact' => auth('contact')->user(), 'hash' => $hash])
+@stop
+
+@push('footer')
+    <script>
+        function updateGatewayFields(companyGatewayId, paymentMethodId) {
+            document.getElementById('company_gateway_id').value = companyGatewayId;
+            document.getElementById('payment_method_id').value = paymentMethodId;
+        }
+
+        Livewire.on('beforePaymentEventsCompleted', () => document.getElementById('payment-method-form').submit());
+    </script>
+@endpush
