2c83abe43279f86bcaeac982e8985922ec8da058
David Bomba <turbo124@gmail.com>
2021-03-27 07:28:56 +1100

    Subscriptions

--- app/Repositories/SubscriptionRepository.php
+++ app/Repositories/SubscriptionRepository.php
@@ -13,16 +13,86 @@
 namespace App\Repositories;
 
 
+use App\Factory\InvoiceFactory;
+use App\Models\Client;
+use App\Models\ClientContact;
+use App\Models\Invoice;
+use App\Models\InvoiceInvitation;
 use App\Models\Subscription;
+use App\Utils\Traits\CleanLineItems;
+use Illuminate\Support\Facades\DB;
 
 class SubscriptionRepository extends BaseRepository
 {
+    use CleanLineItems;
+
     public function save($data, Subscription $subscription): ?Subscription
     {
-        $subscription
-            ->fill($data)
-            ->save();
+        $subscription->fill($data);
+            
+        $subscription->price = $this->calculatePrice($subscription);
+
+        $subscription->save();
 
         return $subscription;
     }
+
+    private function calculatePrices($subscription) :array
+    {
+
+		DB::beginTransaction();
+
+		$data = [];
+
+        $client = Client::factory()->create([
+                'user_id' => $subscription->user_id,
+                'company_id' => $subscription->company_id,
+                'group_settings_id' => $subscription->group_id,
+                'country_id' => $subscription->company->settings->country_id,
+            ]);
+
+        $contact = ClientContact::factory()->create([
+                'user_id' => $subscription->user_id,
+                'company_id' => $subscription->company_id,
+                'client_id' => $client->id,
+                'is_primary' => 1,
+                'send_email' => true,
+            ]);
+
+        $invoice = InvoiceFactory::create($subscription->company_id, $subscription->user_id);
+
+        $invitation = InvoiceInvitation::factory()->create([
+	                'user_id' => $subscription->user_id,
+	                'company_id' => $subscription->company_id,
+                    'invoice_id' => $invoice->id,
+                    'client_contact_id' => $contact->id,
+        ]);
+
+        $invoice->setRelation('invitations', $invitation);
+        $invoice->setRelation('client', $client);
+        $invoice->setRelation('company', $subscription->company);
+        $invoice->load('client');
+
+        $invoice->line_items = $this->generateLineItems($subscription);
+        $data['price'] = $invoice->calc()->getTotal();
+        
+        $invoice->discount = $subscription->promo_discount;
+        $invoice->is_amount_discount = $subscription->is_amount_discount;
+
+        $data['promo_price'] = $invoice->calc()->getTotal();
+
+        DB::rollBack();
+
+        return $data;
+    }
+
+    private function generateLineItems($subscription)
+    {
+
+    	$line_items = [];
+
+    	$line_items = $this->cleanItems($line_items);
+
+    }
+
 }
---
+++ database/migrations/2021_03_26_201148_add_price_column_to_subscriptions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPriceColumnToSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->decimal('price', 20, 6)->default(0);
+            $table->decimal('promo_price', 20, 6)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            //
+        });
+    }
+}
