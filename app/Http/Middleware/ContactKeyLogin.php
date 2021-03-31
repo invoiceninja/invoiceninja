404ee307fd9018157fb50caf1caa986b4b532342
David Bomba <turbo124@gmail.com>
2021-03-31 11:07:54 +1100

    Magic link maker

--- app/Http/Middleware/ContactKeyLogin.php
+++ app/Http/Middleware/ContactKeyLogin.php
@@ -17,6 +17,7 @@ use App\Models\ClientContact;
 use Auth;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ContactKeyLogin
 {
@@ -36,30 +37,48 @@ class ContactKeyLogin
         if (Auth::guard('contact')->check()) {
             Auth::guard('contact')->logout();
         }
+nlog("merp");
+nlog($request->has('magic_link'));
+nlog($request->input('magic_link'));
+nlog($request->all());
 
         if ($request->segment(3) && config('ninja.db.multi_db_enabled')) {
             if (MultiDB::findAndSetDbByContactKey($request->segment(3))) {
-                $client_contact = ClientContact::where('contact_key', $request->segment(3))->first();
-                Auth::guard('contact')->login($client_contact, true);
+
+            if($client_contact = ClientContact::where('contact_key', $request->segment(3))->first()){
+                auth()->guard('contact')->login($client_contact, true);
                 return redirect()->to('client/dashboard');
+             }
+
             }
-        } elseif ($request->has('contact_key')) {
+        } elseif ($request->segment(2) && $request->segment(2) == 'key_login' && $request->segment(3)) {
             if ($client_contact = ClientContact::where('contact_key', $request->segment(3))->first()) {
-                Auth::guard('contact')->login($client_contact, true);
+                auth()->guard('contact')->login($client_contact, true);
                 return redirect()->to('client/dashboard');
             }
         } elseif ($request->has('client_hash') && config('ninja.db.multi_db_enabled')) {
             if (MultiDB::findAndSetDbByClientHash($request->input('client_hash'))) {
-                $client = Client::where('client_hash', $request->input('client_hash'))->first();
-                Auth::guard('contact')->login($client->primary_contact()->first(), true);
-                return redirect()->to('client/dashboard');
+
+                if($client = Client::where('client_hash', $request->input('client_hash'))->first()){
+                    auth()->guard('contact')->login($client->primary_contact()->first(), true);
+                    return redirect()->to('client/dashboard');
+                }
             }
         } elseif ($request->has('client_hash')) {
             if ($client = Client::where('client_hash', $request->input('client_hash'))->first()) {
                 Auth::guard('contact')->login($client->primary_contact()->first(), true);
                 return redirect()->to('client/dashboard');
             }
+        } elseif ($request->segment(2) && $request->segment(2) == 'magic_link' && $request->segment(3)) {
+            $contact_email = Cache::get($request->segment(3));
+nlog("double merp");
+            if($client_contact = ClientContact::where('email', $contact_email)->first()){
+                Auth::guard('contact')->login($client_contact, true);
+                return redirect()->to('client/dashboard');
+            }
         }
+
+nlog("exit");
 
         return $next($request);
     }
