bb943e34998cdf8c4f33a77ab1b2000bba3641b2
hillelcoren <hillelcoren@gmail.com>
2021-04-13 13:54:47 +0000

    New admin portal build

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {
 "assets/assets/images/payment_types/paypal.png": "8e06c094c1871376dfea1da8088c29d1",
 "assets/assets/images/payment_types/maestro.png": "e533b92bfb50339fdbfa79e3dfe81f08",
 "assets/FontManifest.json": "cf3c681641169319e61b61bd0277378f",
-"main.dart.js": "589fe812bacbb3ceb0a5e0714f69a8cd",
+"main.dart.js": "7387bc998747d4eda2e0fec919800d62",
 "version.json": "e021a7a1750aa3e7d1d89b51ac9837e9"
 };
 
