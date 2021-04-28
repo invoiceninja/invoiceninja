f26948b76f6abfb77588e77671d6dc5e40af8fe7
David Bomba <turbo124@gmail.com>
2021-04-29 09:19:00 +1000

    Log query analytics

---
+++ app/DataMapper/Analytics/DbQuery.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\DataMapper\Analytics;
+
+class DbQuery
+{
+    /**
+     * The type of Sample.
+     *
+     * Monotonically incrementing counter
+     *
+     * 	- counter
+     *
+     * @var string
+     */
+    public $type = 'mixed_metric';
+
+    /**
+     * The name of the counter.
+     * @var string
+     */
+    public $name = 'db.queries';
+
+    /**
+     * The datetime of the counter measurement.
+     *
+     * date("Y-m-d H:i:s")
+     *
+     * @var DateTime
+     */
+    public $datetime;
+
+    /**
+     * The Class failure name
+     * set to 0.
+     *
+     * @var string
+     */
+    public $string_metric5 = 'uri';
+    public $string_metric6 = 'uri';
+
+    /**
+     * The counter
+     * set to 1.
+     *
+     * @var string
+     */
+    public $int_metric1 = 1;
+
+    public $double_metric2 = 1;
+
+    public function __construct($string_metric5, $string_metric6, $int_metric1, $double_metric2) {
+        $this->string_metric5 = $string_metric5;
+        $this->double_metric2 = $double_metric2;
+    }
+}
