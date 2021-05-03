cc40b52d67b9712774d1459ab31c08a00a69dc4a
David Bomba <turbo124@gmail.com>
2021-05-03 14:49:15 +1000

    Add fallback for replyto name

--- app/Jobs/Mail/NinjaMailerJob.php
+++ app/Jobs/Mail/NinjaMailerJob.php
@@ -75,7 +75,7 @@ class NinjaMailerJob implements ShouldQueue
 
         if (strlen($this->nmo->settings->reply_to_email) > 1) {
             
-            $reply_to_name = strlen($this->nmo->settings->reply_to_name) > 1 ? $this->nmo->settings->reply_to_name : $this->nmo->company->present()->name();
+            $reply_to_name = strlen($this->nmo->settings->reply_to_name) > 3 ? $this->nmo->settings->reply_to_name : $this->nmo->settings->reply_to_email;
             $this->nmo->mailable->replyTo($this->nmo->settings->reply_to_email, $reply_to_name);
 
         }
