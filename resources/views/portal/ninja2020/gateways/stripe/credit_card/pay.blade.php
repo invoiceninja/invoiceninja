7d245f5ac302d181b3577c607ccaca5a3884262d
David Bomba <turbo124@gmail.com>
2021-05-12 00:09:22 +1000

    Fixes for Stripe Connect

--- resources/views/portal/ninja2020/gateways/stripe/credit_card/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/credit_card/pay.blade.php
@@ -1,8 +1,12 @@
 @extends('portal.ninja2020.layout.payments', ['gateway_title' => 'Credit card', 'card_title' => 'Credit card'])
 
 @section('gateway_head')
-    <meta name="stripe-publishable-key" content="{{ $gateway->getPublishableKey() }}">
+    @if($gateway->company_gateway->getConfigField('account_id'))
     <meta name="stripe-account-id" content="{{ $gateway->company_gateway->getConfigField('account_id') }}">
+    <meta name="stripe-publishable-key" content="{{ config('ninja.ninja_stripe_publishable_key') }}">
+    @else
+    <meta name="stripe-publishable-key" content="{{ $gateway->getPublishableKey() }}">
+    @endif
     <meta name="stripe-secret" content="{{ $intent->client_secret }}">
     <meta name="only-authorization" content="">
     <meta name="client-postal-code" content="{{ $client->postal_code ?? '' }}">
