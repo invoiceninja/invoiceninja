90ee9a4ca6105c8693772f420e425e3f67863c93
David Bomba <turbo124@gmail.com>
2021-05-17 23:15:46 +1000

    Fixes for mailable

--- app/Mail/DownloadBackup.php
+++ app/Mail/DownloadBackup.php
@@ -1,18 +1,23 @@
 <?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
 
 namespace App\Mail;
 
 use App\Models\Company;
-use App\Utils\Traits\MakesInvoiceHtml;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\File;
 
 class DownloadBackup extends Mailable
 {
-    // use Queueable, SerializesModels;
-    use MakesInvoiceHtml;
 
     public $file_path;
 
@@ -30,20 +35,19 @@ class DownloadBackup extends Mailable
      */
     public function build()
     {
-        $template = File::get(resource_path('views/email/admin/download_files.blade.php'));
-
         $company = Company::where('company_key', $this->company->company_key)->first();
-
-        $view = $this->renderView($template, [
-            'url' => $this->file_path,
-            'logo' => $company->present()->logo,
-            'whitelabel' => $company->account->isPaid() ? true : false,
-            'settings' => $company->settings,
-            'greeting' => $company->present()->name(),
-        ]);
 
         return $this->from(config('mail.from.address'), config('mail.from.name'))
                     ->subject(ctrans('texts.download_backup_subject', ['company' => $company->present()->name()]))
-                    ->html($view);
+                    ->markdown(
+                        'email.admin.download_files',
+                        [
+                            'url' => $this->file_path,
+                            'logo' => $company->present()->logo,
+                            'whitelabel' => $company->account->isPaid() ? true : false,
+                            'settings' => $company->settings,
+                            'greeting' => $company->present()->name(),
+                        ]
+                    );
     }
 }
