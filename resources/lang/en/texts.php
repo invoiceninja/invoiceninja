1e1b223d933c95767cd1ba520603c2a0f8260444
Benjamin Beganović <k1pstabug@gmail.com>
2021-05-20 13:37:50 +0200

    Update title for contact passwordless login

--- resources/lang/en/texts.php
+++ resources/lang/en/texts.php
@@ -4251,6 +4251,7 @@ $LANG = array(
     'download_backup_subject' => 'Your company backup is ready for download',
     'contact_details' => 'Contact Details',
     'download_backup_subject' => ':company backup is ready for download',
+    'account_passwordless_login' => 'Account passwordless login',
 );
 
 return $LANG;
