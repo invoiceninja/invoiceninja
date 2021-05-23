9d21b4fd57cf262433e2e009d55c6309db5a5a7b
David Bomba <turbo124@gmail.com>
2021-05-23 18:43:50 +1000

    Refactor LoginController

---
+++ app/Utils/Traits/User/LoginCache.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\Utils\Traits\User;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+
+trait LoginCache
+{
+
+    public function setLoginCache($user)
+    {
+
+        $timeout = $user->company()->default_password_timeout;
+
+        if($timeout == 0)
+            $timeout = 30*60*1000*1000;
+        else
+            $timeout = $timeout/1000;
+
+        Cache::put($user->hashed_id.'_'.$user->account_id.'_logged_in', Str::random(64), $timeout);
+
+    }
+
+}
