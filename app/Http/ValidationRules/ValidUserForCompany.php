3c99c6503341df0c7dc4d13300273efb36a37236
David Bomba <turbo124@gmail.com>
2021-05-24 14:24:16 +1000

    FIxes for multidb

--- app/Http/ValidationRules/ValidUserForCompany.php
+++ app/Http/ValidationRules/ValidUserForCompany.php
@@ -26,7 +26,14 @@ class ValidUserForCompany implements Rule
      */
     public function passes($attribute, $value)
     {
-        return MultiDB::checkUserAndCompanyCoExist($value, auth()->user()->company()->company_key, auth()->user()->company()->id);
+        $current_db = config('database.default');
+
+        $result = MultiDB::checkUserAndCompanyCoExist($value, auth()->user()->company()->company_key, auth()->user()->company()->id);
+
+
+        MultiDB::setDb($current_db);
+
+        return $result;
     }
 
     /**
--- app/Libraries/MultiDB.php
+++ app/Libraries/MultiDB.php
@@ -289,6 +289,34 @@ class MultiDB
         return false;
     }
 
+    public function randomSubdomainGenerator()
+    {
+        $current_db = config('database.default');  
+              
+            do {
+                $length = 8;
+                $string = '';
+                $vowels = array("a","e","i","o","u");  
+                $consonants = array(
+                    'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 
+                    'n', 'p', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+                );  
+
+                $max = $length / 2;
+                for ($i = 1; $i <= $max; $i++)
+                {
+                    $string .= $consonants[rand(0,19)];
+                    $string .= $vowels[rand(0,4)];
+                }
+            }
+            while(!self::checkDomainAvailable($string));
+
+        self::setDb($current_db);
+
+        return $string;
+    }
+
+
     /**
      * @param $database
      */
