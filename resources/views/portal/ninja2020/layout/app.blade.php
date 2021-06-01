e69cd0699e0604535da76dde700327618ecae23d
David Bomba <turbo124@gmail.com>
2021-06-01 20:51:38 +1000

    Fixes for CORS

--- resources/views/portal/ninja2020/layout/app.blade.php
+++ resources/views/portal/ninja2020/layout/app.blade.php
@@ -43,7 +43,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="@yield('meta_description')"/>
-
+        
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
