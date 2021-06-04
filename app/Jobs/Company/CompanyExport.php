6bc26c476b159ed12b4d9ac13069c40aec55b3f7
= <turbo124@gmail.com>
2021-06-04 20:32:43 +1000

    Company Import

--- app/Jobs/Company/CompanyExport.php
+++ app/Jobs/Company/CompanyExport.php
@@ -168,7 +168,7 @@ class CompanyExport implements ShouldQueue
 
         $this->export_data['company'] = $this->company->toArray();
 
-        $this->export_data['company_gateways'] = $this->company->company_gateways->withTrashed()->cursor()->map(function ($company_gateway){
+        $this->export_data['company_gateways'] = $this->company->company_gateways()->withTrashed()->cursor()->map(function ($company_gateway){
 
             $company_gateway = $this->transformArrayOfKeys($company_gateway, ['company_id', 'user_id']);
             $company_gateway->config = decrypt($company_gateway->config);
