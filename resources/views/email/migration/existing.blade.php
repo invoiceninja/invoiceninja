690acec7f8012e06497e4c8b0673ee4e48790585
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-07 18:11:23 +0200

    Admin: migration / existing.blade.php

--- resources/views/email/migration/existing.blade.php
+++ resources/views/email/migration/existing.blade.php
@@ -1,18 +1,6 @@
-@component('email.template.master', ['design' => 'light', 'settings' => $settings])
-
-    @slot('header')
-        @include('email.components.header', ['logo' => $logo])
-    @endslot
-
-    <h2>{{ctrans('texts.migration_already_completed')}}</h2>
-
-    <p>{{ctrans('texts.migration_already_completed_desc', ['company_name' => $company_name])}}</p>
-
-    @if(isset($whitelabel) && !$whitelabel)
-        @slot('footer')
-            @component('email.components.footer', ['url' => 'https://invoiceninja.com', 'url_text' => '&copy; InvoiceNinja'])
-                For any info, please visit InvoiceNinja.
-            @endcomponent
-        @endslot
-    @endif
+@component('email.template.admin-light', ['logo' => $logo, 'settings' => $settings])
+    <div class="center">
+        <h2>{{ ctrans('texts.migration_already_completed') }}</h2>
+        <p>{{ ctrans('texts.migration_already_completed_desc', ['company_name' => $company_name]) }}</p>
+    </div>
 @endcomponent
