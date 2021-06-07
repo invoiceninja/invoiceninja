22f2b2c09528a048836087f11d22ba42a7e415d7
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-07 18:04:29 +0200

    Admin: notification.blade.php

--- resources/views/email/admin/notification.blade.php
+++ resources/views/email/admin/notification.blade.php
@@ -1,18 +1,6 @@
-@component('email.template.master', ['design' => 'light', 'settings' => $settings])
-
-    @slot('header')
-        @include('email.components.header', ['logo' => $logo])
-    @endslot
-
-    <h2>{!! $title !!}</h2>
-
-    <p>{!! $body !!}</p>
-
-    @if(isset($whitelabel) && !$whitelabel)
-        @slot('footer')
-            @component('email.components.footer', ['url' => 'https://invoiceninja.com', 'url_text' => '&copy; InvoiceNinja'])
-                For any info, please visit InvoiceNinja.
-            @endcomponent
-        @endslot
-    @endif
+@component('email.template.admin-light', ['logo' => $logo, 'settings' => $settings])
+    <div class="center">
+        <h2>{!! $title !!}</h2>
+        <p>{!! $body !!}</p>
+    </div>
 @endcomponent
