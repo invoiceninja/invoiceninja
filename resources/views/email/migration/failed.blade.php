456222eda8bdeac0e68b1fbccb2483d2124f7479
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-07 18:11:58 +0200

    Admin: migration / failed.blade.php

--- resources/views/email/migration/failed.blade.php
+++ resources/views/email/migration/failed.blade.php
@@ -1,18 +1,16 @@
-@component('email.template.master', ['design' => 'light', 'settings' => $settings])
-    @slot('header')
-        @include('email.components.header', ['logo' => 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png'])
-    @endslot
+@component('email.template.admin-light', ['logo' => 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png', 'settings' => $settings])
+    <div class="center">
+        <h1>Whoops, migration failed for {{ $company->present()->name() }}.</h1>
+        <p>Looks like your migration failed. Here's the error message:</p>
 
-    <h1>Whoops, migration failed for {{ $company->present()->name() }}.</h1>
-    <p>Looks like your migration failed. Here's the error message:</p>
-
-    <pre>
-    	@if(\App\Utils\Ninja::isHosted())
-	        {!! $exception->getMessage() !!}
-            {!! $content !!}
-    	@else
-        {!! $exception->getMessage() !!}
-        {!! $content !!}
-        @endif
-    </pre>
+        <pre>
+    	    @if(\App\Utils\Ninja::isHosted())
+                {!! $exception->getMessage() !!}
+                {!! $content !!}
+            @else
+                {!! $exception->getMessage() !!}
+                {!! $content !!}
+            @endif
+        </pre>
+    </div>
 @endcomponent
