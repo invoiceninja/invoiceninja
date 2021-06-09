32fc72913b4f534c8272d800e73cc0c697b5aca5
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-09 17:16:16 +0200

    app/Mail/ExistingMigration.php

--- resources/views/email/migration/existing.blade.php
+++ resources/views/email/migration/existing.blade.php
@@ -1,6 +1,6 @@
-@component('email.template.admin-light', ['logo' => $logo, 'settings' => $settings])
+@component('email.template.admin', ['logo' => $logo, 'settings' => $settings])
     <div class="center">
-        <h2>{{ ctrans('texts.migration_already_completed') }}</h2>
-        <p>{{ ctrans('texts.migration_already_completed_desc', ['company_name' => $company_name]) }}</p>
+        <h1>{{ ctrans('texts.migration_already_completed') }}</h1>
+        <p>{!! ctrans('texts.migration_already_completed_desc', ['company_name' => $company_name]) !!}</p>
     </div>
 @endcomponent
