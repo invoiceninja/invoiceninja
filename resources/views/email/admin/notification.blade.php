9c45111a4e4d1a3c3b0c5ebc5debe09d0d4afb6e
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-09 16:57:16 +0200

    app/Mail/User/UserLoggedIn.php

--- resources/views/email/admin/notification.blade.php
+++ resources/views/email/admin/notification.blade.php
@@ -1,4 +1,4 @@
-@component('email.template.admin-light', ['logo' => $logo, 'settings' => $settings])
+@component('email.template.admin', ['logo' => $logo, 'settings' => $settings])
     <div class="center">
         <h2>{!! $title !!}</h2>
         <p>{!! $body !!}</p>
