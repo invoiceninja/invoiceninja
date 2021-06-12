a2fe0cbb6a9bd220d8e275f2ba476d9cff98e3ba
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-12 23:19:56 +0200

    SupportMessageSent

--- resources/views/email/support/cancellation.blade.php
+++ resources/views/email/support/cancellation.blade.php
@@ -1,3 +1,3 @@
-@component('email.template.admin-light', ['logo' => 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png'])
+@component('email.template.admin', ['logo' => 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png'])
     {!! $message !!}
 @endcomponent
--- resources/views/email/support/message.blade.php
+++ resources/views/email/support/message.blade.php
@@ -1,4 +1,4 @@
-@component('email.template.admin-light', ['logo' => 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png'])
+@component('email.template.admin', ['logo' => $logo ?? 'https://www.invoiceninja.com/wp-content/uploads/2015/10/logo-white-horizontal-1.png'])
     {{-- Body --}}
     {{ $message }}
 
