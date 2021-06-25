84281e2323c789ab563bf9616bf6f1d2286e520c
Simponic <loganhunt@simponic.xyz>
2021-06-25 13:20:10 -0600

    Fix Timezone issue

--- app/Ninja/Reports/TaskReport.php
+++ app/Ninja/Reports/TaskReport.php
@@ -3,6 +3,7 @@
 namespace App\Ninja\Reports;
 
 use App\Models\Task;
+use Illuminate\Support\Facades\Log;
 use Utils;
 use Auth;
 
@@ -36,17 +37,25 @@ class TaskReport extends AbstractReport
     public function run()
     {
         $account = Auth::user()->account;
-        $startDate = date_create($this->startDate);
-        $endDate = date_create($this->endDate);
+        $startDate = date_create($this->startDate)->add(new \DateInterval("PT7H")); // Add 7 hours to convert from UTC to MST;
+        $endDate = date_create($this->endDate)->add(new \DateInterval("PT7H"));
+
         $subgroup = $this->options['subgroup'];
 
         $tasks = Task::scope()
                     ->orderBy('created_at', 'desc')
                     ->with('client.contacts', 'project', 'account', 'user')
                     ->withArchived()
-                    ->dateRange($startDate, $endDate);
-
-        foreach ($tasks->get() as $task) {
+                    ->get()
+                    ->filter(function ($x) use ($startDate, $endDate) {
+                        $log = json_decode($x->time_log) ?: false;
+                        if ($log){
+                            $date = $log[0][0];
+                            return (($startDate->getTimestamp() <= $date) && ($date <= $endDate->getTimestamp()));
+                        }
+                        return false;
+                    });
+        foreach ($tasks as $task) {
             $duration = $task->getDuration($startDate->format('U'), $endDate->modify('+1 day')->format('U'));
             $amount = $task->getRate() * ($duration / 60 / 60);
             if ($task->client && $task->client->currency_id) {
