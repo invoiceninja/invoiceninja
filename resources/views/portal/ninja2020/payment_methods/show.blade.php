cca78a9d384b272ae798de2a51fede25f76b946f
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-25 09:15:57 +0200

    Ability to select primary payment method

--- resources/views/portal/ninja2020/payment_methods/show.blade.php
+++ resources/views/portal/ninja2020/payment_methods/show.blade.php
@@ -15,61 +15,61 @@
             <div>
                 <dl>
                     @if(!empty($payment_method->gateway_type->name) && !is_null($payment_method->gateway_type->name))
-                    <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium leading-5 text-gray-500">
-                            {{ ctrans('texts.payment_type') }}
-                        </dt>
-                        <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ ucfirst($payment_method->gateway_type->name) }}
-                        </dd>
-                    </div>
+                        <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium leading-5 text-gray-500">
+                                {{ ctrans('texts.payment_type') }}
+                            </dt>
+                            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ ucfirst($payment_method->gateway_type->name) }}
+                            </dd>
+                        </div>
                     @endif
 
                     @if(!empty($payment_method->meta) && !is_null($payment_method->meta))
-                    <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium leading-5 text-gray-500">
-                            {{ ctrans('texts.type') }}
-                        </dt>
-                        <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ optional($payment_method->meta)->brand }}
-                            {{ optional($payment_method->meta)->scheme }}
-                        </dd>
-                    </div>
+                        <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium leading-5 text-gray-500">
+                                {{ ctrans('texts.type') }}
+                            </dt>
+                            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ optional($payment_method->meta)->brand }}
+                                {{ optional($payment_method->meta)->scheme }}
+                            </dd>
+                        </div>
                     @endif
 
                     @if(!empty($payment_method->meta->last4) && !is_null($payment_method->meta->last4))
-                    <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium leading-5 text-gray-500">
-                            {{ ctrans('texts.card_number') }}
-                        </dt>
-                        <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                            **** {{ ucfirst($payment_method->meta->last4) }}
-                        </dd>
-                    </div>
+                        <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium leading-5 text-gray-500">
+                                {{ ctrans('texts.card_number') }}
+                            </dt>
+                            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                **** {{ ucfirst($payment_method->meta->last4) }}
+                            </dd>
+                        </div>
                     @endif
-                    
+
                     @if(!empty($payment_method->created_at) && !is_null($payment_method->created_at))
-                    <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium leading-5 text-gray-500">
-                            {{ ctrans('texts.date_created') }}
-                        </dt>
-                        <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $payment_method->formatDateTimestamp($payment_method->created_at, auth()->user()->client->date_format()) }}
-                        </dd>
-                    </div>
+                        <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium leading-5 text-gray-500">
+                                {{ ctrans('texts.date_created') }}
+                            </dt>
+                            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ $payment_method->formatDateTimestamp($payment_method->created_at, auth()->user()->client->date_format()) }}
+                            </dd>
+                        </div>
                     @endif
 
                     @if(!empty($payment_method->is_default) && !is_null($payment_method->is_default))
-                    <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        <dt class="text-sm font-medium leading-5 text-gray-500">
-                            {{ ctrans('texts.default') }}
-                        </dt>
-                        <div class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                            {{ $payment_method->is_default ? ctrans('texts.yes') : ctrans('texts.no') }}
+                        <div class="px-4 py-5 bg-gray-50 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium leading-5 text-gray-500">
+                                {{ ctrans('texts.default') }}
+                            </dt>
+                            <div class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                                {{ $payment_method->is_default ? ctrans('texts.yes') : ctrans('texts.no') }}
+                            </div>
                         </div>
-                    </div>
                     @endif
-                    
+
                     @isset($payment_method->meta->exp_month)
                         <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium leading-5 text-gray-500">
@@ -83,7 +83,10 @@
                 </dl>
             </div>
         </div>
-        <div class="mt-4 mb-4 bg-white shadow sm:rounded-lg" translate>
+
+        @livewire('payment-methods.update-default-method', ['token' => $payment_method, 'client' => $client])
+
+        <div class="mt-4 mb-4 bg-white shadow sm:rounded-lg">
             <div class="px-4 py-5 sm:p-6">
                 <div class="sm:flex sm:items-start sm:justify-between">
                     <div>
@@ -108,4 +111,12 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('footer')
+    <script>
+        Livewire.on('UpdateDefaultMethod::method-updated', event => {
+            document.querySelector('span[data-ref=success-label]').classList.remove('hidden');
+        });
+    </script>
 @endsection
