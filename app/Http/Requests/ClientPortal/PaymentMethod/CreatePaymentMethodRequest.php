31e138c41eb6f3dc5ba28257aeeeb32b64f86e29
Benjamin Beganović <k1pstabug@gmail.com>
2021-06-29 12:42:44 +0200

    CreatePaymentMethodRequest.php allow only enabled methods to pass

---
+++ app/Http/Requests/ClientPortal/PaymentMethod/CreatePaymentMethodRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\ClientPortal\PaymentMethod;
+
+use App\Http\Requests\Request;
+use App\Models\Client;
+use Illuminate\Foundation\Http\FormRequest;
+use function auth;
+use function collect;
+
+class CreatePaymentMethodRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        /** @var Client $client */
+        $client = auth()->user()->client;
+
+        $available_methods = [];
+
+        collect($client->service()->getPaymentMethods(1))
+            ->filter(function ($method) use (&$available_methods) {
+                $available_methods[] = $method['gateway_type_id'];
+            });
+
+        if (in_array($this->query('method'), $available_methods)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
