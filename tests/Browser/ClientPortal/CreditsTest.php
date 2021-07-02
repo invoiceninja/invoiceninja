7e6663ac0b3385528f40bd247ecd3ce8645393b5
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-02 16:53:57 +0200

    Credits: Assert page loads

---
+++ tests/Browser/ClientPortal/CreditsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser\ClientPortal;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\ClientPortal\Login;
+use Tests\DuskTestCase;
+
+class CreditsTest extends DuskTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        foreach (static::$browsers as $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        }
+    }
+
+    public function testPageLoads()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit(new Login())
+                ->auth()
+                ->visitRoute('client.credits.index')
+                ->assertSee('Credits');
+        });
+    }
+}
