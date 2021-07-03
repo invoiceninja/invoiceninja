5c1b9d5b2d147405bcc3876a16fa716a433fe1da
David Bomba <turbo124@gmail.com>
2021-07-04 09:02:16 +1000

    Fixes for client credits

--- app/Services/Client/ClientService.php
+++ app/Services/Client/ClientService.php
@@ -65,8 +65,10 @@ class ClientService
         return $this->client->credits()
                   ->where('is_deleted', false)
                   ->where('balance', '>', 0)
-                  ->whereDate('due_date', '<=', now()->format('Y-m-d'))
-                  ->orWhere('due_date', NULL)
+                  ->where(function ($query){
+                        $query->whereDate('due_date', '<=', now()->format('Y-m-d'))
+                              ->orWhereNull('due_date');
+                  })
                   ->orderBy('created_at','ASC');
     }
 
