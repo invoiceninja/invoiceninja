22acc8f14c48affbe9b48e2666b8f04407e446fa
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-05 11:17:50 +0200

    Update update licence/copyright on tests

--- tests/Browser/ClientPortal/RecurringInvoices.php
+++ tests/Browser/ClientPortal/RecurringInvoices.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
 namespace Tests\Browser\ClientPortal;
 
 use Laravel\Dusk\Browser;
