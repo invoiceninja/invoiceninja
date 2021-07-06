68959c86a82ef5f4a0172246a0197ffb084ffc96
hillelcoren <hillelcoren@gmail.com>
2021-07-06 11:09:49 +0000

    New admin portal [hosted]

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "dca91c54388f52eded692718d5a98b8b",
-"main.dart.js": "c9a155ad268da2f218d7ab38359a54e7",
+"main.dart.js": "56a956a8e0dff09a7519576e2db3894e",
 "/": "23224b5e03519aaa87594403d54412cf",
 "manifest.json": "ce1b79950eb917ea619a0a30da27c6a3",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "174c02fc4609e8fc4389f5d21f16a296",
