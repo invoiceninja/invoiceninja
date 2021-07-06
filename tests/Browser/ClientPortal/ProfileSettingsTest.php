a716330bcbafb5caea24856f277e4c5d1d983f1f
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-06 15:28:55 +0200

    Profile settings: Updating billing address

--- tests/Browser/ClientPortal/ProfileSettingsTest.php
+++ tests/Browser/ClientPortal/ProfileSettingsTest.php
@@ -69,8 +69,7 @@ class ProfileSettingsTest extends DuskTestCase
                         ->press('Save');
                 })
                 ->pause(2000)
-                ->refresh()
-                ->pause(2000);
+                ->refresh();
 
             $updated = [
                 'name' => $browser->value('#client_name'),
@@ -110,14 +109,56 @@ class ProfileSettingsTest extends DuskTestCase
                         ->click('button[data-ref="update-contact-details"]');
                 })
                 ->pause(2000)
-                ->refresh()
-                ->pause(2000);
+                ->refresh();
 
             $updated = [
                 'first_name' => $browser->value('#contact_first_name'),
                 'last_name' => $browser->value('#contact_last_name'),
                 'email_address' => $browser->value('#contact_email_address'),
                 'phone' => $browser->value('#contact_phone'),
+            ];
+
+            $this->assertSame($original, $updated);
+        });
+    }
+
+    public function testBillingAddressUpdate()
+    {
+        $original = [
+            'street' => $this->faker->streetName,
+            'apt' => $this->faker->streetAddress,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'postal_code' => $this->faker->postcode,
+        ];
+
+        $this->browse(function (Browser $browser) use ($original) {
+            $browser
+                ->visitRoute('client.invoices.index')
+                ->click('button[data-ref="client-profile-dropdown"]')
+                ->click('a[data-ref="client-profile-dropdown-settings"]')
+                ->waitForText('Client Information');
+
+            $browser
+                ->with('#update_billing_address', function (Browser $form) use ($original) {
+                    $form
+                        ->type('#address1', $original['street'])
+                        ->type('#address2', $original['apt'])
+                        ->type('#city', $original['city'])
+                        ->type('#state', $original['state'])
+                        ->type('#postal_code', $original['postal_code'])
+                        ->select('#country')
+                        ->press('Save');
+                })
+                ->pause(1000)
+                ->refresh();
+
+            $updated = [
+                'street' => $browser->value('#address1'),
+                'apt' => $browser->value('#address2'),
+                'city' => $browser->value('#city'),
+                'state' => $browser->value('#state'),
+                'postal_code' => $browser->value('#postal_code'),
             ];
 
             $this->assertSame($original, $updated);
