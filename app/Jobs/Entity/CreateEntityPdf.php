fdc29e18d343168a18f78e5ac93dc7fa87fc9374
David Bomba <turbo124@gmail.com>
2021-07-07 17:45:28 +1000

    Fixes for pdf emailing

--- app/Jobs/Entity/CreateEntityPdf.php
+++ app/Jobs/Entity/CreateEntityPdf.php
@@ -86,8 +86,8 @@ class CreateEntityPdf implements ShouldQueue
 
         $this->contact = $invitation->contact;
 
-        $this->disk = Ninja::isHosted() ? config('filesystems.default') : $disk;
-        
+        //$this->disk = Ninja::isHosted() ? config('filesystems.default') : $disk;
+        $this->disk = $disk;
         // $this->disk = $disk ?? config('filesystems.default');
     }
 
