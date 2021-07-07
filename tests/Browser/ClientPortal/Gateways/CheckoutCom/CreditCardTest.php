c40c27038be7dc6cc5f0651e607e2d4305fafdd5
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-07 15:58:23 +0200

    Gateways: Checkout.com: Credit card: Removing credit card

--- tests/Browser/ClientPortal/Gateways/CheckoutCom/CreditCardTest.php
+++ tests/Browser/ClientPortal/Gateways/CheckoutCom/CreditCardTest.php
@@ -99,4 +99,17 @@ class CreditCardTest extends DuskTestCase
                 ->waitForText('Details of the payment', 60);
         });
     }
+
+    public function testRemoveCreditCard()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visitRoute('client.payment_methods.index')
+                ->clickLink('View')
+                ->press('Remove Payment Method')
+                ->waitForText('Confirmation')
+                ->click('@confirm-payment-removal')
+                ->assertSee('Payment method has been successfully removed.');
+        });
+    }
 }
