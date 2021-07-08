f1ad846d17e468a0c0225d67b11a15ebd822e2b0
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-08 13:49:07 +0200

    Gateways: Authorize.net: Credit card: Pay with new card

---
+++ tests/Browser/ClientPortal/Gateways/AuthorizeNet/CreditCardTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace Tests\Browser\ClientPortal\Gateways\AuthorizeNet;
+
+use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\ClientPortal\Login;
+use Tests\DuskTestCase;
+
+class CreditCardTest extends DuskTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (getenv('GITHUB_ACTIONS')) {
+            $this->markTestSkipped('Skipping Authorize.net (GitHub Actions)');
+        }
+
+        foreach (static::$browsers as $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        }
+
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit(new Login())
+                ->auth();
+        });
+    }
+
+    public function testPayWithNewCard()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visitRoute('client.invoices.index')
+                ->click('@pay-now')
+                ->press('Pay Now')
+                ->clickLink('Credit Card')
+                ->type('card-number', '4007000000027')
+                ->type('card-holders-name', 'John Doe')
+                ->type('.expiry', '12/28')
+                ->type('cvc', '100')
+                ->press('Pay Now')
+                ->waitForText('Details of the payment', 60);
+        });
+    }
+}
