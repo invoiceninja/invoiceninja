9ce09a8b0505a308c78da5c78fdde455c22adaa0
Benjamin Beganović <k1pstabug@gmail.com>
2021-07-12 14:51:43 +0200

    Update Dusk script

--- dusk.sh
+++ dusk.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+n=1
+TYPE=${!n}
+
+if [ -z "$TYPE" ]; then
+    TYPE="all"
+fi
+
+echo "$ RUNNING: '$TYPE'"
 
 echo "$ php artisan optimize"
 php artisan optimize
 
+echo "=========================================="
+
 GENERIC_TESTS=`find tests/Browser/ClientPortal/ -maxdepth 1  -type f -name '*.php'`
+
+if [ $TYPE == 'gateways' ]; then
+    GENERIC_TESTS=""
+
+    echo "$ Skippping generic tests."
+    echo "=========================================="
+fi
 
 for TEST_CLASS in $GENERIC_TESTS; do
     echo "Test class: $TEST_CLASS"
@@ -15,12 +32,19 @@ for TEST_CLASS in $GENERIC_TESTS; do
     php artisan ninja:create-single-account &> /dev/null
 
     echo "$ php artisan dusk $TEST_CLASS"
-    php artisan dusk --stop-on-error --stop-on-failure $TEST_CLASS || exit 1
+    php artisan dusk ${@:2} --stop-on-error --stop-on-failure $TEST_CLASS || exit 1
 
     echo "=========================================="
 done || exit 1
 
 GATEWAY_TESTS=`find tests/Browser/ClientPortal/Gateways/ -type f -name '*.php'`
+
+if [ $TYPE == 'generic' ]; then
+    GATEWAY_TESTS=""
+
+    echo "$ Skippping gateway tests."
+    echo "=========================================="
+fi
 
 for TEST_CLASS in $GATEWAY_TESTS; do
     echo "Test class: $TEST_CLASS"
@@ -32,7 +56,7 @@ for TEST_CLASS in $GATEWAY_TESTS; do
     php artisan ninja:create-single-account &> /dev/null
 
     echo "$ php artisan dusk $TEST_CLASS"
-    php artisan dusk --stop-on-error --stop-on-failure $TEST_CLASS || exit 1
+    php artisan dusk ${@:2} --stop-on-error --stop-on-failure $TEST_CLASS || exit 1
 
     echo "=========================================="
 done || exit 1
