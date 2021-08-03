373f169b3931bb04268e5e52dd62ce07fe0c901f
Benjamin Beganović <k1pstabug@gmail.com>
2021-08-03 14:27:08 +0200

    Extract credit card scripts into standalone JS file

--- webpack.mix.js
+++ webpack.mix.js
@@ -81,6 +81,10 @@ mix.js("resources/js/app.js", "public/js")
     .js(
         "resources/js/clients/payment_methods/wepay-bank-account.js",
         "public/js/clients/payment_methods/wepay-bank-account.js"
+    )
+    .js(
+        "resources/js/clients/payments/mollie-credit-card.js",
+        "public/js/clients/payments/mollie-credit-card.js"
     );
 
 mix.copyDirectory('node_modules/card-js/card-js.min.css', 'public/css/card-js.min.css');
