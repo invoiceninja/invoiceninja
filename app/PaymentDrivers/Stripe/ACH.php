066c84405055dc72a69d092750186400996b6670
David Bomba <turbo124@gmail.com>
2021-08-04 12:15:13 +1000

    Fixes for Stripe

--- app/PaymentDrivers/Stripe/ACH.php
+++ app/PaymentDrivers/Stripe/ACH.php
@@ -184,7 +184,7 @@ class ACH
             return $this->processUnsuccessfulPayment($state);
         } catch (Exception $e) {
             if ($e instanceof CardException) {
-                return redirect()->route('client.payment_methods.verification', ['payment_method' => ClientGatewayToken::first()->hashed_id, 'method' => GatewayType::BANK_TRANSFER]);
+                return redirect()->route('client.payment_methods.verification', ['payment_method' => $source, 'method' => GatewayType::BANK_TRANSFER]);
             }
 
             throw new PaymentFailed($e->getMessage(), $e->getCode());
