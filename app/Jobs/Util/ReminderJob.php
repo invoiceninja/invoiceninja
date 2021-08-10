3d7b094677d57755755575247b9cc31d597ceea8
David Bomba <turbo124@gmail.com>
2021-08-11 07:48:34 +1000

    minor fixes

--- app/Jobs/Util/ReminderJob.php
+++ app/Jobs/Util/ReminderJob.php
@@ -177,6 +177,7 @@ class ReminderJob implements ShouldQueue
         /**Refresh Invoice values*/
         $invoice = $invoice->calc()->getInvoice();
 
+        nlog("adjusting client balance and invoice balance by ". ($invoice->balance - $temp_invoice_balance));
         $invoice->client->service()->updateBalance($invoice->balance - $temp_invoice_balance)->save();
         $invoice->ledger()->updateInvoiceBalance($invoice->balance - $temp_invoice_balance, "Late Fee Adjustment for invoice {$invoice->number}");
 
