5c8976a50f8d4196a0d227504a7fea20fa4a6e2e
= <turbo124@gmail.com>
2021-08-14 20:09:13 +1000

    Square payments

--- resources/views/portal/ninja2020/gateways/square/credit_card/authorize.blade.php
+++ resources/views/portal/ninja2020/gateways/square/credit_card/authorize.blade.php
@@ -32,27 +32,30 @@
         const appId = "{{ $gateway->company_gateway->getConfigField('applicationId') }}";
         const locationId = "{{ $gateway->company_gateway->getConfigField('locationId') }}";
 
-         async function initializeCard(payments) {
-           const card = await payments.card();
-           await card.attach('#card-container'); 
-           return card; 
-         }
+        async function initializeCard(payments) {
+            const card = await payments.card();
+            await card.attach('#card-container'); 
+            return card; 
+        }
 
-document.addEventListener('DOMContentLoaded', async function () {
-  if (!window.Square) {
-    throw new Error('Square.js failed to load properly');
-  }
-  const payments = window.Square.payments(appId, locationId);
-  let card;
-  try {
-    card = await initializeCard(payments);
-  } catch (e) {
-    console.error('Initializing Card failed', e);
-    return;
-  }
+        document.addEventListener('DOMContentLoaded', async function () {
+
+        if (!window.Square) {
+            throw new Error('Square.js failed to load properly');
+        }
+
+        const payments = window.Square.payments(appId, locationId);
+        let card;
+        
+        try {
+            card = await initializeCard(payments);
+        } catch (e) {
+            console.error('Initializing Card failed', e);
+        return;
+        }
 
   // Step 5.2: create card payment
-});
+        });
     </script>
 
   @endsection
