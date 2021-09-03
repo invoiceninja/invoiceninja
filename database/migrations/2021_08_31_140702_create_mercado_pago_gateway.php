39cee4756538e6719a6efb261ace301fa7d727b7
Lucas Reis <lucas@mreis.dev>
2021-09-03 12:26:27 -0300

    insert make pago payment provider in the database

---
+++ database/migrations/2021_08_31_140702_create_mercado_pago_gateway.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Gateway;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+
+class CreateMercadoPagoGateway extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $gateway = new Gateway();
+        $gateway->name = 'Mercado Pago';
+        $gateway->key = Str::lower(Str::random(32));
+        $gateway->provider = 'MercadoPago';
+        $gateway->is_offsite = false;
+        $gateway->fields = json_encode(['publicKey' => '', 'accessToken' => '']);
+        $gateway->visible = 1;
+        $gateway->site_url = 'https://www.mercadopago.com.br/developers/pt/guides/online-payments/checkout-pro/introduction';
+        $gateway->default_gateway_type_id = 1;
+        $gateway->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Gateway::where('provider', 'RecebeAqui')->delete();
+    }
+}
