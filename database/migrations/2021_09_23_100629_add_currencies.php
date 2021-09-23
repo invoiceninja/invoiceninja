026d3e17a78419341482b7e69cf06eb86223b363
David Bomba <turbo124@gmail.com>
2021-09-23 22:07:07 +1000

    Additional currencieS

--- database/migrations/2021_09_23_100629_add_currencies.php
+++ database/migrations/2021_09_23_100629_add_currencies.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Currency;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 
 class AddCurrencies extends Migration
 {
@@ -13,9 +15,40 @@ class AddCurrencies extends Migration
      */
     public function up()
     {
-        //
-    }
 
+    Model::unguard();
+
+
+    $currencies = [
+['id' => 105, 'name' => 'Gambia Dalasi', 'code' => 'GMD', 'symbol' => 'D', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
+['id' => 106, 'name' => 'Paraguayan Guarani', 'code' => 'PYG', 'symbol' => '₲', 'precision' => '0', 'thousand_separator' => ',', 'decimal_separator' => '.'],
+['id' => 107, 'name' => 'Malawi Kwacha','code' => 'MWK', 'symbol' => 'MK', 'precision' => '2','thousand_separator' => ',', 'decimal_separator' => '.'],
+['id' => 108, 'name' => 'Zimbabwean Dollar', 'code' => 'ZWL', 'symbol' => 'Z$', 'precision' => '0', 'thousand_separator' => ',', 'decimal_separator' => '.'],
+['id' => 109, 'name' => 'Cambodian Riel', 'code' => 'KHR', 'symbol' => '៛', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
+['id' => 110, 'name' => 'Vanuatu Vatu','code' => 'VUV', 'symbol' => 'VT', 'precision' => '0','thousand_separator' => ',','decimal_separator' => '.'],
+
+    ];
+
+        foreach ($currencies as $currency) {
+            $record = Currency::whereCode($currency['code'])->first();
+            if ($record) {
+                $record->name = $currency['name'];
+                $record->symbol = $currency['symbol'];
+                $record->precision = $currency['precision'];
+                $record->thousand_separator = $currency['thousand_separator'];
+                $record->decimal_separator = $currency['decimal_separator'];
+                if (isset($currency['swap_currency_symbol'])) {
+                    $record->swap_currency_symbol = $currency['swap_currency_symbol'];
+                }
+                $record->save();
+            } else {
+                Currency::create($currency);
+            }
+        }
+
+
+    }
+    
     /**
      * Reverse the migrations.
      *
@@ -23,19 +56,5 @@ class AddCurrencies extends Migration
      */
     public function down()
     {
-    
-    $currencies = [
-        ['id' => 105, 'name' => 'Ethiopian Birr', 'code' => 'ETB', 'symbol' => '', 'precision' => '2', 'thousand_separator' => ',', 'decimal_separator' => '.'],
-
-    ];
-
     }
 }
-
-
-// Gambia Dalasi (GMD)
-// Paraguayan Guarani (PYG)
-// Malawi Kwacha (MWK)
-// Zimbabwean Dollar (ZWL)
-// Cambodian Riel (KHR)
-// Vanuatu Vatu (VUV)
