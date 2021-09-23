8ee2d7db19e849c49d2b6cd23e61598eb0641904
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-23 17:57:35 +0200

    Handle payments

--- app/PaymentDrivers/Mollie/Checkout.php
+++ app/PaymentDrivers/Mollie/Checkout.php
@@ -12,11 +12,17 @@
 
 namespace App\PaymentDrivers\Mollie;
 
+use App\Exceptions\PaymentFailed;
 use App\Http\Requests\Request;
 use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+use App\Jobs\Mail\PaymentFailureMailer;
+use App\Jobs\Util\SystemLogger;
+use App\Models\GatewayType;
+use App\Models\SystemLog;
 use App\PaymentDrivers\Common\MethodInterface;
 use App\PaymentDrivers\MolliePaymentDriver;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
 
 class Checkout implements MethodInterface
@@ -26,6 +32,8 @@ class Checkout implements MethodInterface
     public function __construct(MolliePaymentDriver $mollie)
     {
         $this->mollie = $mollie;
+
+        $this->mollie->init();
     }
 
     /**
@@ -50,7 +58,71 @@ class Checkout implements MethodInterface
         return redirect()->back();
     }
 
-    public function paymentView(array $data) { }
+    /**
+     * Handle payment page for the checkout portal.
+     * 
+     * @param array $data 
+     * @return Redirector|RedirectResponse|void 
+     */
+    public function paymentView(array $data)
+    {        
+        $amount = $this->mollie->convertToMollieAmount((float) $this->mollie->payment_hash->data->amount_with_fee);
 
-    public function paymentResponse(PaymentResponseRequest $request) { }
+        $this->mollie->payment_hash
+            ->withData('gateway_type_id', GatewayType::CUSTOM)
+            ->withData('client_id', $this->mollie->client->id);
+
+        try {
+            $payment = $this->mollie->gateway->payments->create([
+                "amount" => [
+                    "currency" => $this->mollie->client->currency()->code,
+                    "value" => $amount,
+                ],
+                "description" => \sprintf('Hash: %s', $this->mollie->payment_hash->hash),
+                "redirectUrl" => route('client.payments.response', [
+                    'company_gateway_id' => $this->mollie->company_gateway->id,
+                    'payment_hash' => $this->mollie->payment_hash->hash,
+                    'payment_method_id' => GatewayType::CREDIT_CARD,
+                ]),
+                "webhookUrl" => $this->mollie->company_gateway->webhookUrl(),
+                "metadata" => [
+                    "client_id" => $this->mollie->client->hashed_id,
+                ],
+            ]);
+
+            $this->mollie->payment_hash->withData('payment_id', $payment->id);
+
+            return redirect($payment->getCheckoutUrl());
+        } catch(\Exception $e) {
+            return $this->processUnsuccessfulPayment($e);
+        }
+    }
+
+    public function processUnsuccessfulPayment(\Exception $e)
+    {
+        PaymentFailureMailer::dispatch(
+            $this->mollie->client,
+            $e->getMessage(),
+            $this->mollie->client->company,
+            $this->mollie->payment_hash->data->amount_with_fee
+        );
+
+        SystemLogger::dispatch(
+            $e->getMessage(),
+            SystemLog::CATEGORY_GATEWAY_RESPONSE,
+            SystemLog::EVENT_GATEWAY_FAILURE,
+            SystemLog::TYPE_MOLLIE,
+            $this->mollie->client,
+            $this->mollie->client->company,
+        );
+
+        throw new PaymentFailed($e->getMessage(), $e->getCode());
+    }
+
+    public function paymentResponse(PaymentResponseRequest $request)
+    {
+        if (! \property_exists('payment_id', $this->mollie->payment_hash->data)) {
+            throw new PaymentFailed('Missing [payment_id] property. Please contact administrator.');
+        }
+    }
 }
