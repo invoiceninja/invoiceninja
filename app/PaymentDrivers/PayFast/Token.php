63d4e1f153bd10d71df7d18817b23c5fbf3e4527
David Bomba <turbo124@gmail.com>
2021-09-23 10:33:11 +1000

    Turning on token billing for PayFast

--- app/PaymentDrivers/PayFast/Token.php
+++ app/PaymentDrivers/PayFast/Token.php
@@ -87,13 +87,13 @@ class Token
             'amount' => $amount,
             'item_name' => 'purchase',
             'm_payment_id' => $payment_hash->hash,
-            'passphrase' => $this->payfast->company_gateway->getConfigField('passphrase'),
         ];        
 
         // $header['signature'] = $this->genSig(array_merge($header, $body));
         $header['signature'] = $this->genSig($body);
 
         $body = array_merge($body,[            
+            'passphrase' => $this->payfast->company_gateway->getConfigField('passphrase'),
             'item_description' => ctrans('texts.invoices') . ': ' . collect($payment_hash->invoices())->pluck('invoice_number'),
         ]);
 
