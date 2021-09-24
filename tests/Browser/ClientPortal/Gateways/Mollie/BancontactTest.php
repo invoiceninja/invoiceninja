9d460527523f15142aa3808ecfbde7c603daf523
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-24 23:47:02 +0200

    Scaffold BancontactTest

---
+++ tests/Browser/ClientPortal/Gateways/Mollie/BancontactTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace Tests\Browser\ClientPortal\Gateways\Mollie;
+
+use App\Models\CompanyGateway;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Tests\Browser\Pages\ClientPortal\Login;
+
+class Bancontact extends DuskTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        foreach (static::$browsers as $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        }
+
+        $this->disableCompanyGateways();
+
+        CompanyGateway::where('gateway_key', '1bd651fb213ca0c9d66ae3c336dc77e8')->restore();
+
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit(new Login())
+                ->auth();
+        });
+    }
+}
