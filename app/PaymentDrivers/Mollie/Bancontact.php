b7e105aa6e0fc356aadfc410cf707a9a56c0bae3
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-24 23:31:18 +0200

    Scaffold `Bancontact`

---
+++ app/PaymentDrivers/Mollie/Bancontact.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\PaymentDrivers\Mollie;
+
+use App\Http\Requests\Request;
+use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+use App\PaymentDrivers\Common\MethodInterface;
+
+class Bancontact implements MethodInterface
+{
+    public function authorizeView(array $data) { }
+
+    public function authorizeResponse(Request $request) { }
+
+    public function paymentView(array $data) { }
+
+    public function paymentResponse(PaymentResponseRequest $request) { }
+}
