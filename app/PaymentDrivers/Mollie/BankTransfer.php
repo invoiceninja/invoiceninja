6c0311dab1c56083f0fe68f4502d8efabf03d034
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-24 21:17:43 +0200

    Payments

--- app/PaymentDrivers/Mollie/BankTransfer.php
+++ app/PaymentDrivers/Mollie/BankTransfer.php
@@ -12,12 +12,22 @@
 
 namespace App\PaymentDrivers\Mollie;
 
+use App\Exceptions\PaymentFailed;
 use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
 use App\Http\Requests\Request;
+use App\Jobs\Mail\PaymentFailureMailer;
+use App\Jobs\Util\SystemLogger;
+use App\Models\GatewayType;
+use App\Models\Payment;
+use App\Models\PaymentType;
+use App\Models\SystemLog;
 use App\PaymentDrivers\Common\MethodInterface;
 use App\PaymentDrivers\MolliePaymentDriver;
+use Exception;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\View\View;
+use Mollie\Api\Resources\Payment as ResourcesPayment;
 
 class BankTransfer implements MethodInterface
 {
@@ -52,11 +62,152 @@ class BankTransfer implements MethodInterface
         return redirect()->route('client.payment_methods.index');
     }
 
+    /**
+     * Show the payment page for bank transfer.
+     *
+     * @param array $data
+     * @return Redirector|RedirectResponse
+     */
     public function paymentView(array $data)
     {
+        $this->mollie->payment_hash
+            ->withData('gateway_type_id', GatewayType::BANK_TRANSFER)
+            ->withData('client_id', $this->mollie->client->id);
+
+        try {
+            $payment = $this->mollie->gateway->payments->create([
+                'method' => 'banktransfer',
+                'amount' => [
+                    'currency' => $this->mollie->client->currency()->code,
+                    'value' => $this->mollie->convertToMollieAmount((float) $this->mollie->payment_hash->data->amount_with_fee),
+                ],
+                'description' => \sprintf('Invoices: %s', collect($data['invoices'])->pluck('invoice_number')),
+                'redirectUrl' => route('client.payments.response', [
+                    'company_gateway_id' => $this->mollie->company_gateway->id,
+                    'payment_hash' => $this->mollie->payment_hash->hash,
+                    'payment_method_id' => GatewayType::BANK_TRANSFER,
+                ]),
+                // 'webhookUrl' => $this->mollie->company_gateway->webhookUrl(),
+                'webhookUrl' => 'https://invoiceninja.com',
+                'metadata' => [
+                    'client_id' => $this->mollie->client->hashed_id,
+                ],
+            ]);
+
+            $this->mollie->payment_hash->withData('payment_id', $payment->id);
+
+            return redirect(
+                $payment->getCheckoutUrl()
+            );
+        } catch (\Mollie\Api\Exceptions\ApiException | \Exception $exception) {
+            return $this->processUnsuccessfulPayment($exception);
+        }
     }
 
+    /**
+     * Handle unsuccessful payment.
+     *
+     * @param Exception $e
+     * @throws PaymentFailed
+     * @return void
+     */
+    public function processUnsuccessfulPayment(\Exception $e): void
+    {
+        PaymentFailureMailer::dispatch(
+            $this->mollie->client,
+            $e->getMessage(),
+            $this->mollie->client->company,
+            $this->mollie->payment_hash->data->amount_with_fee
+        );
+
+        SystemLogger::dispatch(
+            $e->getMessage(),
+            SystemLog::CATEGORY_GATEWAY_RESPONSE,
+            SystemLog::EVENT_GATEWAY_FAILURE,
+            SystemLog::TYPE_MOLLIE,
+            $this->mollie->client,
+            $this->mollie->client->company,
+        );
+
+        throw new PaymentFailed($e->getMessage(), $e->getCode());
+    }
+
+    /**
+     * Handle the payments for the bank transfer.
+     *
+     * @param PaymentResponseRequest $request
+     * @return mixed
+     */
     public function paymentResponse(PaymentResponseRequest $request)
     {
+        if (! \property_exists($this->mollie->payment_hash->data, 'payment_id')) {
+            return $this->processUnsuccessfulPayment(
+                new PaymentFailed('Whoops, something went wrong. Missing required [payment_id] parameter. Please contact administrator. Reference hash: ' . $this->mollie->payment_hash->hash)
+            );
+        }
+
+        try {
+            $payment = $this->mollie->gateway->payments->get(
+                $this->mollie->payment_hash->data->payment_id
+            );
+
+            if ($payment->status === 'paid') {
+                return $this->processSuccessfulPayment($payment);
+            }
+
+            if ($payment->status === 'open') {
+                return $this->processOpenPayment($payment);
+            }
+
+            return $this->processUnsuccessfulPayment(
+                new PaymentFailed(ctrans('texts.status_voided'))
+            );
+        } catch (\Mollie\Api\Exceptions\ApiException | \Exception $exception) {
+            return $this->processUnsuccessfulPayment($exception);
+        }
+    }
+
+    /**
+     * Handle the successful payment for bank transfer.
+     *
+     * @param ResourcesPayment $payment
+     * @param string $status
+     * @return RedirectResponse
+     */
+    public function processSuccessfulPayment(\Mollie\Api\Resources\Payment $payment, $status = 'paid'): RedirectResponse
+    {
+        $data = [
+            'gateway_type_id' => GatewayType::BANK_TRANSFER,
+            'amount' => array_sum(array_column($this->mollie->payment_hash->invoices(), 'amount')) + $this->mollie->payment_hash->fee_total,
+            'payment_type' => PaymentType::CREDIT, // @todo: Missing payment type for bank transfer.
+            'transaction_reference' => $payment->id,
+        ];
+
+        $payment_record = $this->mollie->createPayment(
+            $data,
+            $payment->status === 'paid' ? Payment::STATUS_COMPLETED : Payment::STATUS_PENDING
+        );
+
+        SystemLogger::dispatch(
+            ['response' => $payment, 'data' => $data],
+            SystemLog::CATEGORY_GATEWAY_RESPONSE,
+            SystemLog::EVENT_GATEWAY_SUCCESS,
+            SystemLog::TYPE_MOLLIE,
+            $this->mollie->client,
+            $this->mollie->client->company,
+        );
+
+        return redirect()->route('client.payments.show', ['payment' => $this->mollie->encodePrimaryKey($payment_record->id)]);
+    }
+
+    /**
+     * Handle 'open' payment status for bank transfer.
+     *
+     * @param ResourcesPayment $payment
+     * @return RedirectResponse
+     */
+    public function processOpenPayment(\Mollie\Api\Resources\Payment $payment): RedirectResponse
+    {
+        return $this->processSuccessfulPayment($payment, 'open');
     }
 }
