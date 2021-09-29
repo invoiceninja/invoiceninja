34a90d2ba8ca046b3547f76dbda27155b81ee86d
David Bomba <turbo124@gmail.com>
2021-09-29 21:48:22 +1000

    Map Client Registration fields

---
+++ app/DataMapper/ClientRegistrationFields.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\DataMapper;
+
+class ClientRegistrationFields
+{
+
+    public static function generate()
+    {
+        $data = 
+        [
+            [
+                'key' => 'name',
+                'value' => 'name',
+                'required' => true
+            ],
+
+            
+        ];
+    }
+}
