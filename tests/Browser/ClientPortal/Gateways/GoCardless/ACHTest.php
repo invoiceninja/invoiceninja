078330e1f0726836a6cb775e5497273875b25936
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-04 13:40:16 +0200

    Scaffold ACHTest

---
+++ tests/Browser/ClientPortal/Gateways/GoCardless/ACHTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Browser\ClientPortal\Gateways\GoCardless;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ACHTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
