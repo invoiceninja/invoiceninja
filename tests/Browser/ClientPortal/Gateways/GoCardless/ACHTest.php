cd3d0efb7ae280b86715dae4505187ed3dea740e
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-04 13:40:48 +0200

    Tests: Setup

--- tests/Browser/ClientPortal/Gateways/GoCardless/ACHTest.php
+++ tests/Browser/ClientPortal/Gateways/GoCardless/ACHTest.php
@@ -2,22 +2,29 @@
 
 namespace Tests\Browser\ClientPortal\Gateways\GoCardless;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Models\CompanyGateway;
 use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\ClientPortal\Login;
 use Tests\DuskTestCase;
 
 class ACHTest extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    protected function setUp(): void
     {
+        parent::setUp();
+
+        foreach (static::$browsers as $browser) {
+            $browser->driver->manage()->deleteAllCookies();
+        }
+
+        $this->disableCompanyGateways();
+
+        CompanyGateway::where('gateway_key', 'b9886f9257f0c6ee7c302f1c74475f6c')->restore();
+
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser
+                ->visit(new Login())
+                ->auth();
         });
     }
 }
