a1892c908bddcc24e7cc8c1b86345b7669f0103c
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-04 15:41:15 +0200

    Scaffold `iDEAL`

---
+++ app/PaymentDrivers/Mollie/IDEAL.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\PaymentDrivers\Mollie;
+
+use App\Http\Requests\Request;
+use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+use App\PaymentDrivers\Common\MethodInterface;
+
+class IDEAL implements MethodInterface
+{
+    public function authorizeView(array $data) { }
+
+    public function authorizeResponse(Request $request) { }
+
+    public function paymentView(array $data) { }
+
+    public function paymentResponse(PaymentResponseRequest $request) { }
+}
