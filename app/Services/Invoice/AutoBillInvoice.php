c30a06bd70cbe8019b226adea3ded017e5746007
David Bomba <turbo124@gmail.com>
2021-10-05 11:41:05 +1100

    Fixes for auto bill

--- app/Services/Invoice/AutoBillInvoice.php
+++ app/Services/Invoice/AutoBillInvoice.php
@@ -312,7 +312,7 @@ class AutoBillInvoice extends AbstractService
             $company_gateway = $gateway_token->gateway;
 
             //check if fees and limits are set
-            if (isset($company_gateway->fees_and_limits) && property_exists($company_gateway->fees_and_limits, $gateway_token->gateway_type_id))
+            if (isset($company_gateway->fees_and_limits) && !is_array($company_gateway->fees_and_limits) && property_exists($company_gateway->fees_and_limits, $gateway_token->gateway_type_id))
             {
                 //if valid we keep this gateway_token
                 if ($this->invoice->client->validGatewayForAmount($company_gateway->fees_and_limits->{$gateway_token->gateway_type_id}, $amount))
