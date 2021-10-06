789f2bfa0a0ca1b8e16ba996e910e3e0f2f1f552
Lars Kusch <lars@lars-kusch.de>
2021-10-06 15:01:21 +0200

    Small changes

--- resources/views/portal/ninja2020/gateways/stripe/sepa/sepa_debit.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/sepa/sepa_debit.blade.php
@@ -1,7 +1,5 @@
 <div id="stripe--payment-container">
-    @unless(isset($show_name) && $show_name == false)
-        @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.name')])
-            <input class="input w-full" id="cardholder-name" type="text" placeholder="{{ ctrans('texts.name') }}">
-        @endcomponent
-    @endunless
+    @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.name')])
+        <input class="input w-full" id="cardholder-name" type="text" placeholder="{{ ctrans('texts.name') }}">
+    @endcomponent
 </div>
