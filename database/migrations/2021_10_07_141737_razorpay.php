4c1dd8e03a746d8930897f9bcf99a581d8fef3f4
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-07 16:51:08 +0200

    Add Razorpay to PaymentLibrariesSeeder & update migration

--- database/migrations/2021_10_07_141737_razorpay.php
+++ database/migrations/2021_10_07_141737_razorpay.php
@@ -3,7 +3,6 @@
 use App\Models\Gateway;
 use App\Models\GatewayType;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Str;
 
 class Razorpay extends Migration
 {
@@ -17,10 +16,15 @@ class Razorpay extends Migration
         $gateway = new Gateway();
         $gateway->id = 58;
         $gateway->name = 'Razorpay';
-        $gateway->key = Str::lower(Str::random(32));
+        $gateway->key = 'hxd6gwg3ekb9tb3v9lptgx1mqyg69zu9';
         $gateway->provider = 'Razorpay';
         $gateway->is_offsite = false;
-        $gateway->fields = new \stdClass;
+
+        $configuration = new \stdClass;
+        $configuration->apiKey = '';
+        $configuration->apiSecret = '';
+
+        $gateway->fields = \json_encode($configuration);
         $gateway->visible = true;
         $gateway->site_url = 'https://razorpay.com';
         $gateway->default_gateway_type_id = GatewayType::HOSTED_PAGE;
