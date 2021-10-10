d767a08e21a538681dd61fcceeeacf860d7ca977
Lars Kusch <lars@lars-kusch.de>
2021-10-10 06:58:57 +0200

    Small fix

--- resources/views/portal/ninja2020/gateways/stripe/eps/eps.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/eps/eps.blade.php
@@ -1,7 +1,7 @@
 <div id="stripe--payment-container">
     @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.name')])
         <label for="eps-name">
-            <input class="input w-full" id="ideal-name" type="text" placeholder="{{ ctrans('texts.bank_account_holder') }}">
+            <input class="input w-full" id="eps-name" type="text" placeholder="{{ ctrans('texts.bank_account_holder') }}">
         </label>
         <label for="eps-bank-element"></label>
         <div class="border p-4 rounded">
