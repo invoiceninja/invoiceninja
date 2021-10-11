6348b5fc355bee61c43a5fbf9478b0a977e7c71b
hillelcoren <hillelcoren@gmail.com>
2021-10-11 14:37:16 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "174c02fc4609e8fc4389f5d21f16a296",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
-"main.dart.js": "737a840bed376cb6c2a8198aec8d650b",
+"main.dart.js": "b997ef406edad54ae9bd2ae85b53e993",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40",
-"/": "62039ec03358e95898baf5b41406df65"
+"/": "e16d91811e909dcce48b1032b39b4988"
 };
 
 // The application shell files that are downloaded before a service worker can
