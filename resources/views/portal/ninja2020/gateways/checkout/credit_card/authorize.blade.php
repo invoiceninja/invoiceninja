0db3f3dd573df227bcce8cd0840acac1d5ed7e71
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-14 18:45:48 +0200

    Extract script into separate file

--- resources/views/portal/ninja2020/gateways/checkout/credit_card/authorize.blade.php
+++ resources/views/portal/ninja2020/gateways/checkout/credit_card/authorize.blade.php
@@ -40,44 +40,5 @@
 @endsection
 
 @section('gateway_footer')
-    <script>
-        class CheckoutCreditCardAuthorization {
-            constructor() {
-                this.button = document.querySelector('#pay-button');
-            }
-
-            init() {
-                this.frames = Frames.init(
-                    document.querySelector('meta[name=public-key]').content
-                );
-            }
-
-            handle() {
-                this.init();
-
-                Frames.addEventHandler(Frames.Events.CARD_VALIDATION_CHANGED, (event) => {
-                    this.button.disabled = !Frames.isCardValid();
-                });
-
-                Frames.addEventHandler(Frames.Events.CARD_TOKENIZED, (event) => {
-                    document.querySelector(
-                        'input[name="gateway_response"]'
-                    ).value = JSON.stringify(event);
-
-                    document.getElementById('server_response').submit();
-                });
-
-                document
-                    .querySelector('#authorization-form')
-                    .addEventListener('submit', (event) => {
-                        this.button.disabled = true;
-
-                        event.preventDefault();
-                        Frames.submitCard();
-                    });
-            }
-        }
-
-        new CheckoutCreditCardAuthorization().handle();
-    </script>
+    <script src="{{ asset('js/clients/payment_methods/authorize-checkout-card.js') }}"></script>
 @endsection
