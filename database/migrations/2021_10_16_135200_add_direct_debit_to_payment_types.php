9480215f47471614d4f6db42a01417816385d09f
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-16 15:53:05 +0200

    Add DIRECT_DEBIT for PaymentType

---
+++ database/migrations/2021_10_16_135200_add_direct_debit_to_payment_types.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\GatewayType;
+use App\Models\PaymentType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDirectDebitToPaymentTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payment_types', function (Blueprint $table) {
+            $type = new PaymentType();
+
+            $type->id = 42;
+            $type->name = 'Direct Debit';
+            $type->gateway_type_id = GatewayType::DIRECT_DEBIT;
+
+            $type->save();
+        });
+    }
+}
