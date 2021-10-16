27cd99483d6501b284416b10177931c53d01b5e3
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-16 15:29:06 +0200

    Define DIRECT_DEBIT constant

--- app/Models/GatewayType.php
+++ app/Models/GatewayType.php
@@ -32,6 +32,7 @@ class GatewayType extends StaticModel
     const GIROPAY = 15;
     const PRZELEWY24 = 16;
     const EPS = 17;
+    const DIRECT_DEBIT = 18;
 
     public function gateway()
     {
