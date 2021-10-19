f8921c59f7443bf1b2adaabde24cb485277c5c76
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-19 17:59:38 +0200

    Scaffold SEPA

---
+++ app/PaymentDrivers/GoCardless/SEPA.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\PaymentDrivers\GoCardless;
+
+use App\Http\Requests\Request;
+use App\PaymentDrivers\Common\MethodInterface;
+use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+
+class SEPA implements MethodInterface
+{
+    public function authorizeView(array $data) { }
+
+    public function authorizeResponse(Request $request) { }
+
+    public function paymentView(array $data) { }
+
+    public function paymentResponse(PaymentResponseRequest $request) { }
+}
