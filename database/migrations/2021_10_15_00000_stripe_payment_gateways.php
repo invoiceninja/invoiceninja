244aa6ffa0a4aedd46bb20edaab11f0f615019d4
Lars Kusch <lars@lars-kusch.de>
2021-10-21 21:02:53 +0200

    Fix bug in migration

--- database/migrations/2021_10_15_00000_stripe_payment_gateways.php
+++ database/migrations/2021_10_15_00000_stripe_payment_gateways.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIdealToPaymentTypes extends Migration
+class AddStripePaymentsToPaymentTypes extends Migration
 {
     /**
      * Run the migrations.
