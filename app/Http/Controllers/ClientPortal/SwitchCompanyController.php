e67fea2c33eca1a89b2d06b703019f90d73260a1
David Bomba <turbo124@gmail.com>
2021-10-24 20:33:23 +1100

    Fixes for Company Switcher

--- app/Http/Controllers/ClientPortal/SwitchCompanyController.php
+++ app/Http/Controllers/ClientPortal/SwitchCompanyController.php
@@ -27,7 +27,7 @@ class SwitchCompanyController extends Controller
             ->where('id', $this->transformKeys($contact))
             ->first();
 
-        auth()->guard('contact')->user()->login($client_contact, true);
+        auth()->guard('contact')->login($client_contact, true);
 
         return redirect('/client/dashboard');
     }
