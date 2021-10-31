b8c8a486fd78fc11a6e7966c79824b852310115d
Benjamin Beganović <k1pstabug@gmail.com>
2021-10-31 14:45:01 +0100

    Scaffold BrowserPay class

---
+++ app/PaymentDrivers/Stripe/BrowserPay.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+
+namespace App\PaymentDrivers\Stripe;
+
+use Illuminate\Http\Request;
+use App\PaymentDrivers\Common\MethodInterface;
+use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+
+class BrowserPay implements MethodInterface
+{
+    public function authorizeView(array $data) { }
+
+    public function authorizeResponse(Request $request) { }
+
+    public function paymentView(array $data) { }
+
+    public function paymentResponse(PaymentResponseRequest $request) { }
+}
