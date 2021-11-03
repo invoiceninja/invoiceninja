5d7d4138d170545a013b8a4b9d5ca29331d60461
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-03 14:15:19 +0100

    Add columns for onboarding

---
+++ database/migrations/2021_11_03_131308_onboarding.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Onboarding extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->boolean('is_onboarding')->default(false);
+            $table->json('onboarding')->nullable();
+        });
+    }
+}
