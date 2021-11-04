ebdba30e08e24c4ac176decee08d4a414cb8f9ac
hillelcoren <hillelcoren@gmail.com>
2021-11-04 08:15:57 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b",
-"/": "ee84d6761a9ecd679b9acd603c713ae8",
+"/": "e0f7c2c9bf1ceb6e6a8c5e5ff2ab2be5",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
 "version.json": "3930722e1581f582eefe59c75ab50c99",
@@ -34,7 +34,7 @@ const RESOURCES = {
 "assets/assets/images/google_logo.png": "0f118259ce403274f407f5e982e681c3",
 "assets/assets/images/logo_light.png": "e5f46d5a78e226e7a9553d4ca6f69219",
 "assets/AssetManifest.json": "38d9aea341601f3a5c6fa7b5a1216ea5",
-"main.dart.js": "4ececd9a33bb8b898a3dbfc55f7ad344"
+"main.dart.js": "2bf347ce862c8a0d57ed39ca6b9a4b15"
 };
 
 // The application shell files that are downloaded before a service worker can
