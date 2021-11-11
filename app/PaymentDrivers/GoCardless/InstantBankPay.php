619803f2be9e8bc22dc103736c53f0899e2c9722
Benjamin Beganović <k1pstabug@gmail.com>
2021-11-11 15:57:41 +0100

    Initialize GoCardlessPro\Client

--- app/PaymentDrivers/GoCardless/InstantBankPay.php
+++ app/PaymentDrivers/GoCardless/InstantBankPay.php
@@ -5,9 +5,19 @@ namespace App\PaymentDrivers\GoCardless;
 use Illuminate\Http\Request;
 use App\PaymentDrivers\Common\MethodInterface;
 use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+use App\PaymentDrivers\GoCardlessPaymentDriver;
 
 class InstantBankPay implements MethodInterface
 {
+    protected GoCardlessPaymentDriver $go_cardless;
+
+    public function __construct(GoCardlessPaymentDriver $go_cardless)
+    {
+        $this->go_cardless = $go_cardless;
+
+        $this->go_cardless->init();
+    }
+
     public function authorizeView(array $data) { }
 
     public function authorizeResponse(Request $request) { }
