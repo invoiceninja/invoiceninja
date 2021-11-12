a67f3f8be163a66f42e27d426a9f7612f3490130
hillelcoren <hillelcoren@gmail.com>
2021-11-12 11:14:19 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -6,10 +6,10 @@ const RESOURCES = {
   "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b",
-"/": "ec9c4ffbc8f1dbb256723f4fad39ad17",
+"/": "b4c07dbdd037f673ca8bc623748c8dad",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
-"version.json": "50d3cb8e279d954da5e6323e2dbe42e1",
+"version.json": "9c7b0edc83733da56c726678aacd9fd3",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "9eb7e2eb2888ea5bae5f536720db37cd",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "174c02fc4609e8fc4389f5d21f16a296",
@@ -34,7 +34,7 @@ const RESOURCES = {
 "assets/assets/images/google_logo.png": "0f118259ce403274f407f5e982e681c3",
 "assets/assets/images/logo_light.png": "e5f46d5a78e226e7a9553d4ca6f69219",
 "assets/AssetManifest.json": "38d9aea341601f3a5c6fa7b5a1216ea5",
-"main.dart.js": "0b3f6a62527f2105db84f15ceae1a507"
+"main.dart.js": "14e5394ca49eb2b79256a95f65a7905b"
 };
 
 // The application shell files that are downloaded before a service worker can
