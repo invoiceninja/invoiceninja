995b16d7e3b4f97beb3a5ceb02200d0dc9357473
David Bomba <turbo124@gmail.com>
2021-12-21 20:27:51 +1100

    Stripe Zero Decimal CUrrencies

---
+++ tests/Unit/ZeroDecimalTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+namespace Tests\Unit;
+
+use App\Jobs\Util\UploadFile;
+use App\Models\Document;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Support\Facades\Storage;
+use Tests\MockAccountData;
+use Tests\TestCase;
+
+class ZeroDecimalTest extends TestCase
+{
+
+
+    public array $currencies = ["BIF","CLP","DJF","GNF","JPY","KMF","KRW","MGA","PYG","RWF","UGX","VND","VUV","XAF","XOF","XPF"];
+    
+    public function setUp() :void
+    {
+
+    }
+
+    public function testCurrencyHit()
+    {
+
+        $this->assertTrue(in_array("KRW", $this->currencies));
+
+    }
+
+}
+
