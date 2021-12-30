16880ddc8b66474b7c2f50c0800b04d51f4161e2
David Bomba <turbo124@gmail.com>
2021-12-31 07:41:50 +1100

    Change label for credits

--- app/Mail/User/UserAdded.php
+++ app/Mail/User/UserAdded.php
@@ -15,6 +15,7 @@ namespace App\Mail\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\App;
 
 class UserAdded extends Mailable
 {
@@ -45,6 +46,8 @@ class UserAdded extends Mailable
      */
     public function build()
     {
+        App::setLocale($this->company->getLocale());
+
         return $this->from(config('mail.from.address'), config('mail.from.name'))
             ->subject(ctrans('texts.created_user'))
             ->view('email.admin.user_added')
