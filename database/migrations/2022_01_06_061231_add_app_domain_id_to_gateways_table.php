996b3f246bb9980136271115a355f326837c3da3
David Bomba <turbo124@gmail.com>
2022-01-06 17:21:11 +1100

    Add Apple Domain Verification to Stripe Gateways

---
+++ database/migrations/2022_01_06_061231_add_app_domain_id_to_gateways_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Gateway;
+use App\Utils\Ninja;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAppDomainIdToGatewaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        if(Ninja::isHosted()){
+
+            $stripe_connect = Gateway::find(56);
+            $stripe_connect->fields = '{"account_id":"", "appleDomainVerification":""}';
+            $stripe_connect->save();
+        
+        }
+
+            $stripe_connect = Gateway::find(20);
+            $stripe_connect->fields = '{"account_id":"", "appleDomainVerification":""}';
+            $stripe_connect->save();
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+
+}
