abbca58b4d9260b7a4991931376801d3cd5fdc03
David Bomba <turbo124@gmail.com>
2022-01-06 13:31:44 +1100

    Do not allow an invoice to be created for a deleted client

---
+++ tests/Feature/ClientDeletedInvoiceCreationTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2021. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://opensource.org/licenses/AAL
+ */
+namespace Tests\Feature;
+
+use App\Models\Client;
+use App\Models\ClientContact;
+use App\Models\Invoice;
+use App\Utils\Traits\MakesHash;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Session;
+use Tests\MockAccountData;
+use Tests\TestCase;
+
+/**
+ * @test
+ * @covers App\Http\Controllers\InvoiceController
+ */
+class ClientDeletedInvoiceCreationTest extends TestCase
+{
+    use MakesHash;
+    use DatabaseTransactions;
+    use MockAccountData;
+
+    public function setUp() :void
+    {
+        parent::setUp();
+
+        Session::start();
+
+        $this->faker = \Faker\Factory::create();
+
+        Model::reguard();
+
+        $this->makeTestData();
+    }
+
+    public function testClientedDeletedAttemptingToCreateInvoice()
+    {
+                /* Test fire new invoice */
+        $data = [
+            'client_id' => $this->client->hashed_id,
+            'number' => 'dude',
+        ];
+
+        $response = $this->withHeaders([
+            'X-API-SECRET' => config('ninja.api_secret'),
+            'X-API-TOKEN' => $this->token,
+        ])->post('/api/v1/invoices/', $data)
+        ->assertStatus(200);
+
+        $this->client->is_deleted = true;
+        $this->client->save();
+
+
+        $data = [
+            'client_id' => $this->client->hashed_id,
+            'number' => 'dude2',
+        ];
+
+        $response = $this->withHeaders([
+            'X-API-SECRET' => config('ninja.api_secret'),
+            'X-API-TOKEN' => $this->token,
+        ])->post('/api/v1/invoices/', $data)
+        ->assertStatus(302);
+
+
+    }
+
+}
