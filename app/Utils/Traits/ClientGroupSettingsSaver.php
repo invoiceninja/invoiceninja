386131f61889eb3ac9dabb72358cd84d0de24a0f
David Bomba <turbo124@gmail.com>
2022-01-10 07:33:11 +1100

    minor fixes for settings saver

--- app/Utils/Traits/ClientGroupSettingsSaver.php
+++ app/Utils/Traits/ClientGroupSettingsSaver.php
@@ -214,7 +214,7 @@ trait ClientGroupSettingsSaver
             case 'double':
                 return is_float($value) || is_numeric(strval($value));
             case 'string':
-                return method_exists($value, '__toString') || is_null($value) || is_string($value);
+                return ( is_string( $value ) && method_exists($value, '__toString') ) || is_null($value) || is_string($value);
             case 'bool':
             case 'boolean':
                 return is_bool($value) || (int) filter_var($value, FILTER_VALIDATE_BOOLEAN);
--- app/Utils/Traits/CompanySettingsSaver.php
+++ app/Utils/Traits/CompanySettingsSaver.php
@@ -232,7 +232,6 @@ trait CompanySettingsSaver
                 return is_float($value) || is_numeric(strval($value));
             case 'string':
                 return (is_string($value) && method_exists($value, '__toString')) || is_null($value) || is_string($value);
-                //return is_null($value) || is_string($value);
             case 'bool':
             case 'boolean':
                 return is_bool($value) || (int) filter_var($value, FILTER_VALIDATE_BOOLEAN);
