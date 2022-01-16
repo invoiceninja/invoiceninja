2284e4d83bb8757cf7a59077079fb6ebb1609014
David Bomba <turbo124@gmail.com>
2022-01-16 14:11:48 +1100

    Refactor PostMark Webhooks

--- tests/Feature/InvoiceAmountPaymentTest.php
+++ tests/Feature/InvoiceAmountPaymentTest.php
@@ -111,4 +111,90 @@ class InvoiceAmountPaymentTest extends TestCase
         $this->assertEquals(10, $payment->amount);
         
     }
+
+    public function testMarkPaidRemovesUnpaidGatewayFees()
+    {
+
+        $data = [
+            'name' => 'A Nice Client',
+        ];
+
+        $response = $this->withHeaders([
+                'X-API-SECRET' => config('ninja.api_secret'),
+                'X-API-TOKEN' => $this->token,
+            ])->post('/api/v1/clients', $data);
+
+        $response->assertStatus(200);
+
+        $arr = $response->json();
+
+        $client_hash_id = $arr['data']['id'];
+        $client = Client::find($this->decodePrimaryKey($client_hash_id));
+        
+        $this->assertEquals($client->balance, 0);
+        $this->assertEquals($client->paid_to_date, 0);
+        //create new invoice.
+
+        $line_items = [];
+
+        $item = InvoiceItemFactory::create();
+        $item->quantity = 1;
+        $item->cost = 10;
+
+        $line_items[] = (array)$item;
+
+        $item = InvoiceItemFactory::create();
+        $item->quantity = 1;
+        $item->cost = 10;
+
+        $line_items[] = (array)$item;
+
+        $item = InvoiceItemFactory::create();
+        $item->quantity = 1;
+        $item->cost = 5;
+        $item->type_id = "3";
+
+        $line_items[] = (array)$item;
+
+        $invoice = [
+            'status_id' => 1,
+            'number' => '',
+            'discount' => 0,
+            'is_amount_discount' => 1,
+            'po_number' => '3434343',
+            'public_notes' => 'notes',
+            'is_deleted' => 0,
+            'custom_value1' => 0,
+            'custom_value2' => 0,
+            'custom_value3' => 0,
+            'custom_value4' => 0,
+            'client_id' => $client_hash_id,
+            'line_items' => (array)$line_items,
+        ];
+
+        $response = $this->withHeaders([
+                'X-API-SECRET' => config('ninja.api_secret'),
+                'X-API-TOKEN' => $this->token,
+            ])->post('/api/v1/invoices?mark_sent=true', $invoice)
+            ->assertStatus(200);
+
+        $arr = $response->json();
+    
+        $invoice_one_hashed_id = $arr['data']['id'];
+
+        $invoice = Invoice::find($this->decodePrimaryKey($invoice_one_hashed_id));
+
+        $this->assertEquals(25, $invoice->balance);
+        $this->assertEquals(25, $invoice->amount);
+
+        $invoice->service()->markPaid()->save();
+
+        $invoice->fresh();
+
+        $this->assertEquals(20, $invoice->amount);
+        $this->assertEquals(0, $invoice->balance);
+
+    }
+
+
 }
