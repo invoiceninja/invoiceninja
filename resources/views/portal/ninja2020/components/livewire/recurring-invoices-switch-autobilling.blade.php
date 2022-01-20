519278e23fe1a4d320d1c2a3cd08fbecf0c4e05c
David Bomba <turbo124@gmail.com>
2022-01-20 11:11:21 +1100

    Fixes for recurring invoice opt out autobilling

--- resources/views/portal/ninja2020/components/livewire/recurring-invoices-switch-autobilling.blade.php
+++ resources/views/portal/ninja2020/components/livewire/recurring-invoices-switch-autobilling.blade.php
@@ -1,6 +1,6 @@
 <label class="flex items-center cursor-pointer">
     <input type="checkbox" class="form-checkbox mr-2"
-           wire:change="updateAutoBilling" {{ $invoice->auto_bill_enabled || $invoice->auto_bill === 'optout' ? 'checked' : '' }}>
+           wire:change="updateAutoBilling" {{ $invoice->auto_bill_enabled ? 'checked' : '' }}>
 
     <span class="text-sm leading-5 font-medium text-gray-900">
         {{ $invoice->auto_bill_enabled || $invoice->auto_bill === 'optout' ? ctrans('texts.auto_bill_enabled') : ctrans('texts.auto_bill_disabled') }}
