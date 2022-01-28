d00143bf7716bf0a7baa79fb3a331230cd097ed9
hillelcoren <hillelcoren@gmail.com>
2022-01-28 13:48:27 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-"/": "51844665e8fec0a53d23ef02352f25de",
+"/": "c7951a765e699ffb82e3bf023fee0d50",
 "version.json": "cba1586be50a4df1f43838f82c6ee0c3",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40",
-"main.dart.js": "299c8c3d3960e8f023d17ada686c94ce",
+"main.dart.js": "954e73d7e0bc739731cd91e22e75e9de",
 "canvaskit/profiling/canvaskit.wasm": "6d1b0fc1ec88c3110db88caa3393c580",
 "canvaskit/profiling/canvaskit.js": "3783918f48ef691e230156c251169480",
 "canvaskit/canvaskit.wasm": "b179ba02b7a9f61ebc108f82c5a1ecdb",
