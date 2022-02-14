eb2123bf4ec9c4da62df991451937eaaaafbfc75
David Bomba <turbo124@gmail.com>
2022-02-15 07:39:14 +1100

    Fixes for bad sort query filter

--- app/Filters/InvoiceFilters.php
+++ app/Filters/InvoiceFilters.php
@@ -148,6 +148,10 @@ class InvoiceFilters extends QueryFilters
     {
         $sort_col = explode('|', $sort);
 
+        //catch invalid explode array count
+        if(count($sort_col) == 1)
+            return $this->builder;
+
         return $this->builder->orderBy($sort_col[0], $sort_col[1]);
     }
 
