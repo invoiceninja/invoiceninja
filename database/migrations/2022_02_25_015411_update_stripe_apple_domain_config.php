31e09de230af5dddd6fcc749427b2f87b039f454
David Bomba <turbo124@gmail.com>
2022-02-25 13:02:22 +1100

    Insert Apple Domain Verification into Stripe Gateway config

---
+++ database/migrations/2022_02_25_015411_update_stripe_apple_domain_config.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\CompanyGateway;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStripeAppleDomainConfig extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        CompanyGateway::whereIn('gateway_key', ['d14dd26a47cecc30fdd65700bfb67b34', 'd14dd26a37cecc30fdd65700bfb55b23'])->cursor()->each(function($cg){
+
+            $config = $cg->getConfig();
+
+            if(!property_exists($config, 'appleDomainVerification')){
+
+                $config->appleDomainVerification = "";
+                $cg->setConfig($config);
+                $cg->save();
+
+            }
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
