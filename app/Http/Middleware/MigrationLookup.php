76c4cc982fc17a58f92ecac79c6992ac38a37ab4
David Bomba <turbo124@gmail.com>
2022-03-21 12:53:04 +1100

    Working on migration funnel

--- app/Http/Middleware/MigrationLookup.php
+++ app/Http/Middleware/MigrationLookup.php
@@ -21,10 +21,21 @@ class MigrationLookup
             return $next($request);
         }
 
-        if ($guard == 'user') {
-            
-        }
 
-        return $next($request);
+                //need to wrap an additional block over this to funnel users in a particular range
+
+                if ($guard == 'user') {
+                    
+                    if(request()->is('migration/*') || request()->is('settings/*')) {
+
+                        return $next($request);
+
+                    }
+
+
+                }
+
+                return redirect('/settings/account_management');
+
     }
 }
