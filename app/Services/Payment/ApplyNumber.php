4f10dcd9139f70c291955b169ec131f0f0b835cd
David Bomba <turbo124@gmail.com>
2022-04-07 17:40:59 +1000

    Improve invoice number generation when race conditions encountered

--- app/Services/Payment/ApplyNumber.php
+++ app/Services/Payment/ApplyNumber.php
@@ -14,12 +14,15 @@ namespace App\Services\Payment;
 use App\Models\Payment;
 use App\Services\AbstractService;
 use App\Utils\Traits\GeneratesCounter;
+use Illuminate\Database\QueryException;
 
 class ApplyNumber extends AbstractService
 {
     use GeneratesCounter;
 
     private $payment;
+
+    private bool $completed = true;
 
     public function __construct(Payment $payment)
     {
@@ -34,8 +37,38 @@ class ApplyNumber extends AbstractService
             return $this->payment;
         }
 
-        $this->payment->number = $this->getNextPaymentNumber($this->client, $this->payment);
+        $this->trySaving();
+        // $this->payment->number = $this->getNextPaymentNumber($this->client, $this->payment);
 
         return $this->payment;
+    }
+
+    private function trySaving()
+    {
+
+        $x=1;
+
+        do{
+
+            try{
+
+                $this->payment->number = $this->getNextPaymentNumber($this->client, $this->payment);
+                $this->payment->saveQuietly();
+
+                $this->completed = false;
+                
+
+            }
+            catch(QueryException $e){
+
+                $x++;
+
+                if($x>10)
+                    $this->completed = false;
+            }
+        
+        }
+        while($this->completed);
+
     }
 }
