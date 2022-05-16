c59b7e01875b913dfbfa0c43f0680f27ce18e775
= <turbo124@gmail.com>
2022-05-17 07:30:41 +1000

    Listen for additional webhooks for gocardless

--- app/PaymentDrivers/StripePaymentDriver.php
+++ app/PaymentDrivers/StripePaymentDriver.php
@@ -579,7 +579,7 @@ class StripePaymentDriver extends BaseDriver
                 if(array_key_exists('payment_intent', $transaction))
                 {
                     $payment = Payment::query()
-                        ->where('company_id', $request->getCompany()->id)
+                        // ->where('company_id', $request->getCompany()->id)
                         ->where(function ($query) use ($transaction) {
                             $query->where('transaction_reference', $transaction['payment_intent'])
                                   ->orWhere('transaction_reference', $transaction['id']);
@@ -589,7 +589,7 @@ class StripePaymentDriver extends BaseDriver
                 else
                 {
                      $payment = Payment::query()
-                        ->where('company_id', $request->getCompany()->id)
+                        // ->where('company_id', $request->getCompany()->id)
                         ->where('transaction_reference', $transaction['id'])
                         ->first();
                 }
