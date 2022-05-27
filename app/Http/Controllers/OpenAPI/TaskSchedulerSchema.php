9bd22792905314322b91edb562db5da426edda67
Nikola Cirkovic <cirkoyut@gmail.com>
2022-05-27 02:50:12 +0200

    INA-5 | Update OpenAPI schema

--- app/Http/Controllers/OpenAPI/TaskSchedulerSchema.php
+++ app/Http/Controllers/OpenAPI/TaskSchedulerSchema.php
@@ -6,13 +6,14 @@
  *   schema="TaskSchedulerSchema",
  *   type="object",
  *
- *      *     @OA\Property(property="paused",type="bool",example="false",description="The scheduler paused state"),
+ *
+ *     @OA\Property(property="paused",type="bool",example="false",description="The scheduler paused state"),
  *     @OA\Property(property="repeat_every",type="string",example="DAY",description="Accepted values (DAY,WEEK,MONTH,3MONTHS,YEAR)"),
  *     @OA\Property(property="start_from",type="integer",example="1652898504",description="Timestamp when we should start the scheduler, default is today"),
  *       @OA\Property(property="date_range", type="string", example="last7", description="The string representation of the date range of data to be returned"),
  *       @OA\Property(property="date_key", type="string", example="created_at", description="The date column to search between."),
- *       @OA\Property(property="start_date", type="string", example="2000-10-31", description="The start date to search between"),
- *       @OA\Property(property="end_date", type="string", example="2", description="The end date to search between"),
+ *       @OA\Property(property="start_date", type="string", example="2022-10-31", description="The start date to search between"),
+ *       @OA\Property(property="end_date", type="string", example="2022-10-31", description="The end date to search between"),
  *       @OA\Property(
  *          property="report_keys",
  *          type="array",
@@ -33,7 +34,6 @@
  *   type="object",
  *
  *      @OA\Property(property="paused",type="bool",example="false",description="The scheduler paused state"),
- *      @OA\Property(property="archived",type="bool",example="false",description="The scheduler archived state"),
  *     *     @OA\Property(property="repeat_every",type="string",example="DAY",description="Accepted values (DAY,WEEK,MONTH,3MONTHS,YEAR)"),
  *     @OA\Property(property="start_from",type="integer",example="1652898504",description="Timestamp when we should start the scheduler, default is today"),
  *
