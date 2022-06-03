be81f20de91f820f57aca75bc01c70d4bdc8318f
David Bomba <turbo124@gmail.com>
2022-06-03 16:46:21 +1000

    Fixes for email images

--- resources/views/email/template/client.blade.php
+++ resources/views/email/template/client.blade.php
@@ -108,7 +108,7 @@
                             <img src="{{ $logo ?? '' }}" alt="" width="400" border="0" align="middle" style="display:block;" />
                             <div style="mso-hide:all;">
                             <![endif]-->
-                            <img src="{{ $logo ?? '' }}" alt="" style="margin-top: 40px; height: 40px; display: block; margin-left: auto; margin-right: auto;"/>
+                            <img src="{{ $logo ?? '' }}" alt="" style="margin-top: 40px; display: block; margin-left: auto; margin-right: auto;"/>
                             <!--[if gte mso 9]>
                             </div>
                             <![endif]-->
