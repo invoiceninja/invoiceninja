44352b48f728f9bb68aa823e578a0c02d04b3a20
Nikola Cirkovic <cirkoyut@gmail.com>
2022-06-05 01:47:33 +0200

    INA-7 | Vendor route file

---
+++ routes/vendor.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
