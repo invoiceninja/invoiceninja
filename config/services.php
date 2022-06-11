b2420a817dd7b510c0dae001b01830bf7e4ccbe3
Nikola Cirkovic <cirkoyut@gmail.com>
2022-06-11 05:26:24 +0200

    INA-12 | Add microsoft oauth keys to .env.example

--- config/services.php
+++ config/services.php
@@ -85,4 +85,9 @@ return [
         'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
         'redirect' => env('MICROSOFT_REDIRECT_URI')
     ],
+    'apple' => [
+        'client_id' => env('APPLE_CLIENT_ID'),
+        'client_secret' => env('APPLE_CLIENT_SECRET'),
+        'redirect' => env('APPLE_REDIRECT_URI')
+    ],
 ];
