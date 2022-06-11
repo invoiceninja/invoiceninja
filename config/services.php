26a3720390880be08c57f17063bdf171d216daf4
Nikola Cirkovic <cirkoyut@gmail.com>
2022-06-11 04:07:22 +0200

    INA-12 | add microsoft oauth to services for socialite

--- config/services.php
+++ config/services.php
@@ -80,4 +80,9 @@ return [
     'postmark' => [
         'token' => env('POSTMARK_SECRET'),
     ],
+    'microsoft' => [
+        'client_id' => env('MICROSOFT_CLIENT_ID'),
+        'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
+        'redirect' => env('MICROSOFT_REDIRECT_URI')
+    ],
 ];
