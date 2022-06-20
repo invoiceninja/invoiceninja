e7331c9e2187c4b5e55eeb6c75b9e1af8d0c80a1
Kishan Patel <kishanmnpatel@gmail.com>
2022-06-20 09:41:03 +0530

    some modification implemented for removing double submission.

--- resources/views/portal/ninja2020/gateways/forte/ach/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/forte/ach/pay.blade.php
@@ -1,11 +1,7 @@
 @extends('portal.ninja2020.layout.payments', ['gateway_title' => 'Bank Transfer', 'card_title' => 'Bank Transfer'])
 
 @section('gateway_head')
-    @if($gateway->forte->company_gateway->getConfigField('testMode'))
-    <script type="text/javascript" src="https://sandbox.forte.net/api/js/v1"></script>
-    @else
-    <script type="text/javascript" src="https://api.forte.net/js/v1"></script>
-    @endif
+    <meta name="forte-api-login-id" content="{{$gateway->forte->company_gateway->getConfigField("apiLoginId")}}">
 @endsection
 
 @section('gateway_content')
@@ -41,46 +37,17 @@
             <input class="input w-full" id="account-number" type="text" placeholder="{{ctrans('texts.account_number')}}" required>
         </div>
     @endcomponent
-    <div class="bg-white px-4 py-5 flex justify-end">
-        <button type="button"
-        onclick="submitPay()"
-            class="button button-primary bg-primary {{ $class ?? '' }}">
-                <svg class="animate-spin h-5 w-5 text-white hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-            <span>{{ $slot ?? ctrans('texts.pay_now') }}</span>
-        </button>
-    </div>
+
+    @include('portal.ninja2020.gateways.includes.pay_now')
 
 @endsection
 
 @section('gateway_footer')
-    <script>
-        function onTokenCreated(params) {
-            document.getElementById('payment_token').value=params.onetime_token;
-            let button = document.querySelector("#form_btn");
-            button.click();
-        }
-        function onTokenFailed(params) {
-            var errors = '<div class="alert alert-failure mb-4"><ul><li>'+ params.response_description +'</li></ul></div>';
-            document.getElementById("forte_errors").innerHTML = errors;
-        }
-        function submitPay(){
-            var account_number=document.getElementById('account-number').value;
-            var routing_number=document.getElementById('routing-number').value;
-
-            var data = {
-               api_login_id: '{{$gateway->forte->company_gateway->getConfigField("apiLoginId")}}',
-               account_number: account_number,
-               routing_number: routing_number, 
-               account_type: "checking",
-            }
-
-            forte.createToken(data)
-               .success(onTokenCreated)
-               .error(onTokenFailed);
-            return false;
-        }
-    </script>
+    @if($gateway->forte->company_gateway->getConfigField('testMode'))
+        <script type="text/javascript" src="https://sandbox.forte.net/api/js/v1"></script>
+    @else
+        <script type="text/javascript" src="https://api.forte.net/js/v1"></script>
+    @endif
+    
+    <script src="{{ asset('js/clients/payments/forte-ach-payment.js') }}"></script>
 @endsection
--- resources/views/portal/ninja2020/gateways/forte/credit_card/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/forte/credit_card/pay.blade.php
@@ -1,16 +1,11 @@
 @extends('portal.ninja2020.layout.payments', ['gateway_title' => ctrans('texts.payment_type_credit_card'), 'card_title' => ctrans('texts.payment_type_credit_card')])
 
 @section('gateway_head')
+    <meta name="forte-api-login-id" content="{{$gateway->forte->company_gateway->getConfigField("apiLoginId")}}">
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="{{ asset('js/clients/payments/forte-card-js.min.js') }}"></script>
 
     <link href="{{ asset('css/card-js.min.css') }}" rel="stylesheet" type="text/css">
-
-    @if($gateway->forte->company_gateway->getConfigField('testMode'))
-        <script type="text/javascript" src="https://sandbox.forte.net/api/js/v1"></script>
-    @else
-        <script type="text/javascript" src="https://api.forte.net/js/v1"></script>
-    @endif
 @endsection
 
 @section('gateway_content')
@@ -41,50 +36,16 @@
        @include('portal.ninja2020.gateways.forte.includes.credit_card')
     @endcomponent
 
-    <div class="bg-white px-4 py-5 flex justify-end">
-        <button type="button"
-        onclick="submitPay()"
-            class="button button-primary bg-primary {{ $class ?? '' }}">
-                <svg class="animate-spin h-5 w-5 text-white hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-            <span>{{ $slot ?? ctrans('texts.pay_now') }}</span>
-        </button>
-    </div>
+    @include('portal.ninja2020.gateways.includes.pay_now')
 
 @endsection
 
 @section('gateway_footer')
-    <script>
-        function onTokenCreated(params) {
-            document.getElementById('payment_token').value=params.onetime_token;
-            document.getElementById('card_brand').value=params.card_type;
-            let button = document.querySelector("#form_btn");
-            button.click();
-        }
-        function onTokenFailed(params) {
-            var errors = '<div class="alert alert-failure mb-4"><ul><li>'+ params.response_description +'</li></ul></div>';
-            document.getElementById("forte_errors").innerHTML = errors;
-        }
-        function submitPay(){
-            var month=document.querySelector('input[name=expiry-month]').value;
-            var year=document.querySelector('input[name=expiry-year]').value;
-            var cc=document.getElementById('card_number').value.replaceAll(' ','');
-            var cvv=document.getElementById('cvv').value;
-
-            var data = {
-               api_login_id: '{{$gateway->forte->company_gateway->getConfigField("apiLoginId")}}',
-               card_number: cc,
-               expire_year: year, 
-               expire_month: month,
-               cvv: cvv,
-            }
-
-            forte.createToken(data)
-               .success(onTokenCreated)
-               .error(onTokenFailed);
-            return false;
-        }
-    </script>
+    @if($gateway->forte->company_gateway->getConfigField('testMode'))
+        <script type="text/javascript" src="https://sandbox.forte.net/api/js/v1"></script>
+    @else
+        <script type="text/javascript" src="https://api.forte.net/js/v1"></script>
+    @endif
+    
+    <script src="{{ asset('js/clients/payments/forte-credit-card-payment.js') }}"></script>
 @endsection
