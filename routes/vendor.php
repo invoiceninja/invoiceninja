e1b1114722df8fbda4c3cbaaf392c9d8da494623
David Bomba <turbo124@gmail.com>
2022-06-29 11:42:17 +1000

    Purchase order decorators

--- routes/vendor.php
+++ routes/vendor.php
@@ -20,6 +20,8 @@ Route::get('vendors', [VendorContactLoginController::class, 'catch'])->name('ven
 Route::group(['middleware' => ['invite_db'], 'prefix' => 'vendor', 'as' => 'vendor.'], function () {
     /*Invitation catches*/
     Route::get('purchase_order/{invitation_key}', [InvitationController::class, 'purchaseOrder']);
+    Route::get('purchase_order/{invitation_key}/download', [InvitationController::class, 'download']);
+
  //   Route::get('purchase_order/{invitation_key}/download_pdf', 'PurchaseOrderController@downloadPdf')->name('recurring_invoice.download_invitation_key');
  //   Route::get('purchase_order/{invitation_key}/download', 'ClientPortal\InvitationController@routerForDownload');
 
@@ -40,7 +42,7 @@ Route::group(['middleware' => ['auth:vendor', 'vendor_locale', 'domain_db'], 'pr
 
 });
 
-Route::get('purchase_order/{invitation_key}/download', 'VendorPortal\InvitationController@download');
+
 
 
 Route::fallback('BaseController@notFoundVendor');
