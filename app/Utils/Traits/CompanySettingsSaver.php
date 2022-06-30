0894752c80e240112364f205008cf5f8a6104e58
David Bomba <turbo124@gmail.com>
2022-06-30 17:51:39 +1000

    Fixes for tests

--- app/Utils/Traits/CompanySettingsSaver.php
+++ app/Utils/Traits/CompanySettingsSaver.php
@@ -120,6 +120,9 @@ trait CompanySettingsSaver
             elseif (substr($key, -3) == '_id' || substr($key, -14) == 'number_counter') {
                 $value = 'integer';
 
+                if($key == 'besr_id')
+                    $value = 'string';
+
                 if (! property_exists($settings, $key)) {
                     continue;
                 } elseif (! $this->checkAttribute($value, $settings->{$key})) {
@@ -185,6 +188,9 @@ trait CompanySettingsSaver
                 $value = 'integer';
 
                 if($key == 'gmail_sending_user_id')
+                    $value = 'string';
+
+                if($key == 'besr_id')
                     $value = 'string';
 
                 if (! property_exists($settings, $key)) {
