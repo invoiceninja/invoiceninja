f6a6381050bb7c00b46858f8ac867dfca1b44ee3
hillelcoren <hillelcoren@gmail.com>
2022-07-05 07:37:26 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"/": "822ce4d633db3626533357f47cfc8974",
+"/": "69905e6a3d544c719b18623661078154",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
@@ -38,8 +38,8 @@ const RESOURCES = {
 "assets/assets/images/payment_types/dinerscard.png": "06d85186ba858c18ab7c9caa42c92024",
 "assets/assets/images/payment_types/other.png": "d936e11fa3884b8c9f1bd5c914be8629",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "b62641afc9ab487008e996a5c5865e56",
-"assets/NOTICES": "9b6b63256d3a6491659b71127ee9f3b6",
-"main.dart.js": "a94c1dac6570caa3cd122344d13fad8e"
+"assets/NOTICES": "f02d972147eb81eae2a8202a2914cb88",
+"main.dart.js": "e99e39dff8062c71e7642397479e70cd"
 };
 
 // The application shell files that are downloaded before a service worker can
