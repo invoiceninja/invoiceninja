15da7b841ab5327d326e7f69d34f8f91a387666e
David Bomba <turbo124@gmail.com>
2022-07-10 10:56:37 +1000

    Fixes for indexes

--- database/migrations/2022_07_09_235510_add_index_to_payment_hash.php
+++ database/migrations/2022_07_09_235510_add_index_to_payment_hash.php
@@ -16,6 +16,17 @@ class AddIndexToPaymentHash extends Migration
         Schema::table('payment_hashes', function (Blueprint $table) {
             $table->string('hash', 255)->index()->change();
         });
+
+        Schema::table('activities', function (Blueprint $table) {
+            $table->index(['quote_id', 'company_id']);
+            $table->index(['recurring_invoice_id', 'company_id']);
+            $table->index(['purchase_order_id', 'company_id']);
+            $table->index(['vendor_contact_id', 'company_id']);
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->index(['company_id', 'user_id', 'assigned_user_id', 'updated_at'],'pro_co_us_up_index');
+        });
     }
 
     /**
