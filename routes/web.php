f1550493d24b1f34e0d6bbefe675a3f0e92d81fc
David Bomba <turbo124@gmail.com>
2022-07-17 20:56:17 +1000

    Laravel 9 release candidate

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\BaseController;
 use App\Http\Controllers\ClientPortal;
+use App\Http\Controllers\ClientPortal\ApplePayDomainController;
 use App\Http\Controllers\Gateways;
+use App\Http\Controllers\Gateways\Checkout3dsController;
+use App\Http\Controllers\Gateways\GoCardlessController;
+use App\Http\Controllers\Gateways\Mollie3dsController;
 use App\Http\Controllers\SelfUpdateController;
 use App\Http\Controllers\SetupController;
 use App\Http\Controllers\StripeConnectController;
@@ -24,10 +31,10 @@ Route::post('setup/check_db', [SetupController::class, 'checkDB'])->middleware('
 Route::post('setup/check_mail', [SetupController::class, 'checkMail'])->middleware('guest');
 Route::post('setup/check_pdf', [SetupController::class, 'checkPdf'])->middleware('guest');
 
-Route::get('password/reset', [Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->middleware('domain_db')->name('password.request');
-Route::post('password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [Auth\ResetPasswordController::class, 'showResetForm'])->middleware(['domain_db', 'email_db'])->name('password.reset');
-Route::post('password/reset', [Auth\ResetPasswordController::class, 'reset'])->middleware('email_db')->name('password.update');
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->middleware('domain_db')->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->middleware(['domain_db', 'email_db'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->middleware('email_db')->name('password.update');
 
 Route::get('wepay/signup/{token}', [WePayController::class, 'signup'])->name('wepay.signup');
 Route::get('wepay/finished', [WePayController::class, 'finished'])->name('wepay.finished');
@@ -36,8 +43,7 @@ Route::get('wepay/finished', [WePayController::class, 'finished'])->name('wepay.
  * Social authentication
  */
 
-Route::get('auth/{provider}', [Auth\LoginController::class, 'redirectToProvider']);
-// Route::get('auth/{provider}/create', [Auth\LoginController::class, 'redirectToProviderAndCreate']);
+Route::get('auth/{provider}', [LoginController::class, 'redirectToProvider']);
 
 /*
  * Inbound routes requiring DB Lookup
@@ -50,7 +56,7 @@ Route::middleware('url_db')->group(function () {
 Route::get('stripe/signup/{token}', [StripeConnectController::class, 'initialize'])->name('stripe_connect.initialization');
 Route::get('stripe/completed', [StripeConnectController::class, 'completed'])->name('stripe_connect.return');
 
-Route::get('checkout/3ds_redirect/{company_key}/{company_gateway_id}/{hash}', [Gateways\Checkout3dsController::class, 'index'])->middleware('domain_db')->name('checkout.3ds_redirect');
-Route::get('mollie/3ds_redirect/{company_key}/{company_gateway_id}/{hash}', [Gateways\Mollie3dsController::class, 'index'])->middleware('domain_db')->name('mollie.3ds_redirect');
-Route::get('gocardless/ibp_redirect/{company_key}/{company_gateway_id}/{hash}', [Gateways\GoCardlessController::class, 'ibpRedirect'])->middleware('domain_db')->name('gocardless.ibp_redirect');
-Route::get('.well-known/apple-developer-merchantid-domain-association', [ClientPortal\ApplePayDomainController::class, 'showAppleMerchantId']);
+Route::get('checkout/3ds_redirect/{company_key}/{company_gateway_id}/{hash}', [Checkout3dsController::class, 'index'])->middleware('domain_db')->name('checkout.3ds_redirect');
+Route::get('mollie/3ds_redirect/{company_key}/{company_gateway_id}/{hash}', [Mollie3dsController::class, 'index'])->middleware('domain_db')->name('mollie.3ds_redirect');
+Route::get('gocardless/ibp_redirect/{company_key}/{company_gateway_id}/{hash}', [GoCardlessController::class, 'ibpRedirect'])->middleware('domain_db')->name('gocardless.ibp_redirect');
+Route::get('.well-known/apple-developer-merchantid-domain-association', [ApplePayDomainController::class, 'showAppleMerchantId']);
