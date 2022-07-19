b3707967d8fb1bb9032d8fd39d9841ef74db9d2a
David Bomba <turbo124@gmail.com>
2022-07-20 09:24:38 +1000

    add support for showing related entity documents on invoice

--- resources/views/portal/ninja2020/components/entity-documents.blade.php
+++ resources/views/portal/ninja2020/components/entity-documents.blade.php
@@ -42,45 +42,49 @@
                         </div>
                     @endforeach
 
-                    @if($entity->expense && $entity->expense->invoice_documents)
-                        @foreach ($entity->expense->documents as $document)
-                            <div class="inline-flex items-center space-x-1">
-                                <a href="{{ route('client.documents.show', $document->hashed_id) }}" target="_blank"
-                                    class="block text-sm button-link text-primary">{{ Illuminate\Support\Str::limit($document->name, 40) }}</a>
+                    @if($entity instanceof App\Models\Invoice)
+                        @foreach ($entity->expense_documents() as $expense)
+                            @foreach($expense->documents as $document)
+                                <div class="inline-flex items-center space-x-1">
+                                    <a href="{{ route('client.documents.show', $document->hashed_id) }}" target="_blank"
+                                        class="block text-sm button-link text-primary">{{ Illuminate\Support\Str::limit($document->name, 40) }}</a>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                    class="text-primary h-6 w-4">
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        class="text-primary h-6 w-4">
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
 
-                                @if (!$loop->last)
-                                    <span>&mdash;</span>
-                                @endif
-                            </div>
+                                    @if (!$loop->last)
+                                        <span>&mdash;</span>
+                                    @endif
+                                </div>
+                            @endforeach
                         @endforeach
                     @endif
 
-                    @if($entity->task && $entity->company->invoice_task_documents)
-                        @foreach ($entity->task->documents as $document)
-                            <div class="inline-flex items-center space-x-1">
-                                <a href="{{ route('client.documents.show', $document->hashed_id) }}" target="_blank"
-                                    class="block text-sm button-link text-primary">{{ Illuminate\Support\Str::limit($document->name, 40) }}</a>
+                    @if($entity instanceof App\Models\Invoice)
+                        @foreach ($entity->task_documents() as $task)
+                            @foreach($task->documents as $document)
+                                <div class="inline-flex items-center space-x-1">
+                                    <a href="{{ route('client.documents.show', $document->hashed_id) }}" target="_blank"
+                                        class="block text-sm button-link text-primary">{{ Illuminate\Support\Str::limit($document->name, 40) }}</a>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                    class="text-primary h-6 w-4">
-                                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                    <polyline points="15 3 21 3 21 9"></polyline>
-                                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                                </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        class="text-primary h-6 w-4">
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg>
 
-                                @if (!$loop->last)
-                                    <span>&mdash;</span>
-                                @endif
-                            </div>
+                                    @if (!$loop->last)
+                                        <span>&mdash;</span>
+                                    @endif
+                                </div>
+                            @endforeach
                         @endforeach
                     @endif
                 </div>
