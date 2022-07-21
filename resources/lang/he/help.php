10d7a96a37bc8eacacef02f3a02de84f7e2aeb9a
David Bomba <turbo124@gmail.com>
2022-07-21 14:02:57 +1000

    Add hebrew to repo

---
+++ resources/lang/he/help.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang = [
+    'client_dashboard' => 'Message to be displayed on clients dashboard',
+    'client_currency' => 'The client currency.',
+    'client_language' => 'The client language.',
+    'client_payment_terms' => 'The client payment terms.',
+    'client_paid_invoice' => 'Message to be displayed on a clients paid invoice screen',
+    'client_unpaid_invoice' => 'Message to be displayed on a clients unpaid invoice screen',
+    'client_unapproved_quote' => 'Message to be displayed on a clients unapproved quote screen',
+];
+
+return $lang;
