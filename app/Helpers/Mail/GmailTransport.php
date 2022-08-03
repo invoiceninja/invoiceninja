6ca955164e7245017e0350a5ae63a80121428efd
David Bomba <turbo124@gmail.com>
2022-08-03 12:35:38 +1000

    fixes for bccs

--- app/Helpers/Mail/GmailTransport.php
+++ app/Helpers/Mail/GmailTransport.php
@@ -31,7 +31,7 @@ class GmailTransport extends AbstractTransport
 
     protected function doSend(SentMessage $message): void
     {
-        nlog("in Do Send");
+        nlog("In Do Send");
         $message = MessageConverter::toEmail($message->getOriginalMessage());
 
         $token = $message->getHeaders()->get('gmailtoken')->getValue();
@@ -45,7 +45,25 @@ class GmailTransport extends AbstractTransport
         $service = new Gmail($client);
 
         $body = new Message();
-        $body->setRaw($this->base64_encode($message->toString()));
+
+        $bccs = $message->getHeaders()->get('Bcc');
+
+        $bcc_list = '';
+
+        if($bccs)
+        {
+            $bcc_list = 'Bcc: ';
+
+            foreach($bccs->getAddresses() as $address){
+
+                $bcc_list .= $address->getAddress() .',';
+
+            }
+
+            $bcc_list = rtrim($bcc_list, ",") . "\r\n";
+        }  
+
+        $body->setRaw($this->base64_encode($bcc_list.$message->toString()));
 
         $service->users_messages->send('me', $body, []);
         
