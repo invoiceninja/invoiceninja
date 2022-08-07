776da3b3ca104be62b9e3a4653cab497e11a04fc
David Bomba <turbo124@gmail.com>
2022-08-07 16:47:37 +1000

    Fixes for auto billing

--- app/Jobs/Cron/AutoBill.php
+++ app/Jobs/Cron/AutoBill.php
@@ -14,10 +14,14 @@ namespace App\Jobs\Cron;
 use App\Libraries\MultiDB;
 use App\Models\Invoice;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
-class AutoBill
+class AutoBill implements ShouldQueue
 {
-    use Dispatchable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $tries = 1;
 
