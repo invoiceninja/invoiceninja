baf38e07d39d4307216d0b895e9a836bea55e9c0
David Bomba <turbo124@gmail.com>
2022-08-12 22:35:30 +1000

    OpenAPI

--- app/Http/Controllers/OpenAPI/ProjectSchema.php
+++ app/Http/Controllers/OpenAPI/ProjectSchema.php
@@ -3,7 +3,7 @@
  * @OA\Schema(
  *   schema="Project",
  *   type="object",
- *       @OA\Property(property="id", type="string", example="Opnel5aKBz", description="______"),
- *       @OA\Property(property="name", type="string", example="New Project", description="______"),
+ *       @OA\Property(property="id", type="string", example="Opnel5aKBz", description="The project hashed id"),
+ *       @OA\Property(property="name", type="string", example="New Project", description="The project name"),
  * )
  */
