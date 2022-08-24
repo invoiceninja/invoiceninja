7d0129516842c899b44a753f4a09305018e0fcf2
David Bomba <turbo124@gmail.com>
2022-08-25 08:00:52 +1000

    Add new column for invoice_task_project

--- app/Transformers/CompanyTransformer.php
+++ app/Transformers/CompanyTransformer.php
@@ -178,6 +178,7 @@ class CompanyTransformer extends EntityTransformer
             'track_inventory' => (bool) $company->track_inventory,
             'enable_applying_payments' => (bool) $company->enable_applying_payments,
             'enabled_expense_tax_rates' => (int) $company->enabled_expense_tax_rates,
+            'invoice_task_project' => (bool) $company->invoice_task_project,
         ];
     }
 
