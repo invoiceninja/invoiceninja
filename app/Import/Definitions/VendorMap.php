0c8aaa678265ac58bd3c174705e119702e6fc997
David Bomba <turbo124@gmail.com>
2022-09-01 16:09:25 +1000

    Support for vendor and vendor contact imports

--- app/Import/Definitions/VendorMap.php
+++ app/Import/Definitions/VendorMap.php
@@ -33,6 +33,15 @@ class VendorMap
             14 => 'vendor.state',
             15 => 'vendor.postal_code',
             16 => 'vendor.country_id',
+            17 => 'contact.first_name',
+            18 => 'contact.last_name',
+            19 => 'contact.email',
+            20 => 'contact.phone',
+            21 => 'contact.custom_value1',
+            22 => 'contact.custom_value2',
+            23 => 'contact.custom_value3',
+            24 => 'contact.custom_value4',
+
         ];
     }
 
@@ -56,6 +65,14 @@ class VendorMap
             14 => 'texts.state',
             15 => 'texts.postal_code',
             16 => 'texts.country',
+            17 => 'texts.first_name',
+            18 => 'texts.last_name',
+            19 => 'texts.email',
+            20 => 'texts.phone',
+            21 => 'texts.custom_value',
+            22 => 'texts.custom_value',
+            23 => 'texts.custom_value',
+            24 => 'texts.custom_value',
         ];
     }
 }
