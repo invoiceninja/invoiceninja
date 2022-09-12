0c0687febeffdb3c6f7e71403d6bec81428b3939
David Bomba <turbo124@gmail.com>
2022-09-13 07:54:32 +1000

    Fixes for entity policy not capturing recurring invoice policies

--- app/Policies/EntityPolicy.php
+++ app/Policies/EntityPolicy.php
@@ -47,7 +47,7 @@ class EntityPolicy
     public function edit(User $user, $entity) : bool
     {
         return ($user->isAdmin() && $entity->company_id == $user->companyId())
-            || ($user->hasPermission('edit_'.strtolower(class_basename($entity))) && $entity->company_id == $user->companyId())
+            || ($user->hasPermission('edit_'.strtolower(\Illuminate\Support\Str::snake(class_basename($entity)))) && $entity->company_id == $user->companyId())
             || ($user->hasPermission('edit_all') && $entity->company_id == $user->companyId())
             || $user->owns($entity)
             || $user->assigned($entity);
