5f6de0f56a9c22f947fde0e60b406e5fd700128b
David Bomba <turbo124@gmail.com>
2022-09-21 12:51:38 +1000

    Fixes for seeders

--- database/migrations/2022_09_21_012417_add_threeds_to_braintree.php
+++ database/migrations/2022_09_21_012417_add_threeds_to_braintree.php
@@ -17,12 +17,15 @@ return new class extends Migration
     {
 
         $g = Gateway::find(50);
-        $fields = json_decode($g->fields);
-        $fields->threeds = false;
 
-        $g->fields = json_encode($fields);
-        $g->save();
+        if($g)
+        {
+            $fields = json_decode($g->fields);
+            $fields->threeds = false;
 
+            $g->fields = json_encode($fields);
+            $g->save();
+        }
 
         CompanyGateway::where('gateway_key', 'f7ec488676d310683fb51802d076d713')->cursor()->each(function ($cg){
 
