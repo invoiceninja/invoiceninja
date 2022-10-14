f622b3786acf4d3828524220bc9173edcf24542c
David Bomba <turbo124@gmail.com>
2022-10-14 20:38:20 +1100

    Minor changes for setup intents

--- app/PaymentDrivers/Stripe/Charge.php
+++ app/PaymentDrivers/Stripe/Charge.php
@@ -78,6 +78,7 @@ class Charge
                 'payment_method' => $cgt->token,
                 'customer' => $cgt->gateway_customer_reference,
                 'confirm' => true,
+                // 'off_session' => true,
                 'description' => $description,
                 'metadata' => [
                     'payment_hash' => $payment_hash->hash,
