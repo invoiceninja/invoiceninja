ddc78c3e2ab534ccc1d1eb65fd034c736eaf748b
David Bomba <turbo124@gmail.com>
2022-10-25 13:10:58 +1100

    Minor fixes for bank transformer

--- app/Import/Transformers/Bank/BankTransformer.php
+++ app/Import/Transformers/Bank/BankTransformer.php
@@ -12,7 +12,7 @@
 namespace App\Import\Transformers\Bank;
 
 use App\Import\ImportException;
-use App\Import\Transformer\BaseTransformer;
+use App\Import\Transformers\BaseTransformer;
 use App\Models\BankTransaction;
 use App\Utils\Number;
 
@@ -33,11 +33,11 @@ class BankTransformer extends BaseTransformer
     	$transformed = [
             // 'bank_integration_id' => $this->bank_integration->id,
             'transaction_id' => $this->getNumber($transaction,'bank.transaction_id'),
-            'amount' => $this->getFloat($transaction, 'bank.amount'),
+            'amount' => abs($this->getFloat($transaction, 'bank.amount')),
             'currency_id' => $this->getCurrencyByCode($transaction, 'bank.currency'),
-            'account_type' => array_key_exists('account_type', $transaction) ? $transaction['account_type'] : 'bank',
-            'category_id' => array_key_exists('category_id', $transaction) ? $transaction['category_id'] : null,
-            'category_type' => array_key_exists('category_type', $transaction) ? $transaction['category_type'] : '',
+            'account_type' => strlen($this->getString($transaction, 'bank.account_type')) > 1 ? $this->getString($transaction, 'bank.account_type') : 'bank',
+            'category_id' => $this->getNumber($transaction, 'bank.category_id') > 0 ? $this->getNumber($transaction, 'bank.category_id') : null,
+            'category_type' => $this->getString($transaction, 'category_type'),
             'date' => array_key_exists('date', $transaction) ? date('Y-m-d', strtotime(str_replace("/","-",$transaction['date'])))
                 : now()->format('Y-m-d'),
             'bank_account_id' => array_key_exists('bank_account_id', $transaction) ? $transaction['bank_account_id'] : 0,
