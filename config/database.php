cc842dc3d98b96c608d236adcc9b7630b501514c
Muhammad Nur'Ihsan Bin Berahim <ihsanberahim@gmail.com>
2022-10-27 11:25:08 +0800

    Update database.php
    Include `MYSQL_ATTR_SSL_CA`. It is required for mysql at `planetscale.com`

--- config/database.php
+++ config/database.php
@@ -59,6 +59,9 @@ return [
             'prefix'    => '',
             'strict'    => env('DB_STRICT', false),
             'engine'    => 'InnoDB',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         // multi-database setup
