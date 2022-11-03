f40360e1fe8f9549e32c2c0f6dbbbfb7b7a3d5a1
David Bomba <turbo124@gmail.com>
2022-11-03 19:44:38 +1100

    Fixes for bank transaction imports

--- app/Import/Transformers/Bank/BankTransformer.php
+++ app/Import/Transformers/Bank/BankTransformer.php
@@ -31,17 +31,17 @@ class BankTransformer extends BaseTransformer
     	$now = now();
 
     	$transformed = [
-            // 'bank_integration_id' => $this->bank_integration->id,
+            'bank_integration_id' => $transaction['bank.bank_integration_id'],
             'transaction_id' => $this->getNumber($transaction,'bank.transaction_id'),
             'amount' => abs($this->getFloat($transaction, 'bank.amount')),
             'currency_id' => $this->getCurrencyByCode($transaction, 'bank.currency'),
             'account_type' => strlen($this->getString($transaction, 'bank.account_type')) > 1 ? $this->getString($transaction, 'bank.account_type') : 'bank',
             'category_id' => $this->getNumber($transaction, 'bank.category_id') > 0 ? $this->getNumber($transaction, 'bank.category_id') : null,
-            'category_type' => $this->getString($transaction, 'category_type'),
-            'date' => array_key_exists('date', $transaction) ? $this->parseDate($transaction['date'])
+            'category_type' => $this->getString($transaction, 'bank.category_type'),
+            'date' => array_key_exists('bank.date', $transaction) ? $this->parseDate($transaction['bank.date'])
                 : now()->format('Y-m-d'),
-            'bank_account_id' => array_key_exists('bank_account_id', $transaction) ? $transaction['bank_account_id'] : 0,
-            'description' => array_key_exists('description', $transaction)? $transaction['description'] : '',
+            'bank_account_id' => array_key_exists('bank.bank_account_id', $transaction) ? $transaction['bank.bank_account_id'] : 0,
+            'description' => array_key_exists('bank.description', $transaction) ? $transaction['bank.description'] : '',
             'base_type' => $this->calculateType($transaction),
             'created_at' => $now,
             'updated_at' => $now,
@@ -56,22 +56,22 @@ class BankTransformer extends BaseTransformer
     private function calculateType($transaction)
     {
 
-    	if(array_key_exists('base_type', $transaction) && $transaction['base_type'] == 'CREDIT')
+    	if(array_key_exists('bank.base_type', $transaction) && $transaction['bank.base_type'] == 'CREDIT')
     		return 'CREDIT';
 
-    	if(array_key_exists('base_type', $transaction) && $transaction['base_type'] == 'DEBIT')
+    	if(array_key_exists('bank.base_type', $transaction) && $transaction['bank.base_type'] == 'DEBIT')
     		return 'DEBIT';
 
-    	if(array_key_exists('category_id',$transaction))
+    	if(array_key_exists('bank.category_id', $transaction))
     		return 'DEBIT';
 
-    	if(array_key_exists('category_type', $transaction) && $transaction['category_type'] == 'Income')
+    	if(array_key_exists('bank.category_type', $transaction) && $transaction['bank.category_type'] == 'Income')
     		return 'CREDIT';
 
-    	if(array_key_exists('category_type', $transaction))
+    	if(array_key_exists('bank.category_type', $transaction))
     		return 'DEBIT';
 
-    	if(array_key_exists('amount', $transaction) && is_numeric($transaction['amount']) && $transaction['amount'] > 0)
+    	if(array_key_exists('bank.amount', $transaction) && is_numeric($transaction['bank.amount']) && $transaction['bank.amount'] > 0)
     		return 'CREDIT';
 
     	return 'DEBIT';
