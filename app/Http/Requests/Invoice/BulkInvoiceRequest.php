8fd2254115fef332f7dad332c68350ee4a955a7b
David Bomba <turbo124@gmail.com>
2022-11-05 17:09:11 +1100

    Commit missing request class

---
+++ app/Http/Requests/Invoice/BulkInvoiceRequest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com)
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Http\Requests\Invoice;
+
+use App\Http\Requests\Request;
+
+class BulkInvoiceRequest extends Request
+{
+
+    public function authorize() : bool
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'action' => 'required|string',
+            'ids' => 'required'
+        ];
+    }
+
+}
