7019cb1779f19892f3118d20e1b9cfe4d2f75b5b
David Bomba <turbo124@gmail.com>
2022-11-05 12:27:01 +1100

    Yodlee Webhooks

--- app/Http/Controllers/Bank/YodleeController.php
+++ app/Http/Controllers/Bank/YodleeController.php
@@ -114,7 +114,192 @@ class YodleeController extends BaseController
 
         });
 
+    }
 
+
+ /**
+     * Process Yodlee Refresh Webhook.
+     *
+     *
+     * @OA\Post(
+     *      path="/api/v1/yodlee/refresh",
+     *      operationId="yodleeRefreshWebhook",
+     *      tags={"yodlee"},
+     *      summary="Processing webhooks from Yodlee",
+     *      description="Notifies the system when a data point can be refreshed",
+     *      @OA\Parameter(ref="#/components/parameters/X-Api-Secret"),
+     *      @OA\Parameter(ref="#/components/parameters/X-Api-Token"),
+     *      @OA\Parameter(ref="#/components/parameters/X-Requested-With"),
+     *      @OA\Parameter(ref="#/components/parameters/include"),
+     *      @OA\Response(
+     *          response=200,
+     *          description="",
+     *          @OA\Header(header="X-MINIMUM-CLIENT-VERSION", ref="#/components/headers/X-MINIMUM-CLIENT-VERSION"),
+     *          @OA\Header(header="X-RateLimit-Remaining", ref="#/components/headers/X-RateLimit-Remaining"),
+     *          @OA\Header(header="X-RateLimit-Limit", ref="#/components/headers/X-RateLimit-Limit"),
+     *          @OA\JsonContent(ref="#/components/schemas/Credit"),
+     *       ),
+     *       @OA\Response(
+     *          response=422,
+     *          description="Validation error",
+     *          @OA\JsonContent(ref="#/components/schemas/ValidationError"),
+     *
+     *       ),
+     *       @OA\Response(
+     *           response="default",
+     *           description="Unexpected Error",
+     *           @OA\JsonContent(ref="#/components/schemas/Error"),
+     *       ),
+     *     )
+     */
+
+ /*
+ {
+   "event":{
+      "info":"REFRESH.PROCESS_COMPLETED",
+      "loginName":"fri21",
+      "data":{
+         "providerAccount":[
+            {
+               "id":10995860,
+               "providerId":16441,
+               "isManual":false,
+               "createdDate":"2017-12-22T05:47:35Z",
+               "aggregationSource":"USER",
+               "status":"SUCCESS",
+               "requestId":"NSyMGo+R4dktywIu3hBIkc3PgWA=",
+               "dataset":[
+                  {
+                     "name":"BASIC_AGG_DATA",
+                     "additionalStatus":"AVAILABLE_DATA_RETRIEVED",
+                     "updateEligibility":"ALLOW_UPDATE",
+                     "lastUpdated":"2017-12-22T05:48:16Z",
+                     "lastUpdateAttempt":"2017-12-22T05:48:16Z"
+                  }
+               ]
+            }
+         ]
+      }
+   }
+}*/
+    public function refreshWebhook(Request $request)
+    {
+//we should ignore this one
+        nlog("yodlee refresh");
+        nlog($request->all());
+
+        return response()->json(['message' => 'Success'], 200);
+    
+        //
+
+        // return response()->json(['message' => 'Unauthorized'], 403);
+    }
+    
+/*
+{
+   "event":{
+      "notificationId":"63c73475-4db5-49ef-8553-8303337ca7c3",
+      "info":"LATEST_BALANCE_UPDATES",
+      "loginName":"user1",
+      "data":{
+         "providerAccountId":658552,
+         "latestBalanceEvent":[
+            {
+               "accountId":12345,
+               "status":"SUCCESS"
+            },
+            {
+               "accountId":12346,
+               "status":"FAILED"
+            }
+         ]
+      }
+   }
+}
+*/
+    public function balanceWebhook(Request $request)
+    {
+
+        nlog("yodlee refresh");
+        nlog($request->all());
+
+        return response()->json(['message' => 'Success'], 200);
+    
+        //
+
+        // return response()->json(['message' => 'Unauthorized'], 403);
+    }
+    
+/*
+{  
+   "event":{  
+      "data":[  
+         {  
+            "autoRefresh":{  
+               "additionalStatus":"SCHEDULED",
+               "status":"ENABLED"
+            },
+            "accountIds":[  
+               1112645899,
+               1112645898
+            ],
+            "loginName":"YSL1555332811628",
+            "providerAccountId":11381459
+         }
+      ],
+      "notificationTime":"2019-06-14T04:49:39Z",
+      "notificationId":"4e672150-156048777",
+      "info":"AUTO_REFRESH_UPDATES"
+   }
+}
+*/
+    public function refreshUpdatesWebhook(Request $request)
+    {
+//notifies a user if there are problems with yodlee accessing the data
+        nlog("update refresh");
+        nlog($request->all());
+
+        return response()->json(['message' => 'Success'], 200);
+    
+        //
+
+        // return response()->json(['message' => 'Unauthorized'], 403);
+    }
+
+
+/*
+"event": {
+    "notificationId": "64b7ed1a-1530523285",
+    "info": "DATA_UPDATES.USER_DATA",
+    "data": {
+        "userCount": 1,
+        "fromDate": "2017-11-10T10:18:44Z",
+        "toDate": "2017-11-10T11:18:43Z",
+        "userData": [{
+            "user": {
+                "loginName": "YSL1484052178554"
+            },
+            "links": [{
+                "methodType": "GET",
+                "rel": "getUserData",
+                "href": "dataExtracts/userData?fromDate=2017-11-10T10:18:44Z&toDate=2017-11-10T11:18:43Z&loginName=YSL1484052178554"
+            }]
+        }]
+    }
+}
+*/
+    public function dataUpdatesWebhook(Request $request)
+    {
+//this is the main hook we use for notifications
+
+        nlog("data refresh");
+        nlog($request->all());
+
+        return response()->json(['message' => 'Success'], 200);
+    
+        //
+
+        // return response()->json(['message' => 'Unauthorized'], 403);
     }
 
 }
