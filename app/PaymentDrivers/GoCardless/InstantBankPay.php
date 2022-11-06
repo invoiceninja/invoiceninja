f4b9889f0f12f0516a8c2f40591631707a9b03b9
David Bomba <turbo124@gmail.com>
2022-11-07 10:46:05 +1100

    Improvements for instant bank payments with gocardless

--- app/PaymentDrivers/GoCardless/InstantBankPay.php
+++ app/PaymentDrivers/GoCardless/InstantBankPay.php
@@ -60,6 +60,9 @@ class InstantBankPay implements MethodInterface
                         'amount' => (string) $data['amount_with_fee'] * 100,
                         'currency' => $this->go_cardless->client->getCurrencyCode(),
                     ],
+                    'metadata' => [
+                        'payment_hash' => $this->go_cardless->payment_hash->hash,
+                    ],
                 ],
             ]);
 
@@ -72,6 +75,9 @@ class InstantBankPay implements MethodInterface
                     ]),
                     'links' => [
                         'billing_request' => $billing_request->id,
+                    ],
+                    'metadata' => [
+                        'payment_hash' => $this->go_cardless->payment_hash->hash,
                     ],
                 ],
             ]);
