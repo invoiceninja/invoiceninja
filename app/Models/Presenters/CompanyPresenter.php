85a8edaab19b7f1210503b8ea02fba1f75d9d5ac
David Bomba <turbo124@gmail.com>
2022-11-14 08:09:08 +1100

    Change logo

--- app/Models/Presenters/CompanyPresenter.php
+++ app/Models/Presenters/CompanyPresenter.php
@@ -88,8 +88,10 @@ class CompanyPresenter extends EntityPresenter
             return "data:image/png;base64, ". base64_encode(@file_get_contents($settings->company_logo, false, stream_context_create($context_options)));
         else if(strlen($settings->company_logo) >= 1)
             return "data:image/png;base64, ". base64_encode(@file_get_contents(url('') . $settings->company_logo, false, stream_context_create($context_options)));
-        else
-            return "data:image/png;base64, ". base64_encode(@file_get_contents(asset('images/new_logo.png'), false, stream_context_create($context_options)));
+        else{
+            return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+            //return "data:image/png;base64, ". base64_encode(@file_get_contents(asset('images/new_logo.png'), false, stream_context_create($context_options)));
+        }
 
     }
 
