6b5707caa428f5a43071f24239556c164799f266
hillelcoren <hillelcoren@gmail.com>
2022-11-28 21:06:44 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -296,12 +296,12 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"/": "1fc9dce3a7ed2367f77f767171c9ea1b",
+"/": "83aac01abae3eb27a43bdb1f55895304",
 "version.json": "04dda2a311adc1947afc5c1055f19e16",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
-"main.dart.js": "2b510f8e255a4e6bb8a2e6ccbfc6c7a8"
+"main.dart.js": "91d67096ddd71f22b9cae01c774205f2"
 };
 
 // The application shell files that are downloaded before a service worker can
