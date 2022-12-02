700087a752e7c6ab2585b98ee5e7ba43382cbbb6
Lars Kusch <lars@lars-kusch.de>
2022-12-02 12:34:11 +0100

    Translate payment failure

--- app/PaymentDrivers/Stripe/Bancontact.php
+++ app/PaymentDrivers/Stripe/Bancontact.php
@@ -152,6 +152,6 @@ class Bancontact
             $this->stripe->client->company,
         );
 
-        throw new PaymentFailed('Failed to process the payment.', 500);
+        throw new PaymentFailed(ctrans('texts.payment_provider_failed_process_payment'), 500);
     }
 }
