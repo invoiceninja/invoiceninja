d7f12d62a66cacd2987da7df43c7b8f462457495
David Bomba <turbo124@gmail.com>
2022-12-02 13:19:02 +1100

    Count Bank Account Created on Hosted

---
+++ app/DataMapper/Analytics/BankAccountsCreated.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\DataMapper\Analytics;
+
+use Turbo124\Beacon\ExampleMetric\GenericMixedMetric;
+
+class BankAccountsCreated extends GenericMixedMetric
+{
+    /**
+     * The type of Sample.
+     *
+     * Monotonically incrementing counter
+     *
+     *  - counter
+     *
+     * @var string
+     */
+    public $type = 'mixed_metric';
+
+    /**
+     * The name of the counter.
+     * @var string
+     */
+    public $name = 'bank_accounts.created';
+
+    /**
+     * The datetime of the counter measurement.
+     *
+     * date("Y-m-d H:i:s")
+     *
+     * @var DateTime
+     */
+    public $datetime;
+
+    public $int_metric1 = 0;
+
+    public function __construct($int_metric1)
+    {
+        $this->int_metric1 = $int_metric1;
+    }
+}
