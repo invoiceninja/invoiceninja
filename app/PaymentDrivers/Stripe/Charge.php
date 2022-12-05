d9fd287dbf6bd74a752d480449d13cc2bc00f9bb
Lars Kusch <lars@lars-kusch.de>
2022-12-05 07:56:37 +0100

    Add support for multiple invoices

--- app/PaymentDrivers/Stripe/Charge.php
+++ app/PaymentDrivers/Stripe/Charge.php
@@ -60,12 +60,12 @@ class Charge
         }
 
         $amount = array_sum(array_column($payment_hash->invoices(), 'amount')) + $payment_hash->fee_total;
-        $invoice = Invoice::whereIn('id', $this->transformKeys(array_column($payment_hash->invoices(), 'invoice_id')))->withTrashed()->first();
+        $invoice_numbers = collect($data['invoices'])->pluck('invoice_number')
 
-        if ($invoice) {
-            $description = ctrans('texts.payment_provider_paymenttext_tokenbilling', ['invoicenumber' => $invoice->number, 'amount' => Number::formatMoney($amount, $this->stripe->client), 'client' => $this->stripe->client->present()->name()]);
+        if ($invoice_numbers.length > 0) {
+            $description = ctrans('texts.payment_provider_paymenttext', ['invoicenumber' => $invoice_numbers->implode(', '), 'amount' => Number::formatMoney($amount, $this->stripe->client), 'client' => $this->stripe->client->present()->name()]);
         } else {
-            $description = ctrans('texts.payment_prvoder_paymenttext_without_invoice_tokenbilling', ['amount' => Number::formatMoney($amount, $this->stripe->client), 'client' => $this->stripe->client->present()->name()]);
+            $description = ctrans('texts.payment_prvoder_paymenttext_without_invoice', ['amount' => Number::formatMoney($amount, $this->stripe->client), 'client' => $this->stripe->client->present()->name()]);
         }
 
         $this->stripe->init();
