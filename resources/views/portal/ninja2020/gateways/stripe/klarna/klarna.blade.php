1c1cf36ec30769702c623520d2aa310e3bd07848
Lars Kusch <lars@lars-kusch.de>
2022-12-10 07:06:08 +0100

    Further fixes

--- resources/views/portal/ninja2020/gateways/stripe/klarna/klarna.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/klarna/klarna.blade.php
@@ -1,6 +1,6 @@
-<div id="stripe--payment-container">
+<div id="klarna-name-correction" hidden>
     @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.name')])
-        <div class="form-group mb-[10px]" id="klarna-name-correction" hidden>
+        <div class="form-group mb-[10px]">
             <input class="input w-full" id="klarna-name" type="text" placeholder="{{ ctrans('texts.bank_account_holder') }}" value="{{ $gateway->client->present()->name()}}">
         </div>
 
