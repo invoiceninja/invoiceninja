66e4270531765bfe7c54793b35e5e306ef6c52cc
David Bomba <turbo124@gmail.com>
2022-12-15 08:39:27 +1100

    Move attachments into data

--- app/Mail/TemplateEmail.php
+++ app/Mail/TemplateEmail.php
@@ -118,10 +118,7 @@ class TemplateEmail extends Mailable
                 'logo' => $this->company->present()->logo($settings),
             ]);
 
-        nlog($this->build_email->getAttachments());
-
-
-        foreach (array_reverse($this->build_email->getAttachments()) as $file) {
+        foreach ($this->build_email->getAttachments() as $file) {
             if(array_key_exists('file', $file))
                 $this->attachData(base64_decode($file['file']), $file['name']);
             else
