93f836761db93f5777e50744562baec7e6ae3bdb
David Bomba <turbo124@gmail.com>
2022-12-16 10:02:29 +1100

    Prevent relinking of payments with transactions

--- app/Filters/PaymentFilters.php
+++ app/Filters/PaymentFilters.php
@@ -84,13 +84,11 @@ class PaymentFilters extends QueryFilters
     /**
      * Returns a list of payments that can be matched to bank transactions
      */
-    public function match_transactions($value = '')
+    public function match_transactions($value)
     {
 
-        if($value == 'true')
-        {
+        if($value)
             return $this->builder->where('is_deleted',0)->whereNull('transaction_id');
-        }
 
         return $this->builder;
     }
