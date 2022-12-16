a48e08cbefbaa1752ff39a15a78191561db535d4
Lars Kusch <lars@lars-kusch.de>
2022-12-16 11:09:37 +0100

    Scafold inital classes

---
+++ app/PaymentDrivers/Stripe/BACS.php
@@ -0,0 +1,213 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\PaymentDrivers\Stripe;
+
+use App\Exceptions\PaymentFailed;
+use App\Http\Requests\ClientPortal\Payments\PaymentResponseRequest;
+use App\Jobs\Util\SystemLogger;
+use App\Models\GatewayType;
+use App\Models\Payment;
+use App\Models\PaymentType;
+use App\Models\SystemLog;
+use App\PaymentDrivers\StripePaymentDriver;
+use App\PaymentDrivers\Stripe\Jobs\UpdateCustomer;
+use Stripe\PaymentIntent;
+use Stripe\PaymentMethod;
+use App\Utils\Number;
+
+class BACS
+{
+    public $stripe;
+
+    public function __construct(StripePaymentDriver $stripe)
+    {
+        $this->stripe = $stripe;
+    }
+
+    public function authorizeView(array $data)
+    {
+        $intent['intent'] = $this->stripe->getSetupIntent();
+
+        return render('gateways.stripe.bacs.authorize', array_merge($data, $intent));
+    }
+
+    public function authorizeResponse($request)
+    {
+        $this->stripe->init();
+
+        $stripe_response = json_decode($request->input('gateway_response'));
+
+        $customer = $this->stripe->findOrCreateCustomer();
+
+        $this->stripe->attach($stripe_response->payment_method, $customer);
+
+        $stripe_method = $this->stripe->getStripePaymentMethod($stripe_response->payment_method);
+
+        $this->storePaymentMethod($stripe_method, $request->payment_method_id, $customer);
+
+        return redirect()->route('client.payment_methods.index');
+    }
+
+    public function paymentView(array $data)
+    {
+
+        // $description = $this->stripe->decodeUnicodeString(ctrans('texts.invoices') . ': ' . collect($data['invoices'])->pluck('invoice_number')) . " for client {$this->stripe->client->present()->name()}";
+        $invoice_numbers = collect($data['invoices'])->pluck('invoice_number')->implode(',');
+        $description = ctrans('texts.stripe_payment_text', ['invoicenumber' => $invoice_numbers, 'amount' => Number::formatMoney($data['total']['amount_with_fee'], $this->stripe->client), 'client' => $this->stripe->client->present()->name()]);
+
+        $payment_intent_data = [
+            'amount' => $this->stripe->convertToStripeAmount($data['total']['amount_with_fee'], $this->stripe->client->currency()->precision, $this->stripe->client->currency()),
+            'currency' => $this->stripe->client->getCurrencyCode(),
+            'customer' => $this->stripe->findOrCreateCustomer(),
+            'description' => $description,
+            'metadata' => [
+                'payment_hash' => $this->stripe->payment_hash->hash,
+                'gateway_type_id' => GatewayType::BACS,
+            ],
+            'setup_future_usage' => 'off_session',
+        ];
+
+        $data['intent'] = $this->stripe->createPaymentIntent($payment_intent_data);
+        $data['gateway'] = $this->stripe;
+
+        return render('gateways.stripe.bacs.pay', $data);
+    }
+    public function paymentResponse(PaymentResponseRequest $request)
+    {
+        $this->stripe->init();
+
+        $state = [
+            'server_response' => json_decode($request->gateway_response),
+            'payment_hash' => $request->payment_hash,
+        ];
+
+        $state = array_merge($state, $request->all());
+        $state['store_card'] = boolval($state['store_card']);
+
+        if ($request->has('token') && ! is_null($request->token)) {
+            $state['store_card'] = false;
+        }
+
+        $state['payment_intent'] = PaymentIntent::retrieve($state['server_response']->id, array_merge($this->stripe->stripe_connect_auth, ['idempotency_key' => uniqid("st",true)]));
+        $state['customer'] = $state['payment_intent']->customer;
+
+        $this->stripe->payment_hash->data = array_merge((array) $this->stripe->payment_hash->data, $state);
+        $this->stripe->payment_hash->save();
+
+        $server_response = $this->stripe->payment_hash->data->server_response;
+
+        if ($server_response->status == 'succeeded') {
+            $this->stripe->logSuccessfulGatewayResponse(['response' => json_decode($request->gateway_response), 'data' => $this->stripe->payment_hash], SystemLog::TYPE_STRIPE);
+
+            return $this->processSuccessfulPayment();
+        }
+
+        return $this->processUnsuccessfulPayment($server_response);
+    }
+
+    public function processSuccessfulPayment()
+    {
+        UpdateCustomer::dispatch($this->stripe->company_gateway->company->company_key, $this->stripe->company_gateway->id, $this->stripe->client->id);
+
+        $stripe_method = $this->stripe->getStripePaymentMethod($this->stripe->payment_hash->data->server_response->payment_method);
+
+        $data = [
+            'payment_method' => $this->stripe->payment_hash->data->server_response->payment_method,
+            'payment_type' => PaymentType::parseCardType(strtolower($stripe_method->card->brand)) ?: PaymentType::CREDIT_CARD_OTHER,
+            'amount' => $this->stripe->convertFromStripeAmount($this->stripe->payment_hash->data->server_response->amount, $this->stripe->client->currency()->precision, $this->stripe->client->currency()),
+            'transaction_reference' => isset($this->stripe->payment_hash->data->payment_intent->latest_charge) ? $this->stripe->payment_hash->data->payment_intent->latest_charge : optional($this->stripe->payment_hash->data->payment_intent->charges->data[0])->id,
+            'gateway_type_id' => GatewayType::CREDIT_CARD,
+        ];
+
+        $this->stripe->payment_hash->data = array_merge((array) $this->stripe->payment_hash->data, ['amount' => $data['amount']]);
+        $this->stripe->payment_hash->save();
+
+        if ($this->stripe->payment_hash->data->store_card) {
+            $customer = new \stdClass;
+            $customer->id = $this->stripe->payment_hash->data->customer;
+
+            $this->stripe->attach($this->stripe->payment_hash->data->server_response->payment_method, $customer);
+
+            $stripe_method = $this->stripe->getStripePaymentMethod($this->stripe->payment_hash->data->server_response->payment_method);
+
+            $this->storePaymentMethod($stripe_method, $this->stripe->payment_hash->data->payment_method_id, $customer);
+        }
+
+        $payment = $this->stripe->createPayment($data, Payment::STATUS_COMPLETED);
+
+        SystemLogger::dispatch(
+            ['response' => $this->stripe->payment_hash->data->server_response, 'data' => $data],
+            SystemLog::CATEGORY_GATEWAY_RESPONSE,
+            SystemLog::EVENT_GATEWAY_SUCCESS,
+            SystemLog::TYPE_STRIPE,
+            $this->stripe->client,
+            $this->stripe->client->company,
+        );
+
+        //If the user has come from a subscription double check here if we need to redirect.
+        //08-08-2022
+        if($payment->invoices()->whereHas('subscription')->exists()){
+            $subscription = $payment->invoices()->first()->subscription;
+
+            if($subscription && array_key_exists('return_url', $subscription->webhook_configuration) && strlen($subscription->webhook_configuration['return_url']) >=1)
+            return redirect($subscription->webhook_configuration['return_url']);
+
+        }
+        //08-08-2022
+
+        return redirect()->route('client.payments.show', ['payment' => $this->stripe->encodePrimaryKey($payment->id)]);
+    }
+
+    public function processUnsuccessfulPayment($server_response)
+    {
+        $this->stripe->sendFailureMail($server_response->cancellation_reason);
+
+        $message = [
+            'server_response' => $server_response,
+            'data' => $this->stripe->payment_hash->data,
+        ];
+
+        SystemLogger::dispatch(
+            $message,
+            SystemLog::CATEGORY_GATEWAY_RESPONSE,
+            SystemLog::EVENT_GATEWAY_FAILURE,
+            SystemLog::TYPE_STRIPE,
+            $this->stripe->client,
+            $this->stripe->client->company,
+        );
+
+        throw new PaymentFailed('Failed to process the payment.', 500);
+    }
+
+    private function storePaymentMethod(PaymentMethod $method, $payment_method_id, $customer)
+    {
+        try {
+            $payment_meta = new \stdClass;
+            $payment_meta->exp_month = (string) $method->card->exp_month;
+            $payment_meta->exp_year = (string) $method->card->exp_year;
+            $payment_meta->brand = (string) $method->card->brand;
+            $payment_meta->last4 = (string) $method->card->last4;
+            $payment_meta->type = GatewayType::BACS;
+
+            $data = [
+                'payment_meta' => $payment_meta,
+                'token' => $method->id,
+                'payment_method_id' => $payment_method_id,
+            ];
+
+            $this->stripe->storeGatewayToken($data, ['gateway_customer_reference' => $customer->id]);
+        } catch (\Exception $e) {
+            return $this->stripe->processInternallyFailedPayment($this->stripe, $e);
+        }
+    }
+}
---
+++ resources/views/portal/ninja2020/gateways/stripe/bacs/pay.blade.php
@@ -0,0 +1,97 @@
+@extends('portal.ninja2020.layout.payments', ['gateway_title' => 'Credit card', 'card_title' => 'Credit card'])
+
+@php
+    $gateway_instance = $gateway instanceof \App\Models\CompanyGateway ? $gateway : $gateway->company_gateway;
+    $token_billing_string = 'true';
+
+    if($gateway_instance->token_billing == 'off' || $gateway_instance->token_billing == 'optin'){
+        $token_billing_string = 'false';
+    }
+    
+    
+@endphp
+
+@section('gateway_head')
+    @if($gateway->company_gateway->getConfigField('account_id'))
+        <meta name="stripe-account-id" content="{{ $gateway->company_gateway->getConfigField('account_id') }}">
+        <meta name="stripe-publishable-key" content="{{ config('ninja.ninja_stripe_publishable_key') }}">
+    @else
+        <meta name="stripe-publishable-key" content="{{ $gateway->getPublishableKey() }}">
+    @endif
+
+    <meta name="stripe-secret" content="{{ $intent->client_secret }}">
+    <meta name="only-authorization" content="">
+    <meta name="client-postal-code" content="{{ $client->postal_code ?? '' }}">
+    <meta name="stripe-require-postal-code" content="{{ $gateway->company_gateway->require_postal_code }}">
+@endsection
+
+@section('gateway_content')
+    <form action="{{ route('client.payments.response') }}" method="post" id="server-response">
+        @csrf
+        <input type="hidden" name="gateway_response">
+        <input type="hidden" name="store_card" value="{{ $token_billing_string }}">
+        <input type="hidden" name="payment_hash" value="{{ $payment_hash }}">
+
+        <input type="hidden" name="company_gateway_id" value="{{ $gateway->getCompanyGatewayId() }}">
+        <input type="hidden" name="payment_method_id" value="{{ $payment_method_id }}">
+
+        <input type="hidden" name="token">
+    </form>
+
+    <div class="alert alert-failure mb-4" hidden id="errors"></div>
+
+    @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.payment_type')])
+        {{ ctrans('texts.credit_card') }}
+    @endcomponent
+
+    @include('portal.ninja2020.gateways.includes.payment_details')
+
+    @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.pay_with')])
+        <ul class="list-none hover:list-disc">
+        @if(count($tokens) > 0)
+            @foreach($tokens as $token)
+            <li class="py-2 hover:text-blue hover:bg-blue-600">
+                <label class="mr-4">
+                    <input
+                        type="radio"
+                        data-token="{{ $token->token }}"
+                        name="payment-type"
+                        class="form-check-input text-indigo-600 rounded-full cursor-pointer toggle-payment-with-token toggle-payment-with-token"/>
+                    <span class="ml-1 cursor-pointer">**** {{ $token->meta?->last4 }}</span>
+                </label>
+            </li>
+            @endforeach
+        @endisset
+
+            <li class="py-2 hover:text-blue hover:bg-blue-600">
+                <label>
+                    <input
+                        type="radio"
+                        id="toggle-payment-with-credit-card"
+                        class="form-check-input text-indigo-600 rounded-full cursor-pointer"
+                        name="payment-type"
+                        checked/>
+                    <span class="ml-1 cursor-pointer">{{ __('texts.new_card') }}</span>
+                </label>
+            </li>    
+        </ul>
+        
+    @endcomponent
+
+    @include('portal.ninja2020.gateways.stripe.includes.card_widget')
+    @include('portal.ninja2020.gateways.includes.pay_now')
+    
+@endsection
+
+@section('gateway_footer')
+    <script>
+        Livewire.on('passed-required-fields-check', (event) => {
+            if (event.hasOwnProperty('client_postal_code')) {
+                document.querySelector('meta[name=client-postal-code]').content = event.client_postal_code;
+            }
+        });
+    </script>
+
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="{{ asset('js/clients/payments/stripe-credit-card.js') }}"></script>
+@endsection
