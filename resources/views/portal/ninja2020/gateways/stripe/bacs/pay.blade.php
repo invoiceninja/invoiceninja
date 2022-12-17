04d13f78bd88ec35465aa3b2f5029ee1f07c174b
Lars Kusch <lars@lars-kusch.de>
2022-12-17 05:59:27 +0100

    Scaffold inital payment view

--- resources/views/portal/ninja2020/gateways/stripe/bacs/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/stripe/bacs/pay.blade.php
@@ -7,25 +7,46 @@
     @else
         <meta name="stripe-publishable-key" content="{{ $gateway->company_gateway->getPublishableKey() }}">
     @endif
-    <meta name="return-url" content="{{ $return_url }}">
     <meta name="amount" content="{{ $stripe_amount }}">
-    <meta name="country" content="{{ $country }}">
-    <meta name="customer" content="{{ $customer }}">
     <meta name="pi-client-secret" content="{{ $pi_client_secret }}">
+    <meta name="si-client-secret" content="{{ $si_client_secret ?? '' }}">
 @endsection
 
 @section('gateway_content')
+    <form action="{{ route('client.payments.response') }}" method="post" id="server-response">
+        @csrf
+        <input type="hidden" name="gateway_response">
+        <input type="hidden" name="company_gateway_id" value="{{ $gateway->getCompanyGatewayId() }}">
+        <input type="hidden" name="payment_method_id" value="{{ $payment_method_id }}">
+        <input type="hidden" name="payment_hash" value="{{ $payment_hash }}">
+        <input type="hidden" name="store_card">
+        <input type="hidden" name="token">
+    </form>
+
     <div class="alert alert-failure mb-4" hidden id="errors"></div>
 
     @include('portal.ninja2020.gateways.includes.payment_details')
 
     @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.payment_type')])
-        {{ ctrans('texts.bacs') }} ({{ ctrans('texts.bank_transfer') }})
+        {{ ctrans('texts.sepa') }} ({{ ctrans('texts.bank_transfer') }})
+    @endcomponent
+
+    @component('portal.ninja2020.components.general.card-element', ['title' => ctrans('texts.pay_with')])
+        @if (count($tokens) > 0)
+            @foreach ($tokens as $token)
+                <label class="mr-4">
+                    <input type="radio" data-token="{{ $token->token }}" name="payment-type"
+                           class="form-radio cursor-pointer toggle-payment-with-token" />
+                    <span class="ml-1 cursor-pointer">**** {{ $token->meta?->last4 }}</span>
+                </label>
+            @endforeach
+        @endisset
+
     @endcomponent
     @include('portal.ninja2020.gateways.includes.pay_now')
 @endsection
 
 @push('footer')
     <script src="https://js.stripe.com/v3/"></script>
-    <script src="{{ asset('js/clients/payments/stripe-bacs.js') }}"></script>
+    <script src="{{ asset('js/clients/payments/stripe-becs.js') }}"></script>
 @endpush
