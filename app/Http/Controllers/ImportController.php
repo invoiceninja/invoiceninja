077986d59c2778be8774089b62d51bfc89138d7c
David Bomba <turbo124@gmail.com>
2023-01-12 09:19:38 +1100

    Clean up legacy importer

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -13,7 +13,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Import\ImportRequest;
 use App\Http\Requests\Import\PreImportRequest;
-use App\Jobs\Import\CSVImport;
 use App\Jobs\Import\CSVIngest;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Cache;
--- tests/Feature/Import/CSV/CsvImportTest.php
+++ tests/Feature/Import/CSV/CsvImportTest.php
@@ -13,7 +13,6 @@ namespace Tests\Feature\Import\CSV;
 
 use App\Import\Providers\Csv;
 use App\Import\Transformer\BaseTransformer;
-use App\Jobs\Import\CSVImport;
 use App\Models\Client;
 use App\Models\ClientContact;
 use App\Models\Expense;
@@ -319,34 +318,3 @@ class CsvImportTest extends TestCase
         $this->assertEquals(51.03, round($invoice->payments()->sum('payments.amount'),2));
     }
 }
-
-// public function testClientCsvImport()
-// {
-//  $csv = file_get_contents(base_path().'/tests/Feature/Import/clients.csv');
-//  $hash = Str::random(32);
-//  $column_map = [
-//      1 => 'client.balance',
-//      2 => 'client.paid_to_date',
-//      0 => 'client.name',
-//      19 => 'client.currency_id',
-//      20 => 'client.public_notes',
-//      21 => 'client.private_notes',
-//      22 => 'contact.first_name',
-//      23 => 'contact.last_name',
-//  ];
-
-//  $data = [
-//      'hash'        => $hash,
-//      'column_map'  => [ 'client' => [ 'mapping' => $column_map ] ],
-//      'skip_header' => true,
-//      'import_type' => 'csv',
-//  ];
-
-//  $pre_import = Client::count();
-
-//  Cache::put( $hash . '-client', base64_encode( $csv ), 360 );
-
-//  CSVImport::dispatchNow( $data, $this->company );
-
-//  $this->assertGreaterThan( $pre_import, Client::count() );
-// }
