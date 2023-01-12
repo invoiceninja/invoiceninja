1974f0e5f3660c66ff53b80993f71b073ee1350a
David Bomba <turbo124@gmail.com>
2023-01-13 00:36:25 +1100

    Set invoice autobill based on configuration

--- app/Services/Invoice/InvoiceService.php
+++ app/Services/Invoice/InvoiceService.php
@@ -531,6 +531,10 @@ class InvoiceService
             $this->invoice->exchange_rate = $this->invoice->client->currency()->exchange_rate;
         }
 
+        if ($settings->auto_bill_standard_invoices) {
+            $this->invoice->auto_bill_enabled = true;
+        }
+
         if ($settings->counter_number_applied == 'when_saved') {
             $this->invoice->service()->applyNumber()->save();
         }
