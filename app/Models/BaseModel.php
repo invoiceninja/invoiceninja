ce3bf784cae9787f6da2fce7ef59629d465d71a6
David Bomba <turbo124@gmail.com>
2023-01-22 09:45:46 +1100

    Tests for refactoring entity file names and formatting entity numbers for file names

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -188,7 +188,7 @@ class BaseModel extends Model
 
     public function numberFormatter()
     {
-        $number = strlen($this->number) >= 1 ? class_basename($this) . "_" . $this->number : class_basename($this) . "_" . Str::random(5); 
+        $number = strlen($this->number) >= 1 ? $this->translate_entity() . "_" . $this->number : class_basename($this) . "_" . Str::random(5); 
 
         $formatted_number =  mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $number);
         
