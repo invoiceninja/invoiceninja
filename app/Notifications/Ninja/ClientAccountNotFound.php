6c86d7151ab44e861ed3f53b91dd505434251137
David Bomba <turbo124@gmail.com>
2023-01-29 09:21:40 +1100

    Update copyright year

---
+++ app/Notifications/Ninja/ClientAccountNotFound.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2023. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Notifications\Ninja;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ClientAccountNotFound extends Notification 
+{
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+
+
+    protected string $account_key;
+
+    public function __construct(string $account_key)
+    {
+        $this->account_key = $account_key;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['slack'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toSlack($notifiable)
+    {
+
+        $content = "Client not found, unable to remove account\n";
+        $content .= "Account: {$this->account_key }\n";
+
+        return (new SlackMessage)
+                ->success()
+                ->from(ctrans('texts.notification_bot'))
+                ->image('https://app.invoiceninja.com/favicon.png')
+                ->content($content);
+    }
+}
