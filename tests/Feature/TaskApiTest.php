74873260999ec85ce0dc8256eac3ceaac5cde8c1
David Bomba <turbo124@gmail.com>
2023-02-08 11:27:38 +1100

    Improve resolution of decimals

--- tests/Feature/TaskApiTest.php
+++ tests/Feature/TaskApiTest.php
@@ -43,52 +43,57 @@ class TaskApiTest extends TestCase
         Model::reguard();
     }
 
-    private function checkTimeLog($log)
+    private function checkTimeLog(array $log): bool
     {
+        if(count($log) == 0)
+            return true;
 
+        /*Get first value of all arrays*/
         $result = array_column($log, 0);
 
+        /*Sort the array in ascending order*/
         asort($result);
 
         $new_array = [];
 
+        /*Rebuild the array in order*/
         foreach($result as $key => $value)
             $new_array[] = $log[$key];
 
+        /*Iterate through the array and perform checks*/
         foreach($new_array as $key => $array)
         {
+            /*Flag which helps us know if there is a NEXT timelog*/
             $next = false;
-
+            /* If there are more than 1 time log in the array, ensure the last timestamp is not zero*/
             if(count($new_array) >1 && $array[1] == 0)
                 return false;
 
-            /* First test is to check if the start time is greater than the end time */ 
+            /* Check if the start time is greater than the end time */ 
             /* Ignore the last value for now, we'll do a separate check for this */ 
-            if($array[0] > $array[1] && $array[1] != 0){
+            if($array[0] > $array[1] && $array[1] != 0)
                 return false;
-            }
-
-            if(array_key_exists($key+1, $new_array)){
+            
+            /* Find the next time log value - if it exists */
+            if(array_key_exists($key+1, $new_array))
                 $next = $new_array[$key+1];
-            }
 
             /* check the next time log and ensure the start time is GREATER than the end time of the previous record */
-            if($next && $next[0] < $array[1]){
+            if($next && $next[0] < $array[1])
                 return false;
-            }
 
+            /* Get the last row of the timelog*/
             $last_row = end($new_array);
-
-            if($last_row[1] != 0 && $last_row[0] > $last_row[1]){
-                nlog($last_row[0]. " ".$last_row[1]);
+            
+            /*If the last value is NOT zero, ensure start time is not GREATER than the endtime */
+            if($last_row[1] != 0 && $last_row[0] > $last_row[1])
                 return false;
-            }
 
             return true;
         }
 
     }
-
+    
     public function testTimeLogChecker1()
     {
 
