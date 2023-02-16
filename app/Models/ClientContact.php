f946562c589db381a7803b1331a9bde5d827736d
David Bomba <turbo124@gmail.com>
2023-02-17 07:18:59 +1100

    Add buildCache to clientcontact
    (cherry picked from commit e5017299c608e85158f169043c62a4048647bfd4)

--- app/Models/ClientContact.php
+++ app/Models/ClientContact.php
@@ -18,6 +18,7 @@ use App\Mail\ClientContact\ClientContactResetPasswordObject;
 use App\Models\Presenters\ClientContactPresenter;
 use App\Notifications\ClientContactResetPassword;
 use App\Utils\Ninja;
+use App\Utils\Traits\AppSetup;
 use App\Utils\Traits\MakesHash;
 use Illuminate\Contracts\Translation\HasLocalePreference;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -40,7 +41,8 @@ class ClientContact extends Authenticatable implements HasLocalePreference
     use PresentableTrait;
     use SoftDeletes;
     use HasFactory;
-
+    use AppSetup;
+    
     /* Used to authenticate a contact */
     protected $guard = 'contact';
 
