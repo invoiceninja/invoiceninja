60868003c7738becd972a917e7db8523a06b2f71
David Bomba <turbo124@gmail.com>
2023-02-20 16:47:30 +1100

    Fixes for validation

--- app/Http/Requests/TaskScheduler/StoreSchedulerRequest.php
+++ app/Http/Requests/TaskScheduler/StoreSchedulerRequest.php
@@ -41,8 +41,8 @@ class StoreSchedulerRequest extends Request
             'parameters' => 'bail|array',
             'parameters.clients' => ['bail','sometimes', 'array', new ValidClientIds()],
             'parameters.date_range' => 'bail|sometimes|string|in:last7_days,last30_days,last365_days,this_month,last_month,this_quarter,last_quarter,this_year,last_year,custom',
-            'parameters.start_date' => ['bail', 'sometimes', 'date', 'required_if:parameters.date_rate,custom'],
-            'parameters.end_date' => ['bail', 'sometimes', 'date', 'required_if:parameters.date_rate,custom', 'after_or_equal:parameters.start_date'],
+            'parameters.start_date' => ['bail', 'sometimes', 'date:Y-m-d', 'required_if:parameters.date_rate,custom'],
+            'parameters.end_date' => ['bail', 'sometimes', 'date:Y-m-d', 'required_if:parameters.date_rate,custom', 'after_or_equal:parameters.start_date'],
         ];
 
         return $rules;
