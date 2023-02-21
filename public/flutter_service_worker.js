8e5de4f08628e1785b0c8bf52dbe1100ee83faba
hillelcoren <hillelcoren@gmail.com>
2023-02-21 07:56:10 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -298,8 +298,8 @@ const RESOURCES = {
 "assets/NOTICES": "1a34e70168d56fad075adfb4bdbb20eb",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/AssetManifest.json": "759f9ef9973f7e26c2a51450b55bb9fa",
-"/": "ff2ff88a6b5e69d1fcfbb26d8980cbe9",
-"main.dart.js": "91f0beaa7e463662490b3100ebded88f",
+"/": "8c63427367024461c4d4974648b1c3d8",
+"main.dart.js": "1da2fa15301eae224693625c2ae78995",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed"
 };
