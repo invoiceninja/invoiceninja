1b58f525b9c22d57b756b14186d8b8600c4b81e1
David Bomba <turbo124@gmail.com>
2023-02-22 09:29:06 +1100

    Update designs

--- app/Utils/HtmlEngine.php
+++ app/Utils/HtmlEngine.php
@@ -117,6 +117,8 @@ class HtmlEngine
         $data['$global_margin'] = ['value' => '6.35mm', 'label' => ''];
         $data['$company_logo_size'] = ['value' => $this->resolveCompanyLogoSize(), 'label' => ''];
         $data['$show_shipping_address'] = ['value' => $this->settings?->show_shipping_address ? 'flex' : 'none', 'label' => ''];
+        $data['$show_shipping_address_visibility'] = ['value' => $this->settings?->show_shipping_address ? 'visible' : 'hidden', 'label' => ''];
+        
         $data['$tax'] = ['value' => '', 'label' => ctrans('texts.tax')];
         $data['$app_url'] = ['value' => $this->generateAppUrl(), 'label' => ''];
         $data['$from'] = ['value' => '', 'label' => ctrans('texts.from')];
