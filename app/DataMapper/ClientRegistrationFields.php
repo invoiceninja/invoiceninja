33df867438eeba357280e0587052331a92f72e98
David Bomba <turbo124@gmail.com>
2023-03-01 07:14:07 +1100

    Allow client registration fields to be both visible and required

--- app/DataMapper/ClientRegistrationFields.php
+++ app/DataMapper/ClientRegistrationFields.php
@@ -20,82 +20,102 @@ class ClientRegistrationFields
             [
                 'key' => 'first_name',
                 'required' => true,
+                'visible' => true,
             ],
             [
                 'key' => 'last_name',
                 'required' => true,
+                'visible' => true,
             ],
             [
                 'key' => 'email',
                 'required' => true,
+                'visible' => true,
             ],
             [
                 'key' => 'phone',
                 'required' => false,
+                'visible' => true,
             ],
             [
                 'key' => 'password',
                 'required' => true,
+                'visible' => true,
             ],
             [
                 'key' => 'name',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'website',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'address1',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'address2',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'city',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'state',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'postal_code',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'country_id',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'custom_value1',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'custom_value2',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'custom_value3',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'custom_value4',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'public_notes',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'vat_number',
                 'required' => false,
+                'visible' => false,
             ],
             [
                 'key' => 'currency_id',
                 'required' => false,
+                'visible' => false,
             ],
         ];
 
