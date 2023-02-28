e89ed1185ab469c80ab8dfa2b8015515ed134b5c
David Bomba <turbo124@gmail.com>
2023-03-01 10:55:57 +1100

    Remove trailing . from subscription title

--- resources/views/portal/ninja2020/subscriptions/show.blade.php
+++ resources/views/portal/ninja2020/subscriptions/show.blade.php
@@ -6,7 +6,7 @@
         <div class="bg-white shadow overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
-                   {{ $subscription->name }}.
+                   {{ $subscription->name }}
                 </h3>
             </div>
             <div>
