77c56ef2a8c1c3dc7598322f074129ececb7e9b6
David Bomba <turbo124@gmail.com>
2023-03-16 20:34:20 +1100

    Schedule Invoice

---
+++ app/DataMapper/Schedule/ScheduleInvoice.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2023. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\DataMapper\Schedule;
+
+class ScheduleInvoice
+{
+    /**
+     * Defines the template name
+     *
+     * @var string
+     */
+    public string $template = 'schedule_invoice';
+
+    /**
+     * Defines the template name
+     *
+     * @var string
+     */
+    public string $entity = '';
+
+    /**
+     * Defines the template name
+     *
+     * @var string
+     */
+    public string $entity_id = '';
+    
+}
