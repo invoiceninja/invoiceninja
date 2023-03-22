d7ff984818474088e3f1f1c87b8fc7df1bb423cf
David Bomba <turbo124@gmail.com>
2023-03-22 12:42:38 +1100

    Subscription table view client portal

--- app/Http/Controllers/ClientPortal/SubscriptionController.php
+++ app/Http/Controllers/ClientPortal/SubscriptionController.php
@@ -21,20 +21,20 @@ class SubscriptionController extends Controller
 {
     public function index()
     {
-        if (Ninja::isHosted()) {
-            $count = RecurringInvoice::query()
-                ->where('client_id', auth()->guard('contact')->user()->client->id)
-                ->where('company_id', auth()->guard('contact')->user()->client->company_id)
-                ->where('status_id', RecurringInvoice::STATUS_ACTIVE)
-                ->where('is_deleted', 0)
-                ->whereNotNull('subscription_id')
-                ->withTrashed()
-                ->count();
+        // if (Ninja::isHosted()) {
+        //     $count = RecurringInvoice::query()
+        //         ->where('client_id', auth()->guard('contact')->user()->client->id)
+        //         ->where('company_id', auth()->guard('contact')->user()->client->company_id)
+        //         ->where('status_id', RecurringInvoice::STATUS_ACTIVE)
+        //         ->where('is_deleted', 0)
+        //         ->whereNotNull('subscription_id')
+        //         ->withTrashed()
+        //         ->count();
 
-            if ($count == 0) {
-                return redirect()->route('client.ninja_contact_login', ['contact_key' => auth()->guard('contact')->user()->contact_key, 'company_key' => auth()->guard('contact')->user()->company->company_key]);
-            }
-        }
+        //     if ($count == 0) {
+        //         return redirect()->route('client.ninja_contact_login', ['contact_key' => auth()->guard('contact')->user()->contact_key, 'company_key' => auth()->guard('contact')->user()->company->company_key]);
+        //     }
+        // }
 
         return render('subscriptions.index');
     }
