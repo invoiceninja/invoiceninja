5910011967fe13e1a4391890c1d04705a1809d59
David Bomba <turbo124@gmail.com>
2023-03-31 09:51:16 +1100

    Fixes for activities

--- app/Listeners/Invoice/InvoiceReminderEmailActivity.php
+++ app/Listeners/Invoice/InvoiceReminderEmailActivity.php
@@ -49,7 +49,7 @@ class InvoiceReminderEmailActivity implements ShouldQueue
         $fields->user_id = $user_id;
         $fields->invoice_id = $event->invitation->invoice->id;
         $fields->company_id = $event->invitation->invoice->company_id;
-        $fields->client_contact_id = $event->invitation->invoice->client_contact_id;
+        $fields->client_contact_id = $event->invitation->client_contact_id;
         $fields->activity_type_id = $event->reminder;
 
         $this->activity_repo->save($fields, $event->invitation->invoice, $event->event_vars);
