e881197fd563be73e0e219be168ba4e00d7021e9
David Bomba <turbo124@gmail.com>
2023-04-27 16:42:50 +1000

    Fixes for test namespaces

--- tests/Feature/Export/ArDetailReportTest.php
+++ tests/Feature/Export/ArDetailReportTest.php
@@ -19,7 +19,6 @@ use App\Models\Company;
 use App\Models\Invoice;
 use App\Models\User;
 use App\Services\Report\ARDetailReport;
-use App\Services\Report\UserSalesReport;
 use App\Utils\Traits\MakesHash;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Tests\MockAccountData;
@@ -28,7 +27,7 @@ use Tests\TestCase;
 /**
  * @test
  */
-class ARDetailReportTest extends TestCase
+class ArDetailReportTest extends TestCase
 {
     use MakesHash;
 
