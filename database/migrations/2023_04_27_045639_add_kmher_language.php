9dee71bc92305f15a8f8e96e44367e69d2cce75c
David Bomba <turbo124@gmail.com>
2023-04-27 15:09:55 +1000

    Static analysis

--- database/migrations/2023_04_27_045639_add_kmher_language.php
+++ database/migrations/2023_04_27_045639_add_kmher_language.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Models\Language;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
