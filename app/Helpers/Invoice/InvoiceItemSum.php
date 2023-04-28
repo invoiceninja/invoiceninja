a63b9280702f470c570f24ee3d42bb039224eef0
David Bomba <turbo124@gmail.com>
2023-04-29 09:51:31 +1000

    Restrict tax regions to DE / US /AU

--- app/Helpers/Invoice/InvoiceItemSum.php
+++ app/Helpers/Invoice/InvoiceItemSum.php
@@ -30,33 +30,33 @@ class InvoiceItemSum
     use Taxer;
 
     private array $tax_jurisdictions = [
-        'AT', // Austria
-        'BE', // Belgium
-        'BG', // Bulgaria
-        'CY', // Cyprus
-        'CZ', // Czech Republic
+        // 'AT', // Austria
+        // 'BE', // Belgium
+        // 'BG', // Bulgaria
+        // 'CY', // Cyprus
+        // 'CZ', // Czech Republic
         'DE', // Germany
-        'DK', // Denmark
-        'EE', // Estonia
-        'ES', // Spain
-        'FI', // Finland
-        'FR', // France
-        'GR', // Greece
-        'HR', // Croatia
-        'HU', // Hungary
-        'IE', // Ireland
-        'IT', // Italy
-        'LT', // Lithuania
-        'LU', // Luxembourg
-        'LV', // Latvia
-        'MT', // Malta
-        'NL', // Netherlands
-        'PL', // Poland
-        'PT', // Portugal
-        'RO', // Romania
-        'SE', // Sweden
-        'SI', // Slovenia
-        'SK', // Slovakia
+        // 'DK', // Denmark
+        // 'EE', // Estonia
+        // 'ES', // Spain
+        // 'FI', // Finland
+        // 'FR', // France
+        // 'GR', // Greece
+        // 'HR', // Croatia
+        // 'HU', // Hungary
+        // 'IE', // Ireland
+        // 'IT', // Italy
+        // 'LT', // Lithuania
+        // 'LU', // Luxembourg
+        // 'LV', // Latvia
+        // 'MT', // Malta
+        // 'NL', // Netherlands
+        // 'PL', // Poland
+        // 'PT', // Portugal
+        // 'RO', // Romania
+        // 'SE', // Sweden
+        // 'SI', // Slovenia
+        // 'SK', // Slovakia
 
         'US', // USA
 
