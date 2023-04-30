db86cb6b4035af9c5c34b8563eedda0c36c3180e
David Bomba <turbo124@gmail.com>
2023-04-30 18:09:08 +1000

    Static Analysis cleanup

--- app/Libraries/MultiDB.php
+++ app/Libraries/MultiDB.php
@@ -56,7 +56,7 @@ class MultiDB
     public static function checkDomainAvailable($subdomain) : bool
     {
         if (! config('ninja.db.multi_db_enabled')) {
-            return Company::whereSubdomain($subdomain)->get()->count() == 0;
+            return Company::whereSubdomain($subdomain)->count() == 0;
         }
 
         $current_db = config('database.default');
@@ -105,7 +105,7 @@ class MultiDB
      * a new user request.
      *
      * @param  string $email       The user email
-     * @param  stirng $company_key The company key
+     * @param  string $company_key The company key
      * @return bool             True|False
      */
     public static function checkUserAndCompanyCoExist($email, $company_key) :bool
@@ -156,8 +156,8 @@ class MultiDB
     }
 
     /**
-     * @param array $data
-     * @return User|null
+     * @param string $email
+     * @return ClientContact|null
      */
     public static function hasContact(string $email) : ?ClientContact
     {
@@ -183,8 +183,8 @@ class MultiDB
     }
 
     /**
-     * @param array $data
-     * @return User|null
+     * @param array $search
+     * @return ClientContact|null
      */
     public static function findContact(array $search) : ?ClientContact
     {
@@ -456,8 +456,8 @@ class MultiDB
     }
 
     /**
-     * @param array $data
-     * @return User|null
+     * @param string $phone
+     * @return bool
      */
     public static function hasPhoneNumber(string $phone) : bool
     {
@@ -482,7 +482,7 @@ class MultiDB
 
     
 
-    public static function randomSubdomainGenerator()
+    public static function randomSubdomainGenerator(): string
     {
         $current_db = config('database.default');
 
@@ -509,6 +509,7 @@ class MultiDB
 
     /**
      * @param $database
+     * @return void
      */
     public static function setDB(string $database) : void
     {
