ba68682cedc010fdcb8b59c2df80084ae31b8c72
David Bomba <turbo124@gmail.com>
2023-05-03 09:59:53 +1000

    Fixes for portal - hide deleted invoice from payment view

--- resources/views/portal/ninja2020/payments/show.blade.php
+++ resources/views/portal/ninja2020/payments/show.blade.php
@@ -123,7 +123,7 @@
             <div>
                 <dl>
                     @foreach($payment->invoices as $invoice)
-                        @if(!$invoice->is_proforma)
+                        @if(!$invoice->is_proforma && !$invoice->is_deleted)
                         <div class="px-4 py-5 bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium leading-5 text-gray-500">
                                 {{ ctrans('texts.invoice_number') }}
