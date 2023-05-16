aec715fbfd3ee42705c2bc7ebfdb210e15b5971f
David Bomba <turbo124@gmail.com>
2023-05-16 18:19:04 +1000

    Fixes for encrypted casts

--- app/Casts/EncryptedCast.php
+++ app/Casts/EncryptedCast.php
@@ -16,8 +16,8 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 class EncryptedCast implements CastsAttributes
 {
     public function get($model, string $key, $value, array $attributes)
-    {nlog($value);
-        return !$value ? null : decrypt($value);
+    {   
+        return ! is_null($value) ? decrypt($value) : null;
     }
 
     public function set($model, string $key, $value, array $attributes)
