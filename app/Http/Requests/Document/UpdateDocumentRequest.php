b8070a130e034542994835f35db7a63b08163235
David Bomba <turbo124@gmail.com>
2023-05-24 18:22:36 +1000

    Minor fixes

--- app/Http/Requests/Document/UpdateDocumentRequest.php
+++ app/Http/Requests/Document/UpdateDocumentRequest.php
@@ -23,9 +23,12 @@ class UpdateDocumentRequest extends Request
      *
      * @return bool
      */
-    public function authorize() : bool
+    public function authorize(): bool
     {
-        return auth()->user()->can('edit', $this->document);
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
+
+        return $user->can('edit', $this->document);
     }
 
     public function rules()
