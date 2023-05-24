85c8ea2f4634811534b43e974c125766ef32eedf
hillelcoren <hillelcoren@gmail.com>
2023-05-24 09:37:22 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -293,13 +293,13 @@ const RESOURCES = {
 "assets/packages/window_manager/images/ic_chrome_close.png": "75f4b8ab3608a05461a31fc18d6b47c2",
 "assets/FontManifest.json": "087fb858dc3cbfbf6baf6a30004922f1",
 "assets/AssetManifest.json": "759f9ef9973f7e26c2a51450b55bb9fa",
-"/": "13ec25891f6f72a7a87e5b2e8643296c",
+"/": "520d726bd9ac92c9b45e52ec5efee3d1",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b",
-"version.json": "2879a69a8bdf6ecf0f039cdcca3fb05e",
+"version.json": "7e4267a86708d41867f499ca02deb833",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
 "favicon.ico": "51636d3a390451561744c42188ccd628",
-"main.dart.js": "e020409a13a8449b2069461311d3f438",
+"main.dart.js": "92793e3ecacaeef6c7480613ee9a7f26",
 "manifest.json": "ef43d90e57aa7682d7e2cfba2f484a40"
 };
 
