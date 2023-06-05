863b3bda54879055eae1b8885209cdf2b731e474
David Bomba <turbo124@gmail.com>
2023-06-05 20:18:51 +1000

    Enable all euro taxeS

--- app/Helpers/Invoice/InvoiceItemSum.php
+++ app/Helpers/Invoice/InvoiceItemSum.php
@@ -61,32 +61,32 @@ class InvoiceItemSum
 
     private array $tax_jurisdictions = [
         'AT', // Austria
-        // 'BE', // Belgium
-        // 'BG', // Bulgaria
-        // 'CY', // Cyprus
-        // 'CZ', // Czech Republic
+        'BE', // Belgium
+        'BG', // Bulgaria
+        'CY', // Cyprus
+        'CZ', // Czech Republic
         'DE', // Germany
-        // 'DK', // Denmark
-        // 'EE', // Estonia
-        // 'ES', // Spain
-        // 'FI', // Finland
-        // 'FR', // France
-        // 'GR', // Greece
-        // 'HR', // Croatia
-        // 'HU', // Hungary
-        // 'IE', // Ireland
-        // 'IT', // Italy
-        // 'LT', // Lithuania
-        // 'LU', // Luxembourg
-        // 'LV', // Latvia
-        // 'MT', // Malta
-        // 'NL', // Netherlands
-        // 'PL', // Poland
-        // 'PT', // Portugal
-        // 'RO', // Romania
-        // 'SE', // Sweden
-        // 'SI', // Slovenia
-        // 'SK', // Slovakia
+        'DK', // Denmark
+        'EE', // Estonia
+        'ES', // Spain
+        'FI', // Finland
+        'FR', // France
+        'GR', // Greece
+        'HR', // Croatia
+        'HU', // Hungary
+        'IE', // Ireland
+        'IT', // Italy
+        'LT', // Lithuania
+        'LU', // Luxembourg
+        'LV', // Latvia
+        'MT', // Malta
+        'NL', // Netherlands
+        'PL', // Poland
+        'PT', // Portugal
+        'RO', // Romania
+        'SE', // Sweden
+        'SI', // Slovenia
+        'SK', // Slovakia
 
         'US', // USA
 
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -242,32 +242,32 @@ class Company extends BaseModel
         'US',
         // //EU countries
         'AT', // Austria
-        // 'BE', // Belgium
-        // 'BG', // Bulgaria
-        // 'CY', // Cyprus
-        // 'CZ', // Czech Republic
+        'BE', // Belgium
+        'BG', // Bulgaria
+        'CY', // Cyprus
+        'CZ', // Czech Republic
         'DE', // Germany
-        // 'DK', // Denmark
-        // 'EE', // Estonia
-        // 'ES', // Spain
-        // 'FI', // Finland
-        // 'FR', // France
-        // 'GR', // Greece
-        // 'HR', // Croatia
-        // 'HU', // Hungary
-        // 'IE', // Ireland
-        // 'IT', // Italy
-        // 'LT', // Lithuania
-        // 'LU', // Luxembourg
-        // 'LV', // Latvia
-        // 'MT', // Malta
-        // 'NL', // Netherlands
-        // 'PL', // Poland
-        // 'PT', // Portugal
-        // 'RO', // Romania
-        // 'SE', // Sweden
-        // 'SI', // Slovenia
-        // 'SK', // Slovakia
+        'DK', // Denmark
+        'EE', // Estonia
+        'ES', // Spain
+        'FI', // Finland
+        'FR', // France
+        'GR', // Greece
+        'HR', // Croatia
+        'HU', // Hungary
+        'IE', // Ireland
+        'IT', // Italy
+        'LT', // Lithuania
+        'LU', // Luxembourg
+        'LV', // Latvia
+        'MT', // Malta
+        'NL', // Netherlands
+        'PL', // Poland
+        'PT', // Portugal
+        'RO', // Romania
+        'SE', // Sweden
+        'SI', // Slovenia
+        'SK', // Slovakia
         // //EU Countries
         'AU', // Australia
     ];
