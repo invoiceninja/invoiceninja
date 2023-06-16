8df9b6f8d0eced8670242a619e4fe14eea0d2868
hillelcoren <hillelcoren@gmail.com>
2023-06-16 13:58:04 +0000

    Admin Portal - Hosted

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-"/": "cc048215ef478a796be5b787f23598d0",
+"/": "c3de135eec312a4bf5cf23d228ee6d63",
 "assets/assets/google_fonts/Roboto-Regular.ttf": "8a36205bd9b83e03af0591a004bc97f4",
 "assets/assets/images/google_logo.png": "0f118259ce403274f407f5e982e681c3",
 "assets/assets/images/icon.png": "090f69e23311a4b6d851b3880ae52541",
@@ -296,7 +296,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "favicon.png": "dca91c54388f52eded692718d5a98b8b",
-"main.dart.js": "7409a3887e97fc3dd75e310e37b0df6e",
+"main.dart.js": "3bfe3516a90cec83b69bad47bf987c28",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/Icon-192.png": "bb1cf5f6982006952211c7c8404ffbed",
 "icons/Icon-512.png": "0f9aff01367f0a0c69773d25ca16ef35",
