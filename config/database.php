2440090d3044837bd775e4644e04c242a32512fc
David Bomba <turbo124@gmail.com>
2023-07-07 16:21:48 +1000

    Updates for composer

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
