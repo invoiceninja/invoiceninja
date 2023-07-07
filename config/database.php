33488e7b2399ebc7f0338f9bc3616a9fdd295f16
David Bomba <turbo124@gmail.com>
2023-07-07 16:44:43 +1000

    v5.6.14

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
