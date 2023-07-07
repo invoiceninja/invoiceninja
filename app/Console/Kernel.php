d0b09b5bd10440f5465725e9f99cb32b91b9985e
David Bomba <turbo124@gmail.com>
2023-07-07 15:59:36 +1000

    Updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -130,8 +130,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        // $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__.'/Commands');
 
-        // require base_path('routes/console.php');
+        require base_path('routes/console.php');
     }
 }
