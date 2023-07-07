f9f18907c2f0e440a46ff8ccb729cb1a42245e7a
David Bomba <turbo124@gmail.com>
2023-07-08 09:31:50 +1000

    Render pdf's in client portal dynamically

---
+++ app/Http/Livewire/PdfSlot.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2023. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Http\Livewire;
+
+use App\Libraries\MultiDB;
+use Livewire\Component;
+
+class PdfSlot extends Component
+{
+    public $invitation;
+
+    public $db;
+
+    public $entity;
+
+    public $pdf;
+
+    public function mount()
+    {
+        MultiDB::setDb($this->db);
+    }
+
+    public function render()
+    {
+        return render('components.livewire.pdf-slot', [
+            'invitation' => $this->invitation,
+            'entity' => $this->entity,
+        ]);
+    }
+
+    public function getPdf()
+    {
+        
+        $this->pdf =  $this->entity->fullscreenPdfViewer($this->invitation);
+
+    }
+}
