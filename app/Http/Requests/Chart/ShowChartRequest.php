73274f545ac55515a727a64e7c1fca013b49869d
David Bomba <turbo124@gmail.com>
2023-08-07 07:34:37 +1000

    Static analysis

--- app/Http/Requests/Chart/ShowChartRequest.php
+++ app/Http/Requests/Chart/ShowChartRequest.php
@@ -42,10 +42,14 @@ class ShowChartRequest extends Request
 
     public function prepareForValidation()
     {
+        
+        /**@var \App\Models\User auth()->user */
+        $user = auth()->user();
+
         $input = $this->all();
 
         if(isset($input['date_range'])) {
-            $dates = $this->calculateStartAndEndDates($input, auth()->user()->company());
+            $dates = $this->calculateStartAndEndDates($input, $user->company());
             $input['start_date'] = $dates[0];
             $input['end_date'] = $dates[1];
         }
