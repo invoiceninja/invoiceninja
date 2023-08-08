9765b5eb4d3df3c3b355a9edb985a2152eca52e1
David Bomba <turbo124@gmail.com>
2023-08-09 08:59:32 +1000

    Seeders for new currencieS

--- database/migrations/2023_08_08_212710_add_signature_ip_address_to_purchase_order_invitations.php
+++ database/migrations/2023_08_08_212710_add_signature_ip_address_to_purchase_order_invitations.php
@@ -16,6 +16,37 @@ return new class extends Migration
         Schema::table('purchase_order_invitations', function (Blueprint $table) {
             $table->text('signature_ip')->nullable();
         });
+
+        $xag = \App\Models\Currency::find(116);
+
+        if(!$xag) {
+
+            $xag = new \App\Models\Currency();
+            $xag->id = 115;
+            $xag->code = 'XAG';
+            $xag->name = 'Silver Troy Ounce';
+            $xag->symbol = 'XAG';
+            $xag->thousand_separator = ',';
+            $xag->decimal_separator = '.';
+            $xag->precision = 2;
+            $xag->save();
+        }
+
+        $xau = \App\Models\Currency::find(117);
+
+        if(!$xau) {
+
+            $xau = new \App\Models\Currency();
+            $xau->id = 117;
+            $xau->code = 'XAU';
+            $xau->name = 'Gold Troy Ounce';
+            $xau->symbol = 'XAU';
+            $xau->thousand_separator = ',';
+            $xau->decimal_separator = '.';
+            $xau->precision = 3;
+            $xau->save();
+        }
+
     }
 
     /**
