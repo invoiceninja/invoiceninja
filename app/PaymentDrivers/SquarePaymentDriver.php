a3433a7efc03677724cf7c6f0a7715e29b642659
David Bomba <turbo124@gmail.com>
2023-08-20 21:52:51 +1000

    Add square webhook tests

--- app/PaymentDrivers/SquarePaymentDriver.php
+++ app/PaymentDrivers/SquarePaymentDriver.php
@@ -363,13 +363,11 @@ class SquarePaymentDriver extends BaseDriver
     public function processWebhookRequest(PaymentWebhookRequest $request)
     {
         nlog("square webhook");
-        nlog($request->all());
 
         $signature_key = $this->company_gateway->getConfigField('signatureKey');
         $notification_url = $this->company_gateway->webhookUrl();
-        
-        // header('Content-Type: text/plain');
-        // $webhook_payload = file_get_contents('php://input');
+        nlog($signature_key);
+        nlog($notification_url);
 
         $body = '';   
         $handle = fopen('php://input', 'r');
