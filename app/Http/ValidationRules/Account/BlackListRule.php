8b400020ce8c0ed551ee83b6d56cab14e784173d
David Bomba <turbo124@gmail.com>
2023-08-27 08:41:27 +1000

    Updates for blacklist rules

--- app/Http/ValidationRules/Account/BlackListRule.php
+++ app/Http/ValidationRules/Account/BlackListRule.php
@@ -19,6 +19,7 @@ use Illuminate\Contracts\Validation\Rule;
 class BlackListRule implements Rule
 {
     private array $blacklist = [
+        'pretreer.com',
         'candassociates.com',
         'vusra.com',
         'fourthgenet.com',
