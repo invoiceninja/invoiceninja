56bfdcc32fba33cbc3a9f1d58c4cd7cd8694ec82
David Bomba <turbo124@gmail.com>
2023-09-21 13:53:56 +1000

    Prevent duplicate tax names

--- app/Http/Requests/TaxRate/UpdateTaxRateRequest.php
+++ app/Http/Requests/TaxRate/UpdateTaxRateRequest.php
@@ -23,17 +23,25 @@ class UpdateTaxRateRequest extends Request
      */
     public function authorize() : bool
     {
-        return auth()->user()->isAdmin();
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
+
+        return $user->isAdmin();
+
     }
 
     public function rules()
     {
+
+        /** @var \App\Models\User $user */
+        $user = auth()->user();
+
         $rules = [];
 
-        $rules['rate'] = 'numeric';
+        $rules['rate'] = 'required|numeric';
 
-        if ($this->number) {
-            $rules['number'] = Rule::unique('tax_rates')->where('company_id', auth()->user()->company()->id)->ignore($this->tax_rate->id);
+        if ($this->name) {
+            $rules['name'] = Rule::unique('tax_rates')->where('company_id', auth()->user()->company()->id)->ignore($this->tax_rate->id);
         }
 
         return $rules;
