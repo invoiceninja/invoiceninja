6313761003e46786f0bdcba911a631c1f624fa19
David Bomba <turbo124@gmail.com>
2023-10-25 20:21:02 +1100

    Add vendor display name to vendor transformer

--- app/Transformers/VendorTransformer.php
+++ app/Transformers/VendorTransformer.php
@@ -105,6 +105,7 @@ class VendorTransformer extends EntityTransformer
             'number' => (string) $vendor->number ?: '',
             'language_id' => (string) $vendor->language_id ?: '',
             'classification' => (string) $vendor->classification ?: '',
+            'display_name' =>  (string) $vendor->present()->name(),
         ];
     }
 }
