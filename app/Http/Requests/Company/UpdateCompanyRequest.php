5401ab5354dd2c93cba40afac8bb46743b54edec
David Bomba <turbo124@gmail.com>
2023-12-18 07:06:20 +1100

    filter custom email styles

--- app/Http/Requests/Company/UpdateCompanyRequest.php
+++ app/Http/Requests/Company/UpdateCompanyRequest.php
@@ -115,7 +115,7 @@ class UpdateCompanyRequest extends Request
         }
 
         if (isset($settings['email_style_custom'])) {
-            $settings['email_style_custom'] = str_replace(['{{','}}'], ['',''], $settings['email_style_custom']);
+            $settings['email_style_custom'] = str_replace(['{!!','!!}','{{','}}','@if(','@endif','@isset','@unless','@auth','@empty','@guest','@env','@section','@switch', '@foreach', '@while', '@include', '@each', '@once', '@push', '@use', '@forelse', '@verbatim', '<?php', '@php', '@for'], '', $settings['email_style_custom']);
         }
 
         if (! $account->isFreeHostedClient()) {
