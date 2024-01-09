293ad19bd8d5010b50cf2a6760551d337387f0c1
David Bomba <turbo124@gmail.com>
2024-01-09 20:05:18 +1100

    Tests for product cost migration

---
+++ database/migrations/2024_01_09_084515_product_cost_field_population.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Utils\Ninja;
+use App\Models\Invoice;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(Ninja::isHosted()) {
+            return;
+        }
+        
+        Invoice::withTrashed()
+            ->where('is_deleted', false)
+            ->cursor()
+            ->each(function (Invoice $invoice) {
+                
+                
+                $line_items = $invoice->line_items;
+
+                foreach ($line_items as $key => $item)
+                {
+
+                    if($item?->product_cost == 0 && $product = Product::where('company_id', $invoice->company_id)->where('product_key', $item->product_key)->where('cost', '>', 0)->first())
+                    {
+                        $line_items[$key]->product_cost = $product->cost;
+                    }
+                }
+
+                $invoice->line_items = $line_items;
+                $invoice->saveQuietly();
+
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
