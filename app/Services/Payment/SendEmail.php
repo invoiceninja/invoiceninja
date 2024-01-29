82888cccff9c1408555409219efae0c88ee75ba1
David Bomba <turbo124@gmail.com>
2024-01-29 14:03:54 +1100

    Adjustments for logic for sending payment emails

--- app/Services/Payment/SendEmail.php
+++ app/Services/Payment/SendEmail.php
@@ -14,22 +14,38 @@ namespace App\Services\Payment;
 use App\Jobs\Payment\EmailPayment;
 use App\Models\ClientContact;
 use App\Models\Payment;
+use Illuminate\Database\QueryException;
 
 class SendEmail
 {
-    public function __construct(public Payment $payment, public ?ClientContact $contact)
-    {
-    }
+    public function __construct(public Payment $payment, public ?ClientContact $contact) {}
 
     /**
      * Builds the correct template to send.
      */
     public function run()
     {
-        $this->payment->load('company', 'client.contacts', 'invoices');
+        $this->payment->load('company', 'invoices');
 
         if (!$this->contact) {
-            $this->contact = $this->payment->client->contacts()->first();
+
+            if($invoice = $this->payment->invoices->first() ?? false) {
+
+                $invitation =
+                $invoice
+                    ->invitations()
+                    ->whereHas("contact", function ($q) {
+                        $q->where('send_email', true)->orderBy("is_primary", 'ASC');
+                    })
+                    ->first();
+
+                if($invitation) {
+                    $this->contact = $invitation->contact;
+                } else {
+                    $this->contact = $this->payment->client->contacts()->orderBy('send_email', 'desc')->orderBy('is_primary', 'desc')->first();
+                }
+            }
+
         }
 
         EmailPayment::dispatch($this->payment, $this->payment->company, $this->contact);
