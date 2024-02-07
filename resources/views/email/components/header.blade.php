7d02a48ae3b40602ad83105becb282c0b93f8b3a
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-07 01:53:31 +0100

    Refactor email header image styling

--- resources/views/email/components/header.blade.php
+++ resources/views/email/components/header.blade.php
@@ -1,3 +1,3 @@
 @isset($logo)
-    <img src="{{ $logo }}" style="height: 4rem;">
+    <img src="{{ $logo }}" style="display:block;outline:none;border:none;text-decoration:none;margin:0 auto;max-height:65px" />
 @endisset
