1a2f6ea48d1ff6402bc97779ce982d309c4a3a9d
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-08 19:55:38 +0100

    Add BillingPortal Setup component

---
+++ app/Livewire/BillingPortal/Setup.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Livewire\BillingPortal;
+
+use Livewire\Component;
+
+class Setup extends Component
+{
+    public array $context;
+
+    public function mount()
+    {
+        $this->dispatch('purchase.context', property: 'quantity', value: 1);
+        $this->dispatch('purchase.next');        
+    }
+
+    public function render()
+    {
+        return <<<'HTML'
+            <template></template>
+        HTML;
+    }
+}
