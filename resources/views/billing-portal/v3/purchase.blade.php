05f8e2a72cfd23c4f41875f00763d595aefb6aa1
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-09 17:53:31 +0100

    Refactor purchase.blade.php layout

--- resources/views/billing-portal/v3/purchase.blade.php
+++ resources/views/billing-portal/v3/purchase.blade.php
@@ -1,11 +1,20 @@
-<div class="min-w-full flex h-screen">
-    <div class="w-full lg:w-1/2 m-10">
-        <h1 class="text-3xl">{{ $subscription->name }}</h1>
-        <p>C: {{ $this->component }}</p>
-        <p>Quantity: {{ $this->context['quantity'] }}</p>
+<div class="grid grid-cols-12">
+    <div class="col-span-12 xl:col-span-6 bg-white flex flex-col items-center lg:h-screen">
+        <div class="w-full p-10 lg:mt-24 md:max-w-md">
+            <img 
+                class="h-8" 
+                src="{{ $subscription->company->present()->logo }}"
+                alt="{{ $subscription->company->present()->name }}" />
+
+            <div class="my-10" id="container">
+                @livewire($this->component, ['context' => $context, 'subscription' => $this->subscription], key($this->component))
+            </div>
+        </div>
     </div>
 
-    <div class="w-full lg:w-1/2 m-10">
-        @livewire($this->component, ['context' => $context, 'subscription' => $this->subscription], key($this->component))
+    <div class="col-span-12 xl:col-span-6 bg-gray-50 flex flex-col items-center">
+        <div class="w-full p-10 lg:mt-24 md:max-w-3xl">
+            <h1 class="text-3xl">{{ $subscription->name }}</h1>
+        </div>
     </div>
 </div>
