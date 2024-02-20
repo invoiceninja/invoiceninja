68a0db8725f1374ff6021174c9e7b53d0093b8a5
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-20 17:10:35 +0100

    Refactored payment methods display

--- resources/views/billing-portal/v3/payments/methods.blade.php
+++ resources/views/billing-portal/v3/payments/methods.blade.php
@@ -1,9 +1,11 @@
 <div>
     <h1 class="text-2xl">{{ ctrans('texts.payment_methods') }}</h1>
 
-    @foreach($methods as $method)
-        <button wire:click="handleSelect('{{ $method['company_gateway_id'] }}', '{{ $method['gateway_type_id'] }}')">
-            {{ $method['label'] }}
-        </button>
-    @endforeach
+    <div class="flex flex-col space-y-3 my-3">
+        @foreach($methods as $method)
+            <button class="flex items-center justify-between mb-4 bg-white rounded px-6 py-4 shadow-sm border" wire:click="handleSelect('{{ $method['company_gateway_id'] }}', '{{ $method['gateway_type_id'] }}')">
+                {{ $method['label'] }}
+            </button>
+        @endforeach
+    </div>
 </div>
