1cc17a4af46b950cd0ec453ef862acfa4ff40aa2
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-20 17:24:42 +0100

    Add spinner and observer for loading screen

--- resources/views/billing-portal/v3/index.blade.php
+++ resources/views/billing-portal/v3/index.blade.php
@@ -13,6 +13,21 @@
                     document.getElementById('payment-method-form').submit()
                 }, 2000);
             });
+
+            const target = document.getElementById('container');
+
+            const observer = new MutationObserver((mutationsList) => {
+                for (const mutation of mutationsList) {
+                    if (mutation.type === 'childList' || mutation.type === 'subtree') {
+                        setTimeout(() => {
+                            document.getElementById('spinner').classList.add('hidden');
+                            document.getElementById('container').classList.remove('hidden');
+                        }, 1500);
+                    }
+                }
+            });
+
+            observer.observe(target, { childList: true, subtree: true })
         });
     </script>
 @endpush
