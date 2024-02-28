9bafff750501da822c84e401afee94dc104bfee0
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-28 19:08:02 +0100

    Add steps column to subscriptions table

---
+++ database/migrations/2024_02_28_180250_add_steps_to_subscriptions.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+use App\Livewire\BillingPortal\Purchase;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $steps = collect(Purchase::$dependencies)
+                ->pluck('id')
+                ->implode(',');
+
+            $table->string('steps')->default($steps);
+        });
+    }
+};
