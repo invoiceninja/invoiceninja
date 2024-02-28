6c2d62f0c894d61c71e72c7ea5e24e99cc068cfc
Benjamin Beganović <k1pstabug@gmail.com>
2024-02-28 18:56:46 +0100

    Clean up

--- app/Livewire/BillingPortal/Purchase.php
+++ app/Livewire/BillingPortal/Purchase.php
@@ -36,6 +36,10 @@ class Purchase extends Component
 
     // 
 
+    public int $step = 0;
+
+    public string $id;
+
     public static array $dependencies = [
         Login::class => [],
         RegisterOrLogin::class => [],
@@ -45,7 +49,6 @@ class Purchase extends Component
         RFF::class => [Login::class, RegisterOrLogin::class, Register::class],
     ];
 
-    public int $step = 0;
 
     public static array $steps = [
         Setup::class,
@@ -56,99 +59,6 @@ class Purchase extends Component
         Submit::class,
     ];
 
-    public string $id;
-
-    /**  $context = [   
-     *  'hash' => string,
-     *  'quantity' => int,
-     *  'request_data' => [
-     *      'q'  => string,
-     *  ],
-     *  'campaign => ?string,
-     *  'bundle' => $bundle, 
-     *  'contact' =>  [
-     *     'first_name' => ?string,
-     *     'last_name' => ?string,
-     *     'phone' => ?string,
-     *     'custom_value1' => ?string,
-     *     'custom_value2' => ?string,
-     *     'custom_value3' => ?string,
-     *     'custom_value4' => ?string,
-     *     'email' => ?string,
-     *     'email_verified_at' => ?int,
-     *     'confirmation_code' => ?string,
-     *     'is_primary' => bool,
-     *     'confirmed' => bool,
-     *     'last_login' => ?datetime,
-     *     'failed_logins' => ?int,
-     *     'accepted_terms_version' => ?string,
-     *     'avatar' => ?string,
-     *     'avatar_type' => ?string,
-     *     'avatar_size' => ?string,
-     *     'is_locked' => bool,
-     *     'send_email' => bool,
-     *     'contact_key' => string,
-     *     'created_at' => int,
-     *     'updated_at' => int,
-     *     'deleted_at' => int,
-     *     'hashed_id' => string,  
-     * ], 
-     *  'client_id' => string, 
-     *  'quantity' => int, 
-     *  'products' => [
-     *      [
-     *       'product_key' => string,
-     *       'quantity' => int,
-     *       'total_raw' => float,
-     *       'total' => string,
-     *      ],
-     *  ]
-     * ];
-     * 
-     *  $bundle =[
-     *  'optional_one_time_products' => array, 
-     *  'one_time_products' => array, 
-     *  'optional_recurring_products' => array, 
-     *  'recurring_products' => [
-     *      'hashed_id' => [
-     *          'product' => [
-     *          'id' => int,
-     *          'company_id' => int,
-     *          'user_id' => int,
-     *          'assigned_user_id' => ?int,
-     *          'project_id' => ?int,
-     *          'vendor_id' => ?int,
-     *          'custom_value1' => ?string,
-     *          'custom_value2' => ?string,
-     *          'custom_value3' => ?string,
-     *          'custom_value4' => ?string,
-     *          'product_key' => ?string,
-     *          'notes' => ?string,
-     *          'cost' => float,
-     *          'price' => float,
-     *          'quantity' => float,
-     *          'tax_name1' => ?string,
-     *          'tax_rate1' => float,
-     *          'tax_name2' => ?string,
-     *          'tax_rate2' => float,
-     *          'tax_name3' => ?string,
-     *          'tax_rate3' => float,
-     *          'deleted_at' => ?int,
-     *          'created_at' => ?int,
-     *          'updated_at' => ?int,
-     *          'is_deleted' => bool,
-     *          'in_stock_quantity' => ?int,
-     *          'stock_notification' => bool,
-     *          'stock_notification_threshold' => ?int,
-     *          'max_quantity' => ?int,
-     *          'product_image' => ?string,
-     *          'tax_id' => ?int,
-     *          'hashed_id' => ?string,
-     *          ],
-     *      ],
-     *  ],
-     * ] $bundle
-     */
     public array $context = [];
 
     #[On('purchase.context')]
