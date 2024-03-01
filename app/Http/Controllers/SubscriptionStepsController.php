70aa749f28813e3ff67d0875f45a3c437d1d15ea
Benjamin Beganović <k1pstabug@gmail.com>
2024-03-01 13:42:44 +0100

    Add validation for subscription steps

--- app/Http/Controllers/SubscriptionStepsController.php
+++ app/Http/Controllers/SubscriptionStepsController.php
@@ -1,16 +1,25 @@
 <?php
 
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2022. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
 namespace App\Http\Controllers;
 
 use App\Livewire\BillingPortal\Purchase;
+use App\Rules\Subscriptions\Steps;
 use Illuminate\Http\JsonResponse;
 
 class SubscriptionStepsController extends BaseController
 {
     public function index(): JsonResponse
     {
-        // @todo: perhaps integrate this in statics
-
         $dependencies = collect(Purchase::$dependencies)
             ->map(fn($dependency) => [
                 'id' => $dependency['id'],
@@ -21,5 +30,14 @@ class SubscriptionStepsController extends BaseController
             ->toArray();
 
         return response()->json($dependencies);
+    }
+
+    public function check(): JsonResponse
+    {
+        request()->validate(([
+            'steps' => ['required', new Steps()]
+        ]));
+
+        return response()->json([], 200);
     }
 }
