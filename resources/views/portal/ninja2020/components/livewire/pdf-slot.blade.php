cd0884c77085ef97e10c63c05c36dc17b6495f1a
Lars Kusch <lars@lars-kusch.de>
2024-03-10 16:24:28 +0100

    Added download button in client portal for new types

--- resources/views/portal/ninja2020/components/livewire/pdf-slot.blade.php
+++ resources/views/portal/ninja2020/components/livewire/pdf-slot.blade.php
@@ -20,6 +20,40 @@
         </div>
     </button>
     @endif
+      @if($entity_type == 'credit' && $settings->enable_e_invoice)
+          <button wire:loading.attr="disabled" wire:click="downloadECreit" class="bg-primary text-white px-4 py-4 lg:px-2 lg:py-2 rounded" type="button">
+              <span>{{ ctrans('texts.download_e_credit') }}</span>
+              <div wire:loading wire:target="downloadECredit">
+                  <svg class="animate-spin h-5 w-5 text-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+              </div>
+          </button>
+      @endif
+      @if($entity_type == 'quote' && $settings->enable_e_invoice)
+          <button wire:loading.attr="disabled" wire:click="downloadEQuote" class="bg-primary text-white px-4 py-4 lg:px-2 lg:py-2 rounded" type="button">
+              <span>{{ ctrans('texts.download_e_quote') }}</span>
+              <div wire:loading wire:target="downloadEQuote">
+                  <svg class="animate-spin h-5 w-5 text-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+              </div>
+          </button>
+      @endif
+{{--      Not implemented yet--}}
+{{--      @if($entity_type == 'purchase_order' && $settings->enable_e_invoice)
+          <button wire:loading.attr="disabled" wire:click="downloadEInvoice" class="bg-primary text-white px-4 py-4 lg:px-2 lg:py-2 rounded" type="button">
+              <span>{{ ctrans('texts.download_e_invoice') }}</span>
+              <div wire:loading wire:target="downloadEInvoice">
+                  <svg class="animate-spin h-5 w-5 text-blue" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+              </div>
+          </button>
+      @endif--}}
   </div>
   @if($html_entity_option)
   <div class="hidden lg:block">
