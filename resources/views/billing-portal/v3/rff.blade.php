7d4588d3b583b17e3e7c3d9694f8a52f513bcfba
Benjamin Beganović <k1pstabug@gmail.com>
2024-03-15 01:31:11 +0100

    Refactor required-client-info component instantiation in rff.blade.php

--- resources/views/billing-portal/v3/rff.blade.php
+++ resources/views/billing-portal/v3/rff.blade.php
@@ -10,13 +10,6 @@
     @endif
 
     <div>
-        @livewire('required-client-info', [
-            'fields' => method_exists($gateway, 'getClientRequiredFields') ? $gateway->getClientRequiredFields() : [], 
-            'contact' => auth()->guard('contact')->user(), 
-            'countries' => $countries, 
-            'company' => $company, 
-            'company_gateway_id' => $gateway->company_gateway ? $gateway->company_gateway->id : $gateway->id,
-            'form_only' => true
-        ])
+        @livewire('required-client-info', ['db' => $company->db, 'fields' => method_exists($gateway, 'getClientRequiredFields') ? $gateway->getClientRequiredFields() : [], 'contact_id' => auth()->guard('contact')->user()->id, 'countries' => $countries, 'company_id' => $company->id, 'company_gateway_id' => $gateway->company_gateway ? $gateway->company_gateway->id : $gateway->id, 'form_only' => true])>
     </div>
 </div>
