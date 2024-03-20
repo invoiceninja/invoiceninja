05593f0824fef0fc296c147ab43b54ca87f9a6b2
David Bomba <turbo124@gmail.com>
2024-03-20 20:05:22 +1100

    Minor fixes

--- app/Http/Requests/Payment/RefundPaymentRequest.php
+++ app/Http/Requests/Payment/RefundPaymentRequest.php
@@ -57,8 +57,6 @@ class RefundPaymentRequest extends Request
 
         if (isset($input['credits'])) {
             unset($input['credits']);
-            // foreach($input['credits'] as $key => $credit)
-            //     $input['credits'][$key]['credit_id'] = $this->decodePrimaryKey($credit['credit_id']);
         }
 
         $this->replace($input);
