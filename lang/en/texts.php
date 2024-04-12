3d6d9d709adb4f2f74bd538f260eebd0fe051b62
David Bomba <turbo124@gmail.com>
2024-04-12 17:58:35 +1000

    Updated lang file

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -5277,8 +5277,6 @@ $lang = array(
     'accept_payments_online' => 'Accept Payments Online',
     'all_payment_gateways' => 'View all payment gateways',
     'product_cost' => 'Product cost',
-    'enable_rappen_roudning' => 'Enable Rappen Rounding',
-    'enable_rappen_rounding_help' => 'Rounds totals to nearest 5',
     'duration_words' => 'Duration in words',
     'upcoming_recurring_invoices' => 'Upcoming Recurring Invoices',
     'shipping_country_id' => 'Shipping Country',
@@ -5294,6 +5292,9 @@ $lang = array(
     'e_quote_type' => 'E-Quote Type',
     'unlock_unlimited_clients' => 'Please upgrade to unlock unlimited clients!',
     'download_e_purchase_order' => 'Download E-Purchase Order',
+    'flutter_web_warning' => 'We recommend using the new web app or the desktop app for the best performance',
+    'rappen_rounding' => 'Rappen Rounding',
+    'rappen_rounding_help' => 'Round amount to 5 cents',
 );
 
 return $lang;
