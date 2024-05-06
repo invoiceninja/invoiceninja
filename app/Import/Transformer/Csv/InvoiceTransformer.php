cbfdf53d9eecf1c22cb1d6086404af7032bf5b03
David Bomba <turbo124@gmail.com>
2024-05-07 09:59:00 +1000

    Texts

--- app/Import/Transformer/Csv/InvoiceTransformer.php
+++ app/Import/Transformer/Csv/InvoiceTransformer.php
@@ -93,7 +93,7 @@ class InvoiceTransformer extends BaseTransformer
                 'invoice.custom_value4'
             ),
             'footer' => $this->getString($invoice_data, 'invoice.footer'),
-            'partial' => $this->getFloat($invoice_data, 'invoice.partial') > 0 ?: null,
+            'partial' => $this->getFloat($invoice_data, 'invoice.partial') > 0 ? $this->getFloat($invoice_data, 'invoice.partial') : null,
             'partial_due_date' =>  isset($invoice_data['invoice.partial_due_date']) ? $this->parseDate($invoice_data['invoice.partial_due_date']) : null,
             'custom_surcharge1' => $this->getFloat(
                 $invoice_data,
