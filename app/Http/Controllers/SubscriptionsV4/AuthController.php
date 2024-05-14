2c850ceb1476dc054214a55cfb7ebb2419207e43
Benjamin Beganović <k1pstabug@gmail.com>
2024-05-14 20:31:18 +0200

    implement auth with proper components & check

--- app/Http/Controllers/SubscriptionsV4/AuthController.php
+++ app/Http/Controllers/SubscriptionsV4/AuthController.php
@@ -12,46 +12,83 @@
 
 namespace App\Http\Controllers\SubscriptionsV4;
 
+use App\Http\Requests\SubscriptionsV4\LoginCheckRequest;
+use App\Http\Requests\SubscriptionsV4\LoginRequest;
+use App\Livewire\BillingPortal\Authentication\Login;
+use App\Livewire\BillingPortal\Authentication\Register;
+use App\Livewire\BillingPortal\Authentication\RegisterOrLogin;
 use App\Models\ClientContact;
 use App\Models\Subscription;
+use App\Services\Subscription\StepService;
+use Illuminate\Support\Str;
 
 class AuthController
 {
-    public function login(Subscription $subscription)
+    public function login(Subscription $subscription, LoginRequest $request)
     {
-        $contact = ClientContact::where('email', request()->email)
+        $steps = StepService::mapToClassNames(
+            steps: $subscription->steps,
+        );
+
+        $contact = ClientContact::query()
+            ->where('email', $request->email)
             ->where('company_id', $subscription->company_id)
             ->first();
 
-        if (!$contact) {
+        if ($contact) {
+            $attempt = auth()->guard('contact')->attempt([
+                'email' => request()->email,
+                'password' => request()->password,
+                'company_id' => $subscription->company_id,
+            ]);
+
+            if ($attempt) {
+                $key = Str::random(64);
+
+                cache()->put(
+                    key: $key,
+                    value: $contact->id,
+                );
+
+                return response()->json([
+                    'contact' => auth()->guard('contact')->user()->client,
+                    'key' => $key,
+                ]);
+            }
+
             return response()->noContent(401);
         }
 
-        $attempt = auth()->guard('contact')->attempt([
-            'email' => request()->email,
-            'password' => request()->password,
-            'company_id' => $subscription->company_id,
-        ]);
-
-        if (!$attempt) {
+        if (in_array(Login::class, $steps) && $contact === null) {
             return response()->noContent(401);
         }
 
-        return response()->json(
-            auth()->guard('contact')->user()->client
-        );
+        // Otherwise register user
+        // Send email and configure otp/e-mail auth
     }
 
-    public function check(Subscription $subscription)
+    public function check(Subscription $subscription, LoginCheckRequest $request): \Illuminate\Http\Response
     {
-        $contact = ClientContact::where('email', request()->email)
+        $contact = ClientContact::query()
+            ->where('email', $request->email)
             ->where('company_id', $subscription->company_id)
             ->first();
 
-        if (!$contact) {
-            return response()->noContent(404);
+        if ($contact) {
+            return response()->noContent(200);
         }
 
-        return response()->noContent(200);
+        $steps = StepService::mapToClassNames(
+            steps: $subscription->steps,
+        );
+
+        if (
+            in_array(Register::class, $steps) ||
+            in_array(RegisterOrLogin::class, $steps)
+        ) {
+            return response()->noContent(200);
+        }
+
+        return response()->noContent(404);
     }
 }
