7e155c8b3521c2acf5488a0053685eeb073f013e
Benjamin Beganović <k1pstabug@gmail.com>
2024-05-15 20:01:19 +0200

    authenticate request

---
+++ app/Http/Requests/SubscriptionsV4/AuthenticateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Http\Requests\SubscriptionsV4;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthenticateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email:rfc'],
+            'password' => ['sometimes', 'string'],
+            'registration_fields' => ['sometimes', 'array'],
+        ];
+    }
+}
