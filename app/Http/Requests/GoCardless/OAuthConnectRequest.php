f1ac41a5c4e1dc5515a99024d1b343ac0719614b
Benjamin Beganović <k1pstabug@gmail.com>
2024-06-03 20:00:50 +0200

    request for confirmation connect

---
+++ app/Http/Requests/GoCardless/OAuthConnectRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Http\Requests\GoCardless;
+
+use App\Libraries\MultiDB;
+use App\Models\Company;
+use Illuminate\Foundation\Http\FormRequest;
+
+class OAuthConnectRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function getCompany(): \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Builder|\App\Models\BaseModel
+    {
+        MultiDB::findAndSetDbByCompanyKey(
+            $this->company_key,
+        );
+
+        return Company::query()
+            ->where('company_key', $this->company_key)
+            ->firstOrFail();
+    }
+}
