6a04f1c9b8442810ca59fa5daf6b1dce7bb7b4f5
Benjamin Beganović <k1pstabug@gmail.com>
2024-06-10 19:29:14 +0200

    translations

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -5334,7 +5334,8 @@ $lang = array(
     'btcpay_refund_body' => 'A refund intended for you has been issued. To claim it via BTCPay, please click on this link:',
     'currency_mauritanian_ouguiya' => 'Mauritanian Ouguiya',
     'currency_bhutan_ngultrum' => 'Bhutan Ngultrum',
-    'end_of_month' => 'End Of Month'
+    'end_of_month' => 'End Of Month',
+    'templates_error_title' => 'Whoops, looks like something\'s wrong with the template!',
 );
 
 return $lang;
