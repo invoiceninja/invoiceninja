0877bdb6eaff9704f1944a29bdf53169d432434d
David Bomba <turbo124@gmail.com>
2024-06-12 08:32:26 +1000

    Minor fixes for expense export preview props

--- app/Export/CSV/ExpenseExport.php
+++ app/Export/CSV/ExpenseExport.php
@@ -220,17 +220,17 @@ class ExpenseExport extends BaseExport
         //     $entity['expense.client'] = $expense->client ? $expense->client->present()->name() : '';
         // }
 
-        // if (in_array('expense.invoice_id', $this->input['report_keys'])) {
-        //     $entity['expense.invoice_id'] = $expense->invoice ? $expense->invoice->number : '';
-        // }
+        if (in_array('expense.invoice_id', $this->input['report_keys'])) {
+            $entity['expense.invoice_id'] = $expense->invoice ? $expense->invoice->number : '';
+        }
 
         // if (in_array('expense.category', $this->input['report_keys'])) {
         //     $entity['expense.category'] = $expense->category ? $expense->category->name : '';
         // }
 
-        // if (in_array('expense.vendor_id', $this->input['report_keys'])) {
-        //     $entity['expense.vendor'] = $expense->vendor ? $expense->vendor->name : '';
-        // }
+        if (in_array('expense.vendor_id', $this->input['report_keys'])) {
+            $entity['expense.vendor'] = $expense->vendor ? $expense->vendor->name : '';
+        }
 
         // if (in_array('expense.payment_type_id', $this->input['report_keys'])) {
         //     $entity['expense.payment_type_id'] = $expense->payment_type ? $expense->payment_type->name : '';
