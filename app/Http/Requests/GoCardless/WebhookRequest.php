b5495416f5087949681ebd5ae3c52482f4c95ef3
Benjamin Beganović <k1pstabug@gmail.com>
2024-06-12 19:33:39 +0200

    Add GoCardless webhook request validation rules

---
+++ app/Http/Requests/GoCardless/WebhookRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace App\Http\Requests\GoCardless;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WebhookRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'events' => ['required', 'array'],
+            'events.*.resource_type' => ['required', 'string'],
+        ];
+    }
+}
