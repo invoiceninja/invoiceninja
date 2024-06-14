a428196e92b72db24feb5f50065d9f9db4f052db
Kendall Arneaud <kendall.arneaud@gmail.com>
2024-06-14 16:12:31 +0000

    update aliases

--- app/PaymentDrivers/Rotessa/Helpers/helpers.php
+++ app/PaymentDrivers/Rotessa/Helpers/helpers.php
@@ -3,4 +3,6 @@
 include app_path("PaymentDrivers/Rotessa/vendor/autoload.php");
 
 class_alias("App\\PaymentDrivers\\Rotessa\\PaymentMethod","App\\PaymentDrivers\\Rotessa\\BankTransfer");
-class_alias("App\\PaymentDrivers\\Rotessa\\PaymentMethod","App\\PaymentDrivers\\Rotessa\\ACH");
+class_alias("App\\PaymentDrivers\\Rotessa\\PaymentMethod","App\\PaymentDrivers\\Rotessa\\DirectDebit");
+class_alias("App\\PaymentDrivers\\Rotessa\\PaymentMethod","App\\PaymentDrivers\\Rotessa\\Acss");
+class_alias("App\\PaymentDrivers\\Rotessa\\PaymentMethod","App\\PaymentDrivers\\Rotessa\\Bacs");
