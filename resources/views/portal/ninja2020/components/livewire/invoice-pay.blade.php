c7d6b50778417ce654daec4377a8792949045d5c
David Bomba <turbo124@gmail.com>
2024-06-24 15:44:42 +1000

    Payment float

--- resources/views/portal/ninja2020/components/livewire/invoice-pay.blade.php
+++ resources/views/portal/ninja2020/components/livewire/invoice-pay.blade.php
@@ -1,3 +1,10 @@
-<div>
-    @livewire($this->component,['context' => $context], key($this->componentUniqueId()))
+<div class="grid grid-cols-1 md:grid-cols-2">
+    <div>
+        @livewire('invoice-summary',['context' => $context])
+    </div>
+
+    <div>
+        @livewire($this->component,['context' => $context], key($this->componentUniqueId()))
+    </div>
+    
 </div>
