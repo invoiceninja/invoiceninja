0f27b8474c9ad0c5d66272b9f6d42d7023bca74b
David Bomba <turbo124@gmail.com>
2024-06-24 14:38:53 +1000

    New payment flow

--- resources/views/portal/ninja2020/components/livewire/signature.blade.php
+++ resources/views/portal/ninja2020/components/livewire/signature.blade.php
@@ -1,13 +1,15 @@
-<div class="">
-    <div class="bg-white p-6 rounded-lg shadow-lg">
-        <h2 class="text-xl text-center py-0 px-4">{{ ctrans('texts.sign_here_ux_tip') }}</h2>
-        <canvas id="signature-pad" class="border border-gray-300 w-full h-64"></canvas>
-        <div class="flex justify-between items-center px-4 py-4">
-            <button id="clear-signature" class="px-4 py-2 mr-6 bg-red-500 text-white rounded">{{ ctrans('texts.clear') }}</button>
-            <button id="save-button" class="button button-primary bg-primary hover:bg-primary-darken">{{ ctrans('texts.next') }}</button>
+<div style="w-full">
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+        <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+            <h2 class="text-xl text-center mx-auto py-4 px-4">{{ ctrans('texts.sign_here_ux_tip') }}</h2>
+            <canvas id="signature-pad" class="border border-gray-300 w-full h-64"></canvas>
+            <div class="flex justify-between px-4 py-4">
+                <button id="clear-signature" class="px-4 bg-red-500 text-white rounded">{{ ctrans('texts.clear') }}</button>
+                <button id="save-button" class="button button-primary bg-primary hover:bg-primary-darken inline-flex items-center">{{ ctrans('texts.next') }}</button>
+            </div>
         </div>
     </div>
-    
+
     @assets
     <script src="{{ asset('vendor/signature_pad@5/signature_pad.umd.min.js') }}"></script>
     @endassets
--- resources/views/portal/ninja2020/components/livewire/terms.blade.php
+++ resources/views/portal/ninja2020/components/livewire/terms.blade.php
@@ -1,23 +1,28 @@
-<div class="pb-8">
-    <div class="bg-white p-6 rounded-lg shadow-lg">
-        <h2 class="text-xl text-center py-0 px-4">{{ ctrans('texts.terms') }}</h2>
-        <div class="mt-0 h-64 overflow-y-auto">
-            <div class="py-0">
-                <p class="text-sm leading-6 font-medium text-gray-500">{{ ctrans('texts.invoice') }} {{ $invoice->number }}:</p>
-                @if($variables && $invoice->terms)
-                    <h5 data-ref="entity-terms">{!! $invoice->parseHtmlVariables('terms', $variables) !!}</h5>
-                @elseif($invoice->terms)
-                    <h5 data-ref="entity-terms" class="text-sm leading-5 text-gray-900">{!! $invoice->terms !!}</h5>
-                @else
-                    <i class="text-sm leading-5 text-gray-500">{{ ctrans('texts.not_specified') }}</i>
-                @endif
+<div>
+    <div class="bg-color: white">
+        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-xl leading-6 font-medium text-gray-900">
+                {{ ctrans('texts.terms') }}
+            </h3>
+            <div class="mt-4 h-64 overflow-y-auto">
+                    <div class="mb-4">
+                        <p class="text-sm leading-6 font-medium text-gray-500">{{ ctrans('texts.invoice') }} {{ $invoice->number }}:</p>
+                        @if($variables && $invoice->terms)
+                            <h5 data-ref="entity-terms">{!! $invoice->parseHtmlVariables('terms', $variables) !!}</h5>
+                        @elseif($invoice->terms)
+                            <h5 data-ref="entity-terms" class="text-sm leading-5 text-gray-900">{!! $invoice->terms !!}</h5>
+                        @else
+                            <i class="text-sm leading-5 text-gray-500">{{ ctrans('texts.not_specified') }}</i>
+                        @endif
+                    </div>
             </div>
         </div>
-        <div class="flex justify-end items-center px-4 py-4">
-            <button id="accept-terms-button" class="button button-primary bg-primary hover:bg-primary-darken float-end">{{ ctrans('texts.next') }}</button>
+    </div>
+    <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+        <div class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto" x-data>
+            <button id="accept-terms-button" class="button button-primary bg-primary hover:bg-primary-darken inline-flex items-center">{{ ctrans('texts.next') }}</button>
         </div>
     </div>
-    
     @script
     <script>
             
