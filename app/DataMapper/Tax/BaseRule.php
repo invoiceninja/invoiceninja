6aac4a03b7e8853faab0e27be98e01a7a1de3ecd
David Bomba <turbo124@gmail.com>
2024-06-28 09:54:28 +1000

    Fixes for override taxes for AT region

--- app/DataMapper/Tax/BaseRule.php
+++ app/DataMapper/Tax/BaseRule.php
@@ -379,7 +379,6 @@ class BaseRule implements RuleInterface
 
     public function override($item): self
     {
-        nlog("override");
         $this->tax_rate1 = $item->tax_rate1;
         $this->tax_name1 = $item->tax_name1;
         $this->tax_rate2 = $item->tax_rate2;
@@ -387,7 +386,6 @@ class BaseRule implements RuleInterface
         $this->tax_rate3 = $item->tax_rate3;
         $this->tax_name3 = $item->tax_name3;
 
-        nlog($item);
         return $this;
 
     }
