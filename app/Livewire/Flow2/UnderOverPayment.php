73c1f1b7671d04496ca7626f0a793faed5ee6ad7
David Bomba <turbo124@gmail.com>
2024-07-02 16:13:52 +1000

    improvements for validation of over/under payments

--- app/Livewire/Flow2/UnderOverPayment.php
+++ app/Livewire/Flow2/UnderOverPayment.php
@@ -24,12 +24,43 @@ class UnderOverPayment extends Component
 
     public $currency;
 
+    public $invoice_amount;
+
+    public $errors = '';
+
     public function mount()
     {
         $invoice = $this->context['invoice'];
-        $invoice_amount = $invoice->partial > 0 ? $invoice->partial : $invoice->balance;
+        $this->invoice_amount = $invoice->partial > 0 ? $invoice->partial : $invoice->balance;
         $this->currency = $invoice->client->currency();
-        $this->payableAmount = Number::formatValue($invoice_amount, $this->currency);
+        $this->payableAmount = Number::formatValue($this->invoice_amount, $this->currency);
+    }
+
+    public function checkValue($value)
+    {
+
+        $this->errors = '';
+
+        $settings = $this->context['settings'];
+        $input_amount = Number::parseFloat($value);
+
+        if($settings->client_portal_allow_under_payment && $settings->client_portal_under_payment_minimum != 0)
+        {
+            if($input_amount <= $settings->client_portal_under_payment_minimum){
+                // return error message under payment too low.
+                $this->errors = ctrans('texts.minimum_required_payment', ['amount' => $settings->client_portal_under_payment_minimum]);
+                $this->dispatch('errorMessageUpdate', errors: $this->errors);
+            }
+        }
+
+        if(!$settings->client_portal_allow_over_payment && ($input_amount > $this->invoice_amount)){
+            $this->errors = ctrans('texts.over_payments_disabled');
+            $this->dispatch('errorMessageUpdate', errors: $this->errors);
+
+        }
+
+        if(!$this->errors)
+            $this->dispatch('payable-amount',  payable_amount: $value );
     }
 
     public function render()
