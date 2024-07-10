03fc546e814a36ba747487c229450b1378bd1d4f
Hillel Coren <hillelcoren@gmail.com>
2024-07-10 09:06:13 +0300

    Replace copyable link with pushState

--- resources/views/portal/ninja2020/credits/show.blade.php
+++ resources/views/portal/ninja2020/credits/show.blade.php
@@ -9,16 +9,6 @@
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
                         {{ ctrans('texts.entity_number_placeholder', ['entity' => ctrans('texts.credit'), 'entity_number' => $credit->number]) }}
                     </h3>
-
-                    @if($key)
-                    <div class="btn hidden md:block" data-clipboard-text="{{url("client/credit/{$key}")}}" aria-label="Copied!">
-                        <div class="flex text-sm leading-6 font-medium text-gray-500">
-                            <p class="mr-2">{{url("client/credit/{$key}")}}</p>
-                            <p><img class="h-5 w-5" src="{{ asset('assets/clippy.svg') }}" alt="Copy to clipboard"></p>
-                        </div>
-                    </div>
-                    @endif
-
                 </div>
             </div>
         </div>
@@ -30,11 +20,16 @@
 @endsection
 
 @section('footer')
-    <script src="{{ asset('vendor/clipboard.min.js') }}"></script>
 
     <script type="text/javascript">
 
-        var clipboard = new ClipboardJS('.btn');
+        document.addEventListener('DOMContentLoaded', () => {
+
+            @if($key)
+                window.history.pushState({}, "", "{{ url("client/credit/{$key}") }}");
+            @endif
+
+        });
 
     </script>
 @endsection
