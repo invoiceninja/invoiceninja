08f762e31f9b15b0f2748a9fd99aeef585cf5b4e
Kendall Arneaud <kendall.arneaud@gmail.com>
2024-07-27 12:43:09 -0400

    Update pay.blade.php
    removed. Not needed
    
    Signed-off-by: Kendall Arneaud <kendall.arneaud@gmail.com>

--- resources/views/portal/ninja2020/gateways/rotessa/bank_transfer/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/rotessa/bank_transfer/pay.blade.php
@@ -38,27 +38,6 @@
             <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
                 <input autocomplete="new-password" readonly type="date" min="{{ $due_date }}" name="process_date" id="process_date" required class="input w-full" placeholder="" value="{{ old('process_date', $process_date ) }}">
             </dd>
-            {{--
-            <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-                Insallments
-            </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                <input class="input w-full" id="installments" name="installments" type="number" placeholder="Installments" required value="{{ old('installments',$installments) }}">
-            </dd>
-            
-            <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-                Frequency
-            </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                <input class="input w-full" id="frequency" name="frequency" type="text" placeholder="Once/Weekly/Monthly/Annually" required >
-            </dd>
-            
-            <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-                Comments
-            </dt>
-            <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                <textarea autocomplete="new-password" id="comment" name="comment" type="text" class="w-full py-2 px-3 rounded text-sm disabled:opacity-75 disabled:cursor-not-allowed undefined border border-gray-300" placeholder="" rows="5" style="background-color: rgb(255, 255, 255); border-color: rgb(209, 213, 219); color: rgb(42, 48, 61);"> </textarea>
-            </dd> --}}
         @endcomponent
         </form>
     @else
