ddfafc5418dacc323a7e0fc88d4baf08898ddc32
Kendall Arneaud <kendall.arneaud@gmail.com>
2024-07-28 19:19:24 -0400

    Update texts.php
    update text translations
    
    Signed-off-by: Kendall Arneaud <kendall.arneaud@gmail.com>

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -5301,6 +5301,15 @@ $lang = array(
     'latest_requires_php_version' => 'Note: the latest version requires PHP :version',
     'auto_expand_product_table_notes' => 'Automatically expand products table notes',
     'auto_expand_product_table_notes_help' => 'Automatically expands the notes section within the products table to display more lines.',
+    'institution_number' => 'Institution Number',
+    'transit_number' => 'Transit Number',
+    'personal' => 'Personal',
+    'address_information' => 'Address Information',
+    'enter_the_information_for_the_bank_account' => 'Enter the Information for the Bank Account',
+    'account_holder_information' => 'Account Holder Information',
+    'enter_information_for_the_account_holder' => 'Enter Information for the Account Holder',
+    'customer_type' => 'Customer Type',
+    'process_date' => 'Process Date'
 );
 
 return $lang;
