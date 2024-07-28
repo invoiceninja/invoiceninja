161c68c1df758ce7db5e554fc45b071123a1ba98
Kendall Arneaud <kendall.arneaud@gmail.com>
2024-07-28 18:54:00 -0400

    Update address.blade.php
    Amend translation texts
    
    Signed-off-by: Kendall Arneaud <kendall.arneaud@gmail.com>

--- resources/views/portal/ninja2020/gateways/rotessa/components/address.blade.php
+++ resources/views/portal/ninja2020/gateways/rotessa/components/address.blade.php
@@ -10,7 +10,7 @@
         </div>
     <div class="px-4 py-2 sm:px-6 lg:grid lg:grid-cols-3 lg:gap-4 lg:flex lg:items-center">
         <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-            {{ ctrans('texts.address_line_1') }}
+            {{ ctrans('texts.address1') }}
         </dt>
         <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
             <input class="input w-full" id="address_1" name="address_1" type="text" placeholder="Address Line 1" required value="{{ old('address_1', $address_1) }}">
@@ -19,7 +19,7 @@
 
     <div class="px-4 py-2 sm:px-6 lg:grid lg:grid-cols-3 lg:gap-4 lg:flex lg:items-center">
         <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-            {{ ctrans('texts.address_line_2') }}
+            {{ ctrans('texts.address2') }}
         </dt>
         <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
             <input class="input w-full" id="address_2" name="address_2" type="text" placeholder="Address Line 2" required value="{{ old('address_2', $address_2) }}">
@@ -28,7 +28,7 @@
 
     <div class="px-4 py-2 sm:px-6 lg:grid lg:grid-cols-3 lg:gap-4 lg:flex lg:items-center">
         <dt class="text-sm leading-5 font-medium text-gray-500 mr-4">
-            City
+           {{ ctrans('texts.city') }}
         </dt>
         <dd class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
             <input class="input w-full" id="city" name="city" type="text" placeholder="City" required value="{{ old('city', $city) }}">
