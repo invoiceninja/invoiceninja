fc802874293fb28ac4988f1e6fce70f636d75c71
Benjamin Beganović <k1pstabug@gmail.com>
2024-08-09 01:08:36 +0200

    Paytrace: New payment flow (#74)
    * pass livewirePaymentView & processPaymentView thru base driver
    
    * add paymentData to the interface
    
    * paytrace

--- resources/js/clients/payments/paytrace-credit-card.js
+++ resources/js/clients/payments/paytrace-credit-card.js
@@ -8,6 +8,8 @@
  * @license https://www.elastic.co/licensing/elastic-license
  */
 
+import { instant, wait } from '../wait';
+
 class PayTraceCreditCard {
     constructor() {
         this.clientKey = document.querySelector(
@@ -217,7 +219,15 @@ class PayTraceCreditCard {
 
                 return this.handlePaymentWithToken(e);
             });
+
+        if (Array.from(document.getElementsByClassName('toggle-payment-with-token')).length === 0 && !instant()) {
+            document.getElementById('toggle-payment-with-credit-card').click();
+        }
     }
 }
 
-new PayTraceCreditCard().handle();
+function boot() {
+    new PayTraceCreditCard().handle();
+}
+
+instant() ? boot() : wait('#paytrace-credit-card-payment').then(() => boot())
