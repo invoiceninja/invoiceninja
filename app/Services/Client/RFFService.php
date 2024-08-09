c4f93e4d121e380c83597dff3a485d184969176b
David Bomba <turbo124@gmail.com>
2024-08-09 13:04:59 +1000

    Updates for payment flow

--- app/Services/Client/RFFService.php
+++ app/Services/Client/RFFService.php
@@ -163,8 +163,8 @@ class RFFService
 
 
         $_contact
-            ->fill($contact);
-            // ->push();
+            ->fill($contact)
+            ->push();
 
         $_contact->client
             ->fill($client)
