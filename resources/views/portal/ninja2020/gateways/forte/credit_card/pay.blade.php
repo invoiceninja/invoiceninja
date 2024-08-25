19556eb970301c6f6b20decebd667cda8a78f92e
Benjamin Beganović <k1pstabug@gmail.com>
2024-08-26 00:52:50 +0200

    Fixes for CVV issues on Authorize & Forte (#84)
    * fixes for forte
    
    * fixes for authorize

--- resources/views/portal/ninja2020/gateways/forte/credit_card/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/forte/credit_card/pay.blade.php
@@ -5,9 +5,6 @@
     <meta name="instant-payment" content="yes" />
 
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="{{ asset('js/clients/payments/forte-card-js.min.js') }}"></script>
-
-    <link href="{{ asset('css/card-js.min.css') }}" rel="stylesheet" type="text/css">
 @endsection
 
 @section('gateway_content')
@@ -54,20 +51,11 @@
 
 @push('footer')
 <script defer>
- 
-$(function() {
+    document.querySelector('#date').addEventListener('change', (e) => {
+        const [month, year] = e.target.value.replace(/\s/g, '').split('/');
 
-    document.getElementsByClassName("expiry")[0].addEventListener('change', function() {
-
-    str = document.getElementsByClassName("expiry")[0].value.replace(/\s/g, '');
-    const expiryArray = str.split("/");
-
-    document.getElementsByName('expiry-month')[0].value = expiryArray[0];
-    document.getElementsByName('expiry-year')[0].value = expiryArray[1];
-
+        document.getElementsByName('expiry-month')[0].value  = month;
+        document.getElementsByName('expiry-year')[0].value = `20${year}`;
     });
-
-});
-
 </script>
 @endpush
