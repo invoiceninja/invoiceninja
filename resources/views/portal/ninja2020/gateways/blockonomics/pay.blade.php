80877013bdbf368400f9d9046fe615abaefa7bdd
cnohall <christopher.nohall@gmail.com>
2024-09-13 20:43:03 +0900

    fix typos

--- resources/views/portal/ninja2020/gateways/blockonomics/pay.blade.php
+++ resources/views/portal/ninja2020/gateways/blockonomics/pay.blade.php
@@ -27,10 +27,10 @@
                 </span>
                 <span>Amount of bitcoin (BTC) to send:</span>
                 <span class="input-wrapper">
-                    <div class="full-width-input" id="btc-amount" onclick='copyToClipboard("btc-amount", this, true)''>
+                    <div class="full-width-input" id="btc-amount" onclick='copyToClipboard("btc-amount", this, true)'>
                         {{$btc_amount}}
                     </div>
-                    <img onclick='copyToClipboard("btc-amount", this)'' src="{{ 'data:image/svg+xml;base64,' . base64_encode('<svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><path d="M15.5 1H3.5C2.4 1 1.5 1.9 1.5 3V17H3.5V3H15.5V1ZM18.5 5H7.5C6.4 5 5.5 5.9 5.5 7V21C5.5 22.1 6.4 23 7.5 23H18.5C19.6 23 20.5 22.1 20.5 21V7C20.5 5.9 19.6 5 18.5 5ZM18.5 21H7.5V7H18.5V21Z" fill="#000"/></svg>') }}" class="icon" alt="Copy Icon">
+                    <img onclick='copyToClipboard("btc-amount", this)' src="{{ 'data:image/svg+xml;base64,' . base64_encode('<svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg" ><path d="M15.5 1H3.5C2.4 1 1.5 1.9 1.5 3V17H3.5V3H15.5V1ZM18.5 5H7.5C6.4 5 5.5 5.9 5.5 7V21C5.5 22.1 6.4 23 7.5 23H18.5C19.6 23 20.5 22.1 20.5 21V7C20.5 5.9 19.6 5 18.5 5ZM18.5 21H7.5V7H18.5V21Z" fill="#000"/></svg>') }}" class="icon" alt="Copy Icon">
                 </span>
                 <div class="btc-value-wrapper">
                     <div class="btc-value">1 BTC = {{$btc_price}} {{$currency}}, updates in <span id="countdown"></span></div>
@@ -179,7 +179,7 @@
         const fetchAndDisplayQRCode = async (newBtcAmount = null) => {
             try {
                 const btcAmount = newBtcAmount || '{{$btc_amount}}';
-                const response = await fetch(`/api/v1/get-blockonomics-qr-code?qr_string=bitcoin:${btcAmount}?amount={{$btc_amount}}`);
+                const response = await fetch(`/api/v1/get-blockonomics-qr-code?qr_string=bitcoin:{{$btc_address}}?amount=${btcAmount}`);
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
