397dd5dee7b81c716b616af889d9088c3a6a4777
Benjamin Beganović <k1pstabug@gmail.com>
2024-10-18 18:33:30 +0200

    update payment was updated

--- app/Events/Payment/PaymentWasUpdated.php
+++ app/Events/Payment/PaymentWasUpdated.php
@@ -11,22 +11,20 @@
 
 namespace App\Events\Payment;
 
+use App\Models\BaseModel;
 use App\Models\Company;
 use App\Models\Payment;
-use App\Transformers\ArraySerializer;
+use App\Utils\Traits\Invoice\Broadcasting\DefaultResourceBroadcast;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Queue\SerializesModels;
-use League\Fractal\Manager;
-use League\Fractal\Resource\Item;
 
 /**
  * Class PaymentWasUpdated.
  */
 class PaymentWasUpdated implements ShouldBroadcast
 {
-    use SerializesModels, InteractsWithSockets;
+    use SerializesModels, InteractsWithSockets, DefaultResourceBroadcast;
 
     /**
      * @var Payment
@@ -53,24 +51,8 @@ class PaymentWasUpdated implements ShouldBroadcast
         $this->dontBroadcastToCurrentUser();
     }
 
-    public function broadcastOn(): array
+    public function broadcastModel(): BaseModel
     {
-        return [
-            new PrivateChannel("company-{$this->company->company_key}"),
-        ];
-    }
-
-    public function broadcastWith(): array
-    {
-        $manager = new Manager();
-        $manager->setSerializer(new ArraySerializer());
-        $class = sprintf('App\\Transformers\\%sTransformer', class_basename($this->payment));
-
-        $transformer = new $class();
-
-        $resource = new Item($this->payment, $transformer, $this->payment->getEntityType());
-        $data = $manager->createData($resource)->toArray();
-
-        return [...$data, 'x-socket-id' => $this->socket];
+        return $this->payment;
     }
 }
