dd5ba5a240944e5ecfd097a9125e4d0233f6ec57
David Bomba <turbo124@gmail.com>
2024-10-22 06:31:13 +1100

    Support ids on sidebars

--- app/Http/Controllers/VendorPortal/PurchaseOrderController.php
+++ app/Http/Controllers/VendorPortal/PurchaseOrderController.php
@@ -141,7 +141,7 @@ class PurchaseOrderController extends Controller
         // $data[] = [ 'title' => ctrans('texts.dashboard'), 'url' => 'client.dashboard', 'icon' => 'activity'];
 
         if (self::MODULE_PURCHASE_ORDERS & $enabled_modules) {
-            $data[] = ['title' => ctrans('texts.purchase_orders'), 'url' => 'vendor.purchase_orders.index', 'icon' => 'file-text'];
+            $data[] = ['title' => ctrans('texts.purchase_orders'), 'url' => 'vendor.purchase_orders.index', 'icon' => 'file-text', 'id' => 'purchase_orders'];
         }
 
         // $data[] = ['title' => ctrans('texts.documents'), 'url' => 'client.documents.index', 'icon' => 'download'];
