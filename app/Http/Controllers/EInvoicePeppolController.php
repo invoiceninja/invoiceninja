2441669f8d4dd1a979b3953d8250736d9795687a
Benjamin Beganović <k1pstabug@gmail.com>
2024-10-21 17:38:10 +0200

    connect & disconnect

--- app/Http/Controllers/EInvoicePeppolController.php
+++ app/Http/Controllers/EInvoicePeppolController.php
@@ -12,6 +12,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\EInvoice\Peppol\CreateRequest;
+use App\Http\Requests\EInvoice\Peppol\DisconnectRequest;
 use App\Services\EDocument\Gateway\Storecove\Storecove;
 use Illuminate\Http\Response;
 
@@ -20,21 +21,56 @@ class EInvoicePeppolController extends BaseController
     public function setup(CreateRequest $request, Storecove $storecove): Response
     {
         /**
-         * @var \App\Models\User
+         * @var \App\Models\Company
          */
-        $user = auth()->user();
+        $company = auth()->user()->company();
 
-        $legal_entity_response = $storecove->createLegalEntity($request->validated(), $user->company());
+        $data = [
+            ...$request->validated(),
+            'country' => $request->country()->iso_3166_2,
+        ];
+
+        $legal_entity_response = $storecove->createLegalEntity($data, $company);
 
         $add_identifier_response = $storecove->addIdentifier(
             legal_entity_id: $legal_entity_response['id'],
-            identifier: $user->company()->settings->vat_number,
-            scheme: "{$request->country}:VAT"
-        );  
+            identifier: $company->settings->vat_number,
+            scheme: $request->receiverIdentifier(),
+        );
 
-        $user->company->legal_entity_id = $legal_entity_response['id'];
-        $user->company->save();
-        
-        return response()->noContent();
+        if ($add_identifier_response) {
+            $company->legal_entity_id = $legal_entity_response['id'];
+            $company->save();
+
+            return response()->noContent();
+        }
+
+        // @todo: Improve with proper error.
+
+        return response()->noContent(status: 422);
+    }
+
+    public function disconnect(DisconnectRequest $request, Storecove $storecove): Response
+    {
+        /**
+         * @var \App\Models\Company
+         */
+        $company = auth()->user()->company();
+
+        $response = $storecove->deleteIdentifier(
+            legal_entity_id: $company->legal_entity_id,
+        );
+
+        if ($response) {
+            $company->legal_entity_id = null;
+            $company->save();
+
+            return response()->noContent();
+
+        }
+
+        // @todo: Improve with proper error.
+
+        return response()->noContent(status: 422);
     }
 }
