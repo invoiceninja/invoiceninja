cb82c0b7c7911893c0ee38469a04cfd4091d9931
Benjamin Beganović <k1pstabug@gmail.com>
2024-10-22 17:21:36 +0200

    update translations

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -5403,6 +5403,8 @@ $lang = array(
     'payment_updated' => 'Payment Updated',
     'search_placeholder' => 'Find invoices, clients, and more',
     'invalid_vat_number' => "The VAT number is not valid for the selected country. Format should be Country Code followed by number only ie, DE123456789",
+    'act_as_sender' => 'Act as Sender',
+    'act_as_receiver' => 'Act as Receiver',
 );
 
 return $lang;
