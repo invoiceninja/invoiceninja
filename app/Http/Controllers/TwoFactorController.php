1c3304d302cd944a5889f8c5bdc1a268f17236c0
TechNoNerd87 <113461509+TechNoNerd87@users.noreply.github.com>
2025-11-24 13:54:54 -0600

    Update TwoFactorController to use SHA512 algorithm and timestamp
    Signed-off-by: TechNoNerd87 <113461509+TechNoNerd87@users.noreply.github.com>

--- app/Http/Controllers/TwoFactorController.php
+++ app/Http/Controllers/TwoFactorController.php
@@ -40,13 +40,17 @@ class TwoFactorController extends BaseController
         }
 
         $google2fa = new Google2FA();
-        $secret = $google2fa->generateSecretKey();
+        $google2fa->setAlgorithm(\PragmaRX\Google2FA\Support\Constants::SHA512);
+        $secret = $google2fa->generateSecretKey(32);
 
         $qr_code = $google2fa->getQRCodeUrl(
             config('ninja.app_name'),
             $user->email,
             $secret
         );
+ 
+        // Append algorithm parameter for SHA512
+        $qr_code .= '&algorithm=SHA512';
 
         $data = [
             'secret' => $secret,
@@ -57,21 +61,31 @@ class TwoFactorController extends BaseController
     }
 
     public function enableTwoFactor(EnableTwoFactorRequest $request)
-    {
-        $google2fa = new Google2FA();
-
+    {     
         /** @var \App\Models\User $user */
         $user = auth()->user();
 
         $secret = $request->input('secret');
         $oneTimePassword = $request->input('one_time_password');
 
-        if ($google2fa->verifyKey($secret, $oneTimePassword) && $user->phone && $user->email_verified_at) {
+     // Try SHA512 first (new algorithm)
+        $google2fa = new Google2FA();
+        $google2fa->setAlgorithm(\PragmaRX\Google2FA\Support\Constants::SHA512);
+        $timestamp = $google2fa->verifyKeyNewer($secret, $oneTimePassword, 0);
+
+        // Fall back to SHA1 for manual entry (authenticator apps default to SHA1)
+        if ($timestamp === false) {
+            $google2fa = new Google2FA();
+            $timestamp = $google2fa->verifyKeyNewer($secret, $oneTimePassword, 0);
+        }
+
+        if ($timestamp !== false && $user->phone && $user->email_verified_at) {
             $user->google_2fa_secret = encrypt($secret);
+            $user->google_2fa_ts = $timestamp;
             $user->save();
 
             return response()->json(['message' => ctrans('texts.enabled_two_factor')], 200);
-        } elseif (! $secret || ! $google2fa->verifyKey($secret, $oneTimePassword)) {
+        } elseif (! $secret || $timestamp === false) {
             return response()->json(['message' => ctrans('texts.invalid_one_time_password')], 400);
         } elseif (! $user->phone) {
             return response()->json(['message' => ctrans('texts.set_phone_for_two_factor')], 400);
@@ -94,6 +108,7 @@ class TwoFactorController extends BaseController
         $user = auth()->user();
 
         $user->google_2fa_secret = null;
+           $user->google_2fa_ts = null;
         $user->save();
 
         return $this->itemResponse($user);
