e2441ce3339e195aa835866f65d63b8a3e812d06
bmmh1 <brengels.mendoza@me.com>
2026-02-24 15:36:16 -0600

    Update passwords.php
    Signed-off-by: bmmh1 <brengels.mendoza@me.com>

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -15,9 +15,9 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'We have emailed your password reset link!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "We can't find a user with that email address.",
     'throttled' => 'You have requested password reset recently, please check your email.',
 
 ];
