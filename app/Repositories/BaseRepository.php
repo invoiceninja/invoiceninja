108467157d3702805614a77fb91ec51f5dd8c6c9
David Bomba <turbo124@gmail.com>
2026-03-11 15:07:49 +1100

    Fixes for base repo balance updates

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -21,6 +21,7 @@ use App\Models\Credit;
 use App\Models\Invoice;
 use App\Models\Quote;
 use App\Models\RecurringInvoice;
+use App\Utils\BcMath;
 use App\Utils\Helpers;
 use App\Utils\Ninja;
 use App\Utils\Traits\MakesHash;
@@ -322,9 +323,13 @@ class BaseRepository
             if ($model->status_id != Invoice::STATUS_DRAFT) {
                 $model->service()->updateStatus()->save();
             
-                $adjustment = round($state['finished_amount'] - $state['starting_amount'], 2);
-                if ($adjustment != 0) {
-                    $model->client->service()->updateBalance($adjustment);
+                /** replaced with BcMath */
+                // $adjustment = (float) BcMath::sub($state['finished_amount'], $state['starting_amount'], 2);
+                // if ($adjustment != 0) {
+
+                $adjustment = BcMath::sub($state['finished_amount'], $state['starting_amount'], 2);
+                if (!BcMath::isZero($adjustment)) {
+                    $model->client->service()->updateBalance((float) $adjustment);
                 }
                 
             }
