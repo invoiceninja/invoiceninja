db98dce36f42b119c338a4ab641a29cc2a040fe4
James <jamiephonic@live.co.uk>
2026-03-17 09:48:19 +0000

    Improve transaction description handling
    If 'rawTransactionDescription' exists in remittanceInformationStructuredArray, use it, otherwise implode.
    
    Prevents descriptions appearing as "declined: False rawTransactionDescription: Payment"
    
    Signed-off-by: James <jamiephonic@live.co.uk>

--- app/Helpers/Bank/Nordigen/Transformer/TransactionTransformer.php
+++ app/Helpers/Bank/Nordigen/Transformer/TransactionTransformer.php
@@ -107,12 +107,17 @@ class TransactionTransformer implements BankRevenueInterface
         $amount = (float) $transaction["transactionAmount"]["amount"];
         $base_type = $amount < 0 ? 'DEBIT' : 'CREDIT';
 
-        // description could be in varios places
+        // description could be in various places
         $description = '';
         if (array_key_exists('remittanceInformationStructured', $transaction)) {
             $description = $transaction["remittanceInformationStructured"];
         } elseif (array_key_exists('remittanceInformationStructuredArray', $transaction)) {
-            $description = implode('\n', $transaction["remittanceInformationStructuredArray"]);
+            $remittanceInformationStructuredArray = $transaction["remittanceInformationStructuredArray"];
+            if (array_key_exists('rawTransactionDescription', $remittanceInformationStructuredArray)) {
+                $description = $remittanceInformationStructuredArray["rawTransactionDescription"];
+            } else {
+                $description = implode('\n', $transaction["remittanceInformationStructuredArray"]);
+            }
         } elseif (array_key_exists('remittanceInformationUnstructured', $transaction)) {
             $description = $transaction["remittanceInformationUnstructured"];
         } elseif (array_key_exists('remittanceInformationUnstructuredArray', $transaction)) {
