07c6135aa6fce8fc61bbb4d4f4e168b02c16311e
David Bomba <turbo124@gmail.com>
2026-04-02 15:25:30 +1100

    Remove unreachable  prop

--- app/Jobs/Mail/NinjaMailerJob.php
+++ app/Jobs/Mail/NinjaMailerJob.php
@@ -383,7 +383,7 @@ class NinjaMailerJob implements ShouldQueue
             } catch (\Throwable $e) {
 
                 nlog("problem switching outlook driver - hosted {$email}");
-                nlog($dns);
+                
                 nlog($e->getMessage());
             }
         }
