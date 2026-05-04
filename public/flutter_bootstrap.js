4aa7af009be72eec494b1993498a80b7ede5a783
hillelcoren <hillelcoren@gmail.com>
2026-05-04 12:36:52 +0000

    Admin Portal - Hosted

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "440928902"
+    serviceWorkerVersion: "1721719569"
   }
 });
