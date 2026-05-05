625f6730df2f46ff8a272a3e985fad7165f6b69c
hillelcoren <hillelcoren@gmail.com>
2026-05-05 10:02:14 +0000

    Admin Portal - Hosted

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "795839871" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1067296393" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
