379824cec93d4a0dfd50788a6354c2e7d19bdb78
David Bomba <turbo124@gmail.com>
2026-05-15 07:03:02 +1000

    Fixes for casts

--- app/DataMapper/CompanySettings.php
+++ app/DataMapper/CompanySettings.php
@@ -546,6 +546,8 @@ class CompanySettings extends BaseSettings
     public string $france_reporting_schedule = 'ten_days'; //ten_days, monthly
 
     public static $casts = [
+        'france_reporting_enabled' => 'bool',
+        'france_reporting_schedule' => 'string',
         'e_expense_forward_email' => 'string',
         'ses_from_address' => 'string',
         'ses_topic_arn' => 'string',
