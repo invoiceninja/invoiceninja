3c4a19c2a8002b7347f906942dca4c2c6c1dfac2
David Bomba <turbo124@gmail.com>
2026-06-02 19:23:56 +1000

    Minor fixes

--- app/Services/Email/Email.php
+++ app/Services/Email/Email.php
@@ -576,7 +576,7 @@ class Email implements ShouldQueue
         if (Ninja::isHosted() && (!$this->company->account->isPaid() || ($this->company->account->isNewHostedAccount() && $this->email_object->settings->email_sending_method == 'default'))) {
 
         // if (Ninja::isHosted() && $this->email_object->settings->email_sending_method == 'default' && (!$this->company->account->isPaid() || $this->company->account->isNewHostedAccount())) {
-            $this->email_object->settings->email_sending_method = 'default';   // ← ADD
+            $this->email_object->settings->email_sending_method = 'default';
             $this->mailer = 'mailgun';
             $this->setHostedMailgunMailer();
             return $this;
