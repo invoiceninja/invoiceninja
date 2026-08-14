acc7367461ff82deaee3ffad480bd1811bb790e3
Kewayne <34725741+kewayne@users.noreply.github.com>
2026-08-14 00:03:25 -0500

    Update endless reminder template resolution in BulkInvoiceJob.php
    Adds reminder_endless key mapping to BulkInvoiceJob.php
    
    Signed-off-by: Kewayne <34725741+kewayne@users.noreply.github.com>

--- app/Jobs/Invoice/BulkInvoiceJob.php
+++ app/Jobs/Invoice/BulkInvoiceJob.php
@@ -126,7 +126,7 @@ class BulkInvoiceJob implements ShouldQueue
             'reminder1' => 'email_template_reminder1',
             'reminder2' => 'email_template_reminder2',
             'reminder3' => 'email_template_reminder3',
-            'endless_reminder' => 'email_template_reminder_endless',
+            'endless_reminder', 'reminder_endless', 'email_template_reminder_endless' => 'email_template_reminder_endless',
             'custom1' => 'email_template_custom1',
             'custom2' => 'email_template_custom2',
             'custom3' => 'email_template_custom3',
