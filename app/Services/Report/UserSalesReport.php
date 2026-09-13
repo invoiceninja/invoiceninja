980add3566d4996e8ddcfa871ca9e287abbfbf83
David Bomba <turbo124@gmail.com>
2026-09-14 09:23:59 +1000

    Fixes for user sales report

--- app/Services/Report/UserSalesReport.php
+++ app/Services/Report/UserSalesReport.php
@@ -72,7 +72,7 @@ class UserSalesReport extends BaseExport
 
         $query = $this->filterByClients($query);
 
-        $query = $this->filterByUserPermissions($query);
+        // $query = $this->filterByUserPermissions($query);
 
         $this->csv->insertOne([ctrans('texts.user_sales_report_header', ['client' => $this->client_description, 'start_date' => $this->start_date, 'end_date' => $this->end_date])]);
 
@@ -86,8 +86,7 @@ class UserSalesReport extends BaseExport
 
         $report = $users->map(function ($user) use ($query) {
 
-            $new_query = $query;
-            $new_query->where('user_id', $user->id);
+            $new_query = $query->clone()->where('user_id', $user->id);
 
             return [
                 $user->present()->name(),
